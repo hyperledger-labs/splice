1bd62aaf4b6caab250ac6a10afaace3692e50eb4
Arne Gebert <arne.gebert@digitalasset.com>
2022-06-20 15:16:11 +0200

    Scala code is now tested in CI (#83)
    Fixes #73 as the basic SBT setup is now mostly complete :) 
    Adds the initial CI structure for testing the Scala part of the project

---
+++ .github/pull_request_template.md
@@ -0,0 +1,3 @@
+### Pull Request Checklist
+
+- [ ] Specify fixed issues with `Fixes #n`, and mention issues worked on using `#n`
--- shell.nix
+++ shell.nix
@@ -1,5 +1,9 @@
 let
-  pkgs = import ./nix/default.nix {};
+  pkgs = import ./nix/default.nix {
+    overlays = [(self: super: {
+      sbt = super.sbt.override { jre = super.openjdk11; };}
+    )];
+  };
   # sphinx-autobuild is currently broken on M1 due to
   # https://github.com/NixOS/nixpkgs/issues/174457#issuecomment-1137385758
   x86Pkgs = if builtins.currentSystem == "aarch64-darwin" then import ./nix/default.nix { system = "x86_64-darwin"; } else pkgs;
@@ -8,7 +12,9 @@ in pkgs.mkShell {
     nodejs
     sbt
     sphinx
+    python3
     x86Pkgs.sphinx-autobuild
     python3Packages.sphinx_rtd_theme
+    openjdk11
   ];
 }
