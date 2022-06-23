5d87d0e68c4863180090ca09d21537cb4007273b
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2022-06-23 13:32:47 +0200

    Bump SDK to latest snapshot (#105)
    * Bump SDK to latest snapsho

--- set-sdk.sh
+++ set-sdk.sh
@@ -11,5 +11,5 @@ SDK_VERSION=$1
 echo "Setting SDK version to $SDK_VERSION"
 find . -name daml.yaml -exec sed -i "s/^sdk-version:.*$/sdk-version: $SDK_VERSION/g" '{}' \;
 (cd experiments/pay-with-cc && ./codegen.sh)
-find . -name node_modules -prune -o -name daml.js -prune -o -name package.json -exec sed -i "s|@daml/g\(.*\): \".*\"|@daml/\1: \"$SDK_VERSION\"|g" '{}' \; -exec sh -c 'cd $(dirname $1) && npm install' sh '{}' \;
+find . -name canton -prune -o -name node_modules -prune -o -name daml.js -prune -o -name package.json -exec sed -i "s|@daml/g\(.*\): \".*\"|@daml/\1: \"$SDK_VERSION\"|g" '{}' \; -exec sh -c 'cd $(dirname $1) && npm install' sh '{}' \;
 sed -i "s|\".*\" # sdk-version|\"$SDK_VERSION\" # sdk-version|g" .circleci/config.yml
