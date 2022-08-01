f27410d50ccb30cc71fc977438d95b625eda5061
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2022-08-01 15:53:00 +0200

    Get yq from x86Pkgs (#388)
    * Get yq from x86Pkgs
    
    * revise comment

--- shell.nix
+++ shell.nix
@@ -4,8 +4,9 @@ let
       sbt = super.sbt.override { jre = super.openjdk11; };}
     )];
   };
-  # sphinx-autobuild is currently broken on M1 due to
+  # pyopenssl is currently broken on M1 due to
   # https://github.com/NixOS/nixpkgs/issues/174457#issuecomment-1137385758
+  # To work around this we fetch some packages via rosetta.
   x86Pkgs = if builtins.currentSystem == "aarch64-darwin" then import ./nix/default.nix { system = "x86_64-darwin"; } else pkgs;
 in pkgs.mkShell {
   SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
@@ -25,6 +26,6 @@ in pkgs.mkShell {
     x86Pkgs.sphinx-autobuild
     python3Packages.sphinx_rtd_theme
     openjdk11
-    yq
+    x86Pkgs.yq
   ];
 }
