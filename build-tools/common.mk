9839495dcbf1411616d4b41c249152fcd354f969
Mike Schaeffer <40773844+mschaef-da@users.noreply.github.com>
2022-09-07 14:32:08 -0400

    Simplify the Makefile design by unifying common.mk and docker.mk. (#687)

--- build-tools/common.mk
+++ build-tools/common.mk
@@ -10,29 +10,32 @@ export MAKE
 ifneq ($(_did_common),true)
 
 
+#######
+# vars
+#######
+
+# CircleCI overrides this to true
+export CI ?= false
+
+app  := $(notdir $(abspath .))
+repo_root := $(call must-shell,cd $(dir $(lastword $(MAKEFILE_LIST))).. && pwd)
+
+#######
+# utils
+#######
+
 # execute a makefile shell but crash make if the command fails
 must-shell = $(or $(shell _out="$$($(1))" && echo "$${_out}" || echo "$${_out}" >&2),$(error command '$(1)' failed))
 
 # fails dirty builds in CI
 check-dirty = $(if $(findstring true,$(CI)),$(if $(findstring dirty,$(git_sha)),$(error build is dirty$(\n)$(call must-shell,git status --porcelain)$(\n))))
 
-
-#######
-# vars
-#######
-
-app  := $(notdir $(abspath .))
-repo_root := $(call must-shell,cd $(dir $(lastword $(MAKEFILE_LIST))).. && pwd)
-
-# CircleCI overrides this to true
-export CI ?= false
-
 #######
 # default build
 #######
 
 .PHONY: all
-all:
+all: docker-build
 
 #######
 # clean
@@ -43,5 +46,58 @@ clean:
 	-rm -vfr target
 
 
+#########
+# docker
+#########
+
+docker-src := Dockerfile
+
+#################
+## docker images
+#################
+
+ifeq ($(CI),true)
+    # never use the cache in CI on the master branch
+    cache_opt := --no-cache
+else
+    # Local builds (which may be on an M1) are explicitly constrained
+    # to x86.
+    platform_opt := --platform=linux/amd64
+endif
+
+###############
+# docker build
+###############
+
+image-tag = $(eval image-tag := $$(shell image-tag-gen ${app}))$(image-tag)
+local-image-tag = $(shell echo "${app}:$(shell version-gen)")
+
+docker-build := target/docker.id
+
+.PHONY: docker-build
+docker-build: $(docker-build)
+
+$(docker-build): $(docker-src)
+	mkdir -pv $(@D)
+	@echo docker build triggered because these files changed: $?
+	docker build $(platform_opt) --iidfile $@ $(cache_opt) $(build_arg) -t $(local-image-tag) .
+
+
+##############
+# docker push
+##############
+
+docker-push := target/docker.push
+
+.PHONY: docker-push
+docker-push: $(docker-build)
+	$(check-dirty)
+	docker-push ${image-tag}
+
+.PHONY: docker-push-force
+docker-push-force: $(docker-build)
+	docker-push ${image-tag} --force
+
 _did_common := true
 endif
+
--- build-tools/docker.mk
+++ build-tools/docker.mk
@@ -1,69 +1,10 @@
 # -*- makefile -*-
 
-#########
-# docker
-#########
 
 # only load docker once
 ifneq ($(_did_docker),true)
 
-docker-src := Dockerfile
 
-#################
-## docker images
-#################
-
-ifeq ($(CI),true)
-    # never use the cache in CI on the master branch
-    cache_opt := --no-cache
-else
-    # Local builds (which may be on an M1) are explicitly constrained
-    # to x86.
-    platform_opt := --platform=linux/amd64
-endif
-
-
-#########################
-# skip and ci targets
-#########################
-
-image-tag = $(eval image-tag := $$(shell image-tag-gen ${app}))$(image-tag)
-local-image-tag = $(shell echo "${app}:$(shell version-gen)")
-
-.PHONY: docker-check
-docker-check:
-	docker-check $(image-tag)
-
-
-###############
-# docker build
-###############
-
-docker-build := target/docker.id
-
-.PHONY: docker-build
-docker-build: $(docker-build)
-
-$(docker-build): $(docker-src)
-	mkdir -pv $(@D)
-	@echo docker build triggered because these files changed: $?
-	docker build $(platform_opt) --iidfile $@ $(cache_opt) $(build_arg) -t $(local-image-tag) .
-
-
-##############
-# docker push
-##############
-
-docker-push := target/docker.push
-
-.PHONY: docker-push
-docker-push: $(docker-build)
-	$(check-dirty)
-	docker-push ${image-tag}
-
-.PHONY: docker-push-force
-docker-push-force: $(docker-build)
-	docker-push ${image-tag} --force
 
 _did_docker := true
 endif
