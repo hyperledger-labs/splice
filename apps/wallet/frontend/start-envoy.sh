33cae5a785610a29bb5ca4d30db1aa17ec2c02f8
Itai Segall <itai.segall@digitalasset.com>
2022-09-28 09:21:47 -0400

    cleanup envoy config file (#888)
    Use jsonnet to reduce duplication within the config file, and make it much easier to read and maintain.

--- apps/wallet/frontend/start-envoy.sh
+++ apps/wallet/frontend/start-envoy.sh
@@ -2,8 +2,10 @@
 
 name=envoy-wallet
 
+jsonnet envoy.jsonnet > envoy-out.json
+
 docker run -d --rm --name $name --add-host=host.docker.internal:host-gateway -v $PWD:/conf -it \
     -p 9901:9901 \
     -p 6204:6204 -p 6304:6304 \
     envoyproxy/envoy:v1.23-latest \
-    -c /conf/envoy.yaml
+    -c /conf/envoy-out.json
--- apps/wallet/frontend/stop-envoy.sh
+++ apps/wallet/frontend/stop-envoy.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 docker stop envoy-wallet
+
+rm envoy-out.json
