8bc08f0c3761a2d04f579eb609e46206c7e91626
Mike Schaeffer <40773844+mschaef-da@users.noreply.github.com>
2022-10-11 21:08:15 -0400

    Extend the CircleCI trigger automation logic to cover both triggers. (#1105)

---
+++ .circleci/install-trigger.sh
@@ -0,0 +1,57 @@
+# This script calls the CircleCI API to create / update the scheduled trigger.
+
+# Add your personal API token to the CIRCLECI_TOKEN environment variable before running the script.
+# https://circleci.com/docs/managing-api-tokens
+
+#!/usr/bin/env bash
+set -euo pipefail
+
+set +u
+TRIGGER_DEFINITION_FILE=$1
+
+if [[ -z $TRIGGER_DEFINITION_FILE ]]; then
+    echo "Error: usage $0 trigger-definition-file"
+    exit 1
+fi
+
+if [[ ! -f $TRIGGER_DEFINITION_FILE ]]; then
+    echo "Error: Trigger definition file does not exist: $TRIGGER_DEFINITION_FILE"
+    exit 1
+fi
+
+
+if [[ -z $CIRCLECI_TOKEN ]]; then
+    echo "Error: \$CIRCLECI_TOKEN variable is not set."
+    echo "Create and set your API token: https://circleci.com/docs/managing-api-tokens"
+    exit 1
+fi
+set -u
+
+TRIGGER_NAME=$(jq -c '.name' < ${TRIGGER_DEFINITION_FILE})
+
+if [[ -z $TRIGGER_NAME ]]; then
+    echo "Error: Trigger definition file does not contain a trigger name"
+    exit 1
+fi
+
+verb=POST
+endpoint="https://circleci.com/api/v2/project/github/DACH-NY/the-real-canton-coin/schedule"
+
+existing_schedule=$(curl -s "$endpoint" \
+    -H "Content-Type: application/json" \
+    -H "circle-token: $CIRCLECI_TOKEN" | jq -c '.items[] | select(.name | contains($ARGS.positional[0]))' --jsonargs "${TRIGGER_NAME}")
+
+if [[ -n $existing_schedule ]]; then
+    echo "Trigger ${TRIGGER_NAME} already exists... patching"
+    schedule_id=$(echo $existing_schedule | jq -r .id)
+
+    verb=PATCH
+    endpoint="https://circleci.com/api/v2/schedule/$schedule_id"
+else
+    echo "Trigger ${TRIGGER_NAME} does not exist... creating"
+fi
+
+curl -X $verb $endpoint \
+    -d @$TRIGGER_DEFINITION_FILE \
+    -H "Content-Type: application/json" \
+    -H "circle-token: $CIRCLECI_TOKEN"
--- .circleci/setup-pipeline.sh
+++ .circleci/setup-pipeline.sh
@@ -7,30 +7,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-set +u
-if [[ -z $CIRCLECI_TOKEN ]]; then
-    echo "Error: \$CIRCLECI_TOKEN variable is not set."
-    echo "Create and set your API token: https://circleci.com/docs/managing-api-tokens"
-    exit 1
-fi
-set -u
+# $REPO_ROOT/.circleci/install-trigger.sh $REPO_ROOT/.circleci/run-schedule-pipeline.json
 
-verb=POST
-endpoint="https://circleci.com/api/v2/project/github/DACH-NY/the-real-canton-coin/schedule"
-
-existing_schedule=$(curl -s "$endpoint" \
-    -H "Content-Type: application/json" \
-    -H "circle-token: $CIRCLECI_TOKEN" | jq -c '.items[] | select(.name | contains("Nightly deployment"))')
-
-if [[ -n $existing_schedule ]]; then
-    echo "Schedule already exists... patching"
-    schedule_id=$(echo $existing_schedule | jq -r .id)
-
-    verb=PATCH
-    endpoint="https://circleci.com/api/v2/schedule/$schedule_id"
-fi
-
-curl -X $verb $endpoint \
-    -d @$REPO_ROOT/.circleci/run-schedule-pipeline.json \
-    -H "Content-Type: application/json" \
-    -H "circle-token: $CIRCLECI_TOKEN"
+for TRIGGER in $REPO_ROOT/.circleci/trigger-*.json
+do
+    $REPO_ROOT/.circleci/install-trigger.sh ${TRIGGER}
+done
