32391fa53aea824c60840d350728db673e0b1f47
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2022-11-04 11:55:34 +0100

    Bump lnav (#1467)
    Newer is always better. Also somehow the 0.10.1 is broken for me on my
    new laptop and this one is not which is a clear improvement.

--- nix/lnav.nix
+++ nix/lnav.nix
@@ -3,16 +3,16 @@
 # so we use the binary distribution that ships with a compatible pcre version.
 stdenv.mkDerivation rec {
   name = "lnav";
-  version = "0.10.1";
+  version = "0.11.1";
 
   src =
     if stdenv.isDarwin then
       fetchzip {
-        url = "https://github.com/tstack/lnav/releases/download/v${version}/lnav-${version}-os-x.zip";
-        sha256 = "sha256-Y4++NFmbodxrew7xtoiAdWAFIHAfNeGKSpNq6jHj+4g=";
+        url = "https://github.com/tstack/lnav/releases/download/v${version}/lnav-${version}-x86_64-macos.zip";
+        sha256 = "sha256-pIIUGznYtEAPPPP1erX/LHB1IF5nlB311DPrSP4Nwrc=";
       } else fetchzip {
-        url = "https://github.com/tstack/lnav/releases/download/v${version}/lnav-${version}-musl-64bit.zip";
-        sha256 = "sha256-AeAX1gr3GYOc/dIhJwT9U4Jm7m71yKLzw1UA3j0kVPA=";
+        url = "https://github.com/tstack/lnav/releases/download/v${version}/lnav-${version}-x86_64-linux-musl.zip";
+        sha256 = "sha256-S8OzVOCsDLa2XJH6NskCGKVHe7QslZ6oainKIPHB5/c=";
       };
 
   installPhase = ''
