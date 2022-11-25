3162ac66630095253e8a24397b4c3b170f1ad5eb
Martin Florian <martin.florian@digitalasset.com>
2022-11-25 14:46:41 +0100

    Clean up after http 400/500 response errors debugging (#1735)
    This PR does two things w.r.t. the `http 400/500 response errors` flake we were getting (#1713).
    
    - revert an attempted fix that didn't help; it's adding unnecessary complexity (because it doesn't solve any problem) and might actually be causing more of the above error (because we have an extra navigation operation for each browser)
    
    - reflect in the ignore lists that we will keep ignoring this error for now (until #1734)
    
    Fixes #1713

--- apps/app/src/test/scala/com/daml/network/integration/tests/FrontendIntegrationTest.scala
+++ apps/app/src/test/scala/com/daml/network/integration/tests/FrontendIntegrationTest.scala
@@ -132,14 +132,7 @@ abstract class FrontendIntegrationTest(frontendNames: String*)
     webDrivers.values.flatMap { implicit webDriver =>
       findAll(id("error")).toList.map(e => fail(s"Found unexpected error: ${e.text}"))
     }
-    webDrivers.values.foreach(webDriver => {
-      // The browser might not quit immediately after we tell it to, so we
-      // navigate it to an empty page at the end of each test; this helps avoid
-      // sending gRPC requests to services that are being shut down.
-      webDriver.navigate.to("about:blank")
-      eventually()(webDriver.getTitle() shouldBe "")
-      webDriver.quit()
-    })
+    webDrivers.values.foreach(_.quit())
     super.testFinished(env)
   }
 
