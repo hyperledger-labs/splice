9b1d8be1432229a2b53f92db0d2145c9215443d3
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2022-12-01 18:09:18 +0100

    Drop non-subscription directory entry requests from UI (#1846)
    Part of #1644
    
    Doing UI first to see all the tests that break.

--- apps/app/src/test/scala/com/daml/network/integration/tests/DirectoryFrontendIntegrationTest.scala
+++ apps/app/src/test/scala/com/daml/network/integration/tests/DirectoryFrontendIntegrationTest.scala
@@ -19,45 +19,6 @@ class DirectoryFrontendIntegrationTest extends FrontendIntegrationTest("alice") 
 
   "A directory UI" should {
 
-    "allow requesting an entry and then list it" in { implicit env =>
-      val aliceDamlUser = aliceWallet.config.damlUser
-      onboardWalletUser(this, aliceWallet, aliceValidator)
-      aliceWallet.tap(100.0)
-
-      val entryName = "mycoolentry"
-
-      withFrontEnd("alice") { implicit webDriver =>
-        go to "http://localhost:3004"
-        click on "user-id-field"
-        textField("user-id-field").value = aliceDamlUser
-        click on "login-button"
-        eventually(scaled(10 seconds)) {
-          click on "entry-name-field"
-        }
-        textField("entry-name-field").value = entryName
-        click on "request-entry-button"
-
-        // Alice is redirected to wallet...
-        click on "user-id-field"
-        textField("user-id-field").value = aliceDamlUser
-        click on "login-button"
-        click on className("accept-button")
-        // And then back to directory
-        click on "user-id-field"
-        textField("user-id-field").value = aliceDamlUser
-        click on "login-button"
-
-        eventually(scaled(10 seconds)) {
-          findAll(className("entries-table-row")) should have size 1
-        }
-        val row: Element = inside(findAll(className("entries-table-row")).toList) { case Seq(row) =>
-          row
-        }
-        val name = row.childElement(className("entries-table-name"))
-        name.text should be(entryName)
-      }
-    }
-
     "allow requesting an entry with subscription payments and then list it" in { implicit env =>
       val aliceDamlUser = aliceWallet.config.damlUser
       onboardWalletUser(this, aliceWallet, aliceValidator)
--- apps/directory/frontend/src/views/RequestDirectoryEntry.tsx
+++ apps/directory/frontend/src/views/RequestDirectoryEntry.tsx
@@ -1,10 +1,9 @@
-import { Contract } from 'common-frontend';
-import { SubscriptionButton, TransferButton } from 'common-frontend/lib/components/WalletButtons';
+import { SubscriptionButton } from 'common-frontend/lib/components/WalletButtons';
 import { useState } from 'react';
 
 import { FormGroup, TextField, Typography } from '@mui/material';
 
-import { DirectoryEntryOffer, DirectoryInstall } from '@daml.js/directory/lib/CN/Directory';
+import { DirectoryInstall } from '@daml.js/directory/lib/CN/Directory';
 
 import { useDirectoryLedgerApiClient } from '../contexts/DirectoryLedgerApiContext';
 import { config } from '../utils';
@@ -15,47 +14,6 @@ const RequestDirectoryEntry: React.FC<{ primaryParty: string; provider: string }
 }) => {
   const [entryName, setEntryName] = useState<string>('');
   const ledgerApiClient = useDirectoryLedgerApiClient();
-
-  const waitForDirectoryEntryOffer: (
-    entryName: string,
-    retries: number
-  ) => Promise<Contract<DirectoryEntryOffer>> = async (entryName: string, retries: number) => {
-    if (retries < 0) {
-      throw new Error('Timeout waiting for directory to create an entry offer');
-    }
-    const offer = await ledgerApiClient.queryDirectoryEntryOffer(primaryParty, provider, entryName);
-    if (offer) {
-      return offer;
-    }
-    await new Promise(resolve => setTimeout(resolve, 500));
-    return waitForDirectoryEntryOffer(entryName, retries - 1);
-  };
-
-  const requestEntry = async () => {
-    const directoryInstall = await ledgerApiClient.queryDirectoryInstall(primaryParty, provider);
-    if (!directoryInstall) {
-      throw new Error('Failed to find DirectoryInstall');
-    }
-    await ledgerApiClient.exercise(
-      [primaryParty],
-      [],
-      DirectoryInstall.DirectoryInstall_RequestEntry,
-      directoryInstall.contractId,
-      { name: entryName }
-    );
-    const offer = await waitForDirectoryEntryOffer(entryName, 4);
-    const ret = await ledgerApiClient.queryDirectoryEntryPaymentRequest(
-      primaryParty,
-      offer.contractId
-    );
-    if (!ret) {
-      throw new Error(
-        'Directory entry offer created, but could not find a corresponding payment request'
-      );
-    }
-    console.debug('Created DirectoryEntryRequest');
-    return ret.contractId;
-  };
 
   const requestEntryWithSubscription = async () => {
     const directoryInstall = await ledgerApiClient.queryDirectoryInstall(primaryParty, provider);
@@ -69,6 +27,7 @@ const RequestDirectoryEntry: React.FC<{ primaryParty: string; provider: string }
       directoryInstall.contractId,
       { name: entryName }
     );
+
     console.debug('Created SubscriptionRequest');
     return res._2;
   };
@@ -83,13 +42,6 @@ const RequestDirectoryEntry: React.FC<{ primaryParty: string; provider: string }
           onChange={event => setEntryName(event.target.value)}
           id="entry-name-field"
         ></TextField>
-        <TransferButton
-          variant="contained"
-          id="request-entry-button"
-          text="Request"
-          createPaymentRequest={requestEntry}
-          walletPath={config.wallet.uiUrl}
-        />
         <SubscriptionButton
           variant="contained"
           id="request-entry-with-sub-button"
