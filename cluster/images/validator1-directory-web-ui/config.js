7fbfd5ddea30bf3f3ef04c7888befe45de82e8ea
Itai Segall <itai.segall@digitalasset.com>
2022-12-03 13:49:58 -0500

    Add missing wallet url config (#1859)

--- cluster/images/validator1-directory-web-ui/config.js
+++ cluster/images/validator1-directory-web-ui/config.js
@@ -11,4 +11,8 @@ window.canton_network_config = {
     // URL of the gRPC-Web envoy proxy, proxying the user’s ledger API
     grpcUrl: "https://" + window.location.hostname + "/api/v0/ledger-api",
   },
+  wallet: {
+    // URL of the web-ui, used to forward payment workflows to wallet
+    uiUrl: window.location.origin.replace("directory", "wallet"),
+  },
 };
