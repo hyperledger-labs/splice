b783a98f2fddc4b2dc6c193299420ccee5ee8c87
Claudio Bley <103681498+cbley-da@users.noreply.github.com>
2023-01-27 09:34:22 +0100

    Reduce duplication of `simple-topology.conf` (#2494)
    * Use template from each validator
    
    * Use template for svs
    
    * Use template for remote-directory-apps
    
    * Use template for wallets
    
    Fixes #2216
    
    * Add `print-config` script
    
    This script can be used to render a canonical form of a HOCON file, with includes and substitutions
    resolved, and comments stripped. This is useful to check whether or not configurations differ.

---
+++ scripts/print-config.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+
+( echo "Running sbt bundle for an up-to-date config file definition"
+  echo "If you wish to skip this step, comment out the corresponding line"
+  sbt bundle
+) >&2
+
+if [[ $# -ne 1 ]]; then
+    ( echo "$0: wrong number of arguments"
+      echo ''
+      echo "usage: $0 FILE"
+      echo ''
+      echo 'Prints a HOCON file in a canonical form (resolved, no comments, no origin comments) to stdout.'
+      ) >&2
+else
+    echo "Printing resolved config for \`$1'" >&2
+    scala -classpath "$BUNDLE/lib/coin-0.1.0-SNAPSHOT.jar" ./scripts/print-config.sc "$1"
+fi
