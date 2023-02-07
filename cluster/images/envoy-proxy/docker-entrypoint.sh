e02d723cc385b4bd6650a28db8dc867bd2f12cdd
Mike Schaeffer <40773844+mschaef-da@users.noreply.github.com>
2023-02-07 13:48:48 -0500

    Reduce the envoy-proxy log level to reduce the clutter in an aggregated log view. (#2736)

--- cluster/images/envoy-proxy/docker-entrypoint.sh
+++ cluster/images/envoy-proxy/docker-entrypoint.sh
@@ -5,4 +5,4 @@ echo "Generating envoy.yaml config file..."
 cat /tmpl/envoy.yaml.tmpl | envsubst '$GRPC_ADDRESS,$GRPC_PORT,$GRPC_WEB_PORT' > /etc/envoy.yaml
 
 echo "Starting Envoy..."
-/usr/local/bin/envoy --log-level trace -c /etc/envoy.yaml
+/usr/local/bin/envoy -c /etc/envoy.yaml
