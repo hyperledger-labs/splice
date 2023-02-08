91880dbb309bbc90e6baf7f2f8c72b4af63ca0aa
Alex Matson <alex.matson@digitalasset.com>
2023-02-08 12:40:35 -0500

    Invoke coin binary directly in start-backends script (#2773)

--- scripts/start-backends-for-local-frontend-testing.sh
+++ scripts/start-backends-for-local-frontend-testing.sh
@@ -14,4 +14,4 @@ echo "Generating config file ${OUTPUT_CONFIG} with self-signed tokens"
 scala -classpath $BUNDLE/lib/coin-0.1.0-SNAPSHOT.jar ./scripts/transform-config.sc "useSelfSignedTokensForLedgerApiAuth" "${INPUT_CONFIG}" "${OUTPUT_CONFIG}"
 
 echo "Starting Canton Network apps for local frontend testing"
-sbt --batch "apps-app/runMain com.daml.network.CoinApp --config ${OUTPUT_CONFIG} --bootstrap ./apps/splitwise/frontend/bootstrap.sc --log-level-canton=DEBUG --log-file-name log/coin.log"
+coin --config ${OUTPUT_CONFIG} --bootstrap ./apps/splitwise/frontend/bootstrap.sc --log-level-canton=DEBUG --log-file-name log/coin.log
