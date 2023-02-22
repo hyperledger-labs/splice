hyperledger-labs/splice

commit 02f830ab6ebb03eb7d2a01a5bf3a49dd237466b3

Read tokens from k8s secrets on staging (#3075)
* Read tokens from k8s secrets on staging

part of #3053

Needs some more documentation on testing this for scratch so not yet closing the issue
but given that we seem close to running out of tokens I'd like to
merge this and then add those docs afterwards.

fixes #2869

cluster/manifest/cluster.jsonnet
@@ -63,41 +63,69 @@ local appUserNameEnvBinding(appName, varBaseName=appName) =
 
 local appUserNameEnvBindings(appNames) = std.flatMap(appUserNameEnvBinding, appNames);
 
-local appAuthEnvBinding(appName, varBaseName=appName) =
+local appAuthEnvBinding(clusterName, appName, varBaseName=appName) =
   local name = "CN_APP_" + std.asciiUpper(varBaseName) + "_LEDGER_API_AUTH";
   local secret = std.asciiLower(std.strReplace("CN_APP_" + appName + "_LEDGER_API_AUTH", "_", "-"));
-  [
-    {
-      name: name + "_URL",
-      valueFrom: {
-        secretKeyRef: {
-          name: secret,
-          key: "url",
-          optional: false,
+  // In staging we use fixed tokens read from a k8s secret rather than refreshing through client credentials.
+  // See https://github.com/DACH-NY/the-real-canton-coin/issues/3053 for more details.
+  // We cannot override an object using a substitution so instead we set this through ADDITIONAL_CONFIG
+  // which first resets it back to null to disable object merging and then switches to static token config.
+  if (clusterName == "staging") then
+    [
+      {
+        name: "ADDITIONAL_CONFIG",
+        value: |||
+          _client_credentials_auth_config = null
+          _client_credentials_auth_config = {
+            type = "static"
+            token = ${%s}
+          }
+        ||| % (name + "_TOKEN"),
+      },
+      {
+        name: name + "_TOKEN",
+        valueFrom: {
+          secretKeyRef: {
+            name: secret,
+            key: "token",
+            optional: false,
+          },
         },
       },
-    },
-    {
-      name: name + "_CLIENT_ID",
-      valueFrom: {
-        secretKeyRef: {
-          name: secret,
-          key: "client-id",
-          optional: false,
+    ] + appUserNameEnvBinding(appName, varBaseName)
+  else
+    [
+      {
+        name: name + "_URL",
+        valueFrom: {
+          secretKeyRef: {
+            name: secret,
+            key: "url",
+            optional: false,
+          },
         },
       },
-    },
-    {
-      name: name + "_CLIENT_SECRET",
-      valueFrom: {
-        secretKeyRef: {
-          name: secret,
-          key: "client-secret",
-          optional: false,
+      {
+        name: name + "_CLIENT_ID",
+        valueFrom: {
+          secretKeyRef: {
+            name: secret,
+            key: "client-id",
+            optional: false,
+          },
         },
       },
-    },
-  ] + appUserNameEnvBinding(appName, varBaseName);
+      {
+        name: name + "_CLIENT_SECRET",
+        valueFrom: {
+          secretKeyRef: {
+            name: secret,
+            key: "client-secret",
+            optional: false,
+          },
+        },
+      },
+    ] + appUserNameEnvBinding(appName, varBaseName);
 
 // The amount of memory reserved for the operating system in containers
 // hosting a JVM. The JVM heap size is the container limit less this
@@ -106,10 +134,25 @@ local appAuthEnvBinding(appName, varBaseName=appName) =
 local JVM_SYSTEM_MEMORY_MIB = 1024;
 
 local expandEnvironment(env) =
+  local additional_config =
+    std.join(
+      "\n",
+      std.filterMap(
+        function(binding) binding.name == "ADDITIONAL_CONFIG",
+        function(binding) binding.value,
+        env
+      )
+    );
   std.map(function(binding) (
     local json = std.get(binding, "json");
     if json == null then binding else { name: binding.name, value: std.toString(json) }
-  ), env);
+  ), std.filter(function(binding) binding.name != "ADDITIONAL_CONFIG", env)) +
+  (if additional_config == "" then [] else [
+     {
+       name: "ADDITIONAL_CONFIG",
+       value: additional_config,
+     },
+   ]);
 
 // `image` defaults to `name`
 local deployment(config, name, ports, cpuRequest=1, memoryLimitMiB=1536, ext={}, proxyToGrpcWeb=null, mountConfig=null, tlsCertSecret=null, extraEnvVars=[], image=null, namespace=null) =

.circleci/config.yml
@@ -22,7 +22,7 @@ parameters:
     default: "11"
   run-job:
     type: enum
-    enum: [build, deploy-devnet, deploy-testnet, healthcheck-testnet]
+    enum: [build, deploy-devnet, deploy-testnet, healthcheck-testnet, refresh-staging-tokens]
     default: build
 
 
@@ -390,6 +390,9 @@ commands:
       gce_authenticate:
         type: boolean
         default: false
+      auth0_authenticate:
+        type: boolean
+        default: false
       cluster:
         type: enum
         enum: ["staging", "devnet", "testnet", "none"]
@@ -415,6 +418,10 @@ commands:
             fi
             if [[ << parameters.cluster >> == staging ]]; then
               ADDITIONAL_NIX_ARGS+=" --keep GCP_DA_CN_SCRATCHNET_KEY"
+            fi
+
+            if [[ << parameters.auth0_authenticate >> == true ]]; then
+              ADDITIONAL_NIX_ARGS+=" --keep AUTH0_MANAGEMENT_API_TOKEN"
             fi
 
             nix-shell shell.nix \
@@ -1036,6 +1043,32 @@ jobs:
                 fi
             done
 
+  refresh_tokens:
+    circleci_ip_ranges: true
+    parameters:
+      cluster:
+        type: enum
+        enum: ["staging", "devnet", "testnet"]
+    docker:
+      - image: cimg/base:2021.04
+    environment:
+      <<: *env
+    steps:
+      - checkout
+      - setup_nix
+      - run_bash_command_in_nix:
+          cmd_name: Refresh auth0 tokens
+          cluster: << parameters.cluster >>
+          gce_authenticate: true
+          auth0_authenticate: true
+          cmd: |
+            set -eou pipefail
+            cncluster activate
+            cncluster update_secrets
+          cmd_retry_count: 5
+      - notify-cluster-fail:
+          cluster: << parameters.cluster >>
+
 workflows:
   version: 2
   build:
@@ -1211,6 +1244,17 @@ workflows:
           - deployment_test
           - docker_images_push
           - deploy_cluster
+
+  refresh_staging_tokens:
+    when:
+      equal: [ refresh-staging-tokens, << pipeline.parameters.run-job >> ]
+    jobs:
+    - refresh_tokens:
+        cluster: "staging"
+        <<: *main-branch-only
+        context:
+          - artifactory
+          - slack-context
 
   testnet_health_check:
     when:

build-tools/cncluster
@@ -662,7 +662,7 @@ function _set_auth0_secret() {
   SECRET_NAME=$1
   CLIENT_ID=$2
   CLIENT_SECRET=$3
-  NAMESPACE=${4-default}
+  NAMESPACE=$4
 
   _log "Setting secret $SECRET_NAME ($NAMESPACE)"
 
@@ -675,11 +675,24 @@ function _set_auth0_secret() {
       kubectl delete --namespace "$NAMESPACE" secret ${SECRET_NAME}
   fi
 
-  kubectl create --namespace "$NAMESPACE" secret generic ${SECRET_NAME} \
-    --from-literal=url="${WELL_KNOWN_URL}" \
-    --from-literal=client-id="${CLIENT_ID}" \
-    --from-literal=client-secret="${CLIENT_SECRET}" \
-    --from-literal=ledger-api-user="${USER_NAME}"
+  if [ $GCP_CLUSTER_BASENAME = staging ]; then
+    TOKEN_RESPONSE=$(curl -s --show-error --fail --request POST \
+      --url https://canton-network-dev.us.auth0.com/oauth/token \
+      --header 'content-type: application/json' \
+      --data "{\"client_id\":\"$CLIENT_ID\",\"client_secret\":\"$CLIENT_SECRET\",\"audience\":\"https://canton.network.global\",\"grant_type\":\"client_credentials\"}")
+
+    ACCESS_TOKEN=$(echo $TOKEN_RESPONSE | jq -r ".access_token")
+
+    kubectl create --namespace "$NAMESPACE" secret generic ${SECRET_NAME} \
+      --from-literal=ledger-api-user="${USER_NAME}" \
+      --from-literal=token="${ACCESS_TOKEN}"
+  else
+    kubectl create --namespace "$NAMESPACE" secret generic ${SECRET_NAME} \
+      --from-literal=url="${WELL_KNOWN_URL}" \
+      --from-literal=client-id="${CLIENT_ID}" \
+      --from-literal=client-secret="${CLIENT_SECRET}" \
+      --from-literal=ledger-api-user="${USER_NAME}"
+  fi
 }
 
 function _set_auth0_api_user() {
@@ -746,9 +759,6 @@ function subcmd_update_secrets() {
     client_id="${appToClientId[$2]}"
     secret=$(echo $all | jq -r ".[] | select(.client_id==\"$client_id\") | .client_secret")
     _set_auth0_secret $1 "$client_id" $secret "$3"
-
-    # TODO (#2869) remove once secrets in the "default" namespace are no longer needed
-    _set_auth0_secret $1 "$client_id" $secret
   done
 
   for i in sv1 sv2 sv3 sv4

cluster/images/validator-app/app.conf
@@ -1,3 +1,12 @@
+_client_credentials_auth_config {
+  type = "client-credentials"
+  well-known-config-url = ${?CN_APP_VALIDATOR_LEDGER_API_AUTH_URL}
+  client-id = ${?CN_APP_VALIDATOR_LEDGER_API_AUTH_CLIENT_ID}
+  client-secret = ${?CN_APP_VALIDATOR_LEDGER_API_AUTH_CLIENT_SECRET}
+  # TODO(#1837) use actual audience of the target participant
+  audience = "https://canton.network.global"
+}
+
 canton {
   validator-apps {
     validator_backend {
@@ -16,14 +25,7 @@ canton {
             address = ${CN_APP_VALIDATOR_PARTICIPANT_ADDRESS}
             port = 5001
           }
-          auth-config = {
-            type = "client-credentials"
-            well-known-config-url = ${?CN_APP_VALIDATOR_LEDGER_API_AUTH_URL}
-            client-id = ${?CN_APP_VALIDATOR_LEDGER_API_AUTH_CLIENT_ID}
-            client-secret = ${?CN_APP_VALIDATOR_LEDGER_API_AUTH_CLIENT_SECRET}
-            # TODO(#1837) use actual audience of the target participant
-            audience = "https://canton.network.global"
-          }
+          auth-config = ${_client_credentials_auth_config}
         }
       }
       remote-scan = {

cluster/manifest/canton-network-config.jsonnet
@@ -28,7 +28,7 @@ local defineSvApp(num, config) =
         },
       ],
       image="sv-app",
-      extraEnvVars=c.appAuthEnvBinding("sv") + [
+      extraEnvVars=c.appAuthEnvBinding(config.clusterName, "sv") + [
         { name: "CN_APP_SV_ADMIN_API_PORT", value: std.toString(port) },
         { name: "CN_APP_SV_IS_DEV_NET", value: "true" },
       ] + (
@@ -220,14 +220,14 @@ local svcDeployments(config) = [
       name: "dir-http-api",
       port: 6010,
     },
-  ], namespace="svc", extraEnvVars=c.appAuthEnvBinding("directory")),
+  ], namespace="svc", extraEnvVars=c.appAuthEnvBinding(config.clusterName, "directory")),
 
   c.deployment(config, "svc-app", [
     {
       name: "svc-app-adm-api",
       port: 5005,
     },
-  ], namespace="svc", extraEnvVars=c.appAuthEnvBinding("svc")),
+  ], namespace="svc", extraEnvVars=c.appAuthEnvBinding(config.clusterName, "svc")),
 
   [defineSvApp(num, config) for num in std.range(1, networkDefaults.numberOfSvNodes)],
 
@@ -240,7 +240,7 @@ local svcDeployments(config) = [
       name: "scan-api-http",
       port: 6012,
     },
-  ], namespace="svc", extraEnvVars=c.appAuthEnvBinding("scan")),
+  ], namespace="svc", extraEnvVars=c.appAuthEnvBinding(config.clusterName, "scan")),
 
   c.deployment(config, "scan-web-ui", [
     {
@@ -301,7 +301,7 @@ local validator1Deployments(config) = [
                ],
                image="validator-app",
                namespace="validator1",
-               extraEnvVars=c.appAuthEnvBinding("validator") + c.appUserNameEnvBinding("wallet") +
+               extraEnvVars=c.appAuthEnvBinding(config.clusterName, "validator") + c.appUserNameEnvBinding("wallet") +
                             [
                               { name: "CN_APP_VALIDATOR_WALLET_USER_NAME", value: "auth0|63e3d75ff4114d87a2c1e4f5" },
                               { name: "CN_APP_DARS", json: ["coin-0.1.0-SNAPSHOT/dars/directory-service-0.1.0.dar", "coin-0.1.0-SNAPSHOT/dars/splitwell-0.1.0.dar"] },
@@ -315,7 +315,7 @@ local validator1Deployments(config) = [
       // Internal, we proxy this under /v0/wallet in the UI.
       internalOnly: true,
     },
-  ], image="wallet-app", namespace="validator1", extraEnvVars=c.appAuthEnvBinding("wallet") + [
+  ], image="wallet-app", namespace="validator1", extraEnvVars=c.appAuthEnvBinding(config.clusterName, "wallet") + [
     { name: "CN_APP_WALLET_PARTICIPANT_ADDRESS", value: "canton-participant" },
     { name: "CN_APP_WALLET_VALIDATOR_ADDRESS", value: "validator-app" },
     { name: "CN_APP_WALLET_VALIDATOR_GRPC_PORT", value: "5103" },
@@ -404,7 +404,7 @@ local splitwellDeployments(config) = [
       port: 6003,
       internalOnly: true,
     },
-  ], image="validator-app", namespace="splitwell", extraEnvVars=c.appAuthEnvBinding("validator", "validator") +
+  ], image="validator-app", namespace="splitwell", extraEnvVars=c.appAuthEnvBinding(config.clusterName, "validator", "validator") +
                                                              c.appUserNameEnvBinding("splitwell") +
                                                              c.appUserNameEnvBinding("wallet") +
                                                              [
@@ -429,7 +429,7 @@ local splitwellDeployments(config) = [
       // Internal, we proxy this under /v0/wallet in the UI.
       internalOnly: true,
     },
-  ], image="wallet-app", namespace="splitwell", extraEnvVars=c.appAuthEnvBinding("wallet") + [
+  ], image="wallet-app", namespace="splitwell", extraEnvVars=c.appAuthEnvBinding(config.clusterName, "wallet") + [
     { name: "CN_APP_WALLET_PARTICIPANT_ADDRESS", value: "splitwell-participant" },
     { name: "CN_APP_WALLET_VALIDATOR_ADDRESS", value: "splitwell-validator-app" },
     { name: "CN_APP_WALLET_VALIDATOR_GRPC_PORT", value: "5203" },
@@ -460,7 +460,7 @@ local splitwellDeployments(config) = [
       name: "sw-api",
       port: 5213,
     },
-  ], namespace="splitwell", proxyToGrpcWeb="sw-api", extraEnvVars=c.appAuthEnvBinding("splitwell")),
+  ], namespace="splitwell", proxyToGrpcWeb="sw-api", extraEnvVars=c.appAuthEnvBinding(config.clusterName, "splitwell")),
 ];
 
 local cantonNetwork(config) =

commit 08ce1625b291257738cac37727a9ac2a6b67d21b

Fix cluster check in cluster.jsonnet (#3080)
fallout from #3075

cluster/manifest/cluster.jsonnet
@@ -70,7 +70,7 @@ local appAuthEnvBinding(clusterName, appName, varBaseName=appName) =
   // See https://github.com/DACH-NY/the-real-canton-coin/issues/3053 for more details.
   // We cannot override an object using a substitution so instead we set this through ADDITIONAL_CONFIG
   // which first resets it back to null to disable object merging and then switches to static token config.
-  if (clusterName == "staging") then
+  if (clusterName == "cn-stagingnet") then
     [
       {
         name: "ADDITIONAL_CONFIG",