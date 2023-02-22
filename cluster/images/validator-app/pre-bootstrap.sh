588dc155107f1f9cd820915d24f5f41c51b37009
Martin Florian <martin.florian@digitalasset.com>
2023-02-22 13:50:00 +0100

    Retry when getting the validator onboarding secret failed (#3068)
    Fixes #3056

--- cluster/images/validator-app/pre-bootstrap.sh
+++ cluster/images/validator-app/pre-bootstrap.sh
@@ -3,4 +3,13 @@
 set -eou pipefail
 
 echo "Getting onboarding secret from SV1..."
-curl -sSfL -X POST http://sv-app-1.sv-app-1:6014/devnet/onboard/validator/prepare  | xargs -I _ sed -i 's#PLACEHOLDER#_#' /app/app.conf
+
+while true; do
+  if SECRET=$(curl -sSfL -X POST http://sv-app-1.sv-app-1:6014/devnet/onboard/validator/prepare); then
+    sed -i "s#PLACEHOLDER#$SECRET#" /app/app.conf
+    break
+  else
+    echo "Curl failed. Retrying in 10 seconds..."
+    sleep 10
+  fi
+done
