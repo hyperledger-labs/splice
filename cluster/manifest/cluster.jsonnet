hyperledger-labs/splice

commit a3955cf2be68f24ac0bc08981c1b1d4af2973c2a

Remove `cncluster ports` (#3172)

cluster/manifest/cluster.jsonnet
@@ -308,23 +308,6 @@ local deployment(config, name, ports, cpuRequest=1, memoryLimitMiB=1536, ext={},
     ],
   };
 
-local jsonFileConfigMap(config, name, fileName, data) = {
-  ports: [],
-  deploymentObjects: [
-    {
-      apiVersion: "v1",
-      kind: "ConfigMap",
-      metadata: {
-        name: name,
-      },
-      data: {
-        version: config.imageTag,
-        [fileName]: std.manifestJsonEx(data, "  ", "\n", ": "),
-      },
-    },
-  ],
-};
-
 local externalService(config, ports) = {
   ports: [],
   deploymentObjects: [
@@ -369,15 +352,11 @@ local cluster(config, clusterDeployments) =
   local externalProxyPorts = std.map(function(p) { name: p.name, port: externalPort(p) }, nonInternalPorts);
 
   objects(deployments + [
-    jsonFileConfigMap(config, "cluster-manifest", "manifest.json", {
-      ports: allPorts,
-    }),
     deployment(
       config,
       "external-proxy",
       externalProxyPorts,
       memoryLimitMiB=512,
-      mountConfig="cluster-manifest",
       tlsCertSecret=tlsCertSecret
     ),
     externalService(config, externalProxyPorts),

build-tools/cncluster
@@ -629,19 +629,6 @@ function subcmd_reset() {
 
 ###
 
-subcommand_whitelist[ports]='Display a table of all ports exposed from the cluster.'
-
-function subcmd_ports() {
-    _cluster_show
-    _cluster_must_exist
-
-    curl -L http://${CLUSTER_DNS_NAME}/manifest.json 2> /dev/null \
-        | jq -r '(["SERVICE","PORT-NAME","PORT","AS-GRPC"],(.ports|.[]|[.service, .name, .port, .proxyToGrpc]))|@tsv' \
-        | column -t
-}
-
-###
-
 subcommand_whitelist[info]='Display information on current cluster configuration, including image tags'
 
 function subcmd_info() {

cluster/manifest/Makefile
@@ -20,4 +20,4 @@ $(dir)/test:
 .PHONY: $(dir)/test-update
 $(dir)/test-update:
 	jsonnet $(test-vars) \
-		$(dir)/canton-network-config.jsonnet > $(dir)/test-results/expected.json
+		$(@D)/canton-network-config.jsonnet > $(@D)/test-results/expected.json

commit c0a6d4e4e3340d7ea41badec6e2a7c40dd0080bb

Fix volume mounts for external-proxy (#3176)
This got broken by #3172

Tested preflight on scratch.

cluster/manifest/cluster.jsonnet
@@ -357,6 +357,7 @@ local cluster(config, clusterDeployments) =
       "external-proxy",
       externalProxyPorts,
       memoryLimitMiB=512,
+      mountConfig="cluster-manifest",
       tlsCertSecret=tlsCertSecret
     ),
     externalService(config, externalProxyPorts),