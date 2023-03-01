965b41b52e42908ef4eb84bf599f0eda9dfce431
OriolMunoz-da <oriol.munoz@digitalasset.com>
2023-03-01 13:15:34 +0100

    Add app config to wallet-new (#3178)
    Co-authored-by: Fayi Femi-Balogun <fayimora.femibalogun@digitalasset.com>

---
+++ apps/wallet/frontend-new/public/config.js
@@ -0,0 +1,38 @@
+window.canton_network_config = {
+  // HMAC256-based auth with browser self-signed tokens
+  auth: {
+    algorithm: 'hs-256-unsafe',
+    secret: 'test',
+    token_audience: 'https://canton.network.global',
+  },
+  // OIDC client configuration, see https://authts.github.io/oidc-client-ts/interfaces/UserManagerSettings.html
+  //   auth: {
+  //     algorithm: 'rs-256',
+  //     authority: "",
+  //     client_id: "",
+  //     token_audience: "https://canton.network.global",
+  //     token_scope: "daml_ledger_api",
+  //   },
+  services: {
+    wallet: {
+      // URL of the wallet app HTTP API
+      grpcUrl: 'http://localhost:6004',
+    },
+    validator: {
+      // URL of the validator app HTTP API
+      grpcUrl: 'http://localhost:6003',
+    },
+    // BEGIN_WALLET_CLUSTER_BACKEND_CONFIG
+    directory: {
+      // URL of the directory backend.
+      // Edit this to the cluster you're trying to connect on.
+      grpcUrl: 'https://directory.TARGET_CLUSTER.network.canton.global',
+    },
+    scan: {
+      // URL of scan backend.
+      // Edit this to the cluster you're trying to connect on.
+      grpcUrl: 'https://scan.TARGET_CLUSTER.network.canton.global',
+    },
+    // END_WALLET_CLUSTER_BACKEND_CONFIG
+  },
+};
