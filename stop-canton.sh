c30cd3d226627d6eb4018610a6e8a45968143c87
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2023-03-03 11:23:04 +0100

    Handle non-existent child process in start-canton (#3238)
    * Handle non-existent child process in start-canton
    
    If Canton died you can't run stop-canton atm (which still kills other
    processes, the tmux session, postgres) because this line
    fails.

--- stop-canton.sh
+++ stop-canton.sh
@@ -10,7 +10,7 @@ if tmux has-session -t $tmux_session 2>/dev/null; then
     # parents will usually be some shell process.
     TMUX_PARENT_PIDS="$(tmux list-panes -s -F "#{pane_pid}" -t $tmux_session)"
     # the child is the actual process we want to kill
-    TMUX_CHILD_PIDS=$(echo -n $TMUX_PARENT_PIDS | xargs -d ' ' -I '{}' pgrep -P '{}')
+    TMUX_CHILD_PIDS=$(echo -n $TMUX_PARENT_PIDS | xargs -d ' ' -I '{}' pgrep -P '{}' || true)
     kill -9 $TMUX_CHILD_PIDS $TMUX_PARENT_PIDS
     # Session might be dead at this point because we killed all processes
     # but to be on the safe side we still kill it.
