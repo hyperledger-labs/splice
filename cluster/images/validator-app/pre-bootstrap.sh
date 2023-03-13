cf6005120c458b272394b6f52ce7d814b5472464
Martin Florian <martin.florian@digitalasset.com>
2023-03-13 12:21:34 +0100

    Don't loop forever in validator1 pre-bootstrap (#3420)
    Fixes #3391

--- cluster/images/validator-app/pre-bootstrap.sh
+++ cluster/images/validator-app/pre-bootstrap.sh
@@ -4,12 +4,19 @@ set -eou pipefail
 
 echo "Getting onboarding secret from SV1..."
 
-while true; do
+MAX_RETRY=100
+n=0
+until [ $n -gt $MAX_RETRY ]; do
   if SECRET=$(curl -sSfL -X POST http://sv-app.sv-1:6014/devnet/onboard/validator/prepare); then
     sed -i "s#PLACEHOLDER#$SECRET#" /app/app.conf
     break
   else
-    echo "Curl failed. Retrying in 10 seconds..."
-    sleep 10
+    echo "Curl failed. Retrying in 1 second..."
+    n=$[$n+1]
+    sleep 1
+  fi
+  if [ $n -gt $MAX_RETRY ]; then
+    echo "Getting onboarding secret exceeded max retries" >&2
+    exit 1
   fi
 done
