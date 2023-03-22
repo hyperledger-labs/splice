09b907b55a6c2c8e2863beeed67341b83d19ba3b
Simon Meier <meiersi-da@users.noreply.github.com>
2023-03-22 08:12:41 +0100

    Fix start-backends-for-local-frontend-testing.sh and .envrc wrt cn-node rename (#3649)

--- scripts/start-backends-for-local-frontend-testing.sh
+++ scripts/start-backends-for-local-frontend-testing.sh
@@ -14,4 +14,4 @@ echo "Generating config file ${OUTPUT_CONFIG} with self-signed tokens"
 scala -classpath $BUNDLE/lib/cn-node-0.1.0-SNAPSHOT.jar ./scripts/transform-config.sc "useSelfSignedTokensForLedgerApiAuth" "${INPUT_CONFIG}" "${OUTPUT_CONFIG}"
 
 echo "Starting Canton Network apps for local frontend testing"
-coin --config ${OUTPUT_CONFIG} --bootstrap ./apps/splitwell/frontend/bootstrap.sc --log-level-canton=DEBUG --log-encoder json --log-file-name log/coin.clog
+cn-node --config ${OUTPUT_CONFIG} --bootstrap ./apps/splitwell/frontend/bootstrap.sc --log-level-canton=DEBUG --log-encoder json --log-file-name log/cn-node_local_frontend_testing.clog
