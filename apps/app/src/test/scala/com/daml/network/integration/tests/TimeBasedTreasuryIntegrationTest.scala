5847dfb9d9cc9668833b8125e87709f70fd06dc3
Arne Gebert <arne.gebert@digitalasset.com>
2023-03-29 14:19:43 +0200

    Kill TreasuryShutdownUnderloadTest (#3789)
    I propose to just kill this test. Reasons:
    
    We invested the time to understand the shutdown behaviour and the behaviour is fine which makes this test less useful.
    We can reasonably expect that in practice we won't hit the scenario we were trying to test for the next ~1-2 years as evidenced by our issue with reliably hitting this shutdown-under-load scenario in tests.
    It would be relatively expensive engineering-timewise to update and maintain this test, and its likely more efficient to re-add it for post-MVP scope when we want to test our other apps' shutdown behaviour in a principled manner as well
    fixes #2757 fixes #1942

--- apps/app/src/test/scala/com/daml/network/integration/tests/TimeBasedTreasuryIntegrationTest.scala
+++ apps/app/src/test/scala/com/daml/network/integration/tests/TimeBasedTreasuryIntegrationTest.scala
@@ -1,25 +1,14 @@
 package com.daml.network.integration.tests
 
-import cats.syntax.traverse.*
-import com.daml.network.config.CNNodeConfigTransforms.{
-  setPollingInterval,
-  updateAllWalletAppBackendConfigs_,
-}
+import com.daml.network.config.CNNodeConfigTransforms.setPollingInterval
 import com.daml.network.integration.CNNodeEnvironmentDefinition
 import com.daml.network.integration.tests.CNNodeTests.CNNodeIntegrationTest
 import com.daml.network.util.{CNNodeUtil, TimeTestUtil, WalletTestUtil}
-import com.daml.network.wallet.config.TreasuryConfig
-import com.digitalasset.canton.concurrent.Threading
 import com.digitalasset.canton.logging.SuppressionRule
 import com.digitalasset.canton.{HasExecutionContext, time}
-import io.grpc.StatusRuntimeException
-import monocle.macros.syntax.lens.*
 import org.slf4j.event.Level
 
 import java.time.Duration
-import scala.concurrent.duration.*
-import scala.concurrent.{Await, Future}
-import scala.util.Success
 
 class TimeBasedTreasuryIntegrationTest
     extends CNNodeIntegrationTest
@@ -27,96 +16,13 @@ class TimeBasedTreasuryIntegrationTest
     with WalletTestUtil
     with TimeTestUtil {
 
-  private val batchSize = 10
-  // need a larger queue size so we can guarantee that it is still pretty full once we shutdown.
-  private val queueSize = 80
-
   override def environmentDefinition: CNNodeEnvironmentDefinition = {
     CNNodeEnvironmentDefinition
       .simpleTopologyWithSimTime(this.getClass.getSimpleName)
-      .addConfigTransform((_, cnNodeConfig) =>
-        updateAllWalletAppBackendConfigs_(walletConfig =>
-          walletConfig.focus(_.treasury).replace(TreasuryConfig(batchSize, queueSize))
-        )(cnNodeConfig)
-      )
       .addConfigTransform((_, config) =>
         // for testing non-automation-based coin merging.
         setPollingInterval(time.NonNegativeFiniteDuration.ofSeconds(30))(config)
       )
-      // these settings are needed for the "operate and shutdown cleanly ..." test to pass,
-      // since it requires a lot of open / queued requests
-      .withHttpSettingsForHigherThroughput
-  }
-
-  "operate and shutdown cleanly with lots of coin operations in flight" ignore { implicit env =>
-    onboardWalletUser(aliceWallet, aliceValidator)
-
-    def tapInRange(start: Int, end: Int) = {
-      Range(start, end).toList.traverse(i =>
-        Future {
-          try {
-            aliceWallet.tap(i)
-          } catch {
-            case scala.util.control.NonFatal(ex) =>
-              logger.info("Ignoring exception when executing tap", ex)
-          }
-        }.transform(Success(_))
-      )
-    }
-
-    val inflight = loggerFactory.assertLoggedWarningsAndErrorsSeq(
-      {
-        // waiting for a few taps succeed - so...
-        val futuresWait = tapInRange(1, 2 * batchSize)
-        // .. these taps here can fill up the queue
-        val futuresDontWait = tapInRange(2 * batchSize + 1, (2 * batchSize + 1) + 5 * queueSize)
-
-        Await.result(futuresWait, atMost = 15.seconds)
-        // such that some of them will still be in-flight when we shutdown
-        futuresDontWait.isCompleted shouldBe false
-
-        clue("Stopping alice's wallet") {
-          // In manual runs, we've observed that sometimes the gRPC server reports a slow shutdown.
-          // We accept that for now.
-          aliceWalletBackend.stop()
-        }
-        // sleeping 1s, as sometimes error from TODO(#1942) are logged delayed.
-        Threading.sleep(1000)
-        futuresDontWait
-      },
-      lines => {
-        // \r is carriage return
-        val regexAnything = "(.|\\n|\\r)*"
-        val errorRegex = Seq(
-          s"(Connection reset|Connection refused)"
-        ).mkString(regexAnything)
-
-        val errorRegex2 = // TODO(#1942): these errors should occur during this test.
-          s"(Skipping batch due to unexpected execution failure|Unexpected coin operation execution failure)"
-
-        forAll(lines) { line =>
-          if (line.level == Level.ERROR) {
-            line.throwable match {
-              // TODO(#1942): we are only aware of this unhandled exception during shutdowns.
-              case Some(throwable: StatusRuntimeException) if line.message.matches(errorRegex2) =>
-                throwable.getMessage should include("UNAVAILABLE")
-              case None =>
-              case other => fail(s"unexpected exception: $other")
-            }
-            line.message should (include regex errorRegex or include regex errorRegex2)
-
-          } else if (line.level == Level.WARN) {
-            // If the coin operation buffer is large or batch execution is slow, then shutdown is not quick enough.
-            // We accept that for now, as it is non-trivial to propagate the shutdown signal from the server to the
-            // coin operation batch executor.
-            line.message should include regex ("NettyServer.*shutdown did not complete gracefully")
-          } else {
-            fail(s"unexpected warning or error: $line")
-          }
-        }
-      },
-    )
-    Await.result(inflight, atMost = 60.seconds)
   }
 
   "automatically merge transfer inputs when the automation is triggered" in { implicit env =>
