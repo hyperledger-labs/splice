7cf6ccb3893c6c17e77e8e50cb95743cc47f1f69
Claudio Bley <103681498+cbley-da@users.noreply.github.com>
2023-04-14 07:55:41 +0200

    Remove unneeded Pulumi resource and language plugins (#4042)
    Fixes #3791

--- nix/overlays.nix
+++ nix/overlays.nix
@@ -13,4 +13,29 @@
       proto3-suite = super.haskell.lib.dontCheck (super.haskell.lib.disableCabalFlag hsSuper.proto3-suite "swagger");
     };
   };
+  pulumi-bin = super.pulumi-bin.overrideAttrs (_: previousAttrs:
+    let
+      inherit (super.lib.strings) hasPrefix;
+
+      neededResourcePlugins = [
+        "auth0" "gcp" "google-native" "kubernetes" "postgresql" "random" "tls" "vault"
+      ];
+
+      isResourcePlugin = d: hasPrefix "pulumi-resource-" d.name;
+
+      isRequired = d: builtins.any (p: hasPrefix ("pulumi-resource-" + p) d.name) neededResourcePlugins;
+
+      keepSrc = d: isRequired d || ! isResourcePlugin d;
+    in {
+      srcs = builtins.filter keepSrc previousAttrs.srcs;
+
+      installPhase = ''
+        # remove unneeded language plugins
+        rm -v pulumi-language-{dotnet,go,java,python,python-exec}
+
+        ${previousAttrs.installPhase}
+      '';
+
+      dontPatchELF = true;
+    });
 })]
