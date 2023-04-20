34feb268c6bc910ba37c761a164b89c309b43237
Mike Schaeffer <40773844+mschaef-da@users.noreply.github.com>
2023-04-20 09:18:01 -0400

    Add a degree of configurability to external-proxy to enable it to be used in partial cluster deployments at customer sites. (#4175)

---
+++ cluster/images/external-proxy/90-install-mode-conf-files.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "Installing ingress modes: ${ENABLE_INGRESS_MODES}"
+
+for modename in ${ENABLE_INGRESS_MODES}; do
+    echo "Installing mode: ${modename}"
+
+    cp -v /conf-modes/"${modename}"/* /etc/nginx/conf.d/
+done
+
+echo "Done installing ingres modes."
