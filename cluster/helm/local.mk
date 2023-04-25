4418cb235c820c27f3ddae2eec6419687d66129c
Mike Schaeffer <40773844+mschaef-da@users.noreply.github.com>
2023-04-25 11:33:16 -0400

    Generate Helm charts with a version based on the number of commits in the branch. (#4274)

--- cluster/helm/local.mk
+++ cluster/helm/local.mk
@@ -1,4 +1,6 @@
 charts := \
+	cn-util-lib \
+	\
 	cn-cluster-ingress \
 	cn-directory-web-ui \
 	cn-docs \
@@ -18,7 +20,10 @@ cluster/helm/build: $(foreach chart,$(charts),cluster/helm/$(chart)/helm-build)
 cluster/helm/clean: $(foreach chart,$(charts),cluster/helm/$(chart)/helm-clean)
 	rm -rfv cluster/helm/target
 
-%/values.yaml: %/values-template.yaml
+%/values.yaml: %/values-template.yaml HELM_CHART_VERSION
+	cat $< | version-tag-subst > $@
+
+%/Chart.yaml: %/Chart-template.yaml HELM_CHART_VERSION
 	cat $< | version-tag-subst > $@
 
 #########
@@ -31,12 +36,12 @@ define DEFINE_PHONY_CHART_RULES =
 prefix := cluster/helm/$(1)
 
 .PHONY: $$(prefix)/helm-build
-$$(prefix)/helm-build: $$(prefix)/values.yaml
+$$(prefix)/helm-build: $$(prefix)/values.yaml $$(prefix)/Chart.yaml
 	helm package $$(@D) --dependency-update --destination cluster/helm/target
 
 .PHONY: $$(prefix)/helm-clean
 $$(prefix)/helm-clean:
-	rm -vf $$(@D)/values.yaml
+	rm -vf $$(@D)/values.yaml $$(@D)/Chart.yaml
 
 endef # end DEFINE_PHONY_CHART_RULES
 
