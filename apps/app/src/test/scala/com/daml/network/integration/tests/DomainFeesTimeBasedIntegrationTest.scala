0cb417227804395b840854f9495e4bebf32792c1
Parth Joshi <parth.joshi@digitalasset.com>
2023-04-26 13:18:11 +0200

    Changes to domain fees tests to make them more robust (#4275)
    * moved domain fees tx log check into separate test
    * make domain fees integration test not depend on order of tests

--- apps/app/src/test/scala/com/daml/network/integration/tests/DomainFeesTimeBasedIntegrationTest.scala
+++ apps/app/src/test/scala/com/daml/network/integration/tests/DomainFeesTimeBasedIntegrationTest.scala
@@ -1,8 +1,8 @@
 package com.daml.network.integration.tests
 
 import com.daml.network.config.CNNodeConfigTransforms.updateAllValidatorConfigs_
+import com.daml.network.console.WalletAppClientReference
 import com.daml.network.integration.CNNodeEnvironmentDefinition
-import com.daml.network.wallet.store.UserWalletTxLogParser.TxLogEntry as walletLogEntry
 import com.daml.network.integration.tests.CNNodeTests.{
   CNNodeIntegrationTestWithSharedEnvironment,
   CNNodeTestConsoleEnvironment,
@@ -19,7 +19,6 @@ import scala.util.control.NonFatal
 class DomainFeesTimeBasedIntegrationTest
     extends CNNodeIntegrationTestWithSharedEnvironment
     with WalletTestUtil
-    with WalletTxLogTestUtil
     with TimeTestUtil {
 
   override def environmentDefinition: CNNodeEnvironmentDefinition = {
@@ -42,21 +41,31 @@ class DomainFeesTimeBasedIntegrationTest
 
   "A validator with a correctly configured traffic top-up loop" when {
     s"default throughput is ${DomainFeesConstants.defaultThroughput} txs/s & target throughput is ${DomainFeesConstants.targetThroughput} txs/s" must {
+      // bobValidator is used in this test as an example of a validator that will submit requests
+      // at the default rate only and will not purchase any extra traffic.
+      // TODO(M3-44): Once we're no longer mocking the canton sequencer and the top-up trigger is live for all tests,
+      //  it may be worthwhile to create a separate validator for this purpose to properly isolate this test
+      //  from other tests making use of bobValidator and any residual traffic balance that may be left over as a result.
       s"be limited to default rate if unable to buy extra traffic" in { implicit env =>
         actAndCheck(
           "Create self-hosted wallet with insufficient balance",
-          onboardWalletUser(aliceWallet, aliceValidator),
+          onboardWalletUser(bobWallet, bobValidator),
         )(
           "Taps throttled to default throughput rate",
           _ => {
             val (successes, totalTxs) =
-              testCoinTxsAndAssertLogs(2 * DomainFeesConstants.defaultThroughput.value)
-            successes.toDouble should be > 0.3 * totalTxs
-            successes.toDouble should be < 0.7 * totalTxs
+              testCoinTxsAndAssertLogs(
+                bobWallet,
+                2 * DomainFeesConstants.defaultThroughput.value,
+              )
+            successes.toDouble should be > 0.4 * totalTxs
+            successes.toDouble should be < 0.6 * totalTxs
           },
         )
       }
 
+      // aliceValidator is used in the subsequent tests as an example of a validator that will
+      // purchase extra traffic by paying domain fees to the SVC
       s"be able to hit target throughput by buying extra traffic" in { implicit env =>
         actAndCheck(
           "Create self-hosted wallet with sufficient balance", {
@@ -70,7 +79,10 @@ class DomainFeesTimeBasedIntegrationTest
             // purchased extra traffic for the first time.
             advanceTimeByPollingInterval(aliceValidator)
             val (successes, totalTxs) =
-              testCoinTxsAndAssertLogs(DomainFeesConstants.targetThroughput.value)
+              testCoinTxsAndAssertLogs(
+                aliceWallet,
+                DomainFeesConstants.targetThroughput.value,
+              )
             successes shouldBe totalTxs
           },
         )
@@ -89,70 +101,25 @@ class DomainFeesTimeBasedIntegrationTest
             // purchased extra traffic for the first time.
             advanceTimeByPollingInterval(aliceValidator)
             val (successes, totalTxs) =
-              testCoinTxsAndAssertLogs(2 * DomainFeesConstants.targetThroughput.value)
-            successes.toDouble should be > 0.3 * totalTxs
-            successes.toDouble should be < 0.7 * totalTxs
+              testCoinTxsAndAssertLogs(
+                aliceWallet,
+                2 * DomainFeesConstants.targetThroughput.value,
+              )
+            successes.toDouble should be > 0.4 * totalTxs
+            successes.toDouble should be < 0.6 * totalTxs
           },
         )
       }
     }
   }
 
-  // TODO(#3816): Move this test to WalletTxLogWithRewardsCollectionTimeBasedIntegrationTest once
-  //  the env config flags have been removed
-  "A validator wallet's tx log" should {
-    "handle domain fees properly" in { implicit env =>
-      clue("Create validator wallet with sufficient balance") {
-        onboardWalletUser(bobWallet, bobValidator)
-        bobValidatorWallet.tap(20)
-      }
-      actAndCheck(
-        "Purchase extra traffic",
-        // Advance time by 1 polling interval to allow the automation
-        // to kick in and purchase extra traffic.
-        advanceTimeByPollingInterval(bobValidator),
-      )(
-        "Verify the transaction history",
-        _ => {
-          // Amount of fees paid equals amount of extra traffic purchased since 1 CC = 1 Traffic Unit
-          val domainFeesPaid = BigDecimal(
-            (DomainFeesConstants.targetThroughput.value - DomainFeesConstants.defaultThroughput.value) *
-              bobValidator.config.automation.pollingInterval.duration.toSeconds
-          )
-          bobValidatorWallet.balance().unlockedQty should be < BigDecimal(20)
-          checkTxHistory(
-            bobValidatorWallet,
-            Seq[CheckTxHistoryFn](
-              { case logEntry: walletLogEntry.Transfer =>
-                // Payment of domain fees by validator to SVC
-                logEntry.transactionSubtype shouldBe walletLogEntry.Transfer.ExtraTrafficPurchase
-                inside(logEntry.sender) { case (sender, amount) =>
-                  sender shouldBe bobValidator.getValidatorPartyId().toProtoPrimitive
-                  amount should beWithin(-domainFeesPaid - smallAmount, -domainFeesPaid)
-                }
-                inside(logEntry.receivers) { case Seq((receiver, amount)) =>
-                  receiver shouldBe svcParty.toProtoPrimitive
-                  // domain fees paid is immediately burnt by SVC
-                  amount shouldBe 0
-                }
-              },
-              { case logEntry: walletLogEntry.BalanceChange =>
-                logEntry.transactionSubtype shouldBe walletLogEntry.BalanceChange.Tap
-              },
-            ),
-          )
-        },
-      )
-    }
-
-  }
-
   private def testCoinTxsAndAssertLogs(
-      coinTxsPerSecond: Double
+      wallet: WalletAppClientReference,
+      coinTxsPerSecond: Double,
   )(implicit env: CNNodeTestConsoleEnvironment) = {
     loggerFactory.assertLoggedWarningsAndErrorsSeq(
       {
-        val result = tryCoinTxs(coinTxsPerSecond)
+        val result = tryCoinTxs(wallet, coinTxsPerSecond)
         // Advance time to allow the default traffic limiter to reset.
         // Without this, the initial tap in subsequent tests may get throttled.
         advanceTime(
@@ -171,7 +138,8 @@ class DomainFeesTimeBasedIntegrationTest
   }
 
   private def tryCoinTxs(
-      coinTxsPerSecond: Double
+      wallet: WalletAppClientReference,
+      coinTxsPerSecond: Double,
   )(implicit env: CNNodeTestConsoleEnvironment) = {
     val totalTxs = (coinTxsPerSecond * loadTestDuration.toSeconds).toInt
     val deltaT = Duration.ofMillis((1e3 / coinTxsPerSecond).toLong)
@@ -181,7 +149,7 @@ class DomainFeesTimeBasedIntegrationTest
       def tapAndReturnOneOnSuccess() = {
         try {
           logger.debug(s"executing tap $start")
-          aliceWallet.tap(start)
+          wallet.tap(start)
           1
         } catch {
           case NonFatal(ex) =>
---
+++ apps/app/src/test/scala/com/daml/network/integration/tests/WalletTxLogWithDomainFeesTimeBasedIntegrationTest.scala
@@ -0,0 +1,78 @@
+package com.daml.network.integration.tests
+
+import com.daml.network.config.CNNodeConfigTransforms
+import com.daml.network.config.CNNodeConfigTransforms.updateAllValidatorConfigs_
+import com.daml.network.integration.CNNodeEnvironmentDefinition
+import com.daml.network.integration.tests.CNNodeTests.CNNodeIntegrationTestWithSharedEnvironment
+import com.daml.network.util.{SplitwellTestUtil, WalletTestUtil}
+import com.daml.network.wallet.store.UserWalletTxLogParser.TxLogEntry as walletLogEntry
+import com.digitalasset.canton.HasExecutionContext
+import monocle.Monocle.toAppliedFocusOps
+
+class WalletTxLogWithDomainFeesTimeBasedIntegrationTest
+    extends CNNodeIntegrationTestWithSharedEnvironment
+    with HasExecutionContext
+    with WalletTestUtil
+    with SplitwellTestUtil
+    with WalletTxLogTestUtil {
+
+  private val coinPrice = BigDecimal(1.25).setScale(10)
+
+  override def environmentDefinition: CNNodeEnvironmentDefinition = {
+    CNNodeEnvironmentDefinition
+      .simpleTopologyWithSimTime(this.getClass.getSimpleName)
+      // Set a non-unit coin price to better test CC-USD conversion.
+      .addConfigTransform((_, config) => CNNodeConfigTransforms.setCoinPrice(coinPrice)(config))
+      .addConfigTransform((_, cnNodeConfig) =>
+        updateAllValidatorConfigs_(validatorConfig =>
+          validatorConfig
+            .focus(_.automation.enableAutomaticValidatorTrafficBalanceTopup)
+            .replace(true)
+        )(cnNodeConfig)
+      )
+  }
+
+  "A wallet" should {
+
+    "handle domain fees that has been paid" in { implicit env =>
+      clue("Create validator wallet with sufficient balance") {
+        onboardWalletUser(bobWallet, bobValidator)
+        bobValidatorWallet.tap(20)
+      }
+      actAndCheck(
+        "Purchase extra traffic",
+        // Advance time by 1 polling interval to allow the automation
+        // to kick in and purchase extra traffic.
+        advanceTimeByPollingInterval(bobValidator),
+      )(
+        "Verify the transaction history",
+        _ => {
+          bobValidatorWallet.balance().unlockedQty should be < BigDecimal(20)
+          checkTxHistory(
+            bobValidatorWallet,
+            Seq[CheckTxHistoryFn](
+              { case logEntry: walletLogEntry.Transfer =>
+                // Payment of domain fees by validator to SVC
+                logEntry.transactionSubtype shouldBe walletLogEntry.Transfer.ExtraTrafficPurchase
+                inside(logEntry.sender) { case (sender, amount) =>
+                  sender shouldBe bobValidator.getValidatorPartyId().toProtoPrimitive
+                  amount should be < BigDecimal(0)
+                }
+                inside(logEntry.receivers) { case Seq((receiver, amount)) =>
+                  receiver shouldBe svcParty.toProtoPrimitive
+                  // domain fees paid is immediately burnt by SVC
+                  amount shouldBe 0
+                }
+              },
+              { case logEntry: walletLogEntry.BalanceChange =>
+                logEntry.transactionSubtype shouldBe walletLogEntry.BalanceChange.Tap
+              },
+            ),
+          )
+        },
+      )
+    }
+
+  }
+
+}
