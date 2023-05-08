75b3e1df44b10f7069c0dafa9720314138354591
Simon Meier <meiersi-da@users.noreply.github.com>
2023-05-08 15:12:20 +0200

    Write core of CometBFT topology reconciliation loop (#4547)
    * Write core of CometBFT topology reconciliation loop
    
    Fixes #4466

---
+++ apps/sv/src/main/scala/com/daml/network/sv/cometbft/CometBftNode.scala
@@ -0,0 +1,193 @@
+package com.daml.network.sv.cometbft
+
+import com.digitalasset.canton.drivers as proto
+import com.daml.network.codegen.java.cn as daml
+import com.digitalasset.canton.logging.{NamedLoggerFactory, NamedLogging, TracedLogger}
+import com.digitalasset.canton.logging.pretty.{Pretty, PrettyUtil}
+import com.digitalasset.canton.tracing.TraceContext
+import com.digitalasset.canton.util.ShowUtil.*
+
+import scala.collection.immutable
+import scala.concurrent.{ExecutionContext, Future}
+import scala.jdk.CollectionConverters.*
+
+/** A handle to a CometBFT node.
+  *
+  * @param owningSvNode: the identifier of the SvNode that owns and manages this CometBFT node.
+  */
+class CometBftNode(
+    owningSvNode: String,
+    cometBftService: proto.cometbft.NodeGovernanceServiceGrpc.NodeGovernanceService,
+    protected val loggerFactory: NamedLoggerFactory,
+)(implicit ec: ExecutionContext)
+    extends NamedLogging {
+
+  import com.daml.network.sv.cometbft.CometBftNode.*
+
+  /** Reconcile the CometBFT network config seen by this node with the target configuration specified
+    * in the `SvcRules` contract.
+    *
+    * This method is idempotent and safe to retry.
+    */
+  def reconcileNetworkConfig(target: daml.svcrules.SvcRules): Future[Unit] = for {
+    actualConfig <- cometBftService.getNetworkConfig(com.google.protobuf.empty.Empty())
+    // We minimize latency by issuing all change requests at once, which is safe as we expect <= 16 SV nodes
+    _ <- Future.traverse(diffNetworkConfig(owningSvNode, target.members, actualConfig, logger))(
+      // TODO(M3-47): consider moving `diffNetworkConfig` into this class to minimize the parameter passing; it's currently kept outside for ease of unit testing
+      submitChangeRequest
+    )
+  } yield ()
+
+  private def submitChangeRequest(
+      changeRequest: proto.cometbft.NetworkConfigChangeRequest
+  ): Future[com.google.protobuf.empty.Empty] = {
+    val request = proto.cometbft.UpdateNetworkConfigRequest(
+      changeRequestBytes = changeRequest.toByteString,
+      signature = com.google.protobuf.ByteString.EMPTY, // TODO(M3-47): actually sign :)
+    )
+    cometBftService.updateNetworkConfig(request)
+  }
+}
+
+object CometBftNode {
+
+  /** Compute the set of change requests that, when applied, make the current network config
+    * match the target config specified by the `SvcRules` contract.
+    */
+  def diffNetworkConfig(
+      owningSvNode: String,
+      targetMemberInfos: java.util.Map[String, daml.svcrules.MemberInfo],
+      currentNetworkConfig: proto.cometbft.GetNetworkConfigResponse,
+      logger: TracedLogger,
+  ): Seq[proto.cometbft.NetworkConfigChangeRequest] = {
+    val targetConfig = memberInfosToNetworkConfig(targetMemberInfos)
+    val actualOrPendingConfig = getActualOrPendingConfig(owningSvNode, currentNetworkConfig, logger)
+
+    def mkChangeRequest(
+        svNodeId: String,
+        currentConfigRevision: Long,
+        change: proto.cometbft.SvNodeConfigChange.Kind,
+    ): proto.cometbft.NetworkConfigChangeRequest = {
+      val changeRequest = proto.cometbft.SvNodeConfigChangeRequest(
+        svNodeId = svNodeId,
+        currentConfigRevision = currentConfigRevision,
+        change = Some(proto.cometbft.SvNodeConfigChange(kind = change)),
+      )
+      proto.cometbft.NetworkConfigChangeRequest(
+        chainId = currentNetworkConfig.chainId,
+        submitterSvNodeId = owningSvNode,
+        kind = proto.cometbft.NetworkConfigChangeRequest.Kind.NodeConfigChangeRequest(changeRequest),
+      )
+    }
+
+    // Delete SV node configs for SV nodes that have been deleted, and whose deletion is not yet scheduled
+    val toDelete = actualOrPendingConfig.filter { case (svNodeId, svNodeConfigState) =>
+      !targetConfig.contains(svNodeId) && svNodeConfigState.actualOrPendingConfig.isDefined
+    }
+    val deleteConfigRequests = toDelete.collect { case (svNodeId, svNodeConfigState) =>
+      mkChangeRequest(
+        svNodeId,
+        svNodeConfigState.currentRevision,
+        proto.cometbft.SvNodeConfigChange.Kind.DeleteConfig(com.google.protobuf.empty.Empty()),
+      )
+    }
+
+    // Set SV node configs for SV nodes whose config has changed, and for whom we did not yet
+    // create a pending change request.
+    val missingOrDifferent =
+      targetConfig.collect(Function.unlift { case (svNodeId, targetSvNodeConfig) =>
+        actualOrPendingConfig.get(svNodeId) match {
+          case None => Some((svNodeId, 0L, targetSvNodeConfig))
+          case Some(configState) =>
+            if (configState.actualOrPendingConfig.contains(targetSvNodeConfig))
+              None
+            else
+              Some((svNodeId, configState.currentRevision, targetSvNodeConfig))
+        }
+      })
+    val setConfigRequests = missingOrDifferent.map {
+      case (svNodeId, currentRevision, targetSvNodeConfig) =>
+        mkChangeRequest(
+          svNodeId,
+          currentRevision,
+          proto.cometbft.SvNodeConfigChange.Kind.SetConfig(targetSvNodeConfig),
+        )
+    }
+
+    (deleteConfigRequests ++ setConfigRequests).toSeq
+  }
+
+  /** SvNodeConfig as seen from a specific node. */
+  private case class ReconcilableSvNodeConfigState(
+      currentRevision: Long,
+      actualOrPendingConfig: Option[proto.cometbft.SvNodeConfig],
+  )
+
+  /** Merge the actual and pending config updates of a specific SvNode into a unified view. */
+  private def getActualOrPendingConfig(
+      owningSvNode: String,
+      response: proto.cometbft.GetNetworkConfigResponse,
+      logger: TracedLogger,
+  ): immutable.Map[String, ReconcilableSvNodeConfigState] = {
+    @SuppressWarnings(Array("org.wartremover.warts.Product"))
+    implicit val pendingChangePretty: Pretty[proto.cometbft.SvNodeConfigPendingChange] =
+      PrettyUtil.adHocPrettyInstance
+
+    response.svNodeConfigStates.map { case (svNodeId, svNodeConfigState) =>
+      val actualOrPendingConfig = svNodeConfigState.pendingChanges.get(owningSvNode) match {
+        case None => svNodeConfigState.currentConfig
+        case Some(pendingChange) =>
+          pendingChange.change match {
+            case None =>
+              // The `.change` field is not set. This should not happen, but if it does we
+              // expect the change to not go through and thus assume the current config is what wins.
+              logger.warn(
+                show"Field `change` is unexpectedly not set in pending change for ${svNodeId.singleQuoted} by ${owningSvNode.singleQuoted}: $pendingChange"
+              )(TraceContext.empty)
+              svNodeConfigState.currentConfig
+            case Some(change) =>
+              change.kind match {
+                case proto.cometbft.SvNodeConfigChange.Kind.DeleteConfig(_) => None
+                case proto.cometbft.SvNodeConfigChange.Kind.SetConfig(config) => Some(config)
+                case proto.cometbft.SvNodeConfigChange.Kind.Empty =>
+                  // We do not expect this case, but could encounter it in case a future version of
+                  // the governance proto is used by a new version of this app.
+                  // Given that we don't know better, we assume that the current config wins, which likely
+                  // will lead to us issuing a new change request overriding the one we don't understand,
+                  // which is what we want in case of a downgrade scenario.
+                  logger.warn(
+                    show"Field `kind` is unexpectedly set to `Kind.Empty` in pending change for ${svNodeId.singleQuoted} by ${owningSvNode.singleQuoted}, which might be due to downgrading the SV app: $pendingChange"
+                  )(TraceContext.empty)
+                  svNodeConfigState.currentConfig
+              }
+          }
+      }
+      val currentRevision = svNodeConfigState.currentConfigRevision
+      (svNodeId, ReconcilableSvNodeConfigState(currentRevision, actualOrPendingConfig))
+    }
+  }
+
+  private def cometBftNodeConfigToProto(
+      config: daml.cometbft.CometBftNodeConfig
+  ): proto.cometbft.CometBftNodeConfig =
+    proto.cometbft.CometBftNodeConfig(
+      validatorPubKey = config.validatorPubKey,
+      votingPower = config.votingPower,
+    )
+
+  private def svNodeConfigToProto(config: daml.cometbft.SvNodeConfig): proto.cometbft.SvNodeConfig =
+    proto.cometbft.SvNodeConfig(cometbftNodes =
+      config.cometBftNodes.asScala
+        .map({ case (nodeId, nodeConfig) =>
+          (nodeId, cometBftNodeConfigToProto(nodeConfig))
+        })
+        .toMap
+    )
+
+  private def memberInfosToNetworkConfig(
+      memberInfos: java.util.Map[String, daml.svcrules.MemberInfo]
+  ): immutable.Map[String, proto.cometbft.SvNodeConfig] =
+    memberInfos.asScala.values
+      .map(info => (info.name, svNodeConfigToProto(info.cometBftConfig)))
+      .toMap
+}
---
+++ apps/sv/src/test/scala/com/daml/network/sv/cometbft/CometBftNodeTest.scala
@@ -0,0 +1,285 @@
+package com.daml.network.sv.cometbft
+
+import com.digitalasset.canton.BaseTest
+import com.digitalasset.canton.drivers as proto
+import com.daml.network.codegen.java.cn as daml
+import com.daml.network.codegen.java.cc.api.v1.round.Round
+import com.daml.network.codegen.java.cn.cometbft.SvNodeConfig
+import org.scalatest.wordspec.AnyWordSpec
+
+import scala.jdk.CollectionConverters.*
+
+class CometBftNodeTest extends AnyWordSpec with BaseTest {
+
+  private val owningSvNodeNr = 1
+  private val owningSvNodeId = mkSvNodeId(owningSvNodeNr)
+  private val chainId = "dummy-chain-id"
+
+  private def mkCometBftNodeName(svNodeNr: Int) = "cometBftNode" + svNodeNr.toString
+
+  private def mkSvNodeId(svNodeNr: Int) = "svNode" + svNodeNr.toString
+
+  private def mkChangeRequest(svNodeNr: Int, targetState: Option[String]) = {
+    val svNodeId = mkSvNodeId(svNodeNr)
+    val changeRequest = proto.cometbft.SvNodeConfigChangeRequest(
+      svNodeId = svNodeId,
+      currentConfigRevision = (svNodeNr % 2).toLong,
+      change = Some(mkSvNodeConfigChange(svNodeNr, targetState)),
+    )
+    proto.cometbft.NetworkConfigChangeRequest(
+      chainId = chainId,
+      submitterSvNodeId = owningSvNodeId,
+      kind = proto.cometbft.NetworkConfigChangeRequest.Kind.NodeConfigChangeRequest(changeRequest),
+    )
+  }
+
+  private def mkSvNodeConfig(svNodeNr: Int, validatorKey: String) =
+    proto.cometbft.SvNodeConfig(
+      Map(
+        mkCometBftNodeName(svNodeNr) -> proto.cometbft.CometBftNodeConfig(
+          validatorPubKey = validatorKey,
+          votingPower = 1L,
+        )
+      )
+    )
+
+  private def mkSvNodeConfigChange(svNodeNr: Int, targetState: Option[String]) =
+    proto.cometbft.SvNodeConfigChange(kind = targetState match {
+      case None =>
+        proto.cometbft.SvNodeConfigChange.Kind.DeleteConfig(com.google.protobuf.empty.Empty())
+      case Some(validatorKey) =>
+        proto.cometbft.SvNodeConfigChange.Kind.SetConfig(mkSvNodeConfig(svNodeNr, validatorKey))
+    })
+
+  private def mkNetworkConfig(
+      currentConfigs: Seq[(Int, String, Seq[(Int, Option[String])])]
+  ) = {
+    proto.cometbft.GetNetworkConfigResponse(
+      chainId = chainId,
+      svNodeConfigStates = currentConfigs.map { case (svNodeNr, validatorKey, pendingChanges) =>
+        val currentRevision = (svNodeNr % 2).toLong
+        (
+          mkSvNodeId(svNodeNr),
+          proto.cometbft.SvNodeConfigState(
+            currentConfigRevision = currentRevision,
+            currentConfig =
+              // Even numbered nodes don't yet have a current config
+              Option.unless(currentRevision == 0)(mkSvNodeConfig(svNodeNr, validatorKey)),
+            pendingChanges = pendingChanges.map { case (submitterId, targetState) =>
+              mkSvNodeId(submitterId) -> proto.cometbft.SvNodeConfigPendingChange(change =
+                Some(mkSvNodeConfigChange(svNodeNr, targetState))
+              )
+            }.toMap,
+          ),
+        )
+      }.toMap,
+    )
+  }
+
+  private def mkMemberInfos(
+      members: Seq[(Int, String)]
+  ) =
+    members
+      .map { case (svNodeNr, validatorKey) =>
+        (
+          "svNodeParty" + svNodeNr.toString,
+          new daml.svcrules.MemberInfo(
+            mkSvNodeId(svNodeNr),
+            new Round(0L),
+            new SvNodeConfig(
+              Map(
+                mkCometBftNodeName(svNodeNr) -> new daml.cometbft.CometBftNodeConfig(
+                  validatorKey,
+                  1L,
+                )
+              ).asJava,
+              Seq[daml.cometbft.GovernanceKeyConfig]().asJava,
+              Seq[daml.cometbft.SequencingKeyConfig]().asJava,
+            ),
+          ),
+        )
+      }
+      .toMap
+      .asJava
+
+  private def testCase(
+      targetConfig: Option[String],
+      currentConfig: Option[String],
+      pendingConfig: Option[Option[String]],
+      expectChange: Boolean,
+  ) = {
+    val nodeNr = if (currentConfig.isDefined) 3 else 4
+    val networkConfig = mkNetworkConfig(
+      Seq(
+        (
+          nodeNr,
+          currentConfig.getOrElse("dummy-key"),
+          pendingConfig.map(change => (owningSvNodeNr -> change)).toList,
+        ),
+        (
+          10,
+          "dummy-key-10",
+          Seq(owningSvNodeNr -> Some("key-10"), nodeNr -> None),
+        ), // other not-yet-configured-nodes should not impact the changes for the node of interest
+        (
+          11,
+          "key-11",
+          Seq(owningSvNodeNr -> Some("key-11"), nodeNr -> Some("dummy-key-X")),
+        ), // other configured nodes should not impact the changes for the node of interest
+      )
+    )
+    logger.info(networkConfig.toString)
+    val actualRequests = CometBftNode.diffNetworkConfig(
+      owningSvNodeId,
+      mkMemberInfos(targetConfig.map((nodeNr, _)).toList ++ Seq(10 -> "key-10", 11 -> "key-11")),
+      networkConfig,
+      logger,
+    )
+    val expectedRequests =
+      if (expectChange)
+        Seq(mkChangeRequest(nodeNr, targetConfig))
+      else Seq()
+    actualRequests shouldBe expectedRequests
+  }
+
+  "CometBftNode.diffNetworkConfig" should {
+
+    // The test-cases iterate through all combinations of the Option values and matching or non-matching strings.
+    // They are sorted in lexicographic order for (targetConfig, currentConfig, pendingConfig)
+
+    "be idempotent wrt a not-yet configured node" in testCase(
+      targetConfig = None,
+      currentConfig = None,
+      pendingConfig = None,
+      expectChange = false,
+    )
+
+    "be idempotent wrt a matching pending delete_config request for a not-yet configured node" in testCase(
+      targetConfig = None,
+      currentConfig = None,
+      pendingConfig = Some(None),
+      expectChange = false,
+    )
+
+    "override a pending set_config request for a not-yet configured node" in testCase(
+      targetConfig = None,
+      currentConfig = None,
+      pendingConfig = Some(Some("a")),
+      expectChange = true,
+    )
+
+    "issue a delete_config request for a configured node" in testCase(
+      targetConfig = None,
+      currentConfig = Some("a"),
+      pendingConfig = None,
+      expectChange = true,
+    )
+
+    "be idempotent wrt a matching pending delete_config request for a configured node" in testCase(
+      targetConfig = None,
+      currentConfig = Some("a"),
+      pendingConfig = Some(None),
+      expectChange = false,
+    )
+
+    "override a pending set_config request for a matching configured node that should not exist" in testCase(
+      targetConfig = None,
+      currentConfig = Some("a"),
+      pendingConfig = Some(Some("a")),
+      expectChange = true,
+    )
+
+    "override a pending set_config request for a non-matching configured node that should not exist" in testCase(
+      targetConfig = None,
+      currentConfig = Some("a"),
+      pendingConfig = Some(Some("b")),
+      expectChange = true,
+    )
+
+    "create a missing node" in testCase(
+      targetConfig = Some("a"),
+      currentConfig = None,
+      pendingConfig = None,
+      expectChange = true,
+    )
+
+    "override a delete_config request when creating a missing node" in testCase(
+      targetConfig = Some("a"),
+      currentConfig = None,
+      pendingConfig = Some(None),
+      expectChange = true,
+    )
+
+    "be idempotent wrt a matching write_config request when creating a missing node" in testCase(
+      targetConfig = Some("a"),
+      currentConfig = None,
+      pendingConfig = Some(Some("a")),
+      expectChange = false,
+    )
+
+    "override a mismatching write_config request when creating a missing node" in testCase(
+      targetConfig = Some("a"),
+      currentConfig = None,
+      pendingConfig = Some(Some("b")),
+      expectChange = true,
+    )
+
+    "be idempotent if there already exists a matching node" in testCase(
+      targetConfig = Some("a"),
+      currentConfig = Some("a"),
+      pendingConfig = None,
+      expectChange = false,
+    )
+
+    "override a delete_config request even if the node already exists" in testCase(
+      targetConfig = Some("a"),
+      currentConfig = Some("a"),
+      pendingConfig = Some(None),
+      expectChange = true,
+    )
+
+    "be idempotent wrt a matching write_config request for the same node" in testCase(
+      targetConfig = Some("a"),
+      currentConfig = Some("a"),
+      pendingConfig = Some(Some("a")),
+      expectChange = false,
+    )
+
+    "override a mismatching write_config request even if the node already exists" in testCase(
+      targetConfig = Some("a"),
+      currentConfig = Some("a"),
+      pendingConfig = Some(Some("b")),
+      expectChange = true,
+    )
+
+    "issue a set_config request for an existing node" in testCase(
+      targetConfig = Some("a"),
+      currentConfig = Some("b"),
+      pendingConfig = None,
+      expectChange = true,
+    )
+
+    "override a delete_config request for an existing node that's configured differently" in testCase(
+      targetConfig = Some("a"),
+      currentConfig = Some("b"),
+      pendingConfig = Some(None),
+      expectChange = true,
+    )
+
+    "be idempotent wrt a matching write_config request for the same node that's configured differently" in testCase(
+      targetConfig = Some("a"),
+      currentConfig = Some("b"),
+      pendingConfig = Some(Some("a")),
+      expectChange = false,
+    )
+
+    "override a mismatching write_config request the same node that's configured differently" in testCase(
+      targetConfig = Some("a"),
+      currentConfig = Some("b"),
+      pendingConfig = Some(Some("b")),
+      expectChange = true,
+    )
+
+  }
+
+}
