3c2891cf4dc090e31602919a794b5d2c398224cb
Mike Schaeffer <40773844+mschaef-da@users.noreply.github.com>
2023-05-11 10:34:28 -0400

    Enforce the fact that external-proxy modes on the enable list actually have to exist. (#4713)

--- cluster/images/external-proxy/90-install-mode-conf-files.sh
+++ cluster/images/external-proxy/90-install-mode-conf-files.sh
@@ -5,6 +5,13 @@ echo "Installing ingress modes: ${ENABLE_INGRESS_MODES}"
 for modename in ${ENABLE_INGRESS_MODES}; do
     echo "Installing mode: ${modename}"
 
-    cp -v /conf-modes/"${modename}"/* /etc/nginx/conf.d/
+    mode_directory="/conf-modes/${modename}"
+
+    if [ ! -d "$mode_directory" ]; then
+        echo "Directory for mode ${modename} missing, mode not supported. ($mode_directory)"
+        exit 1
+    fi
+
+    cp -v "$mode_directory"/* /etc/nginx/conf.d/
 done
 echo "Done installing ingres modes."
