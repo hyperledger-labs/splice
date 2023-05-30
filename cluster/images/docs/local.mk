30d5a5f3bc8c8cb2cba1f4203e7618449aa34d73
Robert Autenrieth <robert.autenrieth@digitalasset.com>
2023-05-30 14:37:18 +0200

    Unify the tags used for docker and helm charts

--- cluster/images/docs/local.mk
+++ cluster/images/docs/local.mk
@@ -3,7 +3,7 @@ html := $(dir)/target/html/index.html
 
 
 $(dir)/$(docker-build): $(html)
-$(dir)/$(docker-build): build_arg := --build-arg version=$(shell version-gen)
+$(dir)/$(docker-build): build_arg := --build-arg version=$(shell get-docker-image-tag)
 
 .PHONY: $(dir)/html
 $(dir)/html: $(html)
@@ -11,4 +11,4 @@ $(dir)/html: $(html)
 docs-dir := $(dir)
 $(html): $(shell find $(docs-dir)/src -type f) $(canton-coin-dar) $(wallet-payments-dar) $(directory-service-dar)
 	SKIP_DAML_BUILD=1 $(docs-dir)/gen-daml-docs.sh
-	VERSION=$(shell version-gen) CHART_VERSION=$(shell get-snapshot-version) sphinx-build -M html $(docs-dir)/src $(docs-dir)/target -D version=$(shell version-gen) -W
+	VERSION=$(shell get-docker-image-tag) CHART_VERSION=$(shell get-snapshot-version) sphinx-build -M html $(docs-dir)/src $(docs-dir)/target -D version=$(shell get-docker-image-tag) -W
--- cluster/images/validator-app/local.mk
+++ cluster/images/validator-app/local.mk
@@ -1,6 +1,6 @@
 dir := $(call current_dir)
 
 $(dir)/$(docker-build): $(dir)/app.conf $(dir)/bootstrap.sc $(dir)/pre-bootstrap.sh
-$(dir)/$(docker-build): build_arg := --build-arg base_version=$(shell version-gen)
+$(dir)/$(docker-build): build_arg := --build-arg base_version=$(shell get-docker-image-tag)
 
 include cluster/images/cn-base-image-dep.mk
