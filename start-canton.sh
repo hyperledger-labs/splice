6134905a68fee9b13af82e08a8eb62116b6a2682
Simon Meier <meiersi-da@users.noreply.github.com>
2023-06-01 08:06:51 +0200

    Wait at most 5 minutes for Canton instances to start (#5218)
    Fixes #5203

--- start-canton.sh
+++ start-canton.sh
@@ -212,12 +212,26 @@ if [ $simtime -eq 1 ]; then
   fi
 fi
 
-# Wait for both Cantons to start
-while { [ $wallclocktime -eq 1 ] && [ ! -f canton.tokens ]; } || { [ $simtime -eq 1 ] && [ ! -f canton-simtime.tokens ]; } ; do
-    echo "Waiting for Canton instance(s) to start"
-    sleep 1;
+
+# Wait for canton instance(s) to start within 5 minutes
+timeout=300
+start_time=$(date +%s)
+while (( $(date +%s) - start_time < timeout )); do
+    if { [ $wallclocktime -eq 1 ] && [ ! -f canton.tokens ]; } || { [ $simtime -eq 1 ] && [ ! -f canton-simtime.tokens ]; }; then
+        remaining_time=$((timeout - ($(date +%s) - start_time)))
+        echo "Waiting for Canton instance(s) to start (${remaining_time} seconds left)"
+        sleep 1
+    else
+        echo "Canton instance(s) started"
+        break
+    fi
 done
-echo "Canton instance(s) started"
+
+if (( $(date +%s) - start_time >= timeout )); then
+    echo "Timeout: Canton instance(s) failed to start within $timeout seconds"
+    exit 1
+fi
+
 
 tmux_cmd toxiproxy toxiproxy-server > log/toxi.log 2>&1
 
