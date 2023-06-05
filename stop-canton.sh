fb3cef712738a1e252851943b6417c7dc17e3680
leventeborbely-da <129053164+leventeborbely-da@users.noreply.github.com>
2023-06-05 16:35:40 +0200

    Levente/stop frontends does not kill node processes/4598 (#5394)
    * Created util.sh
    
    * created function to kill a process tree

--- stop-canton.sh
+++ stop-canton.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 set -eou pipefail
+source util.sh
 
 POSTGRES_MODE=${1:-docker}
 
 tmux_session="canton"
-if tmux has-session -t $tmux_session 2>/dev/null; then
-    # kill-session seems to send a SIGHUP which does not seem to be quite enough
-    # to tear down the processes promptly so we manually kill them.
-    # parents will usually be some shell process.
-    readarray -t TMUX_PARENT_PIDS < <(tmux list-panes -s -F "#{pane_pid}" -t $tmux_session)
-    # the child is the actual process we want to kill
-    readarray -t TMUX_CHILD_PIDS < <(echo -n "${TMUX_PARENT_PIDS[@]}" | xargs -d ' ' -I '{}' pgrep -P '{}' || true)
-    kill -9 "${TMUX_CHILD_PIDS[@]}" "${TMUX_PARENT_PIDS[@]}"
-    # Session might be dead at this point because we killed all processes
-    # but to be on the safe side we still kill it.
-    tmux kill-session -t $tmux_session 2>/dev/null || true
-fi
 
+if tmux has-session -t $tmux_session 2>/dev/null; then
+  # kill-session seems to send a SIGHUP which does not seem to be quite enough
+  # to tear down the processes promptly so we manually kill them.
+  # parents will usually be some shell process.
+  readarray -t TMUX_PARENT_PIDS < <(tmux list-panes -s -F "#{pane_pid}" -t $tmux_session)
+  kill_process_tree "${TMUX_PARENT_PIDS[@]}"
+
+  # Session might be dead at this point because we killed all processes
+  # but to be on the safe side we still kill it.
+  tmux kill-session -t $tmux_session 2>/dev/null || true
+fi
 
 rm -f canton.tokens canton-simtime.tokens
 
--- stop-frontends.sh
+++ stop-frontends.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
 set -eou pipefail
+source util.sh
 
 tmux_session="cn-frontends"
+
 if tmux has-session -t $tmux_session 2>/dev/null; then
-    # kill-session seems to send a SIGHUP which does not seem to be quite enough
-    # to tear down the processes promptly so we manually kill them.
-    # parents will usually be some shell process.
-    readarray -t TMUX_PARENT_PIDS < <(tmux list-panes -s -F "#{pane_pid}" -t $tmux_session)
-    # the child is sometimes the actual process we want to kill
-    readarray -t TMUX_CHILD_PIDS < <(echo -n "${TMUX_PARENT_PIDS[@]}" | xargs -d ' ' -I '{}' pgrep -P '{}' || true)
-    # but sometimes (e.g., for envoy) the child is only another shell process and the *grandchild* is the process we want to kill
-    readarray -t TMUX_GRAND_CHILD_PIDS < <(echo -n "${TMUX_CHILD_PIDS[@]}" | xargs -d ' ' -I '{}' pgrep -P '{}' || true)
-    kill -9 "${TMUX_GRAND_CHILD_PIDS[@]}" "${TMUX_CHILD_PIDS[@]}" "${TMUX_PARENT_PIDS[@]}"
-    # Session might be dead at this point because we killed all processes
-    # but to be on the safe side we still kill it.
-    tmux kill-session -t $tmux_session 2>/dev/null || true
+  # kill-session seems to send a SIGHUP which does not seem to be quite enough
+  # to tear down the processes promptly so we manually kill them.
+  # parents will usually be some shell process.
+  readarray -t TMUX_PARENT_PIDS < <(tmux list-panes -s -F "#{pane_pid}" -t $tmux_session)
+  kill_process_tree "${TMUX_PARENT_PIDS[@]}"
+
+  # Session might be dead at this point because we killed all processes
+  # but to be on the safe side we still kill it.
+  tmux kill-session -t $tmux_session 2>/dev/null || true
 fi
 
 if [ -f start-frontends-network-address ]; then
-    rm start-frontends-network-address
+  rm start-frontends-network-address
 fi
