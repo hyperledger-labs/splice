a0545df5d5adfb71dc50d8777db88f41002ea358
Martin Florian <martin.florian@digitalasset.com>
2023-06-22 17:03:03 +0200

    Fix concurrency bug in global lock (#6063)
    Was "only" causing false-positive error messages.

--- apps/sv/src/main/scala/com/daml/network/sv/util/ExpiringLock.scala
+++ apps/sv/src/main/scala/com/daml/network/sv/util/ExpiringLock.scala
@@ -6,6 +6,7 @@ import com.digitalasset.canton.tracing.TraceContext
 
 import java.time.Instant
 import java.util.concurrent.atomic.AtomicReference
+import scala.concurrent.blocking
 
 // TODO(#5855) remove this class and file
 private[sv] class ExpiringLock(
@@ -15,29 +16,39 @@ private[sv] class ExpiringLock(
   private val lockRef: AtomicReference[LockState] = new AtomicReference[LockState](Unlocked)
 
   def tryAcquire()(implicit traceContext: TraceContext): Boolean = {
-    val newLockState = LockState(locked = true, Instant.now().plus(expirationDuration.asJava))
-    lockRef.compareAndSet(Unlocked, newLockState) || {
-      val oldLockState = lockRef.get
-      if (
-        oldLockState.expiresAt
-          .isBefore(Instant.now()) && lockRef.compareAndSet(oldLockState, newLockState)
-      ) {
-        logger.warn(
-          s"Acquired expired lock held since ${oldLockState.expiresAt.minus(expirationDuration.asJava)}, assuming that lock holder has died."
-        )
-        true
-      } else false
+    blocking {
+      // we probably don't need this but we also never want to have to debug this code again
+      synchronized {
+        val oldLockState = lockRef.get
+        val newLockState = LockState(locked = true, Instant.now().plus(expirationDuration.asJava))
+        if (oldLockState == Unlocked) {
+          lockRef.compareAndSet(oldLockState, newLockState)
+        } else if (
+          oldLockState.expiresAt
+            .isBefore(Instant.now()) && lockRef.compareAndSet(oldLockState, newLockState)
+        ) {
+          logger.warn(
+            s"Acquired expired lock held since ${oldLockState.expiresAt.minus(expirationDuration.asJava)}, assuming that lock holder has died."
+          )
+          true
+        } else false
+      }
     }
   }
 
   def release()(implicit traceContext: TraceContext): Unit = {
-    val oldLockState = lockRef.getAndSet(Unlocked)
-    if (oldLockState == Unlocked) {
-      logger.error("Released an already unlocked lock")
-    } else if (oldLockState.expiresAt.isBefore(Instant.now())) {
-      logger.warn(
-        s"Released an expired lock held since ${oldLockState.expiresAt.minus(expirationDuration.asJava)}."
-      )
+    blocking {
+      // we probably don't need this we but also never want to have to debug this code
+      synchronized {
+        val oldLockState = lockRef.getAndSet(Unlocked)
+        if (oldLockState == Unlocked) {
+          logger.error("Released an already unlocked lock")
+        } else if (oldLockState.expiresAt.isBefore(Instant.now())) {
+          logger.warn(
+            s"Released an expired lock held since ${oldLockState.expiresAt.minus(expirationDuration.asJava)}."
+          )
+        }
+      }
     }
   }
 }
