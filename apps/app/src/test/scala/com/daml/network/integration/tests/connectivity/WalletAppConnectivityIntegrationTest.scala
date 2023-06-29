6967240660835b1304eae20a23884cab9f7c0d16
Julien <128390688+julientinguely-da@users.noreply.github.com>
2023-06-29 10:07:37 +0200

    Flake-fix: use ActAndCheck in WalletAppConnectivityIntegrationTest  (#6254)
    Fixes #6154: decreased tick duration

--- apps/app/src/test/scala/com/daml/network/integration/tests/connectivity/WalletAppConnectivityIntegrationTest.scala
+++ apps/app/src/test/scala/com/daml/network/integration/tests/connectivity/WalletAppConnectivityIntegrationTest.scala
@@ -9,6 +9,7 @@ import com.daml.network.integration.tests.CNNodeTests.{
   CNNodeTestConsoleEnvironment,
 }
 import com.daml.network.util.WalletTestUtil
+import com.digitalasset.canton.config.NonNegativeFiniteDuration
 import com.digitalasset.canton.console.CommandFailure
 import com.digitalasset.canton.integration.BaseEnvironmentDefinition
 import com.digitalasset.canton.logging.SuppressionRule
@@ -23,11 +24,16 @@ class WalletAppConnectivityIntegrationTest extends CNNodeIntegrationTest with Wa
       : BaseEnvironmentDefinition[CNNodeEnvironmentImpl, CNNodeTestConsoleEnvironment] =
     CNNodeEnvironmentDefinition
       .simpleTopology(this.getClass.getSimpleName)
-      .addConfigTransform((_, config) =>
-        // we want fine-grained control when we send a CoinOperation from the wallet & query the scan app
-        CNNodeConfigTransforms.updateAllAutomationConfigs(
-          _.focus(_.enableAutomaticRewardsCollectionAndCoinMerging).replace(false)
-        )(config)
+      .addConfigTransforms(
+        (_, config) =>
+          // we want fine-grained control when we send a CoinOperation from the wallet & query the scan app
+          CNNodeConfigTransforms.updateAllAutomationConfigs(
+            _.focus(_.enableAutomaticRewardsCollectionAndCoinMerging).replace(false)
+          )(config),
+        (_, config) =>
+          CNNodeConfigTransforms.updateAllSvAppFoundCollectiveConfigs_(
+            _.copy(initialTickDuration = NonNegativeFiniteDuration.ofSeconds(5))
+          )(config),
       )
 
   private val toxiproxy = UseToxiproxy(createScanAppProxies = true)
@@ -64,26 +70,37 @@ class WalletAppConnectivityIntegrationTest extends CNNodeIntegrationTest with Wa
   "wallet should recover after a longer disconnect from the Scan HTTP API" in { implicit env =>
     val (_, _) = onboardAliceAndBob()
 
-    toxiproxy.disableConnectionViaProxy(UseToxiproxy.scanHttpApiProxyName(aliceValidator.name))
-
-    eventually() {
-      loggerFactory.assertThrowsAndLogsSeq[CommandFailure](
-        aliceWallet.tap(2),
-        entries => {
-          forAtLeast(1, entries)(
-            _.message should include(
-              "failed because of java.net.ConnectException: Connection refused"
+    actAndCheck(
+      "disable proxy connection",
+      toxiproxy.disableConnectionViaProxy(UseToxiproxy.scanHttpApiProxyName(aliceValidator.name)),
+    )(
+      "tapping on alice wallet should fail",
+      _ => {
+        loggerFactory.assertThrowsAndLogsSeq[CommandFailure](
+          aliceWallet.tap(2),
+          entries => {
+            forAtLeast(1, entries)(
+              _.message should include(
+                "failed because of java.net.ConnectException: Connection refused"
+              )
             )
-          )
-        },
-      )
-    }
+          },
+        )
+      },
+    )
 
-    toxiproxy.enableConnectionViaProxy(UseToxiproxy.scanHttpApiProxyName(aliceValidator.name))
+    actAndCheck(
+      "enable back proxy connection",
+      toxiproxy.enableConnectionViaProxy(UseToxiproxy.scanHttpApiProxyName(aliceValidator.name)),
+    )(
+      "tapping on alice wallet should work",
+      _ => {
+        aliceWallet.tap(3)
+        eventually() {
+          aliceWallet.list().coins should have length 1
+        }
+      },
+    )
 
-    aliceWallet.tap(3)
-    eventually() {
-      aliceWallet.list().coins should have length 1
-    }
   }
 }
