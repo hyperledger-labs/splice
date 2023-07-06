89e6572c2c707c28ccc28ba0b32c183ebd9e4260
Raymond Roestenburg (Actually RayRoestenburg) <98821776+ray-roestenburg-da@users.noreply.github.com>
2023-07-06 14:28:58 +0200

    Fix paging up in wait_for_pipelines (#6506)
    - Fixed adding page-token as query param with ? / &
    - Getting all pages takes far too long, the wait_for_pipelines now only fetches pipelines that have been created x seconds ago set by MAX_AGE_SECONDS.
    - if the data has no created_at property all pages are requested.

--- .circleci/wait_for_pipelines.sh
+++ .circleci/wait_for_pipelines.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 set -eou pipefail
-PIPELINE_NUMBER="$1"
+CURRENT_PIPELINE_NUMBER="$1"
 WORKFLOW_NAMES="$2"
+# only fetching pipeline data up to seconds old defined here
+MAX_AGE_SECONDS="$((8*3600))"
 
 pipeline_workflows_complete() {
   pipeline_id=$1
-  if fetch_pages "https://circleci.com/api/v2/pipeline/$pipeline_id/workflow" "/tmp/workflows.json"
-  then
+  if fetch_pages "https://circleci.com/api/v2/pipeline/$pipeline_id/workflow" "/tmp/workflows.json" "$MAX_AGE_SECONDS"; then
     echo "workflows for pipeline $pipeline_id:"
     cat /tmp/workflows.json
     echo ""
@@ -61,38 +62,60 @@ get_url() {
 write_pages() {
     local url=$1
     local output_file=$2
+    local max_age_seconds=$3
     local response
     local next_page_token
+    local most_recent_date
+
     response=$(get_url "$url")
     next_page_token=$(jq -r '.next_page_token' <<< "$response")
     while IFS= read -r page; do
         echo "$page" >> "$output_file"
     done <<< "$response"
-
     if [ "$next_page_token" != "null" ]; then
-        next_page_url="${url}?page-token=${next_page_token}"
-        write_pages "$next_page_url" "$output_file"
+        # shellcheck disable=SC2001
+        new_url=$(sed 's/\(&\|?\)page-token=.*$//' <<< "$url")
+        if [[ $new_url == *'?'* ]]; then
+            next_page_url="${new_url}&page-token=${next_page_token}"
+        else 
+            next_page_url="${new_url}?page-token=${next_page_token}"
+        fi
+
+        most_recent_date=$(jq -r '.items | sort_by(.created_at)[-1].created_at' <<< "$response")
+        if [ "$most_recent_date" != "null" ]; then
+          current_timestamp=$(date +%s)  
+          date_timestamp=$(date -d "${most_recent_date%.*}" +%s)
+          day_ago=$((current_timestamp - max_age_seconds))
+          if [ "$date_timestamp" -gt "$day_ago" ]; then
+            write_pages "$next_page_url" "$output_file" "$max_age_seconds"
+          else
+            return 0
+          fi 
+        else 
+          write_pages "$next_page_url" "$output_file" "$max_age_seconds"  
+        fi         
     fi
 }
 
 fetch_pages() {
     local url=$1
     local output_file=$2
+    local max_age_seconds=$3
     rm -f "$output_file" >/dev/null 2>&1
-    write_pages "$url" "$output_file"
+    write_pages "$url" "$output_file" "$max_age_seconds"
 }
 
 tmp_pipelines_file="/tmp/pipelines.json"
 tmp_workflows_file="/tmp/workflows.json"
-fetch_pages "https://circleci.com/api/v2/project/gh/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pipeline?branch=main" "$tmp_pipelines_file"
+fetch_pages "https://circleci.com/api/v2/project/gh/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pipeline?branch=main" "$tmp_pipelines_file" "$MAX_AGE_SECONDS"
 
-PREVIOUS_JOBS=$(jq -c < $tmp_pipelines_file ".items | map(select(.number < $PIPELINE_NUMBER)) | .[] | { number: .number, id: .id}")
+PREVIOUS_JOBS=$(jq -c < $tmp_pipelines_file ".items | map(select(.number < $CURRENT_PIPELINE_NUMBER)) | .[] | { number: .number, id: .id}")
 if [ "${#PREVIOUS_JOBS}" -gt 0 ]; then
     while IFS= read -r JOB; do
         PIPELINE_NUMBER=$(jq <<< "$JOB" -r '.number')
         PIPELINE_ID=$(jq <<< "$JOB" -r '.id')
         echo "Checking pipeline $PIPELINE_NUMBER ($PIPELINE_ID) ..."
-        fetch_pages "https://circleci.com/api/v2/pipeline/$PIPELINE_ID/workflow" "$tmp_workflows_file"
+        fetch_pages "https://circleci.com/api/v2/pipeline/$PIPELINE_ID/workflow" "$tmp_workflows_file" "$MAX_AGE_SECONDS"
         while IFS= read -r WORKFLOW; do
             WORKFLOW_NAME=$(jq -r <<< "$WORKFLOW" '.name')
             if [[ " ${WORKFLOW_NAMES} " == *" ${WORKFLOW_NAME} "* ]]; then
@@ -103,7 +126,8 @@ if [ "${#PREVIOUS_JOBS}" -gt 0 ]; then
             fi
         done <<< "$(jq -c < "$tmp_workflows_file" ".items | .[]")"
     done <<< "$PREVIOUS_JOBS"
+    echo "Checked all pipelines created up to $((MAX_AGE_SECONDS / 3600)) hours ago before pipeline $CURRENT_PIPELINE_NUMBER"
 else
-    echo "No previous jobs found for pipeline $PIPELINE_NUMBER, failing!"
+    echo "No pipelines found before pipeline $CURRENT_PIPELINE_NUMBER, failing!"
     exit 1
 fi
