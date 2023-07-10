bf26f1ed235c289f87d9d5fd368599d10b535bd7
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2023-07-10 10:50:20 +0200

    Upgrade lnav to version 0.11.2 (#6566)

--- nix/lnav.nix
+++ nix/lnav.nix
@@ -3,16 +3,16 @@
 # so we use the binary distribution that ships with a compatible pcre version.
 stdenv.mkDerivation rec {
   name = "lnav";
-  version = "0.11.1";
+  version = "0.11.2";
 
   src =
     if stdenv.isDarwin then
       fetchzip {
         url = "https://github.com/tstack/lnav/releases/download/v${version}/lnav-${version}-x86_64-macos.zip";
-        sha256 = "sha256-pIIUGznYtEAPPPP1erX/LHB1IF5nlB311DPrSP4Nwrc=";
+        sha256 = "sha256-8TY/N6aYyH+FGvwtLAOcEj6SbVQQty2s4X1GVDNE2vQ=";
       } else fetchzip {
         url = "https://github.com/tstack/lnav/releases/download/v${version}/lnav-${version}-x86_64-linux-musl.zip";
-        sha256 = "sha256-S8OzVOCsDLa2XJH6NskCGKVHe7QslZ6oainKIPHB5/c=";
+        sha256 = "sha256-QJbHYXkd8m7cJYW7fnNeGA4C9s7L3pCyJ1xZ1PjQWuM=";
       };
 
   installPhase = ''
