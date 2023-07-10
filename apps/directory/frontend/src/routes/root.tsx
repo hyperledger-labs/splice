hyperledger-labs/splice

commit 5c74d97a14cb6e893cf381f4beeb1ba72f6ccd0d

Produce decent error in frontends if DAR has not been uploaded (#6455)

apps/directory/frontend/src/routes/root.tsx
@@ -1,4 +1,4 @@
-import { ErrorBoundary, Loading, PartyId, useUserState } from 'common-frontend';
+import { ErrorBoundary, ErrorDisplay, Loading, PartyId, useUserState } from 'common-frontend';
 import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 
@@ -11,13 +11,13 @@ import {
   useProviderParty,
   useRequestDirectoryInstall,
 } from '../hooks';
+import { isDarFileMissingError } from '../utils/errors';
 
 const Root: React.FC = () => {
   const { logout } = useUserState();
   const primaryPartyQuery = usePrimaryParty();
   const primaryPartyId = primaryPartyQuery.data;
-  const { data: directoryInstallContract, isLoading: isDirectoryInstallContractLoading } =
-    useDirectoryInstall();
+  const directoryInstall = useDirectoryInstall();
   const { data: providerPartyId } = useProviderParty();
 
   const ledgerApiClient = useLedgerApiClient();
@@ -26,12 +26,12 @@ const Root: React.FC = () => {
   useEffect(() => {
     if (
       requestDirectoryInstall.isIdle &&
-      !isDirectoryInstallContractLoading &&
+      directoryInstall.isSuccess &&
       primaryPartyId &&
       providerPartyId &&
       ledgerApiClient
     ) {
-      if (directoryInstallContract) {
+      if (directoryInstall.data) {
         console.debug('DirectoryInstall found');
       } else {
         console.debug('DirectoryInstall not found, creating DirectoryInstallRequest');
@@ -39,16 +39,43 @@ const Root: React.FC = () => {
       }
     }
   }, [
-    directoryInstallContract,
-    isDirectoryInstallContractLoading,
+    directoryInstall.isSuccess,
+    directoryInstall.data,
     ledgerApiClient,
     primaryPartyId,
     providerPartyId,
     requestDirectoryInstall,
   ]);
 
-  if (primaryPartyQuery.isLoading) {
-    return <Loading />;
+  let content;
+  if (isDarFileMissingError(directoryInstall.error)) {
+    const missingTemplates = directoryInstall.error.warnings.unknownTemplateIds.join(', ');
+    content = (
+      <ErrorDisplay
+        message="A required template was not found on the participant"
+        userAction="Make sure the application operator has uploaded all relevant DAR files."
+        details={`Missing templates: ${missingTemplates}`}
+        retryFn={() => directoryInstall.refetch()}
+      />
+    );
+  } else if (directoryInstall.error !== null) {
+    content = (
+      <ErrorDisplay
+        message="Problem loading directory install contract"
+        userAction="Please try again later. If the problem persists, contact the application operator."
+      />
+    );
+  } else if (primaryPartyQuery.error !== null) {
+    content = (
+      <ErrorDisplay
+        message="Problem loading primary party"
+        userAction="Please try again later. If the problem persists, contact the application operator."
+      />
+    );
+  } else if (directoryInstall.isLoading || primaryPartyQuery.isLoading) {
+    content = <Loading />;
+  } else {
+    content = <Outlet />;
   }
 
   return (
@@ -78,7 +105,7 @@ const Root: React.FC = () => {
           </Toolbar>
         </Box>
         {/* no need to show the app if it won't be usable. Creating DirectoryInstall should be fast. */}
-        {directoryInstallContract ? <Outlet /> : <Loading />}
+        {content}
       </Box>
     </ErrorBoundary>
   );

apps/app/src/test/scala/com/daml/network/integration/tests/WalletFrontendIntegrationTest.scala
@@ -124,7 +124,7 @@ class WalletFrontendIntegrationTest
             _ => {
               val ccText = find(id("wallet-balance-cc")).value.text.trim
               val usdText = find(id("wallet-balance-usd")).value.text.trim
-              val errorMessage = find(className("error-display-text")).value.text.trim
+              val errorMessage = find(className("error-display-message")).value.text.trim
 
               ccText should not be "..."
               usdText should not be "..."

apps/common/frontend/src/components/ErrorDisplay.tsx
@@ -1,6 +1,7 @@
-import * as React from 'react';
+import React, { useEffect } from 'react';
 
-import { Box, Typography } from '@mui/material';
+import { ErrorOutline } from '@mui/icons-material';
+import { Box, Button, Typography } from '@mui/material';
 
 import { theme } from '../theme';
 
@@ -9,9 +10,16 @@ type ErrorDisplayComponent = 'div' | 'tr';
 interface ErrorProps {
   renderAs?: ErrorDisplayComponent;
   message: string;
+  userAction?: string;
+  details?: string;
+  retryFn?: () => void;
 }
 
 const ErrorDisplay: React.FC<ErrorProps> = props => {
+  useEffect(
+    () => console.debug('Displaying error ', props.message, props.userAction, props.details),
+    [props.message, props.userAction, props.details]
+  );
   return (
     <Box
       component={props.renderAs ?? 'div'}
@@ -19,13 +27,45 @@ const ErrorDisplay: React.FC<ErrorProps> = props => {
         width: '100%',
         height: '100%',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
       }}
     >
-      <Typography className="error-display-text" variant="caption" color={theme.palette.error.main}>
+      <ErrorOutline id="error-icon" color="error" sx={{ width: '40px', height: '40px' }} />
+      <Typography
+        className="error-display-message"
+        variant="subtitle1"
+        color={theme.palette.error.main}
+        gutterBottom
+      >
         {props.message}
       </Typography>
+      {props.userAction && (
+        <Typography
+          className="error-display-useraction"
+          variant="body1"
+          color={theme.palette.error.main}
+          gutterBottom
+        >
+          {props.userAction}
+        </Typography>
+      )}
+      {props.details && (
+        <Typography
+          className="error-display-details"
+          variant="body2"
+          color={theme.palette.error.main}
+          gutterBottom
+        >
+          {props.details}
+        </Typography>
+      )}
+      {props.retryFn && (
+        <Button className="error-display-button" variant="outlined" onClick={props.retryFn}>
+          Try Again
+        </Button>
+      )}
     </Box>
   );
 };

apps/directory/frontend/src/hooks/queries/useDirectoryInstall.ts
@@ -28,6 +28,20 @@ const useDirectoryInstall = (): UseQueryResult<Contract<DirectoryInstall> | null
         return null;
       }
     },
+    refetchInterval: (data, query) => {
+      if (data !== null && data !== undefined) {
+        // Install contracts are not not expected to change. Once a contract is found, stop refetching.
+        console.debug('useDirectoryInstall: contract found, stopping');
+        return false;
+      } else if (query.state.status === 'error') {
+        //
+        console.debug('useDirectoryInstall: query error, stopping');
+        return false;
+      } else {
+        console.debug('useDirectoryInstall: no data, refetching');
+        return 500;
+      }
+    },
     enabled: !!ledgerApi && !!primaryPartyId && !!providerPartyId, // wait for dependencies to be defined
   });
 };

apps/directory/frontend/src/utils/errors.ts
@@ -1,0 +1,21 @@
+export interface DarFileMissingError {
+  errors: string[];
+  warnings: {
+    unknownTemplateIds: string[];
+  };
+}
+
+function hasProp<T extends string>(x: unknown, key: T): x is { [key in T]: unknown } {
+  return typeof x === 'object' && x !== null && key in x;
+}
+
+export function isDarFileMissingError(error: unknown): error is DarFileMissingError {
+  return (
+    hasProp(error, 'errors') &&
+    Array.isArray(error.errors) &&
+    error.errors.includes('Cannot resolve any template ID from request') &&
+    hasProp(error, 'warnings') &&
+    hasProp(error.warnings, 'unknownTemplateIds') &&
+    Array.isArray(error.warnings.unknownTemplateIds)
+  );
+}

commit 932d51b6a882093f5fbd26c27c2808f728e0674f

Fix regression for loading state in directory UI (#6567)
* Fix regression for loading state in directory UI

We introduced a regression in #6455 where we now only check for the
loading state but not if the query actually returned a contract.

That reintroduced #6311

fixes #6311

apps/directory/frontend/src/routes/root.tsx
@@ -103,7 +103,7 @@ const Root: React.FC = () => {
           </Toolbar>
         </Box>
         {/* no need to show the app if it won't be usable. Creating DirectoryInstall should be fast. */}
-        {content}
+        {directoryInstall.data ? content : <Loading />}
       </Box>
     </ErrorBoundary>
   );