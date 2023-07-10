hyperledger-labs/splice

commit 7806919c614d0d1ae1c9e6e533b78a90a8b28953

Align management of gcp bucket service account with dns secrets (#6565)
* Align management of gcp bucket service account with dns secrets

fixes #6251

cluster/pulumi/canton-network/src/backup.ts
@@ -1,11 +1,9 @@
-import * as gcp from '@pulumi/gcp';
 import * as k8s from '@pulumi/kubernetes';
-import * as pulumi from '@pulumi/pulumi';
 import { Bucket, File, Storage } from '@google-cloud/storage';
-import { Key } from '@pulumi/gcp/serviceaccount';
-import { Output } from '@pulumi/pulumi';
-import { ExactNamespace } from 'cn-pulumi-common';
+import { ExactNamespace, config } from 'cn-pulumi-common';
 import { exit } from 'process';
+
+const DATA_EXPORT_BUCKET_SA_KEY_JSON = config.require('DATA_EXPORT_BUCKET_SA_KEY_JSON');
 
 export type GcpBucketConfig = {
   projectId: string;
@@ -15,24 +13,14 @@ export type GcpBucketConfig = {
 export type GcpBucket = {
   config: GcpBucketConfig;
   secretName: string;
-  jsonCredentials: Output<string>;
+  jsonCredentials: string;
 };
 
 export function installGcpBucket(config: GcpBucketConfig): GcpBucket {
-  const projectId = config.projectId;
-  const serviceAccountName = `projects/${projectId}/serviceAccounts/da-cn-data-exports@${projectId}.iam.gserviceaccount.com`;
-
-  // Note, creating a new key can fail with a precondition error on an attempt
-  // to create keys beyond the tenth.
-  const key: Key = new gcp.serviceaccount.Key(`gcp-bucket-${process.env.GCP_CLUSTER_BASENAME}`, {
-    serviceAccountId: serviceAccountName,
-    publicKeyType: 'TYPE_X509_PEM_FILE',
-  });
-
   return {
     config: config,
     secretName: `cn-gcp-bucket-${config.projectId}-${config.bucketName}`,
-    jsonCredentials: key.privateKey,
+    jsonCredentials: DATA_EXPORT_BUCKET_SA_KEY_JSON,
   };
 }
 
@@ -52,7 +40,7 @@ export function installGcpBucketSecret(xns: ExactNamespace, bucket: GcpBucket): 
       },
       type: 'Opaque',
       data: {
-        'json-credentials': bucket.jsonCredentials,
+        'json-credentials': Buffer.from(bucket.jsonCredentials, 'utf-8').toString('base64'),
       },
     },
     {
@@ -124,24 +112,21 @@ async function getLatestParticipantIdentityDump(
 export function getLatestSvcAcsDumpFile(
   xns: ExactNamespace,
   config: BootstrappingDumpConfig
-): pulumi.Output<File> {
-  return config.bucket.jsonCredentials.apply(async credentials => {
-    const bucket = getGcpBucket(config.bucket.config, credentials);
-    const file = await getLatestObjectInDateRange(
-      bucket,
-      `${bucketPath(config.cluster, xns)}/svc_acs_dump_`,
-      '.json',
-      config.start,
-      config.end
-    );
-    return file;
-  });
+): Promise<File> {
+  const bucket = getGcpBucket(config.bucket.config, config.bucket.jsonCredentials);
+  return getLatestObjectInDateRange(
+    bucket,
+    `${bucketPath(config.cluster, xns)}/svc_acs_dump_`,
+    '.json',
+    config.start,
+    config.end
+  );
 }
 
 export function installParticipantIdentitySecret(
   xns: ExactNamespace,
   secretName: string,
-  content: Output<string>
+  content: string
 ): k8s.core.v1.Secret {
   return new k8s.core.v1.Secret(`cn-app-${xns.logicalName}-${secretName}`, {
     metadata: {
@@ -150,7 +135,7 @@ export function installParticipantIdentitySecret(
     },
     type: 'Opaque',
     data: {
-      content: content.apply(content => Buffer.from(content, 'ascii').toString('base64')),
+      content: Buffer.from(content, 'ascii').toString('base64'),
     },
   });
 }
@@ -164,20 +149,17 @@ export type BootstrappingDumpConfig = {
   end: Date;
 };
 
-export function fetchAndInstallParticipantBootstrapDump(
+export async function fetchAndInstallParticipantBootstrapDump(
   xns: ExactNamespace,
   config: BootstrappingDumpConfig
-): k8s.core.v1.Secret {
-  const dumpContent = config.bucket.jsonCredentials.apply(async credentials => {
-    const bucket = getGcpBucket(config.bucket.config, credentials);
-    const content = await getLatestParticipantIdentityDump(
-      bucket,
-      xns,
-      config.cluster,
-      config.start,
-      config.end
-    );
-    return content;
-  });
-  return installParticipantIdentitySecret(xns, participantBootstrapDumpSecretName, dumpContent);
+): Promise<k8s.core.v1.Secret> {
+  const bucket = getGcpBucket(config.bucket.config, config.bucket.jsonCredentials);
+  const content = await getLatestParticipantIdentityDump(
+    bucket,
+    xns,
+    config.cluster,
+    config.start,
+    config.end
+  );
+  return installParticipantIdentitySecret(xns, participantBootstrapDumpSecretName, content);
 }

.envrc.vars
@@ -1,6 +1,6 @@
 # -*- shell-script -*-
 
-### Set UTC timezone, as CometBft always logs in UTC while java tools log using the local timezone 
+### Set UTC timezone, as CometBft always logs in UTC while java tools log using the local timezone
 export TZ=UTC
 
 ### Separate (out of direnv) the env vars that are sourced both locally *and* in CircleCI
@@ -44,6 +44,11 @@ export GCP_DNS_SA_SECRET="clouddns-dns01-solver-svc-acct"
 # DNS Service Account information
 export DNS01_SA_KEY_SECRET=dns01-sa-key-secret
 export DNS01_SA_IAM_ACCOUNT="dns01-solver@${GCP_DNS_PROJECT}.iam.gserviceaccount.com"
+
+# GCP bucket service account information for the
+# bucket that we export ACS and participant identity dumps to.
+export DATA_EXPORT_BUCKET_SA_KEY_SECRET=gcp-bucket-sa-key-secret
+export DATA_EXPORT_BUCKET_SA_IAM_ACCOUNT="da-cn-data-exports@da-cn-devnet.iam.gserviceaccount.com"
 
 # Force auth through gke-gcloud-auth-plugin
 # See https://cloud.google.com/blog/products/containers-kubernetes/kubectl-auth-changes-in-gke

build-tools/cncluster
@@ -115,6 +115,8 @@ function _cluster_apply() {
 
     _info "Applying Pulumi Stack"
 
+    subcmd_install_data_export_bucket_key
+    _get_data_export_bucket_secret | subcmd_pulumi canton-network config set DATA_EXPORT_BUCKET_SA_KEY_JSON
     subcmd_pulumi canton-network up --yes --skip-preview --show-sames "${args[@]}"
 }
 
@@ -447,64 +449,114 @@ function subcmd_set_machine_type() {
 
 ###
 
-subcommand_whitelist[get_dns01_key_id]='Get the gcloud service account key id used for dns challenges.'
-
-function _get_dns01_secret() {
-    gcloud secrets versions access 1 --secret="${DNS01_SA_KEY_SECRET}"
+function _get_gcloud_secret() {
+  local -r secret_name="$1"
+  gcloud secrets versions access 1 --secret="$secret_name"
 }
 
+function _get_dns01_secret() {
+    _get_gcloud_secret "${DNS01_SA_KEY_SECRET}"
+}
+
+function _get_data_export_bucket_secret() {
+    _get_gcloud_secret "${DATA_EXPORT_BUCKET_SA_KEY_SECRET}"
+}
+
+function _get_service_account_key_id() {
+    local -r secret_name="$1"
+    _get_gcloud_secret "$secret_name" | jq -r '.private_key_id'
+}
+
+subcommand_whitelist[get_dns01_key_id]='Get the gcloud service account key id used for dns challenges.'
+
 function subcmd_get_dns01_key_id() {
-    _get_dns01_secret | jq -r '.private_key_id'
+    _get_service_account_key_id "${DNS01_SA_KEY_SECRET}"
+}
+
+subcommand_whitelist[get_data_export_bucket_key_id]='Get the gcloud service account key id used for dns challenges.'
+
+function subcmd_get_data_export_bucket_key_id() {
+    _get_service_account_key_id "${DATA_EXPORT_BUCKET_SA_KEY_SECRET}"
 }
 
 ###
 
-subcommand_whitelist[install_dns01_key]='Allocate an DNS SA Key for the project and store it in a secret.'
+function _install_service_account_key() {
+    local -r secret_name="$1"
+    local -r iam_account="$2"
 
-function subcmd_install_dns01_key() {
-    _prompt_to_confirm --no-lock
-
-    SECRET=$(gcloud secrets list --filter "name:${DNS01_SA_KEY_SECRET}" --format 'value(name)')
+    SECRET=$(gcloud secrets list --filter "name:$secret_name" --format 'value(name)')
 
     if [ -z "${SECRET}" ]; then
         _info "Allocating service account key and storing as secret."
         gcloud iam service-accounts keys create /tmp/key.json \
-               --iam-account "${DNS01_SA_IAM_ACCOUNT}"
+               --iam-account "$iam_account"
 
-        gcloud secrets create "${DNS01_SA_KEY_SECRET}" --data-file=/tmp/key.json
+        gcloud secrets create "$secret_name" --data-file=/tmp/key.json
 
         rm /tmp/key.json
     else
-        SA_KEY_ID=$(subcmd_get_dns01_key_id)
+        SA_KEY_ID=$(_get_service_account_key_id "$secret_name")
 
-        _info "DNS Service Account key secret already exists: ${SA_KEY_ID}"
+        _info "Service Account key secret already exists: ${SA_KEY_ID}"
     fi
 }
+
+subcommand_whitelist[install_dns01_key]='Allocate a DNS SA Key for the project and store it in a secret.'
+
+function subcmd_install_dns01_key() {
+    _prompt_to_confirm --no-lock
+
+    _install_service_account_key "${DNS01_SA_KEY_SECRET}" "${DNS01_SA_IAM_ACCOUNT}"
+}
+
+subcommand_whitelist[install_data_export_bucket_key]='Allocate a data export key for the project and store it in a secret.'
+
+function subcmd_install_data_export_bucket_key() {
+    _prompt_to_confirm --no-lock
+
+    _install_service_account_key "${DATA_EXPORT_BUCKET_SA_KEY_SECRET}" "${DATA_EXPORT_BUCKET_SA_IAM_ACCOUNT}"
+}
 ###
+
+function _uninstall_service_account_key() {
+    local -r secret_name="$1"
+    local -r iam_account="$2"
+
+    SECRET=$(gcloud secrets list --filter "name:$secret_name" --format 'value(name)')
+
+    if [ -z "${SECRET}" ]; then
+        _warning "No service account key secret to delete."
+    else
+        SA_KEY_ID=$(_get_service_account_key_id "$secret_name")
+
+        _info "Deleting SA Key: ${SA_KEY_ID}"
+
+        gcloud iam service-accounts keys delete \
+               --iam-account "$iam_account" \
+               --quiet \
+               "${SA_KEY_ID}" || true
+
+        gcloud secrets delete --quiet "$secret_name"
+
+        _info "Service account key deleted."
+    fi
+}
 
 subcommand_whitelist[uninstall_dns01_key]='Delete the DNS SA Key for the project and the corresponding secret'
 
 function subcmd_uninstall_dns01_key() {
     _prompt_to_confirm --no-lock
 
-    SECRET=$(gcloud secrets list --filter "name:${DNS01_SA_KEY_SECRET}" --format 'value(name)')
+    _uninstall_service_account_key "${DNS01_SA_KEY_SECRET}" "${DNS01_SA_IAM_ACCOUNT}"
+}
 
-    if [ -z "${SECRET}" ]; then
-        _warning "No service account key secret to delete."
-    else
-        SA_KEY_ID=$(subcmd_get_dns01_key_id)
+subcommand_whitelist[uninstall_data_export_bucket_key]='Delete the data export key for the project and the corresponding secret'
 
-        _info "Deleting SA Key: ${SA_KEY_ID}"
+function subcmd_uninstall_data_export_bucket_key() {
+    _prompt_to_confirm --no-lock
 
-        gcloud iam service-accounts keys delete \
-               --iam-account "${DNS01_SA_IAM_ACCOUNT}" \
-               --quiet \
-               "${SA_KEY_ID}" || true
-
-        gcloud secrets delete --quiet "${DNS01_SA_KEY_SECRET}"
-
-        _info "Service account key deleted."
-    fi
+    _uninstall_service_account_key "${DATA_EXPORT_BUCKET_SA_KEY_SECRET}" "${DATA_EXPORT_BUCKET_SA_IAM_ACCOUNT}"
 }
 
 ###
@@ -518,7 +570,7 @@ function subcmd_infra_up() {
 
     _uses_pulumi_stack infra
 
-    _get_dns01_secret | subcmd_pulumi infra config set DNA01_SA_KEY_JSON
+    _get_dns01_secret | subcmd_pulumi infra config set DNS01_SA_KEY_JSON
     subcmd_pulumi infra config set CLUSTER_BASENAME "${GCP_CLUSTER_BASENAME}"
 
     subcmd_pulumi infra up --yes --skip-preview
@@ -1039,6 +1091,9 @@ function subcmd_apply() {
     else
         args+=(-c "FIXED_TOKENS=0")
     fi
+
+    subcmd_install_data_export_bucket_key
+    _get_data_export_bucket_secret | subcmd_pulumi canton-network config set DATA_EXPORT_BUCKET_SA_KEY_JSON
 
     if [ -n "${preview:-}" ]; then
         _info "Previewing apply."

cluster/pulumi/canton-network/src/sv.ts
@@ -113,11 +113,12 @@ export type SvConfig = {
   auth0ValidatorAppName: string;
 };
 
-function getAcsBootstrappingDump(xns: ExactNamespace, config: BootstrappingDumpConfig) {
-  return getLatestSvcAcsDumpFile(xns, config).apply(file => ({
+async function getAcsBootstrappingDump(xns: ExactNamespace, config: BootstrappingDumpConfig) {
+  const file = await getLatestSvcAcsDumpFile(xns, config);
+  return {
     path: file.name,
     bucket: config.bucket,
-  }));
+  };
 }
 
 export async function installSvNode(config: SvConfig): Promise<pulumi.Resource> {
@@ -136,7 +137,7 @@ export async function installSvNode(config: SvConfig): Promise<pulumi.Resource> 
     : undefined;
 
   const participantBootstrapDumpSecret: pulumi.Resource | undefined = config.bootstrappingDumpConfig
-    ? fetchAndInstallParticipantBootstrapDump(xns, config.bootstrappingDumpConfig)
+    ? await fetchAndInstallParticipantBootstrapDump(xns, config.bootstrappingDumpConfig)
     : undefined;
 
   const dependsOn: pulumi.Resource[] = auth0BackendSecrets

cluster/pulumi/canton-network/src/validator.ts
@@ -51,7 +51,7 @@ export type ValidatorConfig = {
 export async function installValidatorApp(config: ValidatorConfig): Promise<pulumi.Resource> {
   const participantBootstrapDumpSecret: pulumi.Resource | undefined =
     config.participantBootstrapDump
-      ? fetchAndInstallParticipantBootstrapDump(config.xns, config.participantBootstrapDump)
+      ? await fetchAndInstallParticipantBootstrapDump(config.xns, config.participantBootstrapDump)
       : undefined;
 
   const dependsOn: pulumi.Resource[] = [

cluster/pulumi/common/src/dump-config-common.ts
@@ -20,6 +20,7 @@ export class SecretsFixtureMap extends Map<string, Auth0ClientSecret> {
 export function initDumpConfig(): void {
   pulumi.runtime.setConfig('test-project:CLUSTER_BASENAME', 'mock');
   pulumi.runtime.setConfig('test-project:DNA01_SA_KEY_JSON', 'mock-json');
+  pulumi.runtime.setConfig('test-project:DATA_EXPORT_BUCKET_SA_KEY_JSON', 'mock-json');
   pulumi.runtime.setConfig('test-project:FIXED_TOKENS', '0');
   pulumi.runtime.setConfig('test-project:VERSION_NUMBER', '0.0.1');
   pulumi.runtime.setConfig('test-project:IMAGE_TAG', '0.0.1-deadbeef');

commit 4fc086fdc58777771136d29f92cbcb5730cf0bf7

Fix encoding of GCP bucket credentials (#6576)
broken by #6565

fixes #6575

cluster/pulumi/canton-network/src/backup.ts
@@ -52,7 +52,7 @@ export function installGcpBucketSecret(xns: ExactNamespace, bucket: GcpBucket): 
 function getGcpBucket(bucketConfig: GcpBucketConfig, credentials: string): Bucket {
   const storage: Storage = new Storage({
     projectId: bucketConfig.projectId,
-    credentials: JSON.parse(Buffer.from(credentials, 'base64').toString('ascii')),
+    credentials: JSON.parse(credentials),
   });
   return storage.bucket(bucketConfig.bucketName);
 }