c53714bc21a632f2e910ea213ed00242c5786b45
Parth Joshi <parth.joshi@digitalasset.com>
2023-08-07 10:18:07 +0200

    Formatting fixes (#7113)
    * fix formatting
    
    * Include the observability module in the fixes
    
    ---------
    
    Co-authored-by: Nicu Reut <nicu.reut@digitalasset.com>

--- cluster/pulumi/observability/grafana-dashboards.ts
+++ cluster/pulumi/observability/grafana-dashboards.ts
@@ -1,37 +1,36 @@
-import * as k8s from "@pulumi/kubernetes";
-import * as fs from "fs";
-import * as path from "path";
-import {Input} from "@pulumi/pulumi";
+import * as k8s from '@pulumi/kubernetes';
+import * as fs from 'fs';
+import * as path from 'path';
+import { Input } from '@pulumi/pulumi';
 
-const dashboardsPath = process.env.GRAFANA_DASHBOARDS
-
+const dashboardsPath = process.env.GRAFANA_DASHBOARDS;
 
 export function createGrafanaDashboards(namespace: Input<string>): void {
-    createConfigMapForFolder(namespace, "platform")
-    createConfigMapForFolder(namespace, "participant")
+  createConfigMapForFolder(namespace, 'platform');
+  createConfigMapForFolder(namespace, 'participant');
 }
 
 function createConfigMapForFolder(namespace: Input<string>, folder: string) {
-    const folderPath = `${dashboardsPath}/${folder}`;
-    const dirFiles = fs.readdirSync(folderPath);
-    const files: { [key: string]: string } = {};
-    dirFiles.forEach((file) => {
-        const filePath = path.join(folderPath, file);
-        if (fs.statSync(filePath).isFile()) {
-            files[file] = fs.readFileSync(filePath, "utf-8");
-        }
-    });
-    new k8s.core.v1.ConfigMap(`grafana-dashboards-${folder}`, {
-        metadata: {
-            name: `grafana-dashboards-${folder}`,
-            namespace: namespace,
-            labels: {
-                grafana_dashboard: "1",
-            },
-            annotations: {
-                folder: `/tmp/dashboards/${folder}`
-            }
-        },
-        data: files,
-    });
+  const folderPath = `${dashboardsPath}/${folder}`;
+  const dirFiles = fs.readdirSync(folderPath);
+  const files: { [key: string]: string } = {};
+  dirFiles.forEach(file => {
+    const filePath = path.join(folderPath, file);
+    if (fs.statSync(filePath).isFile()) {
+      files[file] = fs.readFileSync(filePath, 'utf-8');
+    }
+  });
+  new k8s.core.v1.ConfigMap(`grafana-dashboards-${folder}`, {
+    metadata: {
+      name: `grafana-dashboards-${folder}`,
+      namespace: namespace,
+      labels: {
+        grafana_dashboard: '1',
+      },
+      annotations: {
+        folder: `/tmp/dashboards/${folder}`,
+      },
+    },
+    data: files,
+  });
 }
--- cluster/pulumi/observability/index.ts
+++ cluster/pulumi/observability/index.ts
@@ -1,149 +1,145 @@
 import * as k8s from '@pulumi/kubernetes';
 import * as pulumi from '@pulumi/pulumi';
-import {CLUSTER_DNS_NAME} from 'cn-pulumi-common';
-import {createGrafanaDashboards} from "./grafana-dashboards";
+import { CLUSTER_DNS_NAME } from 'cn-pulumi-common';
+
+import { createGrafanaDashboards } from './grafana-dashboards';
 
 const namespace = new k8s.core.v1.Namespace('observabilty', {
-    metadata: {
-        name: 'observability',
-    },
+  metadata: {
+    name: 'observability',
+  },
 });
 const namespaceName = namespace.metadata.name;
 new k8s.helm.v3.Release('observability-metrics', {
-    name: 'prometheus-grafana-monitoring',
-    chart: 'kube-prometheus-stack',
-    version: '48.2.0',
-    namespace: namespaceName,
-    repositoryOpts: {
-        repo: 'https://prometheus-community.github.io/helm-charts',
+  name: 'prometheus-grafana-monitoring',
+  chart: 'kube-prometheus-stack',
+  version: '48.2.0',
+  namespace: namespaceName,
+  repositoryOpts: {
+    repo: 'https://prometheus-community.github.io/helm-charts',
+  },
+  values: {
+    fullnameOverride: 'prometheus',
+    commonLabels: {
+      'digitalasset.com/scope': 'ci',
+      'digitalasset.com/component': 'prometheus-stack',
     },
-    values: {
-        fullnameOverride: 'prometheus',
-        commonLabels: {
-            'digitalasset.com/scope': 'ci',
-            'digitalasset.com/component': 'prometheus-stack',
+    defaultRules: {
+      // enable recording rules for all the k8s metrics
+      create: true,
+    },
+    kubeControllerManager: {
+      enabled: false,
+    },
+    kubeEtcd: {
+      enabled: false,
+    },
+    kubeScheduler: {
+      enabled: false,
+    },
+    kubeProxy: {
+      enabled: false,
+    },
+    alertmanager: {
+      enabled: false, // not required yet
+    },
+    prometheusOperator: {
+      admissionWebhooks: {
+        enabled: false,
+      },
+      tls: {
+        enabled: false, // because `admissionWebhooks` are disabled, see: https://github.com/prometheus-community/helm-charts/issues/418
+      },
+    },
+    prometheus: {
+      prometheusSpec: {
+        // discover all pod/service monitors across all namespaces
+        podMonitorSelectorNilUsesHelmValues: false,
+        serviceMonitorSelectorNilUsesHelmValues: false,
+        enableFeatures: ['native-histograms', 'memory-snapshot-on-shutdown'],
+        enableRemoteWriteReceiver: true,
+        retention: '1y',
+        retentionSize: '100GB',
+        resources: {
+          requests: {
+            memory: '12Gi',
+            cpu: '4',
+          },
         },
-        defaultRules: {
-            // enable recording rules for all the k8s metrics
-            create: true,
-        },
-        kubeControllerManager: {
-            enabled: false,
-        },
-        kubeEtcd: {
-            enabled: false,
-        },
-        kubeScheduler: {
-            enabled: false,
-        },
-        kubeProxy: {
-            enabled: false,
-        },
-        alertmanager: {
-            enabled: false, // not required yet
-        },
-        prometheusOperator: {
-            admissionWebhooks: {
-                enabled: false,
-            },
-            tls: {
-                enabled: false, // because `admissionWebhooks` are disabled, see: https://github.com/prometheus-community/helm-charts/issues/418
-            },
-        },
-        prometheus: {
-            prometheusSpec: {
-                // discover all pod/service monitors across all namespaces
-                podMonitorSelectorNilUsesHelmValues: false,
-                serviceMonitorSelectorNilUsesHelmValues: false,
-                enableFeatures: ['native-histograms', 'memory-snapshot-on-shutdown'],
-                enableRemoteWriteReceiver: true,
-                retention: '1y',
-                retentionSize: '100GB',
-                resources: {
-                    requests: {
-                        memory: "12Gi",
-                        cpu: "4"
-                    },
+        storageSpec: {
+          volumeClaimTemplate: {
+            spec: {
+              storageClassName: 'premium-rwo',
+              accessModes: ['ReadWriteOnce'],
+              resources: {
+                requests: {
+                  storage: '200Gi',
                 },
-                storageSpec: {
-                    volumeClaimTemplate: {
-                        spec: {
-                            storageClassName: "premium-rwo",
-                            accessModes: ["ReadWriteOnce"],
-                            resources: {
-                                requests: {
-                                    storage: "200Gi"
-                                }
-                            }
-                        }
-                    }
-                },
+              },
             },
+          },
         },
-        grafana: {
-            fullnameOverride: 'grafana',
-            ingress: {
-                enabled: false,
-            },
-            sidecar: {
-                dashboards: {
-                    folderAnnotation: 'folder',
-                    provider: {foldersFromFilesStructure: true, allowUiUpdates: true},
-                },
-            },
-            adminUser: "cn-admin",
-            adminPassword: "canton_network_!password"
+      },
+    },
+    grafana: {
+      fullnameOverride: 'grafana',
+      ingress: {
+        enabled: false,
+      },
+      sidecar: {
+        dashboards: {
+          folderAnnotation: 'folder',
+          provider: { foldersFromFilesStructure: true, allowUiUpdates: true },
         },
-        'kube-state-metrics': {
-            fullnameOverride: 'ksm',
-        },
-        'prometheus-node-exporter': {
-            fullnameOverride: 'node-exporter',
-        },
-    }
+      },
+      adminUser: 'cn-admin',
+      adminPassword: 'canton_network_!password',
+    },
+    'kube-state-metrics': {
+      fullnameOverride: 'ksm',
+    },
+    'prometheus-node-exporter': {
+      fullnameOverride: 'node-exporter',
+    },
+  },
 });
 
-istioVirtualService(
-    "prometheus",
-    "prometheus-prometheus",
-    9090
-)
+istioVirtualService('prometheus', 'prometheus-prometheus', 9090);
 
-
-istioVirtualService(
-    "grafana",
-    "grafana",
-    80
-)
+istioVirtualService('grafana', 'grafana', 80);
 
 function istioVirtualService(name: string, serviceName: string, servicePort: number) {
-    new k8s.apiextensions.CustomResource(`${name}-virtual-service`, {
-        apiVersion: 'networking.istio.io/v1alpha3',
-        kind: 'VirtualService',
-        metadata: {
-            name: name,
-            namespace: namespaceName,
-        },
-        spec: {
-            hosts: [`${name}.${CLUSTER_DNS_NAME}`],
-            gateways: ['cluster-ingress/cn-http-gateway'],
-            http: [
-                {
-                    match: [{port: 443}, {port: 80}],
-                    route: [
-                        {
-                            destination: {
-                                host: pulumi.interpolate`${serviceName}.${namespaceName}.svc.cluster.local`,
-                                port: {
-                                    number: servicePort,
-                                },
-                            },
-                        },
-                    ],
+  new k8s.apiextensions.CustomResource(
+    `${name}-virtual-service`,
+    {
+      apiVersion: 'networking.istio.io/v1alpha3',
+      kind: 'VirtualService',
+      metadata: {
+        name: name,
+        namespace: namespaceName,
+      },
+      spec: {
+        hosts: [`${name}.${CLUSTER_DNS_NAME}`],
+        gateways: ['cluster-ingress/cn-http-gateway'],
+        http: [
+          {
+            match: [{ port: 443 }, { port: 80 }],
+            route: [
+              {
+                destination: {
+                  host: pulumi.interpolate`${serviceName}.${namespaceName}.svc.cluster.local`,
+                  port: {
+                    number: servicePort,
+                  },
                 },
+              },
             ],
-        },
-    }, {deleteBeforeReplace: true});
+          },
+        ],
+      },
+    },
+    { deleteBeforeReplace: true }
+  );
 }
 
-createGrafanaDashboards(namespaceName)
+createGrafanaDashboards(namespaceName);
