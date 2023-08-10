9b2a100672b1b2e3a344dca44cc98191b2e43e1a
Nicu Reut <nicu.reut@digitalasset.com>
2023-08-10 10:33:35 +0200

    Integration test for CometBFT SV offboarding (#7180)

--- apps/app/src/test/scala/com/daml/network/integration/tests/SvCometBftIntegrationTest.scala
+++ apps/app/src/test/scala/com/daml/network/integration/tests/SvCometBftIntegrationTest.scala
@@ -1,5 +1,11 @@
 package com.daml.network.integration.tests
 
+import com.daml.network.codegen.java.cn.svcrules.actionrequiringconfirmation.ARC_SvcRules
+import com.daml.network.codegen.java.cn.svcrules.svcrules_actionrequiringconfirmation.SRARC_RemoveMember
+import com.daml.network.codegen.java.cn.svcrules.{
+  ActionRequiringConfirmation,
+  SvcRules_RemoveMember,
+}
 import com.daml.network.config.CNNodeConfigTransforms
 import com.daml.network.console.SvAppBackendReference
 import com.daml.network.environment.CNNodeEnvironmentImpl
@@ -9,16 +15,18 @@ import com.daml.network.integration.tests.CNNodeTests.{
   CNNodeIntegrationTestWithSharedEnvironment,
   CNNodeTestConsoleEnvironment,
 }
-import com.daml.network.sv.cometbft.CometBftHttpRpcClient.CometBftHttpError
 import com.daml.network.sv.cometbft.{CometBftConnectionConfig, CometBftHttpRpcClient}
 import com.daml.network.sv.config.CometBftConfig
 import com.daml.network.util.SvTestUtil
+import com.digitalasset.canton.drivers.cometbft.data.{CometBftTx, SequencerTx}
 import com.digitalasset.canton.integration.BaseEnvironmentDefinition
 import com.digitalasset.canton.logging.NamedLoggerFactory
 import monocle.macros.syntax.lens.*
 
+import java.util.UUID
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration.*
+import scala.util.Random
 
 class SvCometBftIntegrationTest extends CNNodeIntegrationTestWithSharedEnvironment with SvTestUtil {
 
@@ -55,6 +63,42 @@ class SvCometBftIntegrationTest extends CNNodeIntegrationTestWithSharedEnvironme
     }
   }
 
+  "removed SV member has its node removed" in { implicit env =>
+    eventually(timeUntilSuccess = 2.minute) {
+      sv4Backend.cometBftNodeStatus().votingPower.doubleValue should be(1d)
+    }
+    val action: ActionRequiringConfirmation =
+      new ARC_SvcRules(
+        new SRARC_RemoveMember(
+          new SvcRules_RemoveMember(sv4Backend.getSvcInfo().svParty.toProtoPrimitive)
+        )
+      )
+    sv4Backend.stop()
+    sv1Backend.createVoteRequest(
+      sv1Backend.getSvcInfo().svParty.toProtoPrimitive,
+      action,
+      "url",
+      "description",
+    )
+    Seq(sv2Backend, sv3Backend).foreach { sv =>
+      eventually() {
+        sv.listVoteRequests() should not be empty
+        val voteId = sv.listVoteRequests().head.contractId
+        sv.castVote(voteId, isAccepted = true, "url", "description")
+      }
+    }
+    eventually(timeUntilSuccess = 2.minute, maxPollInterval = 1.second) {
+      sendEmptyTransactionToIncreaseBlockHeight(Random.shuffle(svs).head)
+      cometbftClientForSvApp(sv4Backend)
+        .nodeStatus()
+        .valueOrFail("sv4 node status")
+        .futureValue
+        .validatorInfo
+        .votingPower
+        .toDouble should be(0d)
+    }
+  }
+
   private def cometBFTnodeIsUpToDateValidator(sv: SvAppBackendReference) = {
     // node is up to date
     sv.cometBftNodeStatus().catchingUp shouldBe false
@@ -70,12 +114,17 @@ class SvCometBftIntegrationTest extends CNNodeIntegrationTestWithSharedEnvironme
       sv: SvAppBackendReference
   ): Unit = {
     cometbftClientForSvApp(sv)
-      .sendAndWaitForCommit(Array.emptyByteArray)
-      .recover {
-        case CometBftHttpError(_, error)
-            if error.error.noSpaces.contains("tx already exists in cache") =>
-          ()
-      }
+      .sendAndWaitForCommit(
+        CometBftTx(
+          CometBftTx.Message.SequencerTx(
+            SequencerTx(
+              // We send a sequencer transaction with a random UUID because sending the same transaction would be rejected as a duplicate
+              // by the CometBFT memory pool
+              uuid = UUID.randomUUID().toString
+            )
+          )
+        ).toByteArray
+      )
       .valueOrFail("empty transaction")
       .futureValue
   }
