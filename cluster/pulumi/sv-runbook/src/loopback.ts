f861f4415594426ef44e3b0db378648999439abc
Chun Lok Ling <110462561+chunlokling-da@users.noreply.github.com>
2023-10-09 16:02:21 +0100

    Remove egress from sv runbook pulumi and helm (#7935)

--- cluster/pulumi/sv-runbook/src/loopback.ts
+++ cluster/pulumi/sv-runbook/src/loopback.ts
@@ -1,5 +1,5 @@
 import * as k8s from '@pulumi/kubernetes';
-import { ExactNamespace, ingressPort } from 'cn-pulumi-common';
+import { ExactNamespace } from 'cn-pulumi-common';
 
 import { installCNSVHelmChart } from './helm';
 
@@ -9,38 +9,6 @@ export function installLoopback(
   localCharts: boolean,
   version: string | undefined
 ): k8s.helm.v3.Release {
-  new k8s.helm.v3.Release(
-    'istio-egressgateway',
-    {
-      name: 'istio-egressgateway',
-      chart: 'gateway',
-      namespace: namespace.logicalName,
-      repositoryOpts: {
-        repo: 'https://istio-release.storage.googleapis.com/charts',
-      },
-      values: {
-        service: {
-          type: 'ClusterIP',
-          ports: [
-            ingressPort('status-port', 15021), // istio default
-            ingressPort('http2', 80),
-            ingressPort('https', 443),
-            ingressPort('grpc-domain', 5008),
-            // see notes when installing a CometBft node in the full deployment
-            ingressPort('cometbft-1', 26656),
-            ingressPort('cometbft-2', 26666),
-            ingressPort('cometbft-3', 26676),
-            ingressPort('cometbft-4', 26686),
-            ingressPort('cometbft-sv', 26696),
-          ],
-        },
-      },
-    },
-    {
-      dependsOn: [namespace.ns],
-    }
-  );
-
   return installCNSVHelmChart(
     namespace,
     'loopback',
