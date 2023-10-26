994628e6b1a1c4516d947050a528c9f3ecf6ddc2
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2023-10-26 17:25:32 +0200

    Support decoding old and new models in UI (#8275)
    fixes #8270
    
    This is actually fairly hard to trigger in our current setup because
    our backends will always produce the new encoding but better be on the
    safe side (and ideally backends wouldn't do that either).
    
    Upstream PR is in https://github.com/digital-asset/daml/pull/17674 but
    we run a vendored copy already.

---
+++ apps/common/frontend/src/__tests__/common.test.tsx
@@ -0,0 +1,30 @@
+import { test, expect } from 'vitest';
+
+import { Coin } from '@daml.js/canton-coin/lib/CC/Coin';
+
+import { Contract } from '..';
+
+test('decode contracts from old and new model', async () => {
+  const base = {
+    template_id: 'foobar',
+    contract_id: 'dummyContractId',
+    metadata: {
+      createdAt: 'dummyCreatedAt',
+      contractKeyHash: '',
+      driverMetadata: 'dummyDriverMetadata',
+    },
+  };
+  const oldPayload = {
+    svc: 'svc',
+    owner: 'owner',
+    amount: {
+      initialAmount: '1.0',
+      createdAt: { number: '0' },
+      ratePerRound: { rate: '0.01' },
+    },
+  };
+  const oldCoin = Contract.decodeOpenAPI({ ...base, payload: oldPayload }, Coin);
+  const newPayload = { ...oldPayload, upgrade: null };
+  const newCoin = Contract.decodeOpenAPI({ ...base, payload: newPayload }, Coin);
+  expect(oldCoin).toStrictEqual(newCoin);
+});
