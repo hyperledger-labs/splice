2d09d1c4f4cceeb40ba0ceb3be95a3ba75787b58
Martin Florian <martin.florian@digitalasset.com>
2024-01-08 11:53:47 +0100

    Test login and check balance in non-devnet validator preflights (#9193)
    Fixes #9059 (s.a. discussion on #9164)
    
    I didn't add `VALIDATOR_WEB_UI_PASSWORD` to the list of required private environment variables; we didn't do this for `SV_DEV_NET_WEB_UI_PASSWORD` either and since no one complained about this I'm guessing people rarely run these preflight checks locally. I did set `VALIDATOR_WEB_UI_PASSWORD` on CCI though of course.

--- apps/app/src/test/scala/com/daml/network/integration/tests/runbook/ValidatorNonDevNetPreflightIntegrationTest.scala
+++ apps/app/src/test/scala/com/daml/network/integration/tests/runbook/ValidatorNonDevNetPreflightIntegrationTest.scala
@@ -9,11 +9,18 @@ import com.digitalasset.canton.integration.BaseEnvironmentDefinition
 /** Base for preflight tests running against a deployed non-devnet validator
   */
 abstract class ValidatorNonDevNetPreflightIntegrationTestBase
-    extends FrontendIntegrationTestWithSharedEnvironment("anonymous") {
+    extends FrontendIntegrationTestWithSharedEnvironment("validator-user") {
 
   protected val validatorName: String
+
+  protected lazy val validatorUserName = s"admin@${validatorName}.com"
+  protected lazy val validatorUserPassword = sys.env(s"VALIDATOR_WEB_UI_PASSWORD")
+
   private lazy val walletUiUrl =
     s"https://wallet.${validatorName}.${sys.env("NETWORK_APPS_ADDRESS")}/"
+
+  // TODO(#8848): Remove this option altogether (should always be true)
+  protected val testBalance: Boolean = true
 
   override def environmentDefinition
       : BaseEnvironmentDefinition[CNNodeEnvironmentImpl, CNNodeTestConsoleEnvironment] =
@@ -21,12 +28,23 @@ abstract class ValidatorNonDevNetPreflightIntegrationTestBase
       this.getClass.getSimpleName()
     )
 
-  // TODO(#9059): Expand to more interesting test (with hardcoded users)
-  "The wallet UI is accessible and shows a login page" in { _ =>
-    withFrontEnd("anonymous") { implicit webDriver =>
-      go to walletUiUrl
-      eventually() {
-        find(id("oidc-login-button")) should not be empty
+  "The validator service user's wallet is accessible and has non-zero balance" in { _ =>
+    withFrontEnd("validator-user") { implicit webDriver =>
+      clue(s"Logging in to wallet UI at: ${walletUiUrl}") {
+        completeAuth0LoginWithAuthorization(
+          walletUiUrl,
+          validatorUserName,
+          validatorUserPassword,
+          // Notably, we expect that the service user is always already onboarded.
+          () => find(id("wallet-balance-cc")) should not be empty,
+        )
+      }
+      if (testBalance) {
+        clue("Checking that balance is visible and > 0") {
+          val balanceText = find(id("wallet-balance-cc")).value.text.trim
+          val balance = BigDecimal(balanceText.split(" ").head)
+          balance should be > BigDecimal(0)
+        }
       }
     }
   }
@@ -35,6 +53,7 @@ abstract class ValidatorNonDevNetPreflightIntegrationTestBase
 class RunbookValidatorNonDevNetPreflightIntegrationTest
     extends ValidatorNonDevNetPreflightIntegrationTestBase {
   override protected val validatorName = "validator"
+  override protected val testBalance = false
 }
 
 class Validator1NonDevNetPreflightIntegrationTest
