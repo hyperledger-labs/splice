71fe4a3c104e77764ba4042470f0a348dfa1e020
Parth Joshi <parth.joshi@digitalasset.com>
2024-02-13 16:32:26 +0100

    Filter out duplicate gateway IPs in setup_vpn_routes.sh (#9945)

--- .circleci/vpn-scripts/setup-vpn-routes.sh
+++ .circleci/vpn-scripts/setup-vpn-routes.sh
@@ -8,7 +8,7 @@ echo "Querying for VPN IP"
 VPN_IP=$(/usr/bin/sudo ip --json addr show dev ens5 | tee /dev/stderr | jq -r '.[0].addr_info | .[] | select(.family == "inet" and (has("broadcast") | not)) | .local')
 [[ -z "$VPN_IP" ]] && { echo "Cannot find VPN_IP, please check VPN connectivity" ; exit 1; }
 echo "Querying for network gateway"
-GATEWAY=$(/usr/bin/sudo ip --json route list | tee /dev/stderr | jq -r '.[]|select(.gateway)|.gateway')
+GATEWAY=$(/usr/bin/sudo ip --json route list | tee /dev/stderr | jq -r 'map(select(.gateway)|.gateway)|unique|.[]')
 echo "Querying for egress IP"
 EGRESS_IP=$(/usr/bin/getent hosts "$GCP_CLUSTER_BASENAME".network.canton.global | tee /dev/stderr | awk '{ print $1 }')
 /usr/bin/sudo ip route add "$EGRESS_IP" via "$GATEWAY" table 220 src "$VPN_IP"
