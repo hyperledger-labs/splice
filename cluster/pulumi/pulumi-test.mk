bdb4ca5608e11c878b3d4228a93182afb0e8570a
Oriol Muñoz <oriol.munoz@digitalasset.com>
2024-02-15 19:27:00 +0100

    Ignore local env vars in `make cluster/pulumi/update-expected` (#10007)

--- cluster/pulumi/pulumi-test.mk
+++ cluster/pulumi/pulumi-test.mk
@@ -1,9 +1,16 @@
+# When running locally, we want to run dump-config without any local environment variables,
+# so we use env -i to unload everything except PATH and HOME,
+# and then we direnv exec to restore the repo's environment variables before running dump-config.
+# This doesn't translate well to CI, but that's okay because CI doesn't change variables anyway.
 .PHONY: $(dir)/test.json
 $(dir)/test.json: $(dir $(dir)).build
 	set -o pipefail \
-	&& cd $(@D) \
-	&& npm run --silent dump-config \
-	   | jq --slurp --sort-keys $(JQ_FILTER) > $(@F)
+	&& cd $(@D); \
+	if [ -n "$$CI" ]; then \
+		npm run --silent dump-config | jq --slurp --sort-keys $(JQ_FILTER) > $(@F); \
+	else \
+		env -i PATH=$$PATH HOME=$$HOME IGNORE_PRIVATE_ENVRC=1 direnv exec . npm run --silent dump-config | jq --slurp --sort-keys $(JQ_FILTER) > $(@F); \
+	fi
 
 .PHONY: $(dir)/test
 $(dir)/test: $(dir)/test-config $(dir)/lint
