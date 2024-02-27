24c252bd700e198e1ea68dae2e7de4020ac8af3a
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2024-02-27 12:11:38 +0100

    Upgrade lnav to 0.12 (#10389)
    release notes https://github.com/tstack/lnav/releases/tag/v0.12.0
    
    Not quite sure how useful the gantt stuff is in practice but seems
    potentially nice.

--- nix/lnav.nix
+++ nix/lnav.nix
@@ -3,16 +3,16 @@
 # so we use the binary distribution that ships with a compatible pcre version.
 stdenv.mkDerivation rec {
   name = "lnav";
-  version = "0.11.2";
+  version = "0.12.0";
 
   src =
     if stdenv.isDarwin then
       fetchzip {
         url = "https://github.com/tstack/lnav/releases/download/v${version}/lnav-${version}-x86_64-macos.zip";
-        sha256 = "sha256-8TY/N6aYyH+FGvwtLAOcEj6SbVQQty2s4X1GVDNE2vQ=";
+        sha256 = "sha256-3+mpfqSu1KigrBrnVZ1s9/9da4tce7xQUgWcmdH9//k=";
       } else fetchzip {
-        url = "https://github.com/tstack/lnav/releases/download/v${version}/lnav-${version}-x86_64-linux-musl.zip";
-        sha256 = "sha256-QJbHYXkd8m7cJYW7fnNeGA4C9s7L3pCyJ1xZ1PjQWuM=";
+        url = "https://github.com/tstack/lnav/releases/download/v${version}/lnav-${version}-linux-musl-x86_64.zip";
+        sha256 = "sha256-dr1nkOCOyzLq6eP/s2lK2UvpWNe7WIo6v4QF+riVtEo=";
       };
 
   installPhase = ''
