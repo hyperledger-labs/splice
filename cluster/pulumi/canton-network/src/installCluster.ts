hyperledger-labs/splice

commit 32c63a1de3048c34e4e6129b14141090ef67d621

Add delay to postpone promotion in `SvOnboardingPromoteParticipantToSubmitterTrigger` (#10609)

cluster/deployment/cimain/.envrc.vars
@@ -16,3 +16,5 @@ export GCP_CLUSTER_MAX_NODES=7
 
 export ENABLE_COMETBFT_PRUNING=true
 export COMETBFT_RETAIN_BLOCKS="10000" # corresponds to a retention period of ~2 hours
+
+export ENABLE_ONBOARDING_PARTICIPANT_PROMOTION_DELAY=false

cluster/deployment/scratchneta/.envrc.vars
@@ -15,3 +15,5 @@ export PULUMI_BACKEND_URL=gs://da-cn-pulumi-stacks
 
 export ENABLE_COMETBFT_PRUNING=true
 export COMETBFT_RETAIN_BLOCKS="10000" # corresponds to a retention period of ~2 hours
+
+export ENABLE_ONBOARDING_PARTICIPANT_PROMOTION_DELAY=false

cluster/deployment/scratchnetb/.envrc.vars
@@ -15,3 +15,5 @@ export PULUMI_BACKEND_URL=gs://da-cn-pulumi-stacks
 
 export ENABLE_COMETBFT_PRUNING=true
 export COMETBFT_RETAIN_BLOCKS="10000" # corresponds to a retention period of ~2 hours
+
+export ENABLE_ONBOARDING_PARTICIPANT_PROMOTION_DELAY=false

cluster/deployment/scratchnetc/.envrc.vars
@@ -15,3 +15,5 @@ export PULUMI_BACKEND_URL=gs://da-cn-pulumi-stacks
 
 export ENABLE_COMETBFT_PRUNING=true
 export COMETBFT_RETAIN_BLOCKS="10000" # corresponds to a retention period of ~2 hours
+
+export ENABLE_ONBOARDING_PARTICIPANT_PROMOTION_DELAY=false

cluster/deployment/scratchnetd/.envrc.vars
@@ -15,3 +15,5 @@ export PULUMI_BACKEND_URL=gs://da-cn-pulumi-stacks
 
 export ENABLE_COMETBFT_PRUNING=true
 export COMETBFT_RETAIN_BLOCKS="10000" # corresponds to a retention period of ~2 hours
+
+export ENABLE_ONBOARDING_PARTICIPANT_PROMOTION_DELAY=false

cluster/deployment/scratchnete/.envrc.vars
@@ -15,3 +15,5 @@ export PULUMI_BACKEND_URL=gs://da-cn-pulumi-stacks
 
 export ENABLE_COMETBFT_PRUNING=true
 export COMETBFT_RETAIN_BLOCKS="10000" # corresponds to a retention period of ~2 hours
+
+export ENABLE_ONBOARDING_PARTICIPANT_PROMOTION_DELAY=false

cluster/helm/cn-sv-node/templates/sv.yaml
@@ -210,6 +210,9 @@ spec:
             - name: ADDITIONAL_CONFIG_REWARD_ISSUANCE
               value: |
                 canton.sv-apps.sv.automation.use-new-sv-reward-issuance = true
+            - name: ADDITIONAL_CONFIG_ENABLE_ONBOARDING_PARTICIPANT_PROMOTION_DELAY
+              value: |
+                canton.sv-apps.sv.enable-onboarding-participant-promotion-delay = {{ .Values.enableOnboardingParticipantPromotionDelay }}
             - name: ADDITIONAL_CONFIG_EXTRA_BENEFICIARIES
               value: |
                 canton.sv-apps.sv.extra-beneficiaries = {

cluster/pulumi/canton-network/src/installCluster.ts
@@ -41,6 +41,11 @@ const splitPostgresInstances = envFlag('SPLIT_POSTGRES_INSTANCES') || envFlag('E
 
 const enableChaosMesh = envFlag('ENABLE_CHAOS_MESH');
 
+const enableOnboardingParticipantPromotionDelay = envFlag(
+  'ENABLE_ONBOARDING_PARTICIPANT_PROMOTION_DELAY',
+  true
+);
+
 type BootstrapCliConfig = {
   cluster: string;
   date: string;
@@ -151,6 +156,7 @@ export async function installCluster(auth0Client: Auth0Client): Promise<void> {
     sequencerPruningConfig,
     globalDomainUpgradeConfig,
     onboardingPollingInterval: svOnboardingPollingInterval,
+    enableOnboardingParticipantPromotionDelay: enableOnboardingParticipantPromotionDelay,
   });
 
   const allSvs = await svc.allSvs;

commit dc14e004a6a20a5cc80823bf6649580416315d94

Fix participant promotion delay config in SV runbook (#10709)
This fixes two issues introduced by #10609

1. The SV runbook didn't properly propagate the parameter. This just
   hard-broke all SV runbook deployments but apparently we didn't run
   preflights before :(
2. The parameter was not optional requiring our partners to also set
   it correctly. Helm doesn't distinguish between false and unset so I
   switched it to a disable parameter (not a huge fan of negated
   parameters but seems like the best option here, the scala parameter
   is still enable).

cluster/deployment/cimain/.envrc.vars
@@ -17,4 +17,4 @@ export GCP_CLUSTER_MAX_NODES=7
 export ENABLE_COMETBFT_PRUNING=true
 export COMETBFT_RETAIN_BLOCKS="10000" # corresponds to a retention period of ~2 hours
 
-export ENABLE_ONBOARDING_PARTICIPANT_PROMOTION_DELAY=false
+export DISABLE_ONBOARDING_PARTICIPANT_PROMOTION_DELAY=true

cluster/deployment/scratchneta/.envrc.vars
@@ -16,4 +16,4 @@ export PULUMI_BACKEND_URL=gs://da-cn-pulumi-stacks
 export ENABLE_COMETBFT_PRUNING=true
 export COMETBFT_RETAIN_BLOCKS="10000" # corresponds to a retention period of ~2 hours
 
-export ENABLE_ONBOARDING_PARTICIPANT_PROMOTION_DELAY=false
+export DISABLE_ONBOARDING_PARTICIPANT_PROMOTION_DELAY=true

cluster/deployment/scratchnetb/.envrc.vars
@@ -16,4 +16,4 @@ export PULUMI_BACKEND_URL=gs://da-cn-pulumi-stacks
 export ENABLE_COMETBFT_PRUNING=true
 export COMETBFT_RETAIN_BLOCKS="10000" # corresponds to a retention period of ~2 hours
 
-export ENABLE_ONBOARDING_PARTICIPANT_PROMOTION_DELAY=false
+export DISABLE_ONBOARDING_PARTICIPANT_PROMOTION_DELAY=true

cluster/deployment/scratchnetc/.envrc.vars
@@ -16,4 +16,4 @@ export PULUMI_BACKEND_URL=gs://da-cn-pulumi-stacks
 export ENABLE_COMETBFT_PRUNING=true
 export COMETBFT_RETAIN_BLOCKS="10000" # corresponds to a retention period of ~2 hours
 
-export ENABLE_ONBOARDING_PARTICIPANT_PROMOTION_DELAY=false
+export DISABLE_ONBOARDING_PARTICIPANT_PROMOTION_DELAY=true

cluster/deployment/scratchnetd/.envrc.vars
@@ -16,4 +16,4 @@ export PULUMI_BACKEND_URL=gs://da-cn-pulumi-stacks
 export ENABLE_COMETBFT_PRUNING=true
 export COMETBFT_RETAIN_BLOCKS="10000" # corresponds to a retention period of ~2 hours
 
-export ENABLE_ONBOARDING_PARTICIPANT_PROMOTION_DELAY=false
+export DISABLE_ONBOARDING_PARTICIPANT_PROMOTION_DELAY=true

cluster/deployment/scratchnete/.envrc.vars
@@ -16,4 +16,4 @@ export PULUMI_BACKEND_URL=gs://da-cn-pulumi-stacks
 export ENABLE_COMETBFT_PRUNING=true
 export COMETBFT_RETAIN_BLOCKS="10000" # corresponds to a retention period of ~2 hours
 
-export ENABLE_ONBOARDING_PARTICIPANT_PROMOTION_DELAY=false
+export DISABLE_ONBOARDING_PARTICIPANT_PROMOTION_DELAY=true

cluster/helm/cn-sv-node/templates/sv.yaml
@@ -210,9 +210,11 @@ spec:
             - name: ADDITIONAL_CONFIG_REWARD_ISSUANCE
               value: |
                 canton.sv-apps.sv.automation.use-new-sv-reward-issuance = true
-            - name: ADDITIONAL_CONFIG_ENABLE_ONBOARDING_PARTICIPANT_PROMOTION_DELAY
+            {{- if .Values.disableOnboardingParticipantPromotionDelay }}
+            - name: ADDITIONAL_CONFIG_DISABLE_ONBOARDING_PARTICIPANT_PROMOTION_DELAY
               value: |
-                canton.sv-apps.sv.enable-onboarding-participant-promotion-delay = {{ .Values.enableOnboardingParticipantPromotionDelay }}
+                canton.sv-apps.sv.enable-onboarding-participant-promotion-delay = false
+            {{- end }}
             - name: ADDITIONAL_CONFIG_EXTRA_BENEFICIARIES
               value: |
                 canton.sv-apps.sv.extra-beneficiaries = {

cluster/pulumi/canton-network/src/installCluster.ts
@@ -41,9 +41,9 @@ const splitPostgresInstances = envFlag('SPLIT_POSTGRES_INSTANCES') || envFlag('E
 
 const enableChaosMesh = envFlag('ENABLE_CHAOS_MESH');
 
-const enableOnboardingParticipantPromotionDelay = envFlag(
-  'ENABLE_ONBOARDING_PARTICIPANT_PROMOTION_DELAY',
-  true
+const disableOnboardingParticipantPromotionDelay = envFlag(
+  'DISABLE_ONBOARDING_PARTICIPANT_PROMOTION_DELAY',
+  false
 );
 
 type BootstrapCliConfig = {
@@ -156,7 +156,7 @@ export async function installCluster(auth0Client: Auth0Client): Promise<void> {
     sequencerPruningConfig,
     globalDomainUpgradeConfig,
     onboardingPollingInterval: svOnboardingPollingInterval,
-    enableOnboardingParticipantPromotionDelay: enableOnboardingParticipantPromotionDelay,
+    disableOnboardingParticipantPromotionDelay,
   });
 
   const allSvs = await svc.allSvs;

cluster/pulumi/canton-network/src/sv.ts
@@ -106,7 +106,7 @@ export interface SvConfig extends StaticSvConfig {
   topupConfig?: ValidatorTopupConfig;
   sequencerPruningConfig: SequencerPruningConfig;
   splitPostgresInstances: boolean;
-  enableOnboardingParticipantPromotionDelay: boolean;
+  disableOnboardingParticipantPromotionDelay: boolean;
   onboardingPollingInterval?: string;
 }
 
@@ -451,7 +451,7 @@ function installSvApp(
       config.onboarding.type == 'found-collective'
         ? config.onboarding.foundingSvRewardWeightBps
         : undefined,
-    enableOnboardingParticipantPromotionDelay: config.enableOnboardingParticipantPromotionDelay,
+    disableOnboardingParticipantPromotionDelay: config.disableOnboardingParticipantPromotionDelay,
     cometBFT: {
       enabled: true,
       connectionUri: pulumi.interpolate`http://${globalDomain.cometbftRpcService.metadata.name}:26657`,

cluster/pulumi/canton-network/src/svc.ts
@@ -41,7 +41,7 @@ interface SvcArgs {
   sequencerPruningConfig: SequencerPruningConfig;
   globalDomainUpgradeConfig: GlobalDomainMigrationConfig;
   onboardingPollingInterval?: string;
-  enableOnboardingParticipantPromotionDelay: boolean;
+  disableOnboardingParticipantPromotionDelay: boolean;
 }
 
 export class Svc extends pulumi.ComponentResource {
@@ -107,8 +107,8 @@ export class Svc extends pulumi.ComponentResource {
         topupConfig: this.args.topupConfig,
         splitPostgresInstances: this.args.splitPostgresInstances,
         sequencerPruningConfig: this.args.sequencerPruningConfig,
-        enableOnboardingParticipantPromotionDelay:
-          this.args.enableOnboardingParticipantPromotionDelay,
+        disableOnboardingParticipantPromotionDelay:
+          this.args.disableOnboardingParticipantPromotionDelay,
         onboardingPollingInterval: this.args.onboardingPollingInterval,
       },
       this.args.globalDomainUpgradeConfig,

cluster/pulumi/sv-runbook/dump-config.ts
@@ -52,7 +52,7 @@ async function main() {
   };
   const svAppConfig = {
     onboardingName: utils.SV_NAME,
-    enableOnboardingParticipantPromotionDelay: true,
+    disableOnboardingParticipantPromotionDelay: false,
   };
   const validatorAppConfig = {
     walletUserName: utils.validatorWalletUserName,

cluster/pulumi/sv-runbook/src/config.ts
@@ -1,6 +1,6 @@
 export type SvAppConfig = {
   onboardingName: string;
-  enableOnboardingParticipantPromotionDelay: boolean;
+  disableOnboardingParticipantPromotionDelay: boolean;
 };
 
 export type ValidatorAppConfig = {

cluster/pulumi/sv-runbook/src/index.ts
@@ -5,7 +5,7 @@ import { installNode } from './installNode';
 import {
   SV_NAME,
   SV_NAMESPACE,
-  ENABLE_ONBOARDING_PARTICIPANT_PROMOTION_DELAY,
+  DISABLE_ONBOARDING_PARTICIPANT_PROMOTION_DELAY,
   validatorWalletUserName,
 } from './utils';
 
@@ -14,7 +14,7 @@ async function auth0CacheAndInstallNode(auth0Fetch: Auth0Fetch) {
 
   const svAppConfig = {
     onboardingName: SV_NAME,
-    enableOnboardingParticipantPromotionDelay: ENABLE_ONBOARDING_PARTICIPANT_PROMOTION_DELAY,
+    disableOnboardingParticipantPromotionDelay: DISABLE_ONBOARDING_PARTICIPANT_PROMOTION_DELAY,
   };
   const validatorAppConfig = {
     walletUserName: validatorWalletUserName,

cluster/pulumi/sv-runbook/src/installNode.ts
@@ -120,8 +120,8 @@ export async function installNode(
       svKey,
       onboardingName: svAppConfig.onboardingName,
       validatorWalletUserName: validatorAppConfig.walletUserName,
-      enableOnboardingParticipantPromotionDelay:
-        svAppConfig.enableOnboardingParticipantPromotionDelay,
+      disableOnboardingParticipantPromotionDelay:
+        svAppConfig.disableOnboardingParticipantPromotionDelay,
     },
     resolveValidator1PartyId
   );
@@ -163,7 +163,7 @@ type SvConfig = {
   svKey: CnInput<SvIdKey>;
   onboardingName: string;
   validatorWalletUserName: string;
-  enableOnboardingParticipantPromotionDelay: boolean;
+  disableOnboardingParticipantPromotionDelay: boolean;
 };
 
 async function installSvAndValidator(
@@ -183,6 +183,7 @@ async function installSvAndValidator(
     svKey,
     onboardingName,
     validatorWalletUserName,
+    disableOnboardingParticipantPromotionDelay,
   } = config;
 
   const globalDomain = installGlobalDomainNode(
@@ -313,6 +314,7 @@ async function installSvAndValidator(
       },
     ],
     onboardingPollingInterval: svOnboardingPollingInterval,
+    disableOnboardingParticipantPromotionDelay,
   };
 
   const svValuesWithSpecifiedAud: ChartValues = {

cluster/pulumi/sv-runbook/src/utils.ts
@@ -11,9 +11,9 @@ export const TARGET_CLUSTER = requireEnv(
 export const SV_NAME = 'DA-Helm-Test-Node';
 export const SV_NAMESPACE = 'sv';
 
-export const ENABLE_ONBOARDING_PARTICIPANT_PROMOTION_DELAY = envFlag(
-  'ENABLE_ONBOARDING_PARTICIPANT_PROMOTION_DELAY',
-  true
+export const DISABLE_ONBOARDING_PARTICIPANT_PROMOTION_DELAY = envFlag(
+  'DISABLE_ONBOARDING_PARTICIPANT_PROMOTION_DELAY',
+  false
 );
 
 export const version = process.env.CHARTS_VERSION;