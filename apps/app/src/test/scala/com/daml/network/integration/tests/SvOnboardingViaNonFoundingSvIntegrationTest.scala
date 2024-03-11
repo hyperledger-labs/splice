562fe8ba85691554746696cbd1a2507ea658d335
Julien <128390688+julientinguely-da@users.noreply.github.com>
2024-03-11 14:54:00 +0100

    Test SV onboarding while sv1 is offboarded (#10671)

---
+++ apps/app/src/test/scala/com/daml/network/integration/tests/SvOnboardingViaNonFoundingSvIntegrationTest.scala
@@ -0,0 +1,171 @@
+package com.daml.network.integration.tests
+
+import com.daml.network.codegen.java.cn.svcrules.SvcRules_OffboardMember
+import com.daml.network.codegen.java.cn.svcrules.actionrequiringconfirmation.ARC_SvcRules
+import com.daml.network.codegen.java.cn.svcrules.svcrules_actionrequiringconfirmation.SRARC_OffboardMember
+import com.daml.network.config.{CNNodeConfigTransforms, NetworkAppClientConfig}
+import com.daml.network.environment.CNNodeEnvironmentImpl
+import com.daml.network.integration.CNNodeEnvironmentDefinition
+import com.daml.network.integration.tests.CNNodeTests.{
+  CNNodeIntegrationTest,
+  CNNodeTestConsoleEnvironment,
+}
+import com.daml.network.sv.SvAppClientConfig
+import com.daml.network.sv.config.SvOnboardingConfig.JoinWithKey
+import com.daml.network.util.{StandaloneCanton, SvTestUtil}
+import com.digitalasset.canton.integration.BaseEnvironmentDefinition
+import org.scalatest.time.{Minute, Span}
+
+class SvOnboardingViaNonFoundingSvIntegrationTest
+    extends CNNodeIntegrationTest
+    with SvTestUtil
+    with StandaloneCanton {
+
+  override def dbsSuffix: String = "non_founding_svs"
+
+  // Runs against a temporary Canton instance.
+  override lazy val resetDecentralizedNamespace = false
+
+  override implicit val patienceConfig: PatienceConfig = PatienceConfig(scaled(Span(1, Minute)))
+
+  override def environmentDefinition
+      : BaseEnvironmentDefinition[CNNodeEnvironmentImpl, CNNodeTestConsoleEnvironment] =
+    CNNodeEnvironmentDefinition
+      .simpleTopology4Svs(this.getClass.getSimpleName)
+      .withPreSetup(_ => ())
+      .withOnboardingParticipantPromotionDelayEnabled() // Test onboarding with participant promotion delay
+      .addConfigTransformsToFront(
+        (_, conf) => CNNodeConfigTransforms.bumpCantonPortsBy(22_000)(conf),
+        (_, conf) => CNNodeConfigTransforms.bumpCantonDomainPortsBy(22_000)(conf),
+      )
+      .addConfigTransforms((_, config) =>
+        CNNodeConfigTransforms.updateAllSvAppConfigs { (name, config) =>
+          if (name == "sv3") {
+            config.copy(
+              onboarding = config.onboarding
+                .getOrElse(
+                  throw new IllegalStateException("Onboarding configuration not found.")
+                ) match {
+                case node: JoinWithKey =>
+                  Some(
+                    JoinWithKey(
+                      node.name,
+                      SvAppClientConfig(NetworkAppClientConfig("http://127.0.0.1:5214")),
+                      node.publicKey,
+                      node.privateKey,
+                    )
+                  )
+                case _ => throw new IllegalStateException("JoinWithKey configuration not found.")
+              }
+            )
+          } else config
+        }(config)
+      )
+      .withManualStart
+
+  "A new SV can onboard via a non-founding SV while founding SVs are offboarded from the SVC" in {
+    implicit env =>
+      withCantonSvNodes(
+        (
+          Some(sv1Backend),
+          Some(sv2Backend),
+          Some(sv3Backend),
+          None,
+        ),
+        logSuffix = "sv123-non-founding-svs",
+        sv4 = false,
+      )() {
+        actAndCheck(
+          "Initialize SVC with SV1 and SV2",
+          startAllSync(
+            sv1ScanBackend,
+            sv2ScanBackend,
+            sv1Backend,
+            sv2Backend,
+          ),
+        )(
+          "SV1 and SV2 are part of the SVC",
+          _ => {
+            sv2Backend
+              .getSvcInfo()
+              .svcRules
+              .payload
+              .members
+              .keySet() should contain theSameElementsAs Seq(sv1Backend, sv2Backend).map(
+              _.getSvcInfo().svParty.toProtoPrimitive
+            )
+          },
+        )
+        actAndCheck(
+          "Offboard SV1", {
+            sv2Backend.createVoteRequest(
+              sv2Backend.getSvcInfo().svParty.toProtoPrimitive,
+              new ARC_SvcRules(
+                new SRARC_OffboardMember(
+                  new SvcRules_OffboardMember(sv1Backend.getSvcInfo().svParty.toProtoPrimitive)
+                )
+              ),
+              "url",
+              "description",
+              sv1Backend.getSvcInfo().svcRules.payload.config.voteRequestTimeout,
+            )
+            // note that the vote request requires two votes to be accepted
+            sv1Backend.castVote(
+              sv1Backend.listVoteRequests().loneElement.contractId,
+              isAccepted = true,
+              "url",
+              "description",
+            )
+          },
+        )(
+          "SV2 is now the only member",
+          _ => {
+            sv2Backend
+              .getSvcInfo()
+              .svcRules
+              .payload
+              .members
+              .keySet() should contain theSameElementsAs Seq(
+              sv2Backend
+            ).map(_.getSvcInfo().svParty.toProtoPrimitive)
+          },
+        )
+        clue("SV1 is offboarded from the Decentralized Namespace") {
+          eventually() {
+            val decentralizedNamespaces =
+              sv2Backend.participantClient.topology.decentralized_namespaces
+                .list(
+                  filterStore = globalDomainId.filterString,
+                  filterNamespace = svcParty.uid.namespace.toProtoPrimitive,
+                )
+            inside(decentralizedNamespaces) { case Seq(decentralizedNamespace) =>
+              decentralizedNamespace.item.owners shouldBe Seq(
+                sv2Backend
+              )
+                .map(_.participantClient.id.uid.namespace)
+                .toSet
+            }
+          }
+        }
+        clue("Stop SV1") {
+          sv1Backend.stop()
+        }
+        actAndCheck(
+          "Onboard SV3",
+          sv3Backend.startSync(),
+        )(
+          "SV3 is now member",
+          _ => {
+            sv2Backend
+              .getSvcInfo()
+              .svcRules
+              .payload
+              .members
+              .keySet() should contain theSameElementsAs Seq(sv2Backend, sv3Backend).map(
+              _.getSvcInfo().svParty.toProtoPrimitive
+            )
+          },
+        )
+      }
+  }
+}
