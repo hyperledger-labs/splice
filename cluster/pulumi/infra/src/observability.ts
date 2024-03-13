b5a639ac186687fee4d7270e56258bf5e4e4f9e7
Nicu Reut <nicu.reut@digitalasset.com>
2024-03-13 09:57:34 +0100

    Actually remove deleted alert (#10810)

--- cluster/pulumi/infra/src/observability.ts
+++ cluster/pulumi/infra/src/observability.ts
@@ -339,6 +339,7 @@ function createGrafanaAlerting(namespace: Input<string>) {
           'cometbft_alerts.yaml': readFile('cometbft_alerts.yaml'),
           'automation_alerts.yaml': readFile('automation_alerts.yaml'),
           'sv-status-report_alerts.yaml': readFile('sv-status-report_alerts.yaml'),
+          'deleted_alerts.yaml': readFile('deleted.yaml'),
           'templates.yaml': readFile('templates.yaml')
             .replaceAll('$CLUSTER_BASENAME', CLUSTER_BASENAME)
             .replaceAll('$GRAFANA_EXTERNAL_URL', grafanaExternalUrl),
