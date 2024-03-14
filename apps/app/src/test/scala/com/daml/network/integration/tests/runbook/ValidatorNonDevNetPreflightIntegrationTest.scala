01be97992f8c53a3b138de87ccbec18e507ee0b6
Julien <128390688+julientinguely-da@users.noreply.github.com>
2024-03-14 17:38:30 +0100

    Provide validators with CC on testnet (#10884)

--- apps/app/src/test/scala/com/daml/network/integration/tests/runbook/ValidatorNonDevNetPreflightIntegrationTest.scala
+++ apps/app/src/test/scala/com/daml/network/integration/tests/runbook/ValidatorNonDevNetPreflightIntegrationTest.scala
@@ -4,48 +4,91 @@ import com.daml.network.environment.CNNodeEnvironmentImpl
 import com.daml.network.integration.CNNodeEnvironmentDefinition
 import com.daml.network.integration.tests.CNNodeTests.CNNodeTestConsoleEnvironment
 import com.daml.network.integration.tests.FrontendIntegrationTestWithSharedEnvironment
+import com.daml.network.util.WalletFrontendTestUtil
 import com.digitalasset.canton.integration.BaseEnvironmentDefinition
+import com.digitalasset.canton.topology.PartyId
 
 /** Base for preflight tests running against a deployed non-devnet validator
   */
 abstract class ValidatorNonDevNetPreflightIntegrationTestBase
-    extends FrontendIntegrationTestWithSharedEnvironment("validator-user") {
+    extends FrontendIntegrationTestWithSharedEnvironment("validator-user", "sv1-user")
+    with WalletFrontendTestUtil {
 
   protected val validatorName: String
 
-  protected lazy val validatorUserName = s"admin@${validatorName}.com"
-  protected lazy val validatorUserPassword = sys.env(s"VALIDATOR_WEB_UI_PASSWORD")
+  protected lazy val validatorUserName =
+    s"admin@${if (validatorName != "splitwell") validatorName else "sw"}.com"
+  protected lazy val validatorUserPassword: String = sys.env(s"VALIDATOR_WEB_UI_PASSWORD")
+  private lazy val validatorWalletUiUrl =
+    s"https://wallet.$validatorName.${sys.env("NETWORK_APPS_ADDRESS")}/"
 
-  private lazy val walletUiUrl =
-    s"https://wallet.${validatorName}.${sys.env("NETWORK_APPS_ADDRESS")}/"
+  protected val sv1UserName = s"admin@sv1.com"
+  protected val sv1UserPassword: String = sys.env(s"SV_WEB_UI_PASSWORD")
+  protected val sv1WalletUiUrl = s"https://wallet.sv-1.svc.${sys.env("NETWORK_APPS_ADDRESS")}/"
 
-  // TODO(#8848): Remove this option altogether (should always be true)
-  protected val testBalance: Boolean = true
+  private val minimalBalance = 1_000
+  private val targetBalance = 10_000
 
   override def environmentDefinition
       : BaseEnvironmentDefinition[CNNodeEnvironmentImpl, CNNodeTestConsoleEnvironment] =
     CNNodeEnvironmentDefinition.preflightTopology(
-      this.getClass.getSimpleName()
+      this.getClass.getSimpleName
     )
 
-  "The validator service user's wallet is accessible and has non-zero balance" in { _ =>
-    withFrontEnd("validator-user") { implicit webDriver =>
-      clue(s"Logging in to wallet UI at: ${walletUiUrl}") {
+  "The validator service user's wallet is accessible and has sufficient balance" in { _ =>
+    val (recipient: PartyId, balance: BigDecimal) = withFrontEnd("validator-user") {
+      implicit webDriver =>
+        clue(s"Logging in to wallet UI at: $validatorWalletUiUrl") {
+          completeAuth0LoginWithAuthorization(
+            validatorWalletUiUrl,
+            validatorUserName,
+            validatorUserPassword,
+            // Notably, we expect that the service user is always already onboarded.
+            () => find(id("wallet-balance-cc")) should not be empty,
+          )
+        }
+        val recipient =
+          PartyId.tryFromProtoPrimitive(seleniumText(find(id("logged-in-user"))))
+        clue("Checking that balance is visible and > 0") {
+          val balanceText = find(id("wallet-balance-cc")).value.text.trim
+          (recipient, BigDecimal(balanceText.split(" ").head))
+        }
+    }
+    if (balance <= BigDecimal(targetBalance)) {
+      clue(
+        s"Balance is too low, topping up $validatorName's wallet to have at least $targetBalance CC"
+      ) {
+        sv1TopupValidatorWallet(recipient, targetBalance - balance)
+      }
+      withFrontEnd("validator-user") { implicit webDriver =>
+        val acceptButton = eventually() {
+          findAll(className("transfer-offer")).toSeq.headOption match {
+            case Some(element) =>
+              element.childWebElement(className("transfer-offer-accept"))
+            case None => fail("failed to find transfer offer")
+          }
+        }
+        click on acceptButton
+        eventually() {
+          val balanceText = find(id("wallet-balance-cc")).value.text.trim
+          val balance = BigDecimal(balanceText.split(" ").head)
+          balance should be > BigDecimal(minimalBalance)
+        }
+      }
+    }
+  }
+
+  private def sv1TopupValidatorWallet(recipient: PartyId, amount: BigDecimal): Unit = {
+    withFrontEnd("sv1-user") { implicit webDriver =>
+      clue(s"Logging in to SV1 wallet UI at: $sv1WalletUiUrl") {
         completeAuth0LoginWithAuthorization(
-          walletUiUrl,
-          validatorUserName,
-          validatorUserPassword,
-          // Notably, we expect that the service user is always already onboarded.
+          sv1WalletUiUrl,
+          sv1UserName,
+          sv1UserPassword,
           () => find(id("wallet-balance-cc")) should not be empty,
         )
       }
-      if (testBalance) {
-        clue("Checking that balance is visible and > 0") {
-          val balanceText = find(id("wallet-balance-cc")).value.text.trim
-          val balance = BigDecimal(balanceText.split(" ").head)
-          balance should be > BigDecimal(0)
-        }
-      }
+      createTransferOffer(recipient, amount, 1)
     }
   }
 }
@@ -53,10 +96,14 @@ abstract class ValidatorNonDevNetPreflightIntegrationTestBase
 class RunbookValidatorNonDevNetPreflightIntegrationTest
     extends ValidatorNonDevNetPreflightIntegrationTestBase {
   override protected val validatorName = "validator"
-  override protected val testBalance = false
 }
 
 class Validator1NonDevNetPreflightIntegrationTest
     extends ValidatorNonDevNetPreflightIntegrationTestBase {
   override protected val validatorName = "validator1"
+}
+
+class SplitwellNonDevNetPreflightIntegrationTest
+    extends ValidatorNonDevNetPreflightIntegrationTestBase {
+  override protected val validatorName = "splitwell"
 }
