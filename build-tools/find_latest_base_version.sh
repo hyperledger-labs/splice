7c15d1b5464e9241e34093455273e5250d3be8bf
Oriol Muñoz <oriol.munoz@digitalasset.com>
2024-03-28 15:12:10 +0100

    Fix latest base ignoring timezones (#11374)

--- build-tools/find_latest_base_version.sh
+++ build-tools/find_latest_base_version.sh
@@ -7,22 +7,22 @@ set -euo pipefail
 file_paths=("$REPO_ROOT/nix/canton-sources.json" "$REPO_ROOT/daml/dars.lock" "$REPO_ROOT/cluster/pulumi" "$REPO_ROOT/cluster/helm")
 
 # Find latest commit where canton version or daml lock were modified
-changed_files_commit=$(git log -n 1 --format="%H %cI" -- "${file_paths[@]}")
+changed_files_commit=$(git log -n 1 --format="%H %ct" -- "${file_paths[@]}")
 IFS=" " read -r -a changed_files_arr <<< "$changed_files_commit"
 changed_files_hash=${changed_files_arr[0]}
-changed_files_date=${changed_files_arr[1]}
-# echo "Canton version or daml files were modified on $changed_files_date in commit: $changed_files_hash"
+changed_files_epoch=${changed_files_arr[1]}
+# echo "Canton version or daml files were modified on $changed_files_epoch in commit: $changed_files_hash"
 
 # Find latest commit containing "[breaking]"
-breaking_message_commit=$(git log -n 1 --format="%H %cI" --grep="\[breaking\]")
+breaking_message_commit=$(git log -n 1 --format="%H %ct" --grep="\[breaking\]")
 IFS=" " read -r -a breaking_message_arr <<< "$breaking_message_commit"
 breaking_message_hash=${breaking_message_arr[0]}
-breaking_message_date=${breaking_message_arr[1]}
-# echo "Explicit breaking changes were committed on $breaking_message_date in commit: $breaking_message_hash"
+breaking_message_epoch=${breaking_message_arr[1]}
+# echo "Explicit breaking changes were committed on $breaking_message_epoch in commit: $breaking_message_hash"
 
 # Return whatever is latest as base version
 # echo "Latest base version commit is:"
-if [[ "$breaking_message_date" > "$changed_files_date" ]]; then
+if [[ "$breaking_message_epoch" > "$changed_files_epoch" ]]; then
     echo "$breaking_message_hash"
 else
     echo "$changed_files_hash"
