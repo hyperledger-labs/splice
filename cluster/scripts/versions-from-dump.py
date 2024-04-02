64f451bdfdd21ebd2db9c41a44ebdde47df31771
Oriol Muñoz <oriol.munoz@digitalasset.com>
2024-04-02 18:46:53 +0200

    Refactor cncluster apply & partial_ugprade to write/read from versions file (#11435)

---
+++ cluster/scripts/versions-from-dump.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import json
+import sys
+
+j = json.load(sys.stdin)
+
+result = {}
+resources = j["deployment"]["resources"]
+releases = [r for r in resources if r["type"] == "kubernetes:helm.sh/v3:Release"]
+for release in releases:
+    outputs = release["outputs"]
+    status = outputs["status"]
+    namespace = status["namespace"]
+    chart = status["chart"]
+    appVersion = status["appVersion"]  # Or should we use version?
+    value = result.get(namespace, {})
+    value[chart] = appVersion
+    result[namespace] = value
+
+print(json.dumps(result, sort_keys=True, indent=2))
