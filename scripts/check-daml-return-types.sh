90274134d9d6b5dfb897b78146799ee33d10589b
Itai Segall <itai.segall@digitalasset.com>
2024-04-11 16:12:45 +0200

    static check of daml return types (#11627)

---
+++ scripts/check-daml-return-types.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# shellcheck disable=SC1091
+source "${TOOLS_LIB}/libcli.source"
+
+## Check that all Daml choice return types follow the pattern that choice `X` returns type `XResult`, except for:
+# - tests
+# - splitwell models
+# - `AmuletRules_Transfer` returning `TransferResult` which seems acceptable to keep, given how common it is in the codebase
+outliers=$(rg -IN -e "^[ \\t]*(nonconsuming )?choice " -g '!daml/splitwell' -g '!daml/*test' "${REPO_ROOT}"/daml/  | sed 's/\(nonconsuming \)\?choice //g' | sed 's/ //g' | awk 'BEGIN {FS = ":"} ; {if ($2 != $1"Result" && $1 != $2"_Fetch" && $1 != "AmuletRules_Transfer") print $0 }')
+if [ -n "$outliers" ]; then
+  _error "The following Daml choices have invalid return types:\n$outliers"
+fi
