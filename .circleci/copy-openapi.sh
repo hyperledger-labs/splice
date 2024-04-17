2df3ddf1347f9c74b595855cc5316d1a0997cf86
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2024-04-17 10:29:33 +0200

    Include all openapi specs in tarball (#11732)
    fixes #11614

--- .circleci/copy-openapi.sh
+++ .circleci/copy-openapi.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
-apps=("common" "wallet" "validator" "sv" "splitwell" "scan" "ans" "app-manager")
 mkdir -p /tmp/artifacts
 mkdir -p /tmp/openapi
 cp "apps/common/src/main/openapi/README.md" /tmp/openapi;
-for app in "${apps[@]}"; do
-  cp "apps/$app/src/main/openapi/$app-external.yaml" /tmp/openapi || :
-  cp "apps/$app/src/main/openapi/$app-internal.yaml" /tmp/openapi || :
-done
+find . -ipath '*/openapi/*.yaml' -exec cp '{}' /tmp/openapi \;
 cd /tmp/openapi || exit
 tar -czvf openapi.tar.gz -- *
 mv openapi.tar.gz ../artifacts
