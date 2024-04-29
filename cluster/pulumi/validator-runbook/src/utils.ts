a926d715b1129733ab5c391930c865e71e336589
Oriol Muñoz <oriol.munoz@digitalasset.com>
2024-04-29 16:33:27 +0200

    Fix VALIDATOR_NEW_PARTICIPANT_ID has unexpected value: new-participant (#12035)

--- cluster/pulumi/validator-runbook/src/utils.ts
+++ cluster/pulumi/validator-runbook/src/utils.ts
@@ -10,4 +10,4 @@ export const VALIDATOR_NAMESPACE = config.optionalEnv('VALIDATOR_NAMESPACE') || 
 export const VALIDATOR_PARTY_HINT = config.optionalEnv('VALIDATOR_PARTY_HINT');
 export const VALIDATOR_MIGRATE_PARTY = config.envFlag('VALIDATOR_MIGRATE_PARTY', false);
 
-export const VALIDATOR_NEW_PARTICIPANT_ID = config.envFlag('VALIDATOR_NEW_PARTICIPANT_ID');
+export const VALIDATOR_NEW_PARTICIPANT_ID = config.optionalEnv('VALIDATOR_NEW_PARTICIPANT_ID');
