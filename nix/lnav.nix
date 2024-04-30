7cd11c2b9b03034d47d790e8ae7a792213932923
Nicu Reut <nicu.reut@digitalasset.com>
2024-04-30 09:58:22 +0200

    Upgrade the lnav version (#12046)

--- nix/lnav.nix
+++ nix/lnav.nix
@@ -3,16 +3,16 @@
 # so we use the binary distribution that ships with a compatible pcre version.
 stdenv.mkDerivation rec {
   name = "lnav";
-  version = "0.12.0";
+  version = "0.12.2";
 
   src =
     if stdenv.isDarwin then
       fetchzip {
         url = "https://github.com/tstack/lnav/releases/download/v${version}/lnav-${version}-x86_64-macos.zip";
-        sha256 = "sha256-3+mpfqSu1KigrBrnVZ1s9/9da4tce7xQUgWcmdH9//k=";
+        sha256 = "sha256-3JH7Kflbr6weH7t1YOYqFy7cncZyx+vWntDQ1Ovpd8w=";
       } else fetchzip {
         url = "https://github.com/tstack/lnav/releases/download/v${version}/lnav-${version}-linux-musl-x86_64.zip";
-        sha256 = "sha256-dr1nkOCOyzLq6eP/s2lK2UvpWNe7WIo6v4QF+riVtEo=";
+        sha256 = "sha256-Zahqcior1oWAhDaK1K3u/o6AsLpKuiQxNmGtRb8AEj4=";
       };
 
   installPhase = ''
