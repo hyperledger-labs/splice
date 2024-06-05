78099a69775c4fe3b25fc699ae43afb14d4ef772
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2024-06-05 13:50:33 +0200

    Add check for dars.lock immutability (#12746)
    * Add check for dars.lock immutability
    
    This is a subset of #12559 to catch obviously broken changes like the
    API docs change I messed up.
    
    It has some overlap with the check in the script for conflicting
    hashes but that one can be harmless if we make multiple changes within
    one release becasue we only bump the version number once. This one on
    the other hand should really always be a hard failure.

--- build-tools/dar-lock-checker/src/main/scala/com/daml/network/build_tools/DarLockChecker.scala
+++ build-tools/dar-lock-checker/src/main/scala/com/daml/network/build_tools/DarLockChecker.scala
@@ -2,6 +2,7 @@ package com.daml.network.build_tools
 
 import better.files.*
 import com.daml.lf.archive.{DarDecoder, DarParser}
+import scala.sys.process.*
 
 object DarLockChecker {
   def main(args: Array[String]): Unit = {
@@ -45,8 +46,10 @@ object DarLockChecker {
                   currentHashes,
                 ).mkString(System.lineSeparator())
               )
+            checkDarsLockImmutable()
           case "update" =>
             val _ = File(outputFileName).overwrite(lockStr)
+            checkDarsLockImmutable()
           case _ =>
             printHelpAndError(s"unknown command '$cmd'")
         }
@@ -59,4 +62,40 @@ object DarLockChecker {
     sys.error(
       s"Error: $reason\nSynopsis: dar-mananger (check|update) <outputFile> <inputDar>*"
     )
+
+  private def checkDarsLockImmutable(): Unit = {
+    val lastReleaseNumber = File("LATEST_RELEASE").contentAsString.strip
+    val lastReleaseDarLock =
+      s"git show refs/remotes/origin/release-line-$lastReleaseNumber:daml/dars.lock".!!
+    val lastReleaseDars = parseDarsLock(lastReleaseDarLock)
+    val currentDars = parseDarsLock(File("daml/dars.lock").contentAsString)
+    currentDars.foreach { case (pkg, currentHash) =>
+      lastReleaseDars.get(pkg).foreach { lastReleaseHash =>
+        if (currentHash != lastReleaseHash) {
+          sys.error(
+            s"Package $pkg changed hash from $currentHash to $lastReleaseHash, did you forget to bump the package version?"
+          )
+        }
+      }
+    }
+    lastReleaseDars.keys.foreach { case pkg @ (pkgName, _) =>
+      if (!pkgName.endsWith("-test") && !currentDars.contains(pkg)) {
+        sys.error(s"Package $pkg was in last release but is missing from current release")
+      }
+    }
+  }
+
+  // Parse the contents of the dar lock file into
+  // a map of (package name, package version) -> package id
+  private def parseDarsLock(fileContent: String): Map[(String, String), String] = {
+    fileContent
+      .split("\n")
+      .map { line =>
+        line.split(" ") match {
+          case Array(name, version, hash) => (name, version) -> hash
+          case _ => sys.error(s"Failed to parse line $line")
+        }
+      }
+      .toMap
+  }
 }
