43400840c4ef941fe47644c79059b0a7d68efaf1
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2024-07-02 13:11:40 +0200

    Make find_latest_base_version return the branch instead of the commit (#13227)
    [skip ci]
    
    This just seems simpler, removes the breaking stuff which we wanted to
    remove anyway and hopefully fixes the issues where we deploy the wrong
    version in ciupgrade tests.

--- build-tools/find_latest_base_version.sh
+++ build-tools/find_latest_base_version.sh
@@ -2,17 +2,5 @@
 
 set -euo pipefail
 
-# Get the commit for the latest release
 latest_release=$(cat "LATEST_RELEASE")
-latest_release_hash=$(git rev-parse heads/origin/release-line-"${latest_release}")
-
-# Find latest commit containing "[breaking]"
-breaking_message_hash=$(git log -n 1 --format="%H" --grep="\[breaking\]")
-# echo "Explicit breaking changes were committed in commit: $breaking_message_hash"
-
-# Return whatever is latest as base version
-if git merge-base --is-ancestor "$latest_release_hash" "$breaking_message_hash"; then
-    echo "$breaking_message_hash"
-else
-    echo "$latest_release_hash"
-fi
+echo "release-line-${latest_release}"
