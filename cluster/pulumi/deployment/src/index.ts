1392a4d157e0f98e9c8cbf99b873b680de4b56ad
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2024-07-16 16:43:20 +0200

    Disable validator runbook reset on devnet (#13519)
    part of #13023
    
    just makes it a bit easier to do the reset if the operator does not
    constantly try to redeploy stuff.
    
    tracked reenabling in #13517

--- cluster/pulumi/deployment/src/index.ts
+++ cluster/pulumi/deployment/src/index.ts
@@ -3,13 +3,13 @@ import { config } from 'cn-pulumi-common';
 import { createStackCR } from './stacks/stack';
 
 if (config.envFlag('CN_DEPLOY_SV_RUNBOOK', false)) {
-  createStackCR('sv-runbook', true);
+  createStackCR('sv-runbook', config.envFlag('SUPPORTS_SV_RUNBOOK_RESET'));
 }
 if (config.envFlag('CN_DEPLOY_MULTI_VALIDATOR', false)) {
   createStackCR('multi-validator', false);
 }
 if (config.envFlag('CN_DEPLOY_VALIDATOR_RUNBOOK', false)) {
-  createStackCR('validator-runbook', true);
+  createStackCR('validator-runbook', config.envFlag('SUPPORTS_VALIDATOR_RUNBOOK_RESET'));
 }
 
 export const infraStack = createStackCR('infra', false);
