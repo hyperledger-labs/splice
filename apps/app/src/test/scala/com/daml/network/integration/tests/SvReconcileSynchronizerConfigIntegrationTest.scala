f0504e1d3873e71f3c7859941acb49f437983b77
Julien <128390688+julientinguely-da@users.noreply.github.com>
2024-07-19 16:55:05 +0200

    Fix SvReconcileSynchronizerConfigIntegrationTest by bumping timeUntilSuccess (#13622)

--- apps/app/src/test/scala/com/daml/network/integration/tests/SvReconcileSynchronizerConfigIntegrationTest.scala
+++ apps/app/src/test/scala/com/daml/network/integration/tests/SvReconcileSynchronizerConfigIntegrationTest.scala
@@ -14,6 +14,7 @@ import com.daml.network.util.AmuletConfigSchedule
 
 import java.time.Instant
 import java.time.temporal.ChronoUnit
+import scala.concurrent.duration.*
 
 class SvReconcileSynchronizerConfigIntegrationTest extends SvIntegrationTestBase {
 
@@ -62,7 +63,7 @@ class SvReconcileSynchronizerConfigIntegrationTest extends SvIntegrationTestBase
         )
       )
     )
-    actAndCheck(
+    actAndCheck(timeUntilSuccess = 30.seconds)(
       "Voting on a AmuletRules config change for new fees config", {
         val (_, voteRequest) = actAndCheck(
           "Creating vote request",
