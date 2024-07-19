a0e873c6d82c0d1f91aa85d86f55e9d02dc8e968
Nicu Reut <nicu.reut@digitalasset.com>
2024-07-19 10:12:51 +0200

    Downgrade lnav to 0.12.0 (#13609)
    Reason: https://github.com/tstack/lnav/issues/1282

--- nix/lnav.nix
+++ nix/lnav.nix
@@ -3,16 +3,16 @@
 # so we use the binary distribution that ships with a compatible pcre version.
 stdenv.mkDerivation rec {
   name = "lnav";
-  version = "0.12.2";
+  version = "0.12.0";
 
   src =
     if stdenv.isDarwin then
       fetchzip {
         url = "https://github.com/tstack/lnav/releases/download/v${version}/lnav-${version}-x86_64-macos.zip";
-        sha256 = "sha256-mf7pjOd0lNVb71PZzwusesf6hMhz3I66apZMk8XMSPE";
+        sha256 = "sha256-3+mpfqSu1KigrBrnVZ1s9/9da4tce7xQUgWcmdH9//k=";
       } else fetchzip {
         url = "https://github.com/tstack/lnav/releases/download/v${version}/lnav-${version}-linux-musl-x86_64.zip";
-        sha256 = "sha256-Zahqcior1oWAhDaK1K3u/o6AsLpKuiQxNmGtRb8AEj4=";
+        sha256 = "sha256-dr1nkOCOyzLq6eP/s2lK2UvpWNe7WIo6v4QF+riVtEo=";
       };
 
   installPhase = ''
