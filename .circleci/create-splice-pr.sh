2886076a5ecfebe752ee7e1b93cee47ee683e2f6
Itai Segall <itai.segall@digitalasset.com>
2024-07-23 09:38:54 -0400

    mirror branches in splice (#13674)

---
+++ .circleci/create-splice-pr.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+set -euo pipefail
+
+function push_with_retries() {
+  n=0
+  until [ "$n" -ge 5 ]; do
+    git push origin HEAD && break
+    n=$((n+1))
+    sleep 5
+  done
+  if [ "$n" -ge 5 ]; then
+    echo "Failed to push to branch $branch"
+    exit 1
+  fi
+}
+
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <splice-dir>"
+  exit 1
+fi
+
+splice_dir=$1
+
+commit=$(git rev-parse --short HEAD)
+
+cd "$splice_dir"
+
+# ssh is not in the path in nix in CCI, so helping git find it
+export GIT_SSH_COMMAND="/usr/bin/ssh"
+
+git config user.email "circleci@circleci.com"
+git config user.name "CircleCI"
+
+branch="splice-cci-update-$(date +%s)"
+base_branch="$CIRCLE_BRANCH"
+if [ "$base_branch" != "main" ]; then
+  remote_branch=$(git ls-remote --heads origin "refs/heads/$base_branch")
+  if [ -z "$remote_branch" ]; then
+    echo "Branch $base_branch does not exist in the remote repository, creating it"
+    git checkout -b "$base_branch"
+    push_with_retries
+  fi
+fi
+
+git checkout -b "$branch"
+git add .
+git commit -m "Update Splice from CCI" --allow-empty
+
+date=$(date +%Y-%m-%d)
+push_with_retries
+
+n=0
+until [ "$n" -ge 5 ]; do
+  gh pr create --title "Update Splice from CCI ($date)" \
+    --body "This PR updates Splice from the latest changes as of commit DACH-NY/canton-network-node@$commit" \
+    --base "$base_branch" \
+    --reviewer isegall-da,cocreature,nicu-da,martinflorian-da,ray-roestenburg-da && break
+   n=$((n+1))
+   sleep 5
+done
+if [ "$n" -ge 5 ]; then
+  echo "Failed to create PR"
+  exit 1
+fi
+
