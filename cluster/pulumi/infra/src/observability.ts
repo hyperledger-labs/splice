90d3af75d25d38f88dcf943d8dada2d8165b6ae5
Nicu Reut <nicu.reut@digitalasset.com>
2024-08-26 16:07:01 +0200

    [backport] Fix grafana no data error name (#14391)

--- cluster/pulumi/infra/src/observability.ts
+++ cluster/pulumi/infra/src/observability.ts
@@ -716,7 +716,7 @@ function substituteSlackNotificationTemplate(file: string) {
 }
 
 function defaultAlertSubstitutions(alert: string): string {
-  return alert.replaceAll('$NODATA', ENABLE_NO_DATA_ALERTS ? 'Error' : 'OK');
+  return alert.replaceAll('$NODATA', ENABLE_NO_DATA_ALERTS ? 'Alerting' : 'OK');
 }
 
 function createGrafanaAlerting(namespace: Input<string>) {
