hyperledger-labs/splice

commit 14dc23628ea38e5b3dbd48b4913b642e06c28449

Merge duplicated validator licenses (#14307)

apps/common/src/main/scala/com/daml/network/environment/DarResources.scala
@@ -16,10 +16,11 @@ object DarResources {
   val amulet_0_1_2 = DarResource("splice-amulet-0.1.2.dar")
   val amulet_0_1_3 = DarResource("splice-amulet-0.1.3.dar")
   val amulet_0_1_4 = DarResource("splice-amulet-0.1.4.dar")
+  val amulet_0_1_5 = DarResource("splice-amulet-0.1.5.dar")
   val amulet_current = DarResource("splice-amulet-current.dar")
   val amulet = PackageResource(
     amulet_current,
-    Seq(amulet_0_1_0, amulet_0_1_1, amulet_0_1_2, amulet_0_1_3, amulet_0_1_4),
+    Seq(amulet_0_1_0, amulet_0_1_1, amulet_0_1_2, amulet_0_1_3, amulet_0_1_4, amulet_0_1_5),
   )
 
   val dsoGovernance_0_1_0 = DarResource("splice-dso-governance-0.1.0.dar")
@@ -30,6 +31,7 @@ object DarResources {
   val dsoGovernance_0_1_5 = DarResource("splice-dso-governance-0.1.5.dar")
   val dsoGovernance_0_1_6 = DarResource("splice-dso-governance-0.1.6.dar")
   val dsoGovernance_0_1_7 = DarResource("splice-dso-governance-0.1.7.dar")
+  val dsoGovernance_0_1_8 = DarResource("splice-dso-governance-0.1.8.dar")
   val dsoGovernance_current = DarResource("splice-dso-governance-current.dar")
   val dsoGovernance = PackageResource(
     dsoGovernance_current,
@@ -42,6 +44,7 @@ object DarResources {
       dsoGovernance_0_1_5,
       dsoGovernance_0_1_6,
       dsoGovernance_0_1_7,
+      dsoGovernance_0_1_8,
     ),
   )
 
@@ -50,6 +53,7 @@ object DarResources {
   val amuletNameService_0_1_2 = DarResource("splice-amulet-name-service-0.1.2.dar")
   val amuletNameService_0_1_3 = DarResource("splice-amulet-name-service-0.1.3.dar")
   val amuletNameService_0_1_4 = DarResource("splice-amulet-name-service-0.1.4.dar")
+  val amuletNameService_0_1_5 = DarResource("splice-amulet-name-service-0.1.5.dar")
   val amuletNameService_current = DarResource("splice-amulet-name-service-current.dar")
   val amuletNameService = PackageResource(
     amuletNameService_current,
@@ -59,6 +63,7 @@ object DarResources {
       amuletNameService_0_1_2,
       amuletNameService_0_1_3,
       amuletNameService_0_1_4,
+      amuletNameService_0_1_5,
     ),
   )
 
@@ -67,21 +72,30 @@ object DarResources {
   val splitwell_0_1_2 = DarResource("splitwell-0.1.2.dar")
   val splitwell_0_1_3 = DarResource("splitwell-0.1.3.dar")
   val splitwell_0_1_4 = DarResource("splitwell-0.1.4.dar")
+  val splitwell_0_1_5 = DarResource("splitwell-0.1.5.dar")
   val splitwell_current = DarResource("splitwell-current.dar")
   val splitwell = PackageResource(
     splitwell_current,
-    Seq(splitwell_0_1_0, splitwell_0_1_1, splitwell_0_1_2, splitwell_0_1_3, splitwell_0_1_4),
+    Seq(
+      splitwell_0_1_0,
+      splitwell_0_1_1,
+      splitwell_0_1_2,
+      splitwell_0_1_3,
+      splitwell_0_1_4,
+      splitwell_0_1_5,
+    ),
   )
 
   val wallet_0_1_0 = DarResource("splice-wallet-0.1.0.dar")
   val wallet_0_1_1 = DarResource("splice-wallet-0.1.1.dar")
   val wallet_0_1_2 = DarResource("splice-wallet-0.1.2.dar")
   val wallet_0_1_3 = DarResource("splice-wallet-0.1.3.dar")
-  val wallet_0_1_4 = DarResource("splice-wallet-0.1.4.dar")
+  val wallet_0_1_4 = DarResource("splice-wallet-0.1.5.dar")
+  val wallet_0_1_5 = DarResource("splice-wallet-0.1.5.dar")
   val wallet_current = DarResource("splice-wallet-current.dar")
   val wallet = PackageResource(
     wallet_current,
-    Seq(wallet_0_1_0, wallet_0_1_1, wallet_0_1_2, wallet_0_1_3, wallet_0_1_4),
+    Seq(wallet_0_1_0, wallet_0_1_1, wallet_0_1_2, wallet_0_1_3, wallet_0_1_4, wallet_0_1_5),
   )
 
   val walletPayments_0_1_0 = DarResource("splice-wallet-payments-0.1.0.dar")
@@ -89,6 +103,7 @@ object DarResources {
   val walletPayments_0_1_2 = DarResource("splice-wallet-payments-0.1.2.dar")
   val walletPayments_0_1_3 = DarResource("splice-wallet-payments-0.1.3.dar")
   val walletPayments_0_1_4 = DarResource("splice-wallet-payments-0.1.4.dar")
+  val walletPayments_0_1_5 = DarResource("splice-wallet-payments-0.1.5.dar")
   val walletPayments_current = DarResource("splice-wallet-payments-current.dar")
   val walletPayments = PackageResource(
     walletPayments_current,
@@ -98,6 +113,7 @@ object DarResources {
       walletPayments_0_1_2,
       walletPayments_0_1_3,
       walletPayments_0_1_4,
+      walletPayments_0_1_5,
     ),
   )
 

apps/app/src/test/scala/com/daml/network/integration/tests/SvMergeDuplicatedValidatorLicenseIntegrationTest.scala
@@ -1,0 +1,77 @@
+package com.daml.network.integration.tests
+
+import com.daml.ledger.javaapi.data.Identifier
+import com.daml.network.codegen.java.splice.validatorlicense.ValidatorLicense
+import com.daml.network.integration.EnvironmentDefinition
+import com.daml.network.sv.automation.delegatebased.MergeValidatorLicenseContractsTrigger
+import com.daml.network.util.TriggerTestUtil
+
+import scala.jdk.CollectionConverters.*
+
+class SvMergeDuplicatedValidatorLicenseIntegrationTest
+    extends SvIntegrationTestBase
+    with TriggerTestUtil {
+
+  override def environmentDefinition =
+    EnvironmentDefinition
+      .simpleTopology1Sv(this.getClass.getSimpleName)
+
+  override protected lazy val updateHistoryIgnoredRootCreates: Seq[Identifier] = Seq(
+    ValidatorLicense.TEMPLATE_ID
+  )
+
+  "Duplicated validator licenses for the same validator get merged" in { implicit env =>
+    val dso = sv1Backend.getDsoInfo().dsoParty
+
+    def getValidatorLicenses() =
+      sv1Backend.participantClientWithAdminToken.ledger_api_extensions.acs
+        .filterJava(ValidatorLicense.COMPANION)(
+          dso,
+          _ => true,
+        )
+        .filter(_.data.validator.contains("digital-asset-2"))
+
+    val validatorLicenses = getValidatorLicenses()
+    validatorLicenses should have size 1
+
+    val validatorLicense = inside(validatorLicenses) { case Seq(validatorLicense) =>
+      validatorLicense
+    }
+    setTriggersWithin(
+      triggersToPauseAtStart =
+        Seq(sv1Backend.dsoDelegateBasedAutomation.trigger[MergeValidatorLicenseContractsTrigger]),
+      triggersToResumeAtStart = Seq.empty,
+    ) {
+      actAndCheck(
+        "Create a duplicate Validator License Contract",
+        sv1Backend.participantClientWithAdminToken.ledger_api_extensions.commands.submitJava(
+          Seq(dso),
+          optTimeout = None,
+          commands = validatorLicense.data.create().commands.asScala.toSeq,
+        ),
+      )(
+        "A second validator license gets created",
+        _ => {
+          val newValidatorLicenses = getValidatorLicenses()
+          newValidatorLicenses should have size 2
+        },
+      )
+      loggerFactory.assertLogs(
+        {
+          sv1Backend.dsoDelegateBasedAutomation
+            .trigger[MergeValidatorLicenseContractsTrigger]
+            .resume()
+          clue("Trigger merges the duplicated validator licenses contracts") {
+            eventually() {
+              val newValidatorLicenses = getValidatorLicenses()
+              newValidatorLicenses should have size 1
+            }
+          }
+        },
+        _.warningMessage should include(
+          "has 2 Validator License contracts."
+        ),
+      )
+    }
+  }
+}

apps/common/src/main/scala/com/daml/network/environment/PackageIdResolver.scala
@@ -252,6 +252,16 @@ object PackageIdResolver {
     dsoGovernanceVersion >= DarResources.dsoGovernance_0_1_5.metadata.version
   }
 
+  def supportsMergeDuplicatedValidatorLicense(
+      now: CantonTimestamp,
+      amuletRules: AmuletRules,
+  ): Boolean = {
+    val currentConfig = AmuletConfigSchedule(amuletRules).getConfigAsOf(now)
+    val dsoGovernanceVersion =
+      PackageVersion.assertFromString(currentConfig.packageConfig.dsoGovernance)
+    dsoGovernanceVersion >= DarResources.dsoGovernance_0_1_8.metadata.version
+  }
+
   def supportsLegacySequencerConfig(
       now: CantonTimestamp,
       amuletRules: AmuletRules,

apps/sv/src/main/scala/com/daml/network/sv/automation/DsoDelegateBasedAutomationService.scala
@@ -69,6 +69,8 @@ class DsoDelegateBasedAutomationService(
     registerTrigger(new TerminatedSubscriptionTrigger(triggerContext, svTaskContext))
     registerTrigger(new MergeSvRewardStateContractsTrigger(triggerContext, svTaskContext))
     registerTrigger(new PruneAmuletConfigScheduleTrigger(triggerContext, svTaskContext))
+
+    registerTrigger(new MergeValidatorLicenseContractsTrigger(triggerContext, svTaskContext))
   }
 
 }
@@ -99,5 +101,6 @@ object DsoDelegateBasedAutomationService extends AutomationServiceCompanion {
     aTrigger[TerminatedSubscriptionTrigger],
     aTrigger[MergeSvRewardStateContractsTrigger],
     aTrigger[PruneAmuletConfigScheduleTrigger],
+    aTrigger[MergeValidatorLicenseContractsTrigger],
   )
 }

apps/sv/src/main/scala/com/daml/network/sv/automation/delegatebased/MergeValidatorLicenseContractsTrigger.scala
@@ -1,0 +1,101 @@
+// Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.daml.network.sv.automation.delegatebased
+
+import com.daml.network.automation.{
+  OnAssignedContractTrigger,
+  TaskOutcome,
+  TaskSuccess,
+  TriggerContext,
+}
+import com.daml.network.codegen.java.splice.dsorules.DsoRules_MergeValidatorLicense
+import com.daml.network.codegen.java.splice.validatorlicense.ValidatorLicense
+import com.daml.network.environment.PackageIdResolver
+import com.daml.network.store.PageLimit
+import com.daml.network.util.{AssignedContract, Contract}
+import com.digitalasset.canton.tracing.TraceContext
+import io.opentelemetry.api.trace.Tracer
+import org.apache.pekko.stream.Materializer
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.jdk.CollectionConverters.*
+
+/** Trigger to merge multiple ValidatorLicenseContracts for the same validator.
+  */
+class MergeValidatorLicenseContractsTrigger(
+    override protected val context: TriggerContext,
+    override protected val svTaskContext: SvTaskBasedTrigger.Context,
+)(implicit
+    override val ec: ExecutionContext,
+    mat: Materializer,
+    tracer: Tracer,
+) extends OnAssignedContractTrigger.Template[ValidatorLicense.ContractId, ValidatorLicense](
+      svTaskContext.dsoStore,
+      ValidatorLicense.COMPANION,
+    )
+    with SvTaskBasedTrigger[AssignedContract[ValidatorLicense.ContractId, ValidatorLicense]] {
+
+  private val store = svTaskContext.dsoStore
+
+  private val MAX_VALIDATOR_LICENSE_CONTRACTS = PageLimit.tryCreate(10)
+
+  override def completeTaskAsDsoDelegate(
+      validatorLicense: AssignedContract[ValidatorLicense.ContractId, ValidatorLicense]
+  )(implicit tc: TraceContext): Future[TaskOutcome] = {
+    val validator = validatorLicense.payload.validator
+    for {
+      amuletRules <- store.getAssignedAmuletRules()
+      supportsPruneAmuletConfigSchedule = PackageIdResolver.supportsMergeDuplicatedValidatorLicense(
+        context.clock.now.minus(context.config.clockSkewAutomationDelay.asJava),
+        amuletRules.payload,
+      )
+      validatorLicenses <-
+        if (supportsPruneAmuletConfigSchedule) {
+          store.listValidatorLicensePerValidator(
+            validator,
+            MAX_VALIDATOR_LICENSE_CONTRACTS,
+          )
+        } else {
+          Future.successful(Seq.empty)
+        }
+      outcome <-
+        if (validatorLicenses.length > 1) {
+          logger.warn(
+            s"Validator $validator has ${validatorLicenses.length} Validator License contracts."
+          )
+          mergeValidatorLicenseContracts(validator, validatorLicenses)
+        } else if (supportsPruneAmuletConfigSchedule) {
+          Future.successful(
+            TaskSuccess(s"Only one Validator License contract for $validator, nothing to merge.")
+          )
+        } else {
+          Future.successful(
+            TaskSuccess(
+              s"Skipping merging Validator License contracts for $validator as the package does not support it."
+            )
+          )
+        }
+    } yield outcome
+  }
+
+  private def mergeValidatorLicenseContracts(
+      validator: String,
+      validatorLicenses: Seq[Contract[ValidatorLicense.ContractId, ValidatorLicense]],
+  )(implicit tc: TraceContext): Future[TaskOutcome] = {
+    for {
+      dsoRules <- store.getDsoRules()
+      arg = new DsoRules_MergeValidatorLicense(
+        validatorLicenses.map(_.contractId).asJava
+      )
+      cmd = dsoRules.exercise(_.exerciseDsoRules_MergeValidatorLicense(arg))
+      _ <- svTaskContext.connection
+        .submit(Seq(store.key.svParty), Seq(store.key.dsoParty), cmd)
+        .noDedup
+        .yieldResult()
+    } yield TaskSuccess(
+      s"Merged ${validatorLicenses.length} ValidatorLicense contracts for $validator"
+    )
+  }
+
+}

apps/sv/src/main/scala/com/daml/network/sv/store/SvDsoStore.scala
@@ -10,8 +10,8 @@ import com.daml.network.automation.MultiDomainExpiredContractTrigger.ListExpired
 import com.daml.network.automation.TransferFollowTrigger.Task as FollowTask
 import com.daml.network.codegen.java.splice.amulet.UnclaimedReward
 import com.daml.network.codegen.java.splice.amuletrules.{
-  AppTransferContext,
   AmuletRules_MiningRound_Archive,
+  AppTransferContext,
 }
 import com.daml.network.codegen.java.splice.types.Round
 import com.daml.network.codegen.java.splice.validatorlicense as vl
@@ -25,13 +25,14 @@ import com.daml.network.codegen.java.splice.dsorules.amuletrules_actionrequiring
 import com.daml.network.codegen.java.splice.dsorules.dsorules_actionrequiringconfirmation.SRARC_ConfirmSvOnboarding
 import com.daml.network.codegen.java.splice.dsorules.{
   ActionRequiringConfirmation,
+  DsoRules_CloseVoteRequestResult,
   DsoRules_ConfirmSvOnboarding,
   VoteRequest,
-  DsoRules_CloseVoteRequestResult,
 }
 import com.daml.network.codegen.java.splice.svonboarding as so
 import com.daml.network.codegen.java.splice.wallet.subscriptions as sub
 import com.daml.network.codegen.java.splice
+import com.daml.network.codegen.java.splice.validatorlicense.ValidatorLicense
 import com.daml.network.environment.{PackageIdResolver, RetryProvider}
 import com.daml.network.migration.DomainMigrationInfo
 import com.daml.network.scan.admin.api.client.ScanConnection.GetAmuletRulesDomain
@@ -699,6 +700,10 @@ trait SvDsoStore
     multiDomainAcsStore
       .listContracts(vl.ValidatorLicense.COMPANION, limit)
       .map(_ map (_.contract))
+
+  def listValidatorLicensePerValidator(validator: String, limit: Limit)(implicit
+      tc: TraceContext
+  ): Future[Seq[Contract[ValidatorLicense.ContractId, ValidatorLicense]]]
 
   def getTotalPurchasedMemberTraffic(memberId: Member, domainId: DomainId)(implicit
       tc: TraceContext

apps/sv/src/main/scala/com/daml/network/sv/store/db/DbSvDsoStore.scala
@@ -860,6 +860,26 @@ class DbSvDsoStore(
     )).getOrRaise(offsetExpectedError())
   }
 
+  override def listValidatorLicensePerValidator(validator: String, limit: Limit)(implicit
+      tc: TraceContext
+  ): Future[Seq[Contract[ValidatorLicense.ContractId, ValidatorLicense]]] =
+    for {
+      result <- storage
+        .query(
+          selectFromAcsTable(
+            DsoTables.acsTableName,
+            storeId,
+            domainMigrationId,
+            where =
+              sql"""template_id_qualified_name = ${QualifiedName(ValidatorLicense.TEMPLATE_ID)}
+              AND validator = ${lengthLimited(validator)}
+            """,
+            orderLimit = sql"""limit ${sqlLimit(limit)}""",
+          ),
+          "listValidatorLicensePerValidator",
+        )
+    } yield result.map(contractFromRow(ValidatorLicense.COMPANION)(_))
+
   override def getTotalPurchasedMemberTraffic(memberId: Member, domainId: DomainId)(implicit
       tc: TraceContext
   ): Future[Long] = waitUntilAcsIngested {

commit 63ea7e1e01475e8b9402b39f0275a10186e6519f

Fix wallet_0_1_4 definition (#14434)
Broken in https://github.com/DACH-NY/canton-network-node/commit/14dc23628ea38e5b3dbd48b4913b642e06c28449#diff-cb85d3cc4a095a86bdd3cadc99d6bcc0fa1bac5195c3f5f6779a24a170a85566R93

Co-authored-by: Moritz Kiefer <moritz.kiefer@purelyfunctional.org>

apps/common/src/main/scala/com/daml/network/environment/DarResources.scala
@@ -90,7 +90,7 @@ object DarResources {
   val wallet_0_1_1 = DarResource("splice-wallet-0.1.1.dar")
   val wallet_0_1_2 = DarResource("splice-wallet-0.1.2.dar")
   val wallet_0_1_3 = DarResource("splice-wallet-0.1.3.dar")
-  val wallet_0_1_4 = DarResource("splice-wallet-0.1.5.dar")
+  val wallet_0_1_4 = DarResource("splice-wallet-0.1.4.dar")
   val wallet_0_1_5 = DarResource("splice-wallet-0.1.5.dar")
   val wallet_current = DarResource("splice-wallet-current.dar")
   val wallet = PackageResource(