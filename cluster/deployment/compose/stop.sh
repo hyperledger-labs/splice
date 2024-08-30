75e0a1c37e9b144554cade3e83c5f62cc2651516
Iggy <64114680+iggy-da@users.noreply.github.com>
2024-08-30 22:25:10 +1000

    Improve validator docker compose definition (best practices, cleanup, practicality for downstream) (#14486)

--- cluster/deployment/compose/stop.sh
+++ cluster/deployment/compose/stop.sh
@@ -26,4 +26,4 @@ export IMAGE_TAG=""
 
 docker compose -f "$script_dir/compose.yaml" down
 
-_info "Validator stopped. Note that its data is persisted in ${script_dir}/postgres-data, and will be reused if started again."
+_info "Validator stopped. Note that its data is persisted in the postgres-splice volume, and will be reused if started again."
