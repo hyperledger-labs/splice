7518dad48117ac2423b0c2484b61f1fa37d83ab0
Itai Segall <itai.segall@digitalasset.com>
2024-09-18 20:22:19 -0400

    Update SA user details for the code dump (#14877)

--- .circleci/create-splice-pr.sh
+++ .circleci/create-splice-pr.sh
@@ -34,8 +34,8 @@ cd "$splice_dir"
 # ssh is not in the path in nix in CCI, so helping git find it
 export GIT_SSH_COMMAND="/usr/bin/ssh"
 
-git config user.email "circleci@circleci.com"
-git config user.name "CircleCI"
+git config user.email "splice-maintainers@digitalasset.com"
+git config user.name "DA Automation"
 
 branch="splice-cci-update-$(date +%s)"
 base_branch="$CIRCLE_BRANCH"
