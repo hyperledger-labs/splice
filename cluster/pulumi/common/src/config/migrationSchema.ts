8c6764e18725ef1db5acd113cbbb69f451a2aa58
Nicu Reut <nicu.reut@digitalasset.com>
2024-09-25 17:35:21 +0200

    Fix pulumi config to validate only active migration (#15022)

--- cluster/pulumi/common/src/config/migrationSchema.ts
+++ cluster/pulumi/common/src/config/migrationSchema.ts
@@ -1,3 +1,5 @@
+import * as _ from 'lodash';
+import * as util from 'node:util';
 import { z } from 'zod';
 
 import { CHARTS_VERSION, CnChartVersion, parsedVersion } from '../artifacts';
@@ -13,43 +15,29 @@ export const defaultActiveMigration = {
   version: CHARTS_VERSION,
   provider: MigrationProvider.INTERNAL,
 };
+const repositoryValue = spliceEnvConfig.optionalEnv('SPLICE_ARTIFACTS_REPOSITORY');
 
+const migrationVersion = z
+  .string()
+  .optional()
+  .transform<CnChartVersion>((version, ctx) => {
+    if (!version && !CHARTS_VERSION && spliceEnvConfig.optionalEnv('SPLICE_OPERATOR_DEPLOYMENT')) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: `No active version or CHARTS_VERSION specified`,
+      });
+      return z.NEVER;
+    } else {
+      return parsedVersion(version || CHARTS_VERSION, repositoryValue);
+    }
+  });
 export const MigrationInfoSchema = z
   .object({
     id: z
       .number()
       .lt(10, 'Migration id must be less than or equal to 10 as we use in the cometbft ports.')
       .gte(0),
-    version: z
-      .string()
-      .optional()
-      .transform<CnChartVersion>((version, ctx) => {
-        if (CHARTS_VERSION && version && CHARTS_VERSION !== version) {
-          ctx.addIssue({
-            code: z.ZodIssueCode.custom,
-            message: `Specified different active version and CHARTS_VERSION: ${version} - ${CHARTS_VERSION}`,
-          });
-          return z.NEVER;
-        } else {
-          if (
-            !version &&
-            !CHARTS_VERSION &&
-            spliceEnvConfig.optionalEnv('SPLICE_OPERATOR_DEPLOYMENT')
-          ) {
-            ctx.addIssue({
-              code: z.ZodIssueCode.custom,
-              message: `No active version or CHARTS_VERSION specified`,
-            });
-            return z.NEVER;
-          } else {
-            return parsedVersion(
-              version || CHARTS_VERSION,
-              // eslint-disable-next-line no-process-env
-              process.env.SPLICE_ARTIFACTS_REPOSITORY
-            );
-          }
-        }
-      }),
+    version: migrationVersion,
     provider: z.nativeEnum(MigrationProvider),
     releaseReference: z.string().optional(),
   })
@@ -64,6 +52,18 @@ export const SynchronizerMigrationSchema = z
     legacy: NonActiveMigrationSchema,
     active: MigrationInfoSchema.extend({
       migratingFrom: z.number().optional(),
+      version: migrationVersion.transform((version, ctx) => {
+        const parsedChartsVersion = parsedVersion(CHARTS_VERSION, repositoryValue);
+        if (CHARTS_VERSION && !_.isEqual(parsedChartsVersion, version)) {
+          ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: `Specified different active version and CHARTS_VERSION: ${util.inspect(version)} - ${util.inspect(parsedChartsVersion)}`,
+          });
+          return z.NEVER;
+        } else {
+          return version;
+        }
+      }),
     })
       .strict()
       .default(defaultActiveMigration),
