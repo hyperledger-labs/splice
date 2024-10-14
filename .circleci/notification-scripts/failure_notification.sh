3929772202961a25995a73cf34d8b4a0d29b8b7e
Itai Segall <itai.segall@digitalasset.com>
2024-10-14 15:24:56 -0400

    [skip ci] fix install-deps (#15424)

--- .circleci/notification-scripts/failure_notification.sh
+++ .circleci/notification-scripts/failure_notification.sh
@@ -6,5 +6,5 @@ set -eou pipefail
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-"${SCRIPT_DIR}/install-deps.sh"
+"${SCRIPT_DIR}/../install-deps.sh"
 "${SCRIPT_DIR}/failure_notification.py" "$@"
