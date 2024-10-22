928bf297778d97265af3475d01b0c970e9dd0abe
Oriol Muñoz <oriol.munoz@digitalasset.com>
2024-10-22 17:12:36 +0200

    [skip ci] Add logs to failure_notification.py (#15599)

--- .circleci/notification-scripts/failure_notification.py
+++ .circleci/notification-scripts/failure_notification.py
@@ -8,6 +8,10 @@ from failure_github_issue import *
 from failure_notification_args import *
 
 if __name__ == "__main__":
+  print("Parsing arguments")
   args = parse_args()
+  print(f"Creating issue with args {args}")
   gh_url = failure_github_issue(args)
+  print(f"Sending notification, url: {gh_url}")
   slack_notification(args, gh_url)
+  print("Sent notification")
