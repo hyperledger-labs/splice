5cd1fe7bfe73dca3446b6d74869a54fab437c24f
Julien <128390688+julientinguely-da@users.noreply.github.com>
2024-12-02 13:22:21 +0100

    Fix cncluster reset (#16446)

--- cluster/pulumi/pulumiDown.ts
+++ cluster/pulumi/pulumiDown.ts
@@ -5,7 +5,10 @@ import {
 
 import { awaitAllOrThrowAllExceptions, downStack, PulumiAbortController, stack } from './pulumi';
 
-async function runCoreStackDown() {
+import { cancelAllTheStacks, downAllTheStacks } from "./sv-canton/pulumiHelper";
+import { runStacksCancel, runStacksRefresh } from "./pulumiHelper";
+
+async function runStacksDown() {
   const mainStack = await stack('canton-network', 'canton-network', true, {});
   const operations: Promise<void>[] = [];
   const abortController = new PulumiAbortController();
@@ -18,10 +21,45 @@ async function runCoreStackDown() {
     const splitwell = await stack('splitwell', 'splitwell', true, {});
     operations.push(downStack(splitwell, abortController));
   }
+  const multiValidatorStack = await stack('multi-validator', 'multi-validator', true, {});
+  operations.push(downStack(multiValidatorStack, abortController));
+  const svRunbookStack = await stack('sv-runbook', 'sv-runbook', true, {});
+  operations.push(downStack(svRunbookStack, abortController));
+  const validatorRunbookStack = await stack('validator-runbook', 'validator-runbook', true, {});
+  operations.push(downStack(validatorRunbookStack, abortController));
+  const deploymentStack = await stack('deployment', 'deployment', true, {});
+  operations.push(downStack(deploymentStack, abortController));
   await awaitAllOrThrowAllExceptions(operations);
 }
 
-runCoreStackDown().catch(e => {
-  console.error(e);
-  process.exit(1);
-});
+function runAllStacksDown(cancelStacks: boolean = true) {
+  if (cancelStacks) {
+    runStacksCancel().catch(e => {
+      console.error(e);
+    });
+  }
+  runStacksDown().catch(e => {
+    console.error(e);
+    if ("the stack is currently locked" in e) {
+      runStacksCancel().catch(e => {
+        console.error(e);
+      });
+      runStacksRefresh().catch(e => {
+        console.error(e);
+      });
+      runAllStacksDown(false)
+    } else {
+      console.error("Failed uninstalling stack, aborting")
+    }
+    process.exit(1);
+  });
+  cancelAllTheStacks().catch(e => {
+      console.error(e);
+  });
+  downAllTheStacks().catch(e => {
+    console.error(e);
+    process.exit(1);
+  });
+}
+
+runAllStacksDown()
