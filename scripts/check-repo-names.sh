6beb4b6565048ea5e30f37f6a1f81c424347b728
Stephen Compall <stephen.compall@digitalasset.com>
2024-12-10 13:56:48 -0500

    replace 'Canton Network' with 'Splice' or 'Global Synchronizer' in helm descriptions (#16619)
    * refer to Global Synchronizer instead of Splice in a few cases
    
    * remove helm description exclusion from check-repo-names

--- scripts/check-repo-names.sh
+++ scripts/check-repo-names.sh
@@ -36,7 +36,6 @@ function check_patterns_locally() {
     'grafana-dashboards.*("cn"|"cn,daml")' # TODO (#15738) grafana cn query identifiers
     'grafana-dashboards.*postgres_exporter\.json:.*"pge-cn-apps-pg-scan-sv-3"' # TODO (#15746) grafana pulumi pg reference
     'databaseName = "cn_apps"|"cn_apps_reonboard"' # TODO (#15709) rename database to splice_apps
-    '(Chart-template\.yaml|helm/.*/NOTES.txt):.*Canton Network' # TODO (#15710) remove Canton Network from helm descriptions
     '^[^:]+V001__create_schema\.sql:' # TODO (#15491) avoiding changing hashes
     'AUTH0_CN_MANAGEMENT_API_CLIENT_(ID|SECRET)|"dev" => ."AUTH0_CN"' # TODO (#15747) auth0 env names
     '/helm/.*(Values\.ingress\.cns|cns: |name: cns-|- "cns\.)' # TODO (#15739) cns in splice-cluster-ingress-runbook
