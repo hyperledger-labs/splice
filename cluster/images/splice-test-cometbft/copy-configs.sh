765c365a511bbbe6a95c2e1332a76e74a3598d4a
DA Automation <splice-maintainers@digitalasset.com>
2024-12-12 19:32:34 +0000

    Update Splice from CCI
    Signed-off-by: DA Automation <splice-maintainers@digitalasset.com>

---
+++ cluster/images/splice-test-cometbft/copy-configs.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+set -eou pipefail
+
+SRC=$1
+DEST=$2
+
+mkdir -p "$DEST"
+cp -r "$SRC"/* "$DEST"
+
+for i in $(seq 1 4); do
+  sed -i "s/0.0.0.0:2665/0.0.0.0:266$i/g" "$DEST/sv$i/config/config.toml"
+  sed -i 's/sv1:26656/127.0.0.1:26616/g' "$DEST/sv$i/config/config.toml"
+  sed -i "s/sv$i:26656/127.0.0.1:266${i}6/g" "$DEST/sv$i/config/config.toml"
+  cp "$DEST/genesis-multi-validator.json" "$DEST/sv$i/config/genesis.json"
+done
+
+sed -i 's/sv1:26656/127.0.0.1:26616/g' "$DEST/sv2Local/config/config.toml"
+sed -i "s/sv2:26656/127.0.0.1:26656/g" "$DEST/sv2Local/config/config.toml"
+cp "$DEST/genesis-multi-validator.json" "$DEST/sv2Local/config/genesis.json"
---
+++ cluster/images/splice-test-cometbft/local.mk
@@ -0,0 +1,14 @@
+# Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+dir := $(call current_dir)
+
+$(dir)/$(docker-build): $(dir)/configs
+
+$(dir)/clean: $(dir)/clean-configs
+
+$(dir)/clean-configs: $(dir)/configs
+	rm -rfv $<
+
+$(dir)/configs: ${REPO_ROOT}/apps/sv/src/test/resources/cometbft
+	${REPO_ROOT}/cluster/images/splice-test-cometbft/copy-configs.sh $< $@
