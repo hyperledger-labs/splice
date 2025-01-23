d9359d0ffd572e19d9bca1d60eb8d84407e6f9a7
DA Automation <splice-maintainers@digitalasset.com>
2025-01-23 08:22:44 +0000

    Update Splice from CCI
    Signed-off-by: DA Automation <splice-maintainers@digitalasset.com>

--- cluster/images/pulumi-kubernetes-operator/local.mk
+++ cluster/images/pulumi-kubernetes-operator/local.mk
@@ -3,4 +3,8 @@
 
 dir := $(call current_dir)
 
+$(dir)/$(docker-build): $(dir)/target/LICENSE
 $(dir)/$(docker-build): platform_opt := --platform=linux/amd64 --build-arg pulumi_version=${PULUMI_VERSION}
+
+$(dir)/target/LICENSE: LICENSE
+	cp $< $@
