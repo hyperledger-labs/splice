63b8998f7e3ba521149141f3cb2a79500bf56842
DA Automation <splice-maintainers@digitalasset.com>
2025-02-12 12:19:11 +0000

    Update Splice from CCI
    Signed-off-by: DA Automation <splice-maintainers@digitalasset.com>

--- apps/common/src/main/scala/org/lfdecentralizedtrust/splice/environment/MediatorAdminConnection.scala
+++ apps/common/src/main/scala/org/lfdecentralizedtrust/splice/environment/MediatorAdminConnection.scala
@@ -32,7 +32,7 @@ class MediatorAdminConnection(
     loggerFactory: NamedLoggerFactory,
     grpcClientMetrics: GrpcClientMetrics,
     retryProvider: RetryProvider,
-)(implicit ec: ExecutionContextExecutor, tracer: Tracer)
+)(implicit protected val ec: ExecutionContextExecutor, tracer: Tracer)
     extends TopologyAdminConnection(
       config,
       apiLoggingConfig,
@@ -60,7 +60,8 @@ class MediatorAdminConnection(
       MediatorAdministrationCommands.Initialize(
         domainId,
         SequencerConnections.single(sequencerConnection),
-        SequencerConnectionValidation.All,
+        // TODO(#10985) Consider enabling this.
+        SequencerConnectionValidation.Disabled,
       )
     )
 
