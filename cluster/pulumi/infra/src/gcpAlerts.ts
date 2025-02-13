496394c62c64abe38e5663194392d05afb5b6bcb
Martin Florian <martin.florian@digitalasset.com>
2025-02-13 14:48:50 +0100

    Fix Google logs filter (#17656)
    Fixes a typo in #17642

--- cluster/pulumi/infra/src/gcpAlerts.ts
+++ cluster/pulumi/infra/src/gcpAlerts.ts
@@ -89,7 +89,7 @@ resource.labels.namespace_name=~"sv|validator1|multi-validator|splitwell"
 -- TODO(#15720): Don't just ignore this - investigate!
 -(resource.labels.container_name="multi-validator" AND jsonPayload.message=~"Request to.*/accept resulted in a timeout")
 -- TODO(#17636): Our apps can't handle ingesting bursts of transactions after delays due to the record order publisher
--(jsonPayload.message~="signalWhenIngested.* has not completed after .* milliseconds")
+-(jsonPayload.message=~"signalWhenIngested.* has not completed after .* milliseconds")
 ${conditionalString(
   !isMainNet,
   '-- TODO(#17025): Stop ignoring these again once we have topology-aware package selection\n' +
