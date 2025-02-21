f04210aa5385b17913f55063d962329280bc9ad0
Itai Segall <itai.segall@digitalasset.com>
2025-02-21 15:56:58 -0500

    bump images for runners (#17896)
    Co-authored-by: DA Automation <team-canton-network@digitalasset.com>

--- cluster/pulumi/gha/src/runners.ts
+++ cluster/pulumi/gha/src/runners.ts
@@ -496,7 +496,7 @@ function installK8sRunnerScaleSet(
 
   // TODO(#15988): use a release once 0.3.13 is out
   const runnerImage =
-    'digitalasset-canton-network-docker.jfrog.io/digitalasset/splice-test-runner-hook:0.3.13-snapshot.20250221.8377.0.v5b91d6cb';
+    'digitalasset-canton-network-docker.jfrog.io/digitalasset/splice-test-runner-hook:0.3.13-snapshot.20250221.8384.0.v94412fc9';
 
   return new k8s.helm.v3.Release(
     name,
