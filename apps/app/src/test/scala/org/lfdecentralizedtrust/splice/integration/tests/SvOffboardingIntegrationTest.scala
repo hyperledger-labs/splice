0ef0bc7ad388f6fa156da07849fecb83f87d8c69
Raymond Roestenburg <98821776+ray-roestenburg-da@users.noreply.github.com>
2025-03-13 15:16:05 +0100

    Wait a bit longer for the transfer command counter in SvOffboardingIntegrationTest. (#18305)
    Fixes DACH-NY/cn-test-failures#3070
    
    [ci]

--- apps/app/src/test/scala/org/lfdecentralizedtrust/splice/integration/tests/SvOffboardingIntegrationTest.scala
+++ apps/app/src/test/scala/org/lfdecentralizedtrust/splice/integration/tests/SvOffboardingIntegrationTest.scala
@@ -37,6 +37,7 @@ import cats.instances.seq.*
 
 import java.time.Instant
 import java.time.temporal.ChronoUnit
+import scala.concurrent.duration.*
 import scala.jdk.CollectionConverters.CollectionHasAsScala
 import scala.jdk.OptionConverters.RichOptional
 
@@ -238,7 +239,7 @@ class SvOffboardingIntegrationTest
       sv1ScanBackend.lookupTransferCommandCounterByParty(
         sv1Backend.getDsoInfo().svParty
       ) shouldBe None
-      actAndCheck(
+      actAndCheck(timeUntilSuccess = 60.seconds)(
         "Resume ExecuteConfirmedActionTrigger",
         sv1Backend.dsoDelegateBasedAutomation.trigger[ExecuteConfirmedActionTrigger].resume(),
       )(
