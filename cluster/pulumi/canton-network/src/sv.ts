684ebb78862b5a9dd3e6fd3e333da5b107673c9d
Nicu Reut <nicu.reut@digitalasset.com>
2025-04-14 15:17:31 +0200

    [static] Remove dependency on sv1 if inter app dependencies are disabled (#19022)

--- cluster/pulumi/canton-network/src/sv.ts
+++ cluster/pulumi/canton-network/src/sv.ts
@@ -183,7 +183,9 @@ export async function installSvNode(
         : []
     )
     .concat(
-      config.onboarding.type == 'join-with-key' && config.onboarding.sponsorRelease
+      config.onboarding.type == 'join-with-key' &&
+        config.onboarding.sponsorRelease &&
+        spliceConfig.pulumiProjectConfig.interAppsDependencies
         ? [config.onboarding.sponsorRelease]
         : []
     )
