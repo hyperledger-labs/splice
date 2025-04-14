5f98810897f335e381f462a9883de56ef3697297
Stephen Compall <stephen.compall@digitalasset.com>
2025-04-14 10:43:13 -0400

    use consistent output directory when downloading damlc in gen-daml-docs (#18999)
    * define root directory of damlc consistently
    - sometimes we ignore XDG_CACHE_HOME, sometimes we don't
    
    * stricter OS detection

--- docs/gen-daml-docs.sh
+++ docs/gen-daml-docs.sh
@@ -6,28 +6,32 @@
 set -eou pipefail
 
 DOCS_DIR="$SPLICE_ROOT/docs"
+DAMLC_VERSION_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/daml-build/${DAML_COMPILER_VERSION}"
 
 gen_project_docs () (
     echo "(docs) generating $1"
     cd "$SPLICE_ROOT/$1"
     local -a DAML_FILES
     readarray -t DAML_FILES < <(find daml -name '*.daml')
-    "${XDG_CACHE_HOME:-$HOME/.cache}/daml-build/${DAML_COMPILER_VERSION}/damlc/damlc" docs --index-template "$DOCS_DIR/api-templates/$2-index-template.rst" "${DAML_FILES[@]}" --exclude-modules '**.Scripts.**' -f rst -o "$DOCS_DIR/src/app_dev/api/$2"
+    "$DAMLC_VERSION_DIR/damlc/damlc" docs --index-template "$DOCS_DIR/api-templates/$2-index-template.rst" "${DAML_FILES[@]}" --exclude-modules '**.Scripts.**' -f rst -o "$DOCS_DIR/src/app_dev/api/$2"
     # Workaround to fix indentation issues in rst output due to https://github.com/digital-asset/daml/issues/16956
     find "$DOCS_DIR/src/app_dev/api/$2" -name '*.rst' -exec sed -i -z 's!\( *\)\(Controller\\: [^\n]*map\)\n *\([^\n]*\)\n *\([^\n]*\)\n!\1\2\n\1\3\n\1\4\n!g' {} +
 )
 
 ensure_damlc_exists() {
-    if [ ! -f "${XDG_CACHE_HOME:-$HOME/.cache}/daml-build/${DAML_COMPILER_VERSION}/damlc/damlc" ]; then
-        dir=$HOME/.cache/daml-build/${DAML_COMPILER_VERSION}/
-        if [ "$(uname -s)" == "Linux" ]; then
-            os="linux-intel"
-        else
-            os="macos"
-        fi
-        mkdir -p "$dir"
-        curl -sSL --fail -o "$dir"/damlc-"$DAML_COMPILER_VERSION"-"$os".tar.gz https://storage.googleapis.com/daml-binaries/split-releases/"$DAML_COMPILER_VERSION"/damlc-"$DAML_COMPILER_VERSION"-"$os".tar.gz
-        pushd "$dir"
+    if [[ ! -f $DAMLC_VERSION_DIR/damlc/damlc
+          || ! -d $DAMLC_VERSION_DIR/damlc/resources ]]; then
+        case "$(uname -s)" in
+            Linux) os="linux-intel";;
+            Darwin) os="macos";;
+            *)
+                echo "Unsupported OS for damlc download: $(uname -s)" >&2
+                exit 1;;
+        esac
+        mkdir -p "$DAMLC_VERSION_DIR"
+        curl -sSL --fail -o "$DAMLC_VERSION_DIR"/damlc-"$DAML_COMPILER_VERSION"-"$os".tar.gz \
+            https://storage.googleapis.com/daml-binaries/split-releases/"$DAML_COMPILER_VERSION"/damlc-"$DAML_COMPILER_VERSION"-"$os".tar.gz
+        pushd "$DAMLC_VERSION_DIR"
         tar -zxf damlc-"$DAML_COMPILER_VERSION"-"$os".tar.gz
         popd
     fi
