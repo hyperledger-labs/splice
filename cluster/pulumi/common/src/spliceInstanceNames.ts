04055509751dda774d2d9233540a410d26c79b65
Itai Segall <itai.segall@digitalasset.com>
2025-05-05 09:12:24 -0400

    use PUBLIC_CONFIGS_PATH for reading ui config vals (#19445)

--- cluster/pulumi/common/src/spliceInstanceNames.ts
+++ cluster/pulumi/common/src/spliceInstanceNames.ts
@@ -1,8 +1,8 @@
 import { config } from './config';
-import { loadYamlFromFile, SPLICE_ROOT } from './utils';
+import { loadYamlFromFile, PUBLIC_CONFIGS_PATH } from './utils';
 
 export const spliceInstanceNames = config.envFlag('ENABLE_CN_INSTANCE_NAMES')
-  ? loadYamlFromFile(SPLICE_ROOT + '/cluster/configs/configs/ui-config-values.yaml')
+  ? loadYamlFromFile(PUBLIC_CONFIGS_PATH + '/configs/ui-config-values.yaml')
   : {
       spliceInstanceNames: {
         networkName: 'Splice',
