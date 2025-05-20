ed4067cbcb9dec8a47f631af4bee70697848554f
Itai Segall <itai.segall@digitalasset.com>
2025-05-20 09:08:45 -0400

    Use the Cloud Run wrapper for failure notifications (#19629)

--- .github/actions/scripts/notification-scripts/slack_notification.py
+++ .github/actions/scripts/notification-scripts/slack_notification.py
@@ -1,40 +1,15 @@
 # Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-import os
-import json
-import requests
-import sys
-import re
-from abc import ABC, abstractmethod
 from failure_notification_args import FailureArgs
 
-class SlackNotification(ABC):
-  @abstractmethod
+class SlackNotification():
   def build_msg(self, gh_url: str):
-    pass
+    cluster_text = f" on cluster {self.args.cluster}" if self.args.cluster else ""
+    text=f"""*GHA job {self.args.gha_workflow_name}:{self.args.job_name} {self.args.gha_run_id} failed {cluster_text}:dumpster-fire:.
+    (<{gh_url}|Issue in GitHub>)"""
 
-  def send_notification(self, gh_url: str):
-
-    if not re.match(self.args.branch_pattern, self.args.branch):
-      print(f"Branch {self.args.branch} does not match pattern {self.args.branch_pattern}, skipping notification")
-      sys.exit(0)
-
-    text = self.build_msg(gh_url)
-    msg = {"text": text, "channel": self.args.slack_channel}
-
-    print(f"Notification: {json.dumps(msg)}")
-
-    if not self.args.dry_run:
-      requests.post(
-        "https://slack.com/api/chat.postMessage",
-        json=msg,
-        headers={
-          "Authorization": f"Bearer {os.environ.get('SLACK_ACCESS_TOKEN')}",
-          "Content-Type": "application/json"})
-
+    return text
 
   def __init__(self, args: FailureArgs):
     self.args = args
-
-
