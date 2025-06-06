b0b9c45ff46fffcbf7f2ba78ae5ce6bc4e7ab121
Martin Florian <martin.florian@digitalasset.com>
2025-06-06 16:47:18 +0200

    Restore relevant `cluster/scripts/` (#1032)
    We still reference those in places, such as `cncluster` and CircleCI flows...
    
    And they don't look secret to me; so let's push now to fix things and clean up later.
    
    Fixes https://github.com/DACH-NY/cn-test-failures/issues/4472
    
    Signed-off-by: Martin Florian <martin.florian@digitalasset.com>

---
+++ cluster/scripts/find-recent-backup.sh
@@ -0,0 +1,152 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+set -euo pipefail
+
+# shellcheck disable=SC1091
+source "${TOOLS_LIB}/libcli.source"
+# shellcheck disable=SC1091
+source "${SPLICE_ROOT}/cluster/scripts/utils.source"
+
+function usage() {
+  _info "Usage: $0 <namespace> <migration_id> <internal (true|false)>"
+}
+
+function is_full_backup_kube() {
+  local component_backup_names=$1
+  local expected_components=$2
+
+  # Check if all expected components can be found in the component_backup_names
+  for component in $expected_components; do
+    count=$(echo "$component_backup_names" | grep -c "$component")
+    if [ "$count" -ne 1 ]; then
+      return 1
+    fi
+  done
+
+  return 0
+}
+
+function get_component_backup_names_kube() {
+  local migration_id=$1
+  local run_id=$2
+  component_backup_names=$(kubectl get volumesnapshot -n "$namespace" --sort-by=.metadata.creationTimestamp -o json | jq "[.items[] | select(.metadata.annotations[\"migrationId\"] == \"$migration_id\") | select(.metadata.name | endswith(\"$run_id\"))]" | jq -r '.[].metadata.name // empty' )
+  echo "$component_backup_names"
+}
+
+function latest_full_backup_run_id_kube() {
+  local namespace=$1
+  local migration_id=$2
+  local is_sv=$3
+  local expected_components=$4
+  if [ "$is_sv" == "true" ]; then
+      expected_components="$expected_components cometbft"
+  fi
+
+  local all_run_ids
+  # get all run id postgres
+  all_run_ids=$(kubectl get volumesnapshot -n "$namespace" --sort-by=.metadata.creationTimestamp -o json | jq "[.items[] | select(.metadata.annotations[\"migrationId\"] == \"$migration_id\")]" | jq -r '.[].metadata.name // empty' | grep -o '[^-]*$' | sort -rn | uniq )
+
+  while read -r run_id; do
+    component_backup_names=$(get_component_backup_names_kube "$migration_id" "$run_id")
+    if is_full_backup_kube "$component_backup_names" "$expected_components"; then
+      echo "$run_id"
+      return 0
+    fi
+  done <<< "$all_run_ids"
+  return 1
+}
+
+function latest_full_backup_run_id_gcloud() {
+  local namespace=$1
+  local migration_id=$2
+  local is_sv=$3
+  local expected_components=$4
+  local internal=$5
+  local num_components
+  num_components=$(echo "$expected_components" | wc -w)
+  local stack
+
+  declare -A run_ids_dict
+
+  for component in $expected_components; do
+    stack=$(get_stack_for_namespace_component "$namespace" "$component" "$internal")
+    instance="$(create_component_instance "$component" "$migration_id" "$namespace" "$internal")"
+    local full_component_instance="$namespace-$instance-pg"
+
+    local cloudsql_id
+    cloudsql_id=$(get_cloudsql_id "$full_component_instance" "$stack")
+    # We always create backups with a description field while this field could be missing for automated backups done by Google Cloud.
+    # So we filter those out while looking for the most recent backup.
+    mapfile -t run_ids < <(gcloud sql backups list --instance "$cloudsql_id" --filter=type="ON_DEMAND" --format=json | jq -r '.[] | select(has("description")) | .description')
+    run_ids_dict[$component]="${run_ids[*]}"
+  done
+
+  if [ "$is_sv" == "true" ]; then
+    mapfile -t cometbft_run_ids < <(kubectl get volumesnapshot -n "$namespace" --sort-by=.metadata.creationTimestamp -o json | jq "[.items[] | select(.metadata.annotations[\"migrationId\"] == \"$migration_id\") | select(.metadata.name | contains(\"cometbft\"))]" | jq -r '.[].metadata.name // empty' | grep -o '[^-]*$' | sort -rn | uniq )
+    run_ids_dict["cometbft"]="${cometbft_run_ids[*]}"
+    ((num_components++))
+  fi
+
+  declare -A count_dict
+  for key in "${!run_ids_dict[@]}"; do
+    for value in ${run_ids_dict[$key]}; do
+      if [ -z "${count_dict[$value]+_}" ]; then
+        count_dict[$value]=0
+      fi
+      ((count_dict[$value]++))
+    done
+  done
+
+  largest_run_id=$(for key in "${!count_dict[@]}"; do
+    if [ "${count_dict[$key]}" -eq "$num_components" ]; then
+      echo "$key"
+    fi
+  done | sort -n | tail -n 1)
+
+  echo "$largest_run_id"
+}
+
+function main() {
+  if [ "$#" -lt 3 ]; then
+      usage
+      exit 1
+  fi
+
+  local namespace=$1
+  local migration_id=$2
+  local internal=$3
+
+  case "$namespace" in
+      sv-1|sv-2|sv-3|sv-4)
+          is_sv=true
+          full_instance="$namespace-cn-apps-pg"
+          expected_components="cn-apps sequencer participant mediator"
+          stack=$(get_stack_for_namespace_component "$namespace" "cn-apps" "$internal")
+          ;;
+      *)
+          is_sv=false
+          full_instance="$namespace-validator-pg"
+          expected_components="validator participant"
+          stack=$(get_stack_for_namespace_component "$namespace" "participant" "$internal")
+          ;;
+  esac
+
+  type=$(get_postgres_type "$full_instance" "$stack")
+  # We only check the postgres type of one component and assume other components have the same type.
+  if [ "$type" == "canton:network:postgres" ]; then
+    backup_run_id=$(latest_full_backup_run_id_kube "$namespace" "$migration_id" "$is_sv" "$expected_components")
+    echo "$backup_run_id"
+  elif [ "$type" == "canton:cloud:postgres" ]; then
+    backup_run_id=$(latest_full_backup_run_id_gcloud "$namespace" "$migration_id" "$is_sv" "$expected_components" "$internal")
+    echo "$backup_run_id"
+  elif [ -z "$type" ]; then
+    _error "No postgres instance $full_instance found in stack ${stack}. Is the cluster deployed with split DB instances?"
+  else
+    _error "Unknown postgres type: $type"
+  fi
+}
+
+main "$@"
---
+++ cluster/scripts/vote-for-migration.sh
@@ -0,0 +1,133 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+set -eou pipefail
+
+# Schedules a global domain upgrade in a CI cluster.
+# Used to be implemented as a preflight test, but loading all the environment for it took forever.
+# See #14665
+
+# curl uses --retry-all-errors because some 4xx returned by canton are transient errors that must be retried
+
+function usage() {
+  echo "Usage: ./vote-for-migration.sh <migration_id>"
+}
+
+if [ $# -lt 1 ]; then
+  usage
+  exit 1
+fi
+
+migration_id=$1
+
+echo "Creating vote request for migration id $migration_id"
+
+sv1_token=$(cncluster get_token sv-1 sv)
+
+current_dso_config=$(curl -s --fail-with-body --show-error --retry 10 --retry-delay 10 --retry-all-errors -X GET "https://sv.sv-2.$GCP_CLUSTER_HOSTNAME/api/sv/v0/dso")
+echo "DSO: $current_dso_config"
+current_config=$(echo "$current_dso_config" | jq -r '.dso_rules.contract.payload.config')
+echo "Current config: $current_config"
+
+requester=$(echo "$current_dso_config" | jq '.sv_party_id')
+scheduled_time=$(date -u -d '+80 seconds' '+%Y-%m-%dT%H:%M:%SZ')
+expiration_microseconds='60000000'
+new_config=$(echo "$current_config" | jq '.nextScheduledSynchronizerUpgrade = {"time": "'"$scheduled_time"'", "migrationId": "'"$migration_id"'"}')
+
+data='
+{
+  "requester": '$requester',
+  "action": {
+    "tag": "ARC_DsoRules",
+    "value": {
+      "dsoAction": {
+        "tag": "SRARC_SetConfig",
+        "value": {
+          "newConfig": '"$new_config"'
+        }
+      }
+    }
+  },
+  "url": "No URL",
+  "description": "Triggered via trigger-migration script.",
+  "expiration": {
+    "microseconds": "'$expiration_microseconds'"
+  }
+}
+'
+
+echo "Sending vote request: $data"
+
+curl -s --fail-with-body --show-error --retry 10 --retry-delay 10 --retry-all-errors \
+  -X POST "https://sv.sv-2.$GCP_CLUSTER_HOSTNAME/api/sv/v0/admin/sv/voterequest/create" \
+  -H 'Content-Type: application/json' \
+  -H "Authorization: Bearer $sv1_token" \
+  --data-raw "$data"
+
+vote_requests=$(curl -s --fail-with-body --show-error --retry 10 --retry-delay 10 --retry-all-errors \
+  -X GET "https://sv.sv-2.$GCP_CLUSTER_HOSTNAME/api/sv/v0/admin/sv/voterequests" \
+  -H "Authorization: Bearer $sv1_token")
+echo "Found vote requests: $vote_requests"
+
+vote_request_cid=$(echo "$vote_requests" | jq -r '.dso_rules_vote_requests[0].contract_id')
+
+vote_data='{
+  "vote_request_contract_id":"'"$vote_request_cid"'",
+  "is_accepted":true,
+  "reason_url":"No URL",
+  "reason_description":"Accepted via trigger-migration script"
+}'
+echo "Casting votes on $vote_request_cid with: $vote_data"
+
+
+DSO_SIZE=${DSO_SIZE:-4}
+other_svs=()
+for ((i=2; i<=DSO_SIZE; i++)); do
+  other_svs+=("sv-$i")
+done
+
+for sv in "${other_svs[@]}"
+do
+  token=$(cncluster get_token "$sv" sv)
+  echo "Casting vote on $sv"
+  curl -s --fail-with-body --show-error --retry 10 --retry-delay 10 --retry-all-errors \
+    -X POST "https://sv.$sv-eng.$GCP_CLUSTER_HOSTNAME/api/sv/v0/admin/sv/votes" \
+    -H 'Content-Type: application/json' \
+    -H "Authorization: Bearer $token" \
+    --data-raw "$vote_data"
+done
+
+# poll until the vote has gone through
+# 300 to account for
+# - 60s expiration time of the vote request
+# - 60s buffer because we only set minutes not seconds
+# - 30s polling interval for the trigger to kick in
+# - general slowness
+MAX_RETRIES=300
+retry_count=0
+until [ $retry_count -gt $MAX_RETRIES ]; do
+  echo "Checking whether DSO info has been updated"
+  retry_count=$((retry_count+1))
+
+  # disabling exit on error to allow for retries
+  set +e
+  new_dso_config=$(curl -s -X GET "https://sv.sv-2.$GCP_CLUSTER_HOSTNAME/api/sv/v0/dso")
+  echo "DSO info: $new_dso_config"
+  next_migration_id=$(echo "$new_dso_config" | jq -r '.dso_rules.contract.payload.config.nextScheduledSynchronizerUpgrade.migrationId')
+  set -e
+  echo "Migration id: $next_migration_id"
+
+  if [ "$next_migration_id" == "$migration_id" ]; then
+    echo "Migration has been successfully scheduled"
+    break
+  fi
+
+  if [ $retry_count -gt $MAX_RETRIES ]; then
+    echo "DSO info was not updated after $MAX_RETRIES retries"
+    exit 1
+  fi
+
+  sleep 1
+done
