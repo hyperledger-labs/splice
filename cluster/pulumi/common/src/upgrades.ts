b7b552434aa1af2b2d0fb35c9877cc175e546fea
Nicu Reut <nicu.reut@digitalasset.com>
2025-06-10 17:11:02 +0000

    [static] Remove some repeated env reads from pulumi code
    Cleaning up the pulumi output for easier debugging
    
    Signed-off-by: Nicu Reut <nicu.reut@digitalasset.com>

--- cluster/pulumi/common/src/upgrades.ts
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
-// SPDX-License-Identifier: Apache-2.0
-import { config } from './config';
-
-export function failOnAppVersionMismatch(): boolean {
-  return config.envFlag('FAIL_ON_APP_VERSION_MISMATCH', true);
-}
