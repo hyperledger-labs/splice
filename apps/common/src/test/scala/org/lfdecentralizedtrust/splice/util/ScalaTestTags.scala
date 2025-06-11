75ca5c75b10bc29b0b5b7099af6aa0cd21bdd052
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2025-06-11 08:45:13 +0000

    .
    [ci]
    
    Signed-off-by: Moritz Kiefer <moritz.kiefer@purelyfunctional.org>

--- apps/common/src/test/scala/org/lfdecentralizedtrust/splice/util/ScalaTestTags.scala
+++ apps/common/src/test/scala/org/lfdecentralizedtrust/splice/util/ScalaTestTags.scala
@@ -3,7 +3,7 @@ package org.lfdecentralizedtrust.splice.util
 import org.scalatest.Tag
 
 object Tags {
-  object NoDamlCompatibiltyCheck
+  object NoDamlCompatibilityCheck
       extends Tag("org.lfdecentralizedtrust.splice.util.scalatesttags.NoDamlCompatibilityCheck")
   object Amulet_0_1_9 extends Tag("org.lfdecentralizedtrust.splice.util.scalatesttags.Amulet_0_1_9")
 }
