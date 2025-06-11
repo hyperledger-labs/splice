641eedb9fdde7d5f59675d783251b7006ab830ed
Oriol Muñoz <oriol.munoz@digitalasset.com>
2025-06-11 14:54:05 +0000

    [ci] Allocations create
    Signed-off-by: Oriol Muñoz <oriol.munoz@digitalasset.com>

---
+++ apps/wallet/frontend/src/components/CreateAllocation.tsx
@@ -0,0 +1,301 @@
+// Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+import React, { useState } from 'react';
+import { useWalletClient } from '../contexts/WalletServiceContext';
+import { useMutation } from '@tanstack/react-query';
+import {
+  AllocateAmuletRequest,
+  AllocateAmuletRequestSettlement,
+  AllocateAmuletRequestTransferLeg,
+} from 'wallet-openapi';
+import { Alert, Button, Card, CardContent, Stack, TextField, Typography } from '@mui/material';
+import { DisableConditionally } from '@lfdecentralizedtrust/splice-common-frontend';
+import BftAnsField from './BftAnsField';
+import AmountInput from './AmountInput';
+import { Add, Remove } from '@mui/icons-material';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import dayjs from 'dayjs';
+
+const CreateAllocation: React.FC = () => {
+  const { createAllocation } = useWalletClient();
+  const [error, setError] = useState<object | null>(null);
+  const [allocation, setAllocation] = useState<PartialAllocateAmuletRequest>(emptyForm());
+  const validated = validatedForm(allocation);
+  const createAllocationMutation = useMutation({
+    mutationFn: async () => {
+      return validated && (await createAllocation(validated));
+    },
+    onSuccess: () => setError(null),
+    onError: error => {
+      console.error('Failed to submit allocation', error);
+      setError(error);
+    },
+  });
+
+  return (
+    <Stack mt={4} spacing={4} direction="column" justifyContent="center">
+      <Typography mt={6} variant="h4">
+        Create Allocation
+      </Typography>
+      <Card variant="outlined">
+        <CardContent sx={{ paddingX: '64px' }}>
+          <Stack spacing={1}>
+            {error ? (
+              <Alert severity="error">Failed to submit: {JSON.stringify(error)}</Alert>
+            ) : null}
+            <Typography variant="h6">Transfer Leg ID</Typography>
+            <TextField
+              value={allocation.transfer_leg_id}
+              onChange={event =>
+                setAllocation({ ...allocation, transfer_leg_id: event.target.value })
+              }
+            />
+            <Typography variant="h6">Settlement Ref</Typography>
+            <Stack direction="row" alignItems="center" spacing={2}>
+              <Typography variant="h6">ID</Typography>
+              <TextField
+                value={allocation.settlement.settlement_ref?.id || ''}
+                onChange={event =>
+                  setAllocation({
+                    ...allocation,
+                    settlement: {
+                      ...allocation.settlement,
+                      settlement_ref: {
+                        id: event.target.value,
+                        cid: allocation.settlement.settlement_ref?.cid,
+                      },
+                    },
+                  })
+                }
+              />
+              <Typography variant="h6">Contract ID (optional)</Typography>
+              <TextField
+                value={allocation.settlement.settlement_ref?.cid || ''}
+                onChange={event =>
+                  setAllocation({
+                    ...allocation,
+                    settlement: {
+                      ...allocation.settlement,
+                      settlement_ref: {
+                        id: allocation.settlement.settlement_ref?.id || '',
+                        cid: event.target.value || undefined,
+                      },
+                    },
+                  })
+                }
+              />
+            </Stack>
+            <Typography variant="h6">Recipient</Typography>
+            <BftAnsField
+              name="Receiver"
+              label="Receiver"
+              aria-label="Receiver"
+              id="create-offer-receiver"
+              onPartyChanged={party =>
+                setAllocation({
+                  ...allocation,
+                  transfer_leg: { ...allocation.transfer_leg, receiver: party },
+                })
+              }
+            />
+            <Typography variant="h6">Executor</Typography>
+            <BftAnsField
+              name="Executor"
+              label="Executor"
+              aria-label="Executor"
+              id="create-offer-receiver"
+              onPartyChanged={party =>
+                setAllocation({
+                  ...allocation,
+                  settlement: { ...allocation.settlement, executor: party },
+                })
+              }
+            />
+            <AmountInput
+              ccAmountText={allocation.transfer_leg.amount || ''}
+              setCcAmountText={ccAmountText =>
+                setAllocation({
+                  ...allocation,
+                  transfer_leg: { ...allocation.transfer_leg, amount: ccAmountText },
+                })
+              }
+            />
+            <Typography variant="h6">Settle before</Typography>
+            <DateTimePicker
+              value={dayjs(allocation.settlement.settle_before)}
+              onChange={value =>
+                value &&
+                setAllocation({
+                  ...allocation,
+                  settlement: { ...allocation.settlement, settle_before: value.toDate() },
+                })
+              }
+            />
+            <Typography variant="h6">Allocate before</Typography>
+            <DateTimePicker
+              value={dayjs(allocation.settlement.allocate_before)}
+              onChange={value =>
+                value &&
+                setAllocation({
+                  ...allocation,
+                  settlement: { ...allocation.settlement, allocate_before: value.toDate() },
+                })
+              }
+            />
+            <Typography variant="h6">Settlement meta</Typography>
+            <MetaEditor
+              meta={allocation.settlement.meta || {}}
+              setMeta={meta =>
+                setAllocation({
+                  ...allocation,
+                  settlement: { ...allocation.settlement, meta: meta },
+                })
+              }
+            />
+            <Typography variant="h6">Transfer leg meta</Typography>
+            <MetaEditor
+              meta={allocation.transfer_leg.meta || {}}
+              setMeta={meta =>
+                setAllocation({
+                  ...allocation,
+                  transfer_leg: { ...allocation.transfer_leg, meta: meta },
+                })
+              }
+            />
+            <DisableConditionally
+              conditions={[
+                {
+                  disabled: createAllocationMutation.isPending,
+                  reason: 'Creating allocation...',
+                },
+                {
+                  disabled: !validated,
+                  reason: 'Form is not valid, please check the fields.',
+                },
+              ]}
+            >
+              <Button
+                variant="pill"
+                fullWidth
+                size="large"
+                onClick={() => createAllocationMutation.mutate()}
+              >
+                Send
+              </Button>
+            </DisableConditionally>
+          </Stack>
+        </CardContent>
+      </Card>
+    </Stack>
+  );
+};
+
+interface PartialAllocateAmuletRequest {
+  settlement: Partial<AllocateAmuletRequestSettlement>;
+  transfer_leg_id: string;
+  transfer_leg: Partial<AllocateAmuletRequestTransferLeg>;
+}
+
+function emptyForm(): PartialAllocateAmuletRequest {
+  const now = new Date();
+  return {
+    settlement: {
+      executor: '',
+      meta: {},
+      allocate_before: now,
+      settle_before: now,
+      settlement_ref: {
+        id: '',
+        cid: undefined,
+      },
+    },
+    transfer_leg_id: '',
+    transfer_leg: {
+      amount: '',
+      receiver: '',
+      meta: {},
+    },
+  };
+}
+
+function validatedForm(partial: PartialAllocateAmuletRequest): AllocateAmuletRequest | null {
+  if (
+    !partial.settlement.executor ||
+    !partial.settlement.settlement_ref?.id ||
+    !partial.settlement.allocate_before ||
+    !partial.settlement.settle_before ||
+    !partial.transfer_leg_id ||
+    !partial.transfer_leg.amount ||
+    !partial.transfer_leg.receiver
+  ) {
+    return null;
+  }
+  return {
+    settlement: {
+      executor: partial.settlement.executor,
+      meta: partial.settlement.meta,
+      allocate_before: partial.settlement.allocate_before,
+      settle_before: partial.settlement.settle_before,
+      settlement_ref: {
+        id: partial.settlement.settlement_ref.id,
+        cid: partial.settlement.settlement_ref.cid,
+      },
+    },
+    transfer_leg_id: partial.transfer_leg_id,
+    transfer_leg: {
+      amount: partial.transfer_leg.amount,
+      receiver: partial.transfer_leg.receiver,
+      meta: partial.transfer_leg.meta,
+    },
+  };
+}
+
+export default CreateAllocation;
+
+type Meta = { [key: string]: string };
+const MetaEditor: React.FC<{ meta: Meta; setMeta: (meta: Meta) => void }> = ({ meta, setMeta }) => {
+  const keys = Object.keys(meta);
+  // will always have at least keys(meta).length
+  const [nEntries, setNEntries] = useState(keys.length);
+  const emptyEntries: string[] = Array.from({ length: nEntries - keys.length }).map(() => '');
+  const allEntries = keys.concat(emptyEntries);
+  const deleteEntry = (key: string) => {
+    const newMeta = { ...meta };
+    delete newMeta[key];
+    setMeta(newMeta);
+    setNEntries(nEntries - 1);
+  };
+  return (
+    <Stack direction="column">
+      {allEntries.map((k, idx) => {
+        const key = k || '';
+        const value = meta[key] || '';
+        const updateKey = (newKey: string) => {
+          const newMeta = { ...meta };
+          delete newMeta[key];
+          newMeta[newKey] = value;
+          setMeta(newMeta);
+        };
+        const updateValue = (newValue: string) => setMeta({ ...meta, [key]: newValue });
+        return (
+          <Stack direction="row" key={idx}>
+            <TextField
+              placeholder="key"
+              value={key}
+              onChange={event => updateKey(event.target.value)}
+            />
+            <TextField
+              placeholder="value"
+              value={value}
+              onChange={event => updateValue(event.target.value)}
+            />
+            <Button startIcon={<Remove />} onClick={() => deleteEntry(key)} />
+          </Stack>
+        );
+      })}
+      <Button startIcon={<Add />} onClick={() => setNEntries(nEntries + 1)}>
+        Add Entry
+      </Button>
+    </Stack>
+  );
+};
