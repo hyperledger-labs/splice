331aad0eef8c59577f0fb5f814456e4ff6c58d7c
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2025-06-11 12:08:51 +0000

    add header
    [ci]
    
    Signed-off-by: Moritz Kiefer <moritz.kiefer@purelyfunctional.org>

--- scripts/initial-package-config.py
+++ scripts/initial-package-config.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+# Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 import itertools
 import json
 import semver
