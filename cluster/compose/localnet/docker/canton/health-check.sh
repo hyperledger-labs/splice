af2f4efdc1a2e0e953ce397b0b8859a320fb758a
Peter Kvokacka <peter.kvokacka@digitalasset.com>
2025-06-13 19:15:06 +1000

    Add ans-web-ui for app-user and app-provider. … (#1071)
    ---------
    
    Signed-off-by: Peter Kvokacka <peter.kvokacka@digitalasset.com>

--- cluster/compose/localnet/docker/canton/health-check.sh
+++ cluster/compose/localnet/docker/canton/health-check.sh
@@ -5,14 +5,14 @@
 set -eou pipefail
 
 if [ "$APP_USER_PROFILE" = "on" ]; then
-  echo "Checking 2${CANTON_GRPC_HEALTHCHECK_PORT}"
-  grpcurl -plaintext "localhost:2${CANTON_GRPC_HEALTHCHECK_PORT}" grpc.health.v1.Health/Check
+  echo "Checking 2${CANTON_GRPC_HEALTHCHECK_PORT_SUFFIX}"
+  grpcurl -plaintext "localhost:2${CANTON_GRPC_HEALTHCHECK_PORT_SUFFIX}" grpc.health.v1.Health/Check
 fi
 if [ "$APP_PROVIDER_PROFILE" = "on" ]; then
-  echo "Checking 3${CANTON_GRPC_HEALTHCHECK_PORT}"
-  grpcurl -plaintext "localhost:3${CANTON_GRPC_HEALTHCHECK_PORT}" grpc.health.v1.Health/Check
+  echo "Checking 3${CANTON_GRPC_HEALTHCHECK_PORT_SUFFIX}"
+  grpcurl -plaintext "localhost:3${CANTON_GRPC_HEALTHCHECK_PORT_SUFFIX}" grpc.health.v1.Health/Check
 fi
 if [ "$SV_PROFILE" = "on" ]; then
-  echo "Checking 4${CANTON_GRPC_HEALTHCHECK_PORT}"
-  grpcurl -plaintext "localhost:4${CANTON_GRPC_HEALTHCHECK_PORT}" grpc.health.v1.Health/Check
+  echo "Checking 4${CANTON_GRPC_HEALTHCHECK_PORT_SUFFIX}"
+  grpcurl -plaintext "localhost:4${CANTON_GRPC_HEALTHCHECK_PORT_SUFFIX}" grpc.health.v1.Health/Check
 fi
--- cluster/compose/localnet/docker/splice/health-check.sh
+++ cluster/compose/localnet/docker/splice/health-check.sh
@@ -5,13 +5,13 @@
 set -eou pipefail
 
 if [ "$APP_USER_PROFILE" = "on" ]; then
-  curl -f "http://localhost:2${VALIDATOR_ADMIN_API_PORT}/api/validator/readyz"
+  curl -f "http://localhost:2${VALIDATOR_ADMIN_API_PORT_SUFFIX}/api/validator/readyz"
 fi
 if [ "$APP_PROVIDER_PROFILE" = "on" ]; then
-  curl -f "http://localhost:3${VALIDATOR_ADMIN_API_PORT}/api/validator/readyz"
+  curl -f "http://localhost:3${VALIDATOR_ADMIN_API_PORT_SUFFIX}/api/validator/readyz"
 fi
 if [ "$SV_PROFILE" = "on" ]; then
-  curl -f "http://localhost:4${VALIDATOR_ADMIN_API_PORT}/api/validator/readyz"
+  curl -f "http://localhost:4${VALIDATOR_ADMIN_API_PORT_SUFFIX}/api/validator/readyz"
   curl -f http://localhost:5012/api/scan/readyz
   curl -f http://localhost:5014/api/sv/readyz
 fi
