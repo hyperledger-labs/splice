ab571aa7ebcfd28ad26207543e117a4e78f93528
Nicu Reut <nicu.reut@digitalasset.com>
2025-06-24 14:26:06 +0000

    [static] Remove custom versions for the pulumi operator and multi validators
    This will allow us to remove the env variables with the versions from the prod clusters to simplify updates
    
    Signed-off-by: Nicu Reut <nicu.reut@digitalasset.com>

--- cluster/pulumi/multi-validator/version.ts
+++ cluster/pulumi/multi-validator/version.ts
@@ -1,10 +1,8 @@
 // Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { config, activeVersion } from 'splice-pulumi-common';
+import { activeVersion } from 'splice-pulumi-common';
 
-const OPERATOR_IMAGE_VERSION = config.optionalEnv('OPERATOR_IMAGE_VERSION');
-
-export const Version = OPERATOR_IMAGE_VERSION || versionFromDefault();
+export const Version = versionFromDefault();
 
 function versionFromDefault() {
   if (activeVersion.type == 'remote') {
