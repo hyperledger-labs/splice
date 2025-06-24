b4fe27a15e68715e731cc10e0e52a88ba46d8aad
BuiVanQuangHung <hung.bui@andelhack.com>
2025-06-24 10:20:58 +0700

    feat: improve transfer offer component and remove pending offer

--- apps/wallet/frontend/src/components/SendTransfer.tsx
+++ apps/wallet/frontend/src/components/SendTransfer.tsx
@@ -32,7 +32,7 @@ import useLookupTransferPreapproval from '../hooks/scan-proxy/useLookupTransferP
 import BftAnsField from './BftAnsField';
 import { useFeatureSupport } from '../hooks/useFeatureSupport';
 import AmountInput from './AmountInput';
-import { PendingOffers } from './PendingOffers';
+import { TransferOffers } from './TransferOffers';
 
 const SendTransfer: React.FC = () => {
   const { createTransferOffer, transferPreapprovalSend, createTransferViaTokenStandard } =
@@ -253,7 +253,7 @@ const SendTransfer: React.FC = () => {
           </DisableConditionally>
         </CardContent>
       </Card>
-      <PendingOffers />
+      <TransferOffers mode="received" />
     </Stack>
   );
 };
