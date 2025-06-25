bd0c6c40f550c42024827b481514665c85bd5fe6
Nicu Reut <nicu.reut@digitalasset.com>
2025-06-25 11:49:36 +0200

    [static] Remove charts version and override version (#1218)

--- cluster/pulumi/common/src/config/migrationSchema.ts
+++ cluster/pulumi/common/src/config/migrationSchema.ts
@@ -1,34 +1,29 @@
 // Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
-import * as _ from 'lodash';
-import * as util from 'node:util';
 import { z } from 'zod';
 
-import { CHARTS_VERSION, CnChartVersion, parsedVersion } from '../artifacts';
+import { CnChartVersion, parsedVersion } from '../artifacts';
 import { spliceEnvConfig } from './envConfig';
 
 export const defaultActiveMigration = {
   id: 0,
-  version: CHARTS_VERSION,
+  version: 'local',
   sequencer: {
     enableBftSequencer: false,
   },
 };
 
-const migrationVersion = z
-  .string()
-  .optional()
-  .transform<CnChartVersion>((version, ctx) => {
-    if (!version && !CHARTS_VERSION && spliceEnvConfig.optionalEnv('SPLICE_OPERATOR_DEPLOYMENT')) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.custom,
-        message: `No active version or CHARTS_VERSION specified`,
-      });
-      return z.NEVER;
-    } else {
-      return parsedVersion(version || CHARTS_VERSION);
-    }
-  });
+const migrationVersion = z.string().transform<CnChartVersion>((version, ctx) => {
+  if (version == 'local' && spliceEnvConfig.optionalEnv('SPLICE_OPERATOR_DEPLOYMENT')) {
+    ctx.addIssue({
+      code: z.ZodIssueCode.custom,
+      message: `Using a local version for the operator deployment is not supported.`,
+    });
+    return z.NEVER;
+  } else {
+    return parsedVersion(version);
+  }
+});
 
 export const GitReferenceSchema = z.object({
   repoUrl: z.string(),
@@ -63,18 +58,7 @@ export const SynchronizerMigrationSchema = z
     legacy: MigrationInfoSchema.optional(),
     active: MigrationInfoSchema.extend({
       migratingFrom: z.number().optional(),
-      version: migrationVersion.transform((version, ctx) => {
-        const parsedChartsVersion = parsedVersion(CHARTS_VERSION);
-        if (CHARTS_VERSION && !_.isEqual(parsedChartsVersion, version)) {
-          ctx.addIssue({
-            code: z.ZodIssueCode.custom,
-            message: `Specified different active version and CHARTS_VERSION: ${util.inspect(version)} - ${util.inspect(parsedChartsVersion)}`,
-          });
-          return z.NEVER;
-        } else {
-          return version;
-        }
-      }),
+      version: migrationVersion,
     })
       .strict()
       .default(defaultActiveMigration),
