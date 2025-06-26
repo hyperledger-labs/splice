b03e95b34d40fc21e0720afb6f9d9247a09d4b47
Stephen Compall <stephen.compall@digitalasset.com>
2025-06-26 20:45:27 +0000

    splat outputs, retrieve receivers, uniquify [skip ci]
    Signed-off-by: Stephen Compall <stephen.compall@digitalasset.com>

--- apps/app/src/test/resources/transfer-participants.sql
+++ apps/app/src/test/resources/transfer-participants.sql
@@ -3,19 +3,25 @@ DECLARE
 DECLARE
     migration_id int64;
 
-CREATE TEMP FUNCTION
-    iso_timestamp(iso8601_string string)
-    RETURNS timestamp AS (PARSE_TIMESTAMP('%FT%TZ', iso8601_string));
-
-SET as_of_record_time = iso_timestamp('2025-06-26T00:00:00Z');
+-- configuration parameters
+SET as_of_record_time = PARSE_TIMESTAMP('%FT%TZ', '2025-06-26T00:00:00Z');
 SET migration_id = 3;
 
-SELECT JSON_QUERY_ARRAY(e.argument, '$.record.fields[0].value.record.fields[3].value.list.elements')
-FROM mainnet_da2_scan.scan_sv_1_update_history_exercises e
-WHERE e.choice = 'AmuletRules_Transfer'
-  AND e.template_id_module_name = 'Splice.AmuletRules'
-  AND e.template_id_entity_name = 'AmuletRules'
-  AND (e.migration_id < migration_id
-    OR (e.migration_id = migration_id
-        AND e.record_time <= UNIX_MICROS(as_of_record_time)))
-LIMIT 100;
+WITH daml_TransferOutput_jsons AS (
+       SELECT JSON_QUERY_ARRAY(e.argument,
+                               -- .transfer.outputs[]
+                               '$.record.fields[0].value.record.fields[3].value.list.elements')
+              TransferOutput_array
+       FROM mainnet_da2_scan.scan_sv_1_update_history_exercises e
+       WHERE e.choice = 'AmuletRules_Transfer'
+         AND e.template_id_module_name = 'Splice.AmuletRules'
+         AND e.template_id_entity_name = 'AmuletRules'
+         AND (e.migration_id < migration_id
+           OR (e.migration_id = migration_id
+               AND e.record_time <= UNIX_MICROS(as_of_record_time)))
+       LIMIT 100) -- TODO (DACH-NY/canton-network-internal#703) remove limit for full test
+  SELECT DISTINCT JSON_VALUE(TransferOutput,
+                             -- .receiver
+                             '$.record.fields[0].value.party')
+  FROM daml_TransferOutput_jsons
+       INNER JOIN daml_TransferOutput_jsons.TransferOutput_array AS TransferOutput;
