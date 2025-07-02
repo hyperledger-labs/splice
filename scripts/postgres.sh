2914894183dc312af06fd4e8c6010d621233415c
Divam <dfordivam@gmail.com>
2025-07-02 08:18:01 +0000

    Specify empty value for local postgres socket dir
    This prevents the 'postgres' process from trying to create socket and
    lock file, which may be at a system dir and not accessible to the user
    without priviledged access.
    The socket file is not used by splice testing infrasturcture
    
    Signed-off-by: Divam <dfordivam@gmail.com>

--- scripts/postgres.sh
+++ scripts/postgres.sh
@@ -54,7 +54,7 @@ function pgctl_start() {
   initdb -D "$LOCAL_POSTGRES_DATA_DIRECTORY" -U "$POSTGRES_USER" >> "$LOG_FILE"
 
   echo "Starting local Postgres instance"
-  pg_ctl -D "$LOCAL_POSTGRES_DATA_DIRECTORY" -l "$LOCAL_POSTGRES_LOG_FILE" -o "-c port=$POSTGRES_PORT -c max_connections=16000 -c shared_buffers=4GB -c effective_cache_size=6GB -c maintenance_work_mem=512MB -c checkpoint_completion_target=0.9 -c wal_buffers=16MB -c default_statistics_target=100 -c random_page_cost=1.1 -c effective_io_concurrency=200 -c work_mem=4194kB -c huge_pages=off -c min_wal_size=2GB -c max_wal_size=8GB -c max_worker_processes=4 -c max_parallel_workers_per_gather=2 -c max_parallel_workers=4 -c max_parallel_maintenance_workers=2" start >> "$LOG_FILE"
+  pg_ctl -D "$LOCAL_POSTGRES_DATA_DIRECTORY" -l "$LOCAL_POSTGRES_LOG_FILE" -o "-k '' -c port=$POSTGRES_PORT -c max_connections=16000 -c shared_buffers=4GB -c effective_cache_size=6GB -c maintenance_work_mem=512MB -c checkpoint_completion_target=0.9 -c wal_buffers=16MB -c default_statistics_target=100 -c random_page_cost=1.1 -c effective_io_concurrency=200 -c work_mem=4194kB -c huge_pages=off -c min_wal_size=2GB -c max_wal_size=8GB -c max_worker_processes=4 -c max_parallel_workers_per_gather=2 -c max_parallel_workers=4 -c max_parallel_maintenance_workers=2" start >> "$LOG_FILE"
 }
 
 function docker_stop() {
