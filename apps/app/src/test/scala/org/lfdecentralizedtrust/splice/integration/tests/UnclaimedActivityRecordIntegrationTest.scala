18101251cfd0ff96a0cea557888dc635c96caea7
Jose Velasco - IEU <jose.velasco@intellecteu.com>
2025-07-03 09:25:07 +0000

    cip-0058-scala: simplify integration test
    Signed-off-by: Jose Velasco - IEU <jose.velasco@intellecteu.com>

--- apps/app/src/test/scala/org/lfdecentralizedtrust/splice/integration/tests/UnclaimedActivityRecordIntegrationTest.scala
+++ apps/app/src/test/scala/org/lfdecentralizedtrust/splice/integration/tests/UnclaimedActivityRecordIntegrationTest.scala
@@ -9,17 +9,19 @@ import org.lfdecentralizedtrust.splice.codegen.java.splice.dsorules.actionrequir
 import org.lfdecentralizedtrust.splice.codegen.java.splice.dsorules.dsorules_actionrequiringconfirmation.SRARC_CreateUnallocatedUnclaimedActivityRecord
 import org.lfdecentralizedtrust.splice.codegen.java.splice.dsorules.DsoRules_CreateUnallocatedUnclaimedActivityRecord
 import org.lfdecentralizedtrust.splice.codegen.java.splice.dsorules.UnallocatedUnclaimedActivityRecord
+import org.lfdecentralizedtrust.splice.console.ValidatorAppBackendReference
 import org.lfdecentralizedtrust.splice.integration.EnvironmentDefinition
 import org.lfdecentralizedtrust.splice.sv.automation.delegatebased.{
   AllocateUnallocatedUnclaimedActivityRecordTrigger,
   ExpiredUnallocatedUnclaimedActivityRecordTrigger,
   ExpiredUnclaimedActivityRecordTrigger,
 }
+import org.lfdecentralizedtrust.splice.integration.tests.SpliceTests.SpliceTestConsoleEnvironment
 import org.lfdecentralizedtrust.splice.util.TriggerTestUtil
 import org.lfdecentralizedtrust.splice.util.WalletTestUtil
 import org.lfdecentralizedtrust.splice.wallet.automation.CollectRewardsAndMergeAmuletsTrigger
-
 import com.daml.ledger.javaapi.data.Identifier
+
 import java.time.Instant
 import java.time.temporal.ChronoUnit
 
@@ -37,108 +39,36 @@ class UnclaimedActivityRecordIntegrationTest
     UnclaimedReward.TEMPLATE_ID_WITH_PACKAGE_ID
   )
 
+  private def allocateTriggers(implicit env: SpliceTestConsoleEnvironment): Seq[Trigger] =
+    activeSvs.map(
+      _.dsoDelegateBasedAutomation.trigger[AllocateUnallocatedUnclaimedActivityRecordTrigger]
+    )
+
+  private def expiredUnallocatedTriggers(implicit env: SpliceTestConsoleEnvironment): Seq[Trigger] =
+    activeSvs.map(
+      _.dsoDelegateBasedAutomation.trigger[ExpiredUnallocatedUnclaimedActivityRecordTrigger]
+    )
+
+  private def expiredUnclaimedTriggers(implicit env: SpliceTestConsoleEnvironment): Seq[Trigger] =
+    activeSvs.map(_.dsoDelegateBasedAutomation.trigger[ExpiredUnclaimedActivityRecordTrigger])
+
+  private def mergeAmuletsTrigger(
+      validatorBackend: ValidatorAppBackendReference,
+      userName: String,
+  ): Trigger =
+    validatorBackend
+      .userWalletAutomation(userName)
+      .futureValue
+      .trigger[CollectRewardsAndMergeAmuletsTrigger]
+
   "An UnallocatedUnclaimedActivityRecord gets expired" in { implicit env =>
     val sv1Party = sv1Backend.getDsoInfo().svParty
     val aliceUserName = aliceWalletClient.config.ledgerApiUser
     val aliceParty = aliceValidatorBackend.onboardUser(aliceUserName)
     val amountToMint = 15.0
 
-    def allocateTriggers: Seq[Trigger] =
-     activeSvs.map(_.dsoDelegateBasedAutomation.trigger[AllocateUnallocatedUnclaimedActivityRecordTrigger])
-    def expiredUnallocatedTriggers: Seq[Trigger] =
-     activeSvs.map(_.dsoDelegateBasedAutomation.trigger[ExpiredUnallocatedUnclaimedActivityRecordTrigger])
     setTriggersWithin(
-     triggersToPauseAtStart = allocateTriggers ++ expiredUnallocatedTriggers,
-     triggersToResumeAtStart = Seq.empty,
-    ) {
-     actAndCheck(
-       "Creating vote request", {
-         val action = new ARC_DsoRules(
-           new SRARC_CreateUnallocatedUnclaimedActivityRecord(
-             new DsoRules_CreateUnallocatedUnclaimedActivityRecord(
-               aliceParty.toProtoPrimitive,
-               BigDecimal(amountToMint).bigDecimal,
-               "alice is doing great - vote",
-               Instant.now().plus(5, ChronoUnit.SECONDS),
-             )
-           )
-         )
-         eventuallySucceeds() {
-           sv1Backend.createVoteRequest(
-             sv1Party.toProtoPrimitive,
-             action,
-             "url",
-             "alice is doing great",
-             sv1Backend.getDsoInfo().dsoRules.payload.config.voteRequestTimeout,
-             None,
-           )
-         }
-       },
-     )("vote request and UnallocatedUnclaimedActivityRecord have been created",
-        _ => {
-         sv1Backend.listVoteRequests().loneElement
-          eventually(){
-           sv1Backend.participantClient.ledger_api_extensions.acs
-             .filterJava(UnallocatedUnclaimedActivityRecord.COMPANION)(dsoParty) should not be empty
-          }
-        }
-      )
-    }
-
-    setTriggersWithin(
-     triggersToPauseAtStart = allocateTriggers,
-     triggersToResumeAtStart = Seq.empty,
-    ) {
-      clue("UnallocatedUnclaimedActivityRecord gets archived"){
-        eventually(){
-         sv1Backend.participantClient.ledger_api_extensions.acs
-           .filterJava(UnallocatedUnclaimedActivityRecord.COMPANION)(dsoParty) shouldBe empty
-        }
-      }
-    }
-  }
-
-  "An UnclaimedActivityRecord gets expired" in { implicit env =>
-    val sv1UserId = sv1WalletClient.config.ledgerApiUser
-    val sv1Party = sv1Backend.getDsoInfo().svParty
-    val aliceUserName = aliceWalletClient.config.ledgerApiUser
-    val aliceParty = aliceValidatorBackend.onboardUser(aliceUserName)
-    val amountToMint = 15.0
-    val unclaimedRewardsToMint = Seq(15.0)
-
-    def allocateTriggers: Seq[Trigger] =
-      activeSvs.map(_.dsoDelegateBasedAutomation.trigger[AllocateUnallocatedUnclaimedActivityRecordTrigger])
-    def expiredUnallocatedTriggers: Seq[Trigger] =
-      activeSvs.map(_.dsoDelegateBasedAutomation.trigger[ExpiredUnallocatedUnclaimedActivityRecordTrigger])
-    def expiredUnclaimedTriggers: Seq[Trigger] =
-      activeSvs.map(_.dsoDelegateBasedAutomation.trigger[ExpiredUnclaimedActivityRecordTrigger])
-    // The trigger that merges amulets for alice
-    // Note: using `def`, as the trigger may be destroyed and recreated (when user is offboarded and onboarded)
-    def aliceMergeAmuletsTrigger: Trigger =
-      aliceValidatorBackend
-        .userWalletAutomation(aliceUserName)
-        .futureValue
-        .trigger[CollectRewardsAndMergeAmuletsTrigger]
-
-    actAndCheck(
-      "Mint some unclaimed rewards",
-      unclaimedRewardsToMint.foreach { amount =>
-        createUnclaimedReward(
-          sv1ValidatorBackend.participantClientWithAdminToken,
-          sv1UserId,
-          amount
-        )
-      },
-    )(
-      "Unclaimed rewards gets created",
-      _ => {
-        sv1Backend.participantClient.ledger_api_extensions.acs
-          .filterJava(UnclaimedReward.COMPANION)(dsoParty) should not be empty
-      },
-    )
-
-    setTriggersWithin(
-      triggersToPauseAtStart = allocateTriggers ++ expiredUnallocatedTriggers ++ expiredUnclaimedTriggers,
+      triggersToPauseAtStart = allocateTriggers ++ expiredUnallocatedTriggers,
       triggersToResumeAtStart = Seq.empty,
     ) {
       actAndCheck(
@@ -153,49 +83,106 @@ class UnclaimedActivityRecordIntegrationTest
               )
             )
           )
-          eventuallySucceeds() {
-            sv1Backend.createVoteRequest(
-              sv1Party.toProtoPrimitive,
-              action,
-              "url",
-              "alice is doing great",
-              sv1Backend.getDsoInfo().dsoRules.payload.config.voteRequestTimeout,
-              None,
-            )
-          }
+          sv1Backend.createVoteRequest(
+            sv1Party.toProtoPrimitive,
+            action,
+            "url",
+            "alice is doing great",
+            sv1Backend.getDsoInfo().dsoRules.payload.config.voteRequestTimeout,
+            None,
+          )
         },
-      )("vote request and UnallocatedUnclaimedActivityRecord have been created",
+      )(
+        "vote request and UnallocatedUnclaimedActivityRecord have been created",
         _ => {
-          sv1Backend.listVoteRequests().loneElement
-          eventually(){
-            sv1Backend.participantClient.ledger_api_extensions.acs
-              .filterJava(UnallocatedUnclaimedActivityRecord.COMPANION)(dsoParty) should not be empty
-          }
-        }
+          sv1Backend.participantClient.ledger_api_extensions.acs
+            .filterJava(UnallocatedUnclaimedActivityRecord.COMPANION)(
+              dsoParty
+            ) should not be empty
+        },
       )
     }
 
     setTriggersWithin(
-      triggersToPauseAtStart = expiredUnclaimedTriggers :+ aliceMergeAmuletsTrigger,
+      triggersToPauseAtStart = allocateTriggers,
       triggersToResumeAtStart = Seq.empty,
     ) {
-      clue("UnclaimedActivityRecord has been created"){
-        eventually(){
+      clue("UnallocatedUnclaimedActivityRecord gets archived") {
+        eventually() {
           sv1Backend.participantClient.ledger_api_extensions.acs
-            .filterJava(UnclaimedActivityRecord.COMPANION)(dsoParty) should not be empty
-
-          sv1Backend.participantClient.ledger_api_extensions.acs
-            .filterJava(UnclaimedReward.COMPANION)(dsoParty) shouldBe empty
+            .filterJava(UnallocatedUnclaimedActivityRecord.COMPANION)(dsoParty) shouldBe empty
         }
+      }
+    }
+  }
+
+  "An UnclaimedActivityRecord gets expired" in { implicit env =>
+    val sv1UserId = sv1WalletClient.config.ledgerApiUser
+    val sv1Party = sv1Backend.getDsoInfo().svParty
+    val aliceUserName = aliceWalletClient.config.ledgerApiUser
+    val aliceParty = aliceValidatorBackend.onboardUser(aliceUserName)
+    val amountToMint = 15.0
+    val unclaimedRewardsToMint = Seq(15.0)
+
+    clue("Mint some unclaimed rewards") {
+      unclaimedRewardsToMint.foreach { amount =>
+        createUnclaimedReward(
+          sv1ValidatorBackend.participantClientWithAdminToken,
+          sv1UserId,
+          amount,
+        )
+      }
+      eventually() {
+        sv1Backend.participantClient.ledger_api_extensions.acs
+          .filterJava(UnclaimedReward.COMPANION)(dsoParty) should not be empty
       }
     }
 
     setTriggersWithin(
-      triggersToPauseAtStart = Seq(aliceMergeAmuletsTrigger),
+      triggersToPauseAtStart = mergeAmuletsTrigger(
+        aliceValidatorBackend,
+        aliceUserName,
+      ) +: (expiredUnallocatedTriggers ++ expiredUnclaimedTriggers),
       triggersToResumeAtStart = Seq.empty,
     ) {
-      clue("UnclaimedActivityRecord gets archived"){
-        eventually(){
+      actAndCheck(
+        "Creating vote request", {
+          val action = new ARC_DsoRules(
+            new SRARC_CreateUnallocatedUnclaimedActivityRecord(
+              new DsoRules_CreateUnallocatedUnclaimedActivityRecord(
+                aliceParty.toProtoPrimitive,
+                BigDecimal(amountToMint).bigDecimal,
+                "alice is doing great - vote",
+                Instant.now().plus(5, ChronoUnit.SECONDS),
+              )
+            )
+          )
+          sv1Backend.createVoteRequest(
+            sv1Party.toProtoPrimitive,
+            action,
+            "url",
+            "alice is doing great",
+            sv1Backend.getDsoInfo().dsoRules.payload.config.voteRequestTimeout,
+            None,
+          )
+        },
+      )(
+        "UnclaimedActivityRecord has been created",
+        _ => {
+          sv1Backend.participantClient.ledger_api_extensions.acs
+            .filterJava(UnclaimedActivityRecord.COMPANION)(dsoParty) should not be empty
+          sv1Backend.participantClient.ledger_api_extensions.acs
+            .filterJava(UnclaimedReward.COMPANION)(dsoParty) shouldBe empty
+        },
+      )
+    }
+
+    setTriggersWithin(
+      triggersToPauseAtStart = Seq(mergeAmuletsTrigger(aliceValidatorBackend, aliceUserName)),
+      triggersToResumeAtStart = Seq.empty,
+    ) {
+      clue("UnclaimedActivityRecord gets archived") {
+        eventually() {
           sv1Backend.participantClient.ledger_api_extensions.acs
             .filterJava(UnclaimedActivityRecord.COMPANION)(dsoParty) shouldBe empty
         }
@@ -211,98 +198,71 @@ class UnclaimedActivityRecordIntegrationTest
     val unclaimedRewardsToMint = Seq(12.0, 2.0, 5.0)
     val amountToMint = 15.0
 
-    def allocateTriggers: Seq[Trigger] =
-     activeSvs.map(_.dsoDelegateBasedAutomation.trigger[AllocateUnallocatedUnclaimedActivityRecordTrigger])
-    // The trigger that merges amulets for alice
-    // Note: using `def`, as the trigger may be destroyed and recreated (when user is offboarded and onboarded)
-    def aliceMergeAmuletsTrigger: Trigger =
-     aliceValidatorBackend
-       .userWalletAutomation(aliceUserName)
-       .futureValue
-       .trigger[CollectRewardsAndMergeAmuletsTrigger]
-
     aliceWalletClient.list().amulets should have length 0
 
-    actAndCheck(
-     "Mint some unclaimed rewards",
-     unclaimedRewardsToMint.foreach { amount =>
-       createUnclaimedReward(
-         sv1ValidatorBackend.participantClientWithAdminToken,
-         sv1UserId,
-         amount
-       )
-     },
-    )(
-      "Unclaimed rewards gets created",
-      _ => {
+    clue("Mint some unclaimed rewards") {
+      unclaimedRewardsToMint.foreach { amount =>
+        createUnclaimedReward(
+          sv1ValidatorBackend.participantClientWithAdminToken,
+          sv1UserId,
+          amount,
+        )
+      }
+      eventually() {
         sv1Backend.participantClient.ledger_api_extensions.acs
           .filterJava(UnclaimedReward.COMPANION)(dsoParty) should not be empty
-      },
-    )
-
-    setTriggersWithin(
-     triggersToPauseAtStart = allocateTriggers,
-     triggersToResumeAtStart = Seq.empty,
-    ) {
-      actAndCheck(
-       "Creating vote request", {
-         val action = new ARC_DsoRules(
-           new SRARC_CreateUnallocatedUnclaimedActivityRecord(
-             new DsoRules_CreateUnallocatedUnclaimedActivityRecord(
-               aliceParty.toProtoPrimitive,
-               BigDecimal(amountToMint).bigDecimal,
-               "alice is doing great - vote",
-               Instant.now().plus(1, ChronoUnit.DAYS),
-             )
-           )
-         )
-         eventuallySucceeds() {
-           sv1Backend.createVoteRequest(
-             sv1Party.toProtoPrimitive,
-             action,
-             "url",
-             "alice is doing great",
-             sv1Backend.getDsoInfo().dsoRules.payload.config.voteRequestTimeout,
-             None,
-           )
-         }
-       },
-      )("vote request and UnallocatedUnclaimedActivityRecord have been created",
-        _ => {
-         sv1Backend.listVoteRequests().loneElement
-         eventually(){
-           sv1Backend.participantClient.ledger_api_extensions.acs
-             .filterJava(UnallocatedUnclaimedActivityRecord.COMPANION)(dsoParty) should not be empty
-         }
-        }
-      )
-    }
-
-    setTriggersWithin(
-     triggersToPauseAtStart = Seq(aliceMergeAmuletsTrigger),
-     triggersToResumeAtStart = Seq.empty,
-    ) {
-      clue("UnclaimedActivityRecord has been created"){
-        eventually(){
-         sv1Backend.participantClient.ledger_api_extensions.acs
-           .filterJava(UnclaimedActivityRecord.COMPANION)(dsoParty) should not be empty
-
-         val unclaimedRewardLeftover: BigDecimal =
-           sv1Backend.participantClient.ledger_api_extensions.acs
-             .filterJava(UnclaimedReward.COMPANION)(dsoParty).map(r => scala.math.BigDecimal(r.data.amount)).sum
-         unclaimedRewardLeftover shouldBe BigDecimal(unclaimedRewardsToMint.sum - amountToMint)
-        }
       }
     }
 
-    clue("Amulet gets minted"){
-      eventually(){
+    setTriggersWithin(
+      triggersToPauseAtStart = Seq(mergeAmuletsTrigger(aliceValidatorBackend, aliceUserName)),
+      triggersToResumeAtStart = Seq.empty,
+    ) {
+      actAndCheck(
+        "Creating vote request", {
+          val action = new ARC_DsoRules(
+            new SRARC_CreateUnallocatedUnclaimedActivityRecord(
+              new DsoRules_CreateUnallocatedUnclaimedActivityRecord(
+                aliceParty.toProtoPrimitive,
+                BigDecimal(amountToMint).bigDecimal,
+                "alice is doing great - vote",
+                Instant.now().plus(1, ChronoUnit.DAYS),
+              )
+            )
+          )
+          sv1Backend.createVoteRequest(
+            sv1Party.toProtoPrimitive,
+            action,
+            "url",
+            "alice is doing great",
+            sv1Backend.getDsoInfo().dsoRules.payload.config.voteRequestTimeout,
+            None,
+          )
+        },
+      )(
+        "UnclaimedActivityRecord has been created",
+        _ => {
+          sv1Backend.participantClient.ledger_api_extensions.acs
+            .filterJava(UnclaimedActivityRecord.COMPANION)(dsoParty) should not be empty
+
+          val unclaimedRewardLeftover: BigDecimal =
+            sv1Backend.participantClient.ledger_api_extensions.acs
+              .filterJava(UnclaimedReward.COMPANION)(dsoParty)
+              .map(r => scala.math.BigDecimal(r.data.amount))
+              .sum
+          unclaimedRewardLeftover shouldBe BigDecimal(unclaimedRewardsToMint.sum - amountToMint)
+        },
+      )
+    }
+
+    clue("Amulet gets minted") {
+      eventually() {
         aliceWalletClient.list().amulets should have length 1
       }
     }
 
-    clue("UnclaimedActivityRecord gets archived"){
-      eventually(){
+    clue("UnclaimedActivityRecord gets archived") {
+      eventually() {
         sv1Backend.participantClient.ledger_api_extensions.acs
           .filterJava(UnclaimedActivityRecord.COMPANION)(dsoParty) shouldBe empty
       }
