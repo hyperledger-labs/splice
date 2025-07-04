8fa10723743d83cca446c94ad6abfb34619c7b30
Robert Autenrieth <robert.autenrieth@digitalasset.com>
2025-07-04 13:43:12 +0000

    Disable getLastImportUpdateId when feature flag is off
    [ci]
    
    Signed-off-by: Robert Autenrieth <robert.autenrieth@digitalasset.com>

--- apps/common/src/main/scala/org/lfdecentralizedtrust/splice/store/UpdateHistory.scala
+++ apps/common/src/main/scala/org/lfdecentralizedtrust/splice/store/UpdateHistory.scala
@@ -1713,8 +1713,9 @@ class UpdateHistory(
   def getLastImportUpdateId(
       migrationId: Long
   )(implicit tc: TraceContext): Future[Option[String]] = {
-    storage.query(
-      sql"""
+    if (enableImportUpdateBackfill) {
+      storage.query(
+        sql"""
         select
           -- Note: to make update ids consistent across SVs, we use the contract id as the update id.
           max(c.contract_id)
@@ -1725,8 +1726,11 @@ class UpdateHistory(
           migration_id = $migrationId and
           record_time = ${CantonTimestamp.MinValue}
       """.as[Option[String]].head,
-      s"getLastImportUpdateId",
-    )
+        s"getLastImportUpdateId",
+      )
+    } else {
+      Future.successful(None)
+    }
   }
 
   def getPreviousMigrationId(migrationId: Long)(implicit
