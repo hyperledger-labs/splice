c50354e353aaad81d1b349ad361550b062444fbb
Nicu Reut <nicu.reut@digitalasset.com>
2025-07-04 10:06:38 +0200

    [ci] Add caching to most of the store read methods for scan (#1319)

--- apps/scan/src/main/scala/org/lfdecentralizedtrust/splice/scan/store/db/DbScanStoreMetrics.scala
+++ apps/scan/src/main/scala/org/lfdecentralizedtrust/splice/scan/store/db/DbScanStoreMetrics.scala
@@ -6,14 +6,20 @@ package org.lfdecentralizedtrust.splice.scan.store.db
 import com.daml.metrics.api.MetricHandle.{Gauge, LabeledMetricsFactory}
 import com.daml.metrics.api.MetricQualification.Latency
 import com.daml.metrics.api.{MetricInfo, MetricName, MetricsContext}
+import com.digitalasset.canton.lifecycle.LifeCycle
+import com.digitalasset.canton.logging.NamedLoggerFactory
 import com.digitalasset.canton.metrics.CacheMetrics
 import org.lfdecentralizedtrust.splice.environment.SpliceMetrics
 import org.lfdecentralizedtrust.splice.store.HistoryMetrics
+
+import java.util.concurrent.ConcurrentLinkedQueue
+import scala.jdk.CollectionConverters.CollectionHasAsScala
 
 class DbScanStoreMetrics(
     metricsFactory: LabeledMetricsFactory
 ) extends AutoCloseable {
 
+  private val createdCaches = new ConcurrentLinkedQueue[CacheMetrics]()
   val prefix: MetricName = SpliceMetrics.MetricsPrefix :+ "scan_store"
 
   val earliestAggregatedRound: Gauge[Long] =
@@ -38,14 +44,26 @@ class DbScanStoreMetrics(
       -1L,
     )(MetricsContext.Empty)
 
-  val totalAmuletBalanceCache =
-    new CacheMetrics(prefix :+ "total_amulet_balance_cache", metricsFactory)
-  val svNodeStateCache = new CacheMetrics(prefix :+ "sv_node_state_cache", metricsFactory)
+  def registerNewCacheMetrics(
+      cacheName: String
+  ): CacheMetrics = {
+    val cacheMetrics = new CacheMetrics(cacheName, metricsFactory)
+    createdCaches.add(cacheMetrics)
+    cacheMetrics
+  }
 
   val history = new HistoryMetrics(metricsFactory)(MetricsContext.Empty)
 
   override def close() = {
-    try earliestAggregatedRound.close()
-    finally latestAggregatedRound.close()
+    LifeCycle.close(
+      (Seq(earliestAggregatedRound, latestAggregatedRound) ++
+        createdCaches.asScala
+          .map(cache =>
+            new AutoCloseable {
+              override def close(): Unit = cache.closeAcquired()
+            }
+          )
+          .toSeq)*
+    )(NamedLoggerFactory.root.getTracedLogger(getClass))
   }
 }
