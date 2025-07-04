hyperledger-labs/splice

commit 03c017359fabc019d7c954afe94b78a4f56013d5

Fail hard if migration ID incremented without restoring from migration dump (#1297)
Fixes DACH-NY/canton-network-internal#730

Signed-off-by: Martin Florian <martin.florian@digitalasset.com>

apps/validator/src/main/scala/org/lfdecentralizedtrust/splice/validator/ValidatorApp.scala
@@ -42,7 +42,7 @@ import org.lfdecentralizedtrust.splice.setup.{
   ParticipantInitializer,
   ParticipantPartyMigrator,
 }
-import org.lfdecentralizedtrust.splice.store.AppStoreWithIngestion
+import org.lfdecentralizedtrust.splice.store.{AppStoreWithIngestion, UpdateHistory}
 import org.lfdecentralizedtrust.splice.store.MultiDomainAcsStore.QueryResult
 import org.lfdecentralizedtrust.splice.util.{
   AmuletConfigSchedule,
@@ -151,18 +151,27 @@ class ValidatorApp(
             migrationDump.participant
           )
         case None =>
-          logger.info(
-            "Ensuring participant is initialized"
-          )
-          val cantonIdentifierConfig =
-            ValidatorCantonIdentifierConfig.resolvedNodeIdentifierConfig(config)
-          ParticipantInitializer.ensureParticipantInitializedWithExpectedId(
-            cantonIdentifierConfig.participant,
-            participantAdminConnection,
-            config.participantBootstrappingDump,
-            loggerFactory,
-            retryProvider,
-          )
+          UpdateHistory.getHighestKnownMigrationId(storage).flatMap {
+            case Some(migrationId) if migrationId < config.domainMigrationId =>
+              throw Status.INVALID_ARGUMENT
+                .withDescription(
+                  s"Migration ID was incremented (to ${config.domainMigrationId}) but no migration dump for restoring from was specified."
+                )
+                .asRuntimeException()
+            case _ =>
+              logger.info(
+                "Ensuring participant is initialized"
+              )
+              val cantonIdentifierConfig =
+                ValidatorCantonIdentifierConfig.resolvedNodeIdentifierConfig(config)
+              ParticipantInitializer.ensureParticipantInitializedWithExpectedId(
+                cantonIdentifierConfig.participant,
+                participantAdminConnection,
+                config.participantBootstrappingDump,
+                loggerFactory,
+                retryProvider,
+              )
+          }
       }
     }
   } yield ()

apps/common/src/main/scala/org/lfdecentralizedtrust/splice/store/UpdateHistory.scala
@@ -44,7 +44,7 @@ import com.digitalasset.canton.config.CantonRequireTypes.String256M
 import com.digitalasset.canton.data.CantonTimestamp
 import com.digitalasset.canton.lifecycle.CloseContext
 import com.digitalasset.canton.logging.{NamedLoggerFactory, NamedLogging}
-import com.digitalasset.canton.resource.DbStorage
+import com.digitalasset.canton.resource.{Storage, DbStorage}
 import com.digitalasset.canton.topology.{ParticipantId, PartyId, SynchronizerId}
 import com.digitalasset.canton.tracing.TraceContext
 import com.google.protobuf.ByteString
@@ -2174,6 +2174,33 @@ class UpdateHistory(
 }
 
 object UpdateHistory {
+
+  // Separate method so we can use this without a full UpdateHistory instance.
+  // Since we're interested in the highest known migration id, we don't need to filter by anything
+  // (store ID, participant ID, etc. are not even known at the time we want to call this).
+  def getHighestKnownMigrationId(
+      storage: Storage
+  )(implicit
+      ec: ExecutionContext,
+      closeContext: CloseContext,
+      tc: TraceContext,
+  ): Future[Option[Long]] = {
+    storage match {
+      case storage: DbStorage =>
+        for {
+          queryResult <- storage.query(
+            sql"""
+               select max(migration_id) from update_history_last_ingested_offsets
+            """.as[Option[Long]],
+            "getHighestKnownMigrationId",
+          )
+        } yield {
+          queryResult.headOption.flatten
+        }
+      case storageType => throw new RuntimeException(s"Unsupported storage type $storageType")
+    }
+  }
+
   sealed trait BackfillingRequirement
   object BackfillingRequirement {
 

apps/common/src/test/scala/org/lfdecentralizedtrust/splice/store/UpdateHistoryTest.scala
@@ -690,5 +690,43 @@ class UpdateHistoryTest extends UpdateHistoryTestBase {
         }
       }
     }
+
+    "getHighestKnownMigrationId" should {
+
+      "return None if there is no store" in {
+        UpdateHistory.getHighestKnownMigrationId(storage).futureValue shouldBe None
+      }
+
+      "return None if the store is not initialized" in {
+        mkStore()
+        for {
+          migrationId <- UpdateHistory.getHighestKnownMigrationId(storage)
+        } yield {
+          migrationId shouldBe None
+        }
+      }
+
+      "return a migration id if there is an initialized store" in {
+        val store = mkStore()
+        for {
+          _ <- initStore(store)
+          migrationId <- UpdateHistory.getHighestKnownMigrationId(storage)
+        } yield {
+          migrationId shouldBe Some(migration1)
+        }
+      }
+
+      "return the highest migration id if there are multiple initialized stores" in {
+        val store1 = mkStore(party1, migration1, participant1)
+        val store2 = mkStore(party1, migration2, participant1)
+        for {
+          _ <- initStore(store1)
+          _ <- initStore(store2)
+          migrationId <- UpdateHistory.getHighestKnownMigrationId(storage)
+        } yield {
+          migrationId shouldBe Some(migration2)
+        }
+      }
+    }
   }
 }

apps/sv/src/main/scala/org/lfdecentralizedtrust/splice/sv/SvApp.scala
@@ -32,7 +32,7 @@ import org.lfdecentralizedtrust.splice.http.v0.sv.SvResource
 import org.lfdecentralizedtrust.splice.http.v0.sv_admin.SvAdminResource
 import org.lfdecentralizedtrust.splice.migration.AcsExporter
 import org.lfdecentralizedtrust.splice.setup.{NodeInitializer, ParticipantInitializer}
-import org.lfdecentralizedtrust.splice.store.AppStoreWithIngestion
+import org.lfdecentralizedtrust.splice.store.{AppStoreWithIngestion, UpdateHistory}
 import org.lfdecentralizedtrust.splice.store.MultiDomainAcsStore.QueryResult
 import org.lfdecentralizedtrust.splice.sv.admin.http.{HttpSvAdminHandler, HttpSvHandler}
 import org.lfdecentralizedtrust.splice.sv.automation.{
@@ -160,19 +160,28 @@ class SvApp(
               )
 
             case _ =>
-              logger.info(
-                "Ensuring participant is initialized"
-              )
-              val cantonIdentifierConfig = config.cantonIdentifierConfig.getOrElse(
-                SvCantonIdentifierConfig.default(config)
-              )
-              ParticipantInitializer.ensureParticipantInitializedWithExpectedId(
-                cantonIdentifierConfig.participant,
-                participantAdminConnection,
-                config.participantBootstrappingDump,
-                loggerFactory,
-                retryProvider,
-              )
+              UpdateHistory.getHighestKnownMigrationId(storage).flatMap {
+                case Some(migrationId) if migrationId < config.domainMigrationId =>
+                  throw Status.INVALID_ARGUMENT
+                    .withDescription(
+                      s"Migration ID was incremented (to ${config.domainMigrationId}) but no migration dump for restoring from was specified."
+                    )
+                    .asRuntimeException()
+                case _ =>
+                  logger.info(
+                    "Ensuring participant is initialized"
+                  )
+                  val cantonIdentifierConfig = config.cantonIdentifierConfig.getOrElse(
+                    SvCantonIdentifierConfig.default(config)
+                  )
+                  ParticipantInitializer.ensureParticipantInitializedWithExpectedId(
+                    cantonIdentifierConfig.participant,
+                    participantAdminConnection,
+                    config.participantBootstrappingDump,
+                    loggerFactory,
+                    retryProvider,
+                  )
+              }
           }
         }
     } yield ()).andThen { case _ => participantAdminConnection.close() }

commit ca862c49b0724f666583aa6d4863322d9833198e

Fix migrations for SV validators
Fixes https://github.com/DACH-NY/cn-test-failures/issues/4829

Bug was introduced in https://github.com/hyperledger-labs/splice/pull/1297

This was not caught in our `DecentralizedSynchronizerMigrationIntegrationTest` because we don't migrate SV2's validator there.
Extending the (already slow and complex) test to do so doesn't seem worthwhile to me though; also as we plan to phase out synchronizer migrations entirely in the not-too-long term.

[ci]

Signed-off-by: Martin Florian <martin.florian@digitalasset.com>

apps/validator/src/main/scala/org/lfdecentralizedtrust/splice/validator/ValidatorApp.scala
@@ -152,7 +152,8 @@ class ValidatorApp(
           )
         case None =>
           UpdateHistory.getHighestKnownMigrationId(storage).flatMap {
-            case Some(migrationId) if migrationId < config.domainMigrationId =>
+            case Some(migrationId)
+                if !config.svValidator && migrationId < config.domainMigrationId =>
               throw Status.INVALID_ARGUMENT
                 .withDescription(
                   s"Migration ID was incremented (to ${config.domainMigrationId}) but no migration dump for restoring from was specified."