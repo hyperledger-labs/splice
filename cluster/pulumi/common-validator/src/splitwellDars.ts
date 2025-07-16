cb7d4ffc34e6e355d0b2666d7b66ec835a10820d
Nicu Reut <nicu.reut@digitalasset.com>
2025-07-16 17:33:17 +0000

    [static] .
    Signed-off-by: Nicu Reut <nicu.reut@digitalasset.com>

--- cluster/pulumi/common-validator/src/splitwellDars.ts
+++ cluster/pulumi/common-validator/src/splitwellDars.ts
@@ -1,3 +1,5 @@
+// Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
 import * as fs from 'fs';
 import * as semver from 'semver';
 import { SPLICE_ROOT } from 'splice-pulumi-common';
