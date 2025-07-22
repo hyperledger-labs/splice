767f06aa3c3606ba6d3dcbf2c0e73892b5c75bb3
Julien Tinguely <julien.tinguely@digitalasset.com>
2025-07-22 09:26:56 +0000

    [ci] nit
    Signed-off-by: Julien Tinguely <julien.tinguely@digitalasset.com>

--- apps/validator/src/main/scala/org/lfdecentralizedtrust/splice/validator/ValidatorApp.scala
+++ apps/validator/src/main/scala/org/lfdecentralizedtrust/splice/validator/ValidatorApp.scala
@@ -255,7 +255,6 @@ class ValidatorApp(
                       migrationDump.participantUsers
                     )
                   }
-
                 } yield ()
               case None =>
                 if (config.svValidator)
