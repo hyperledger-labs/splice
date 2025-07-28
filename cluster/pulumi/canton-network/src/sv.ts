da55a6c23c3c3fb26b364e5691098ee9b5971fe3
moritzkiefer-da <45630097+moritzkiefer-da@users.noreply.github.com>
2025-07-28 16:35:25 +0200

    [main] Support custom additionalEnvVars for sv-app (#1702)
    forward port from #1697
    
    [static]
    
    
    
    ---------
    
    Signed-off-by: Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
    Co-authored-by: Moritz Kiefer <moritz.kiefer@purelyfunctional.org>

--- cluster/pulumi/canton-network/src/sv.ts
+++ cluster/pulumi/canton-network/src/sv.ts
@@ -408,6 +408,17 @@ function installSvApp(
   const svDbName = `sv_${sanitizedForPostgres(config.nodeName)}`;
 
   const useCantonBft = decentralizedSynchronizerMigrationConfig.active.sequencer.enableBftSequencer;
+  const topologyChangeDelayEnvVars = svsConfig?.synchronizer?.topologyChangeDelay
+    ? [
+        {
+          name: 'ADDITIONAL_CONFIG_TOPOLOGY_CHANGE_DELAY',
+          value: `canton.sv-apps.sv.topology-change-delay-duration=${svsConfig.synchronizer.topologyChangeDelay}`,
+        },
+      ]
+    : [];
+  const additionalEnvVars = (config.svApp?.additionalEnvVars || []).concat(
+    topologyChangeDelayEnvVars
+  );
   const svValues = {
     ...decentralizedSynchronizerMigrationConfig.migratingNodeConfig(),
     ...spliceInstanceNames,
@@ -492,14 +503,7 @@ function installSvApp(
     expectedTaskDuration: expectedTaskDuration,
     expiredRewardCouponBatchSize: expiredRewardCouponBatchSize,
     logLevel: config.logging?.appsLogLevel,
-    additionalEnvVars: svsConfig?.synchronizer?.topologyChangeDelay
-      ? [
-          {
-            name: 'ADDITIONAL_CONFIG_TOPOLOGY_CHANGE_DELAY',
-            value: `canton.sv-apps.sv.topology-change-delay-duration=${svsConfig.synchronizer.topologyChangeDelay}`,
-          },
-        ]
-      : undefined,
+    additionalEnvVars,
   } as ChartValues;
 
   if (config.onboarding.type == 'join-with-key') {
