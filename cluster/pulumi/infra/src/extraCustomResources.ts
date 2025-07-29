c73fa35de83dcdc6c242e2d35cd1b04f2d7ac379
Martin Florian <martin.florian@digitalasset.com>
2025-07-29 10:20:07 +0000

    Use list for namespaces and fix header
    [static]
    
    Signed-off-by: Martin Florian <martin.florian@digitalasset.com>

--- cluster/pulumi/infra/src/extraCustomResources.ts
+++ cluster/pulumi/infra/src/extraCustomResources.ts
@@ -1,4 +1,4 @@
-// Copyright (c) 2025 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 import * as k8s from '@pulumi/kubernetes';
 
@@ -8,8 +8,8 @@ import { infraConfig } from './config';
 export function installExtraCustomResources(): void {
   const extraCrs = infraConfig.extraCustomResources;
   Object.entries(extraCrs).forEach(([name, spec]) => {
-    if (spec.metadata && spec.metadata.namespace) {
-      spec.metadata.namespace.split(' ').forEach((ns: string) => {
+    if (Array.isArray(spec.metadata?.namespace)) {
+      spec.metadata.namespace.forEach((ns: string) => {
         const patchedName = `${ns}-${name}`;
         const patchedSpec = {
           ...spec,
