08d6a94589e48ea8945f6d40b1e3faf38a91fe60
Itai Segall <itai.segall@digitalasset.com>
2025-08-04 20:56:32 +0000

    [static] remove dummy helm chart
    Signed-off-by: Itai Segall <itai.segall@digitalasset.com>

--- cluster/pulumi/common/src/loopback.ts
+++ cluster/pulumi/common/src/loopback.ts
@@ -5,41 +5,11 @@ import * as pulumi from '@pulumi/pulumi';
 import { dsoSize } from 'splice-pulumi-common-sv/src/dsoConfig';
 import { cometBFTExternalPort } from 'splice-pulumi-common-sv/src/synchronizer/cometbftConfig';
 
-import { chartPath, CnChartVersion, isDevNet } from '../../common';
+import { isDevNet } from '../../common';
 import { DecentralizedSynchronizerUpgradeConfig } from './domainMigration';
-import {
-  CLUSTER_HOSTNAME,
-  ExactNamespace,
-  HELM_CHART_TIMEOUT_SEC,
-  HELM_MAX_HISTORY_SIZE,
-} from './utils';
+import { CLUSTER_HOSTNAME, ExactNamespace } from './utils';
 
 export function installLoopback(namespace: ExactNamespace): pulumi.Resource[] {
-  // TODO(#1776): remove this once we migrated to this everywhere
-  const version: CnChartVersion = {
-    type: 'remote',
-    version: '0.4.10-snapshot.20250731.589.0.v5e776fc4',
-  };
-  const chart = chartPath('splice-dummy', version);
-  const loopbackResourceName =
-    namespace.logicalName == 'sv' ||
-    namespace.logicalName == 'validator' ||
-    namespace.logicalName == 'multi-validator'
-      ? 'loopback'
-      : `${namespace.logicalName}-loopback`;
-  const dummyLoopbackRelease = new k8s.helm.v3.Release(
-    loopbackResourceName,
-    {
-      name: `loopback`,
-      namespace: namespace.ns.metadata.name,
-      chart,
-      version: version.version,
-      timeout: HELM_CHART_TIMEOUT_SEC,
-      maxHistory: HELM_MAX_HISTORY_SIZE,
-    },
-    { deleteBeforeReplace: true }
-  );
-
   const numMigrations = DecentralizedSynchronizerUpgradeConfig.highestMigrationId + 1;
   // For DevNet-like clusters, we always assume at least 5 SVs to reduce churn on the gateway definition,
   // and support easily deploying without refreshing the infra stack.
@@ -86,7 +56,7 @@ export function installLoopback(namespace: ExactNamespace): pulumi.Resource[] {
         resolution: 'DNS',
       },
     },
-    { dependsOn: [namespace.ns, dummyLoopbackRelease] }
+    { dependsOn: [namespace.ns] }
   );
 
   const svHosts = Array.from({ length: numSVs }, (_, i) =>
@@ -154,7 +124,7 @@ export function installLoopback(namespace: ExactNamespace): pulumi.Resource[] {
         ],
       },
     },
-    { dependsOn: [namespace.ns, dummyLoopbackRelease] }
+    { dependsOn: [namespace.ns] }
   );
 
   const cometBftVirtualService = new k8s.apiextensions.CustomResource(
