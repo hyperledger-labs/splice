a7677446af839e5dcf596dd726602732ae7b299b
Itai Segall <itai.segall@digitalasset.com>
2025-08-05 16:10:15 -0400

    [0.4.8] fix off-by-one in sv node indices (#1806)
    Signed-off-by: Itai Segall <itai.segall@digitalasset.com>
    Signed-off-by: Nicu Reut <nicu.reut@digitalasset.com>
    Co-authored-by: Nicu Reut <nicu.reut@digitalasset.com>

--- cluster/pulumi/common/src/loopback.ts
+++ cluster/pulumi/common/src/loopback.ts
@@ -5,53 +5,31 @@ import * as pulumi from '@pulumi/pulumi';
 import { dsoSize } from 'splice-pulumi-common-sv/src/dsoConfig';
 import { cometBFTExternalPort } from 'splice-pulumi-common-sv/src/synchronizer/cometbftConfig';
 
-import { chartPath, CnChartVersion, isDevNet } from '../../common';
+import { isDevNet, isMainNet } from '../../common';
 import { DecentralizedSynchronizerUpgradeConfig } from './domainMigration';
-import {
-  CLUSTER_HOSTNAME,
-  ExactNamespace,
-  HELM_CHART_TIMEOUT_SEC,
-  HELM_MAX_HISTORY_SIZE,
-} from './utils';
+import { CLUSTER_HOSTNAME, ExactNamespace } from './utils';
 
 export function installLoopback(namespace: ExactNamespace): pulumi.Resource[] {
-  // TODO(#1776): remove this once we migrated to this everywhere
-  const version: CnChartVersion = {
-    type: 'remote',
-    version: '0.4.10-snapshot.20250731.589.0.v5e776fc4',
-  };
-  const chart = chartPath('splice-dummy', version);
-  const loopbackResourceName =
-    namespace.logicalName == 'sv' ||
-    namespace.logicalName == 'validator' ||
-    namespace.logicalName == 'multi-validator'
-      ? 'loopback'
-      : `${namespace.logicalName}-loopback`;
-  const dummyLoopbackRelease = new k8s.helm.v3.Release(
-    loopbackResourceName,
-    {
-      name: `loopback`,
-      namespace: namespace.ns.metadata.name,
-      chart,
-      version: version.version,
-      timeout: HELM_CHART_TIMEOUT_SEC,
-      maxHistory: HELM_MAX_HISTORY_SIZE,
-    },
-    { deleteBeforeReplace: true }
-  );
-
   const numMigrations = DecentralizedSynchronizerUpgradeConfig.highestMigrationId + 1;
-  // For DevNet-like clusters, we always assume at least 5 SVs to reduce churn on the gateway definition,
+  // For DevNet-like clusters, we always assume at least 4 SVs (not including sv-runbook) to reduce churn on the gateway definition,
   // and support easily deploying without refreshing the infra stack.
-  const numSVs = dsoSize < 5 && isDevNet ? 5 : dsoSize;
+  const numSVs = dsoSize < 4 && isDevNet ? 4 : dsoSize;
 
-  const cometBFTPorts = Array.from({ length: numMigrations }, (_, i) => i).flatMap(migration =>
-    Array.from({ length: numSVs }, (_, node) => node).map(node => ({
-      number: cometBFTExternalPort(migration, node),
-      name: `cometbft-${migration}-${node}-p2p`,
-      protocol: 'TCP',
-    }))
-  );
+  const port = (migration: number, node: number) => ({
+    number: cometBFTExternalPort(migration, node),
+    name: `cometbft-${migration}-${node}-p2p`,
+    protocol: 'TCP',
+  });
+  const cometBFTPorts = Array.from({ length: numMigrations }, (_, i) => i).flatMap(migration => {
+    const ret = Array.from({ length: numSVs }, (_, node) => node).map(node =>
+      port(migration, node + 1)
+    );
+    if (!isMainNet) {
+      // For non-mainnet clusters, include "node 0" for the sv runbook
+      ret.unshift(port(migration, 0));
+    }
+    return ret;
+  });
 
   const clusterHostname = CLUSTER_HOSTNAME;
   const serviceEntry = new k8s.apiextensions.CustomResource(
@@ -86,7 +64,7 @@ export function installLoopback(namespace: ExactNamespace): pulumi.Resource[] {
         resolution: 'DNS',
       },
     },
-    { dependsOn: [namespace.ns, dummyLoopbackRelease] }
+    { dependsOn: [namespace.ns] }
   );
 
   const svHosts = Array.from({ length: numSVs }, (_, i) =>
@@ -154,7 +132,7 @@ export function installLoopback(namespace: ExactNamespace): pulumi.Resource[] {
         ],
       },
     },
-    { dependsOn: [namespace.ns, dummyLoopbackRelease] }
+    { dependsOn: [namespace.ns] }
   );
 
   const cometBftVirtualService = new k8s.apiextensions.CustomResource(
