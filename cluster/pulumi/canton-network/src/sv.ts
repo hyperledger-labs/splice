672ae03930a5f14f40e6da2ebcace7668cf30e45
Oriol Muñoz <oriol.munoz@digitalasset.com>
2025-08-27 13:54:49 +0200

    Add additional env vars to scan (#2027)
    ---------
    
    Signed-off-by: Oriol Muñoz <oriol.munoz@digitalasset.com>
    (cherry picked from commit 97f46bc221220084872710578feca87f02aa2e87)

--- cluster/pulumi/canton-network/src/sv.ts
+++ cluster/pulumi/canton-network/src/sv.ts
@@ -581,6 +581,7 @@ function installScan(
       : {}),
     enablePostgresMetrics: true,
     logLevel: config.logging?.appsLogLevel,
+    additionalEnvVars: config.scanApp?.additionalEnvVars || [],
     ...updateHistoryBackfillingValues,
   };
 
