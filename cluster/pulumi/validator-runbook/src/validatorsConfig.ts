4f260501df0cce8f89882db27cafc5584a439db6
Nicu Reut <nicu.reut@digitalasset.com>
2025-09-12 15:45:05 +0000

    [static] .
    Signed-off-by: Nicu Reut <nicu.reut@digitalasset.com>

--- cluster/pulumi/validator-runbook/src/validatorsConfig.ts
+++ cluster/pulumi/validator-runbook/src/validatorsConfig.ts
@@ -1,3 +1,5 @@
+// Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
 import { ValidatorNodeConfigSchema } from '@lfdecentralizedtrust/splice-pulumi-common-validator/src/config';
 import { clusterSubConfig } from 'splice-pulumi-common/src/config/configLoader';
 import { z } from 'zod';
