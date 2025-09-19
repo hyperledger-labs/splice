2622f6a187a19c14fb601a4f17edb01478a38049
Itai Segall <itai.segall@digitalasset.com>
2025-09-19 13:08:53 +0000

    upgrade test
    Signed-off-by: Itai Segall <itai.segall@digitalasset.com>

---
+++ cluster/pulumi/pulumi-test.mk
@@ -0,0 +1,5 @@
+# TODO(XX): delete this file once the base version of upgrade tests is 0.4.18 (after we cut the release)
+
+# This file is empty on purpose, it is not really in use, but make breaks if it does not exist.
+# In upgrade tests, we use a base splice version, but still latest cn-internal, so makefiles in splice fail to find this file.
+# We don't really need its content, as we don't test expected files in this scenario.
