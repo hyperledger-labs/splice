4208ef7b75f05405844baacbc46a038468ad8d11
Stephen Compall <stephen.compall@digitalasset.com>
2025-09-24 20:08:08 +0000

    Revert "test a failure" [static]
    This reverts commit 3206bd8f17a0ff2e34b62f572570fc4ff640a379.
    
    Signed-off-by: Stephen Compall <stephen.compall@digitalasset.com>

--- apps/app/src/test/scala/org/lfdecentralizedtrust/splice/integration/tests/WalletSubscriptionsIntegrationTest.scala
+++ apps/app/src/test/scala/org/lfdecentralizedtrust/splice/integration/tests/WalletSubscriptionsIntegrationTest.scala
@@ -24,10 +24,6 @@ class WalletSubscriptionsIntegrationTest
       .withSequencerConnectionsFromScanDisabled()
 
   "A wallet" should {
-    "just fail" in {
-      fail("test failure, please remove")
-    }
-
     "fail to get a non-existent subscription request" in { implicit env =>
       onboardWalletUser(aliceWalletClient, aliceValidatorBackend)
 
