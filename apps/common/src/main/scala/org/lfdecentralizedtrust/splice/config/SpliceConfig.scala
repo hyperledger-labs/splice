dbd35e342648eeb6105f033ebf11372bd9e3e80b
Julien Tinguely <julien.tinguely@digitalasset.com>
2025-10-01 08:27:34 +0200

    Bump default circuit breaker thresholds (#2475)
    * [ci] init
    
    Signed-off-by: Julien Tinguely <julien.tinguely@digitalasset.com>
    
    * [ci] fix comment
    
    Signed-off-by: Julien Tinguely <julien.tinguely@digitalasset.com>
    
    * [ci] add todo next to values
    
    Signed-off-by: Julien Tinguely <julien.tinguely@digitalasset.com>
    
    ---------
    
    Signed-off-by: Julien Tinguely <julien.tinguely@digitalasset.com>

--- apps/common/src/main/scala/org/lfdecentralizedtrust/splice/config/SpliceConfig.scala
+++ apps/common/src/main/scala/org/lfdecentralizedtrust/splice/config/SpliceConfig.scala
@@ -34,7 +34,8 @@ abstract class GrpcClientConfig extends NodeConfig {}
 abstract class HttpClientConfig extends NetworkAppNodeConfig {}
 
 final case class CircuitBreakerConfig(
-    maxFailures: Int = 20,
+    // TODO(hyperledger-labs/splice#2462) Revert back to lower values once we also reset failures after some time of inactivity
+    maxFailures: Int = 40,
     callTimeout: NonNegativeFiniteDuration =
       NonNegativeFiniteDuration.ofSeconds(0), // disable timeout
     resetTimeout: NonNegativeFiniteDuration = NonNegativeFiniteDuration.ofSeconds(30),
@@ -48,11 +49,13 @@ final case class CircuitBreakersConfig(
       maxResetTimeout = NonNegativeFiniteDuration.ofMinutes(2)
     ),
     mediumPriority: CircuitBreakerConfig = CircuitBreakerConfig(
-      maxFailures = 10,
+      // TODO(hyperledger-labs/splice#2462) Revert back to lower values once we also reset failures after some time of inactivity
+      maxFailures = 20,
       maxResetTimeout = NonNegativeFiniteDuration.ofMinutes(3),
     ),
     lowPriority: CircuitBreakerConfig = CircuitBreakerConfig(
-      maxFailures = 5,
+      // TODO(hyperledger-labs/splice#2462) Revert back to lower values once we also reset failures after some time of inactivity
+      maxFailures = 10,
       maxResetTimeout = NonNegativeFiniteDuration.ofMinutes(7),
     ),
     // Amulet expiry is different from essentially any other trigger run in the SV app in that for it to complete successfully
