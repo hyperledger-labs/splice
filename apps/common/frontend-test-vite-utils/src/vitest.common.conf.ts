52d9d5097d0ba322b03b04892e56a80fd0e63499
fayi-da <fayimora.femibalogun@digitalasset.com>
2025-10-14 08:55:33 +0000

    Bump timeout for all UI tests [ci]
    Seems like some ui tests run slower in ci. It's random so maybe a
    resource thing. I noticed in the docs that there's a new beta feature
    that sets browser tests to have a 15s timeout. I dont want to use a beta
    feature so I'm just manually bumping the global timeout here to 15_000ms
    
    https://vitest.dev/config/#testtimeout
    
    https://vitest.dev/guide/browser/
    Signed-off-by: fayi-da <fayimora.femibalogun@digitalasset.com>

--- apps/common/frontend-test-vite-utils/src/vitest.common.conf.ts
+++ apps/common/frontend-test-vite-utils/src/vitest.common.conf.ts
@@ -1,11 +1,11 @@
 export const vitest_common_conf = {
   test: {
-    environment: 'happy-dom',
-    typecheck: {
-      include: ['**/*.{test,spec}-d.?(c|m)[jt]s?(x)', '**/*.{test,spec}.?(c|m)[jt]s?(x)'],
-    },
-    exclude: ['../lib/**'],
-    silent: false,
     disableConsoleIntercept: true,
+    environment: 'happy-dom',
+    exclude: ['../lib/**'],
+    include: ['**/*.{test,spec}-d.?(c|m)[jt]s?(x)', '**/*.{test,spec}.?(c|m)[jt]s?(x)'],
+    silent: false,
+    testTimeout: 15000,
+    typecheck: {},
   },
 };
