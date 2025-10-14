4c28cca42fbe727fa3bd416798e9a7cc9a837055
fayi-da <fayimora.femibalogun@digitalasset.com>
2025-10-14 10:13:49 +0000

    oops, should be in index [ci]
    Signed-off-by: fayi-da <fayimora.femibalogun@digitalasset.com>

--- apps/common/frontend-test-vite-utils/src/vitest.common.conf.ts
+++ apps/common/frontend-test-vite-utils/src/vitest.common.conf.ts
@@ -1,12 +1,11 @@
 export const vitest_common_conf = {
   test: {
-    disableConsoleIntercept: true,
     environment: 'happy-dom',
-    exclude: ['../lib/**'],
-    silent: false,
-    testTimeout: 15000,
     typecheck: {
       include: ['**/*.{test,spec}-d.?(c|m)[jt]s?(x)', '**/*.{test,spec}.?(c|m)[jt]s?(x)'],
     },
+    exclude: ['../lib/**'],
+    silent: false,
+    disableConsoleIntercept: true,
   },
 };
