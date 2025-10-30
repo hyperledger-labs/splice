812e0f237ab2cf5dfdceb32e980833e23cbc0ba7
Nicu Reut <nicu.reut@digitalasset.com>
2025-10-30 09:31:42 +0000

    [ci] Add the ability to trigger a sync migration dumps at a given timestamp without the sync being paused
    Signed-off-by: Nicu Reut <nicu.reut@digitalasset.com>

---
+++ apps/app/src/test/scala/org/lfdecentralizedtrust/splice/integration/tests/SvMigrationApiIntegrationTest.scala
@@ -0,0 +1,53 @@
+package org.lfdecentralizedtrust.splice.integration.tests
+
+import org.lfdecentralizedtrust.splice.config.ConfigTransforms
+import org.lfdecentralizedtrust.splice.integration.EnvironmentDefinition
+import org.lfdecentralizedtrust.splice.integration.tests.DecentralizedSynchronizerMigrationIntegrationTest.migrationDumpDir
+import better.files.File.*
+import cats.implicits.catsSyntaxOptionId
+import com.digitalasset.canton.console.CommandFailure
+
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+
+class SvMigrationApiIntegrationTest extends SvIntegrationTestBase {
+
+  override def environmentDefinition: EnvironmentDefinition =
+    super.environmentDefinition.addConfigTransform((_, conf) =>
+      ConfigTransforms.updateAllSvAppConfigs((name, config) =>
+        config.copy(
+          domainMigrationDumpPath = Some(migrationDumpPathForSv(name).path)
+        )
+      )(conf)
+    )
+
+  "we can trigger a migration dump on a non paused sync" in { implicit env =>
+    initDso()
+
+    clue("triggering the dump fails if no timestamp is provided as the sync must be paused") {
+      a[CommandFailure] should be thrownBy {
+        sv1Backend.triggerDecentralizedSynchronizerMigrationDump(0)
+      }
+    }
+    val dumpTimestamp = Instant.now().minus(30, ChronoUnit.SECONDS)
+    sv1Backend.triggerDecentralizedSynchronizerMigrationDump(
+      0,
+      dumpTimestamp.some,
+    )
+    val expectedDirectory = migrationDumpPathForSv(
+      sv1Backend.name
+    ).parent / s"export_at_${dumpTimestamp.toEpochMilli}"
+
+    clue(s"export is written at ${expectedDirectory.toString()}") {
+      expectedDirectory.exists shouldBe true
+      (expectedDirectory / "domain_migration_dump.json").exists shouldBe true
+      (expectedDirectory / s"$dumpTimestamp-genesis-file").exists shouldBe true
+      (expectedDirectory / s"$dumpTimestamp-acs-snapshot").exists shouldBe true
+    }
+  }
+
+  private def migrationDumpPathForSv(name: String) = {
+    migrationDumpDir(name) / "domain_migration_dump.json"
+  }
+
+}
