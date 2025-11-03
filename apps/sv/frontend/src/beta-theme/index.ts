cca4c3f9945e6c4290dc7d3fef6beb207e0458a3
Paweł Perek <pawel.perek@digitalasset.com>
2025-11-03 11:36:21 +0000

    integrate with PageHeader changes [ci]
    Signed-off-by: Paweł Perek <pawel.perek@digitalasset.com>

--- apps/sv/frontend/src/beta-theme/index.ts
+++ apps/sv/frontend/src/beta-theme/index.ts
@@ -1,6 +1,7 @@
 // Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 import 'common-typeface-termina/index.css';
+import 'common-typeface-lato/index.css';
 
 import { createTheme, TypographyStyle } from '@mui/material';
 
