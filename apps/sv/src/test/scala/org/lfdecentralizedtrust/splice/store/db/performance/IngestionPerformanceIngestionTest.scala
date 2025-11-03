245a27b72c547c1274ba395e7e11412b3fe2d88b
Oriol Muñoz <oriol.munoz@digitalasset.com>
2025-11-03 13:40:44 +0000

    fixes
    Signed-off-by: Oriol Muñoz <oriol.munoz@digitalasset.com>

--- apps/sv/src/test/scala/org/lfdecentralizedtrust/splice/store/db/performance/IngestionPerformanceIngestionTest.scala
+++ apps/sv/src/test/scala/org/lfdecentralizedtrust/splice/store/db/performance/IngestionPerformanceIngestionTest.scala
@@ -15,10 +15,7 @@ import org.apache.pekko.Done
 import org.apache.pekko.stream.connectors.csv.scaladsl.CsvParsing
 import org.apache.pekko.stream.connectors.csv.scaladsl.CsvToMap
 import org.apache.pekko.stream.scaladsl.{FileIO, Sink}
-import org.lfdecentralizedtrust.splice.environment.ledger.api.{
-  TransactionTreeUpdate,
-  TreeUpdateOrOffsetCheckpoint,
-}
+import org.lfdecentralizedtrust.splice.environment.ledger.api.TransactionTreeUpdate
 import org.lfdecentralizedtrust.splice.environment.{DarResources, RetryProvider}
 import org.lfdecentralizedtrust.splice.migration.DomainMigrationInfo
 import org.lfdecentralizedtrust.splice.store.StoreTest
@@ -26,6 +23,7 @@ import org.lfdecentralizedtrust.splice.store.db.SplicePostgresTest
 import org.lfdecentralizedtrust.splice.sv.store.SvStore
 import org.lfdecentralizedtrust.splice.sv.store.db.DbSvDsoStore
 import org.lfdecentralizedtrust.splice.util.{
+  PackageQualifiedName,
   ResourceTemplateDecoder,
   TemplateJsonDecoder,
   ValueJsonCodecProtobuf as ProtobufCodec,
@@ -56,58 +54,63 @@ class IngestionPerformanceIngestionTest
       .via(CsvParsing.lineScanner(maximumLineLength = Int.MaxValue))
       .via(CsvToMap.toMapAsStrings(StandardCharsets.UTF_8))
       .zipWithIndex
-      .runWith(Sink.foreachAsync(parallelism = 1) { case (line, index) =>
+      .runWith(Sink.foreachAsync(parallelism = 1) { case (_line, index) =>
+        val line = _line.map { case (key, value) =>
+          key -> value.replace(
+            "DSO::12209471e1a52edc2995ad347371597a5872f2704cb2cb4bb330a849e7309598259e",
+            dsoParty.toProtoPrimitive,
+          )
+        }
         println(s"Ingesting $index")
         val synchronizerId = SynchronizerId.tryFromString(line("domain_id"))
         val recordTime = CantonTimestamp.MinValue.plusSeconds(index)
-        val before = System.currentTimeMillis()
-        store.multiDomainAcsStore.ingestionSink
-          .ingestUpdate(
-            TreeUpdateOrOffsetCheckpoint.Update(
-              update = TransactionTreeUpdate(
-                new Transaction(
-                  UUID.randomUUID().toString, // updateId
-                  "canton-network-acs-import-something", // commandId
-                  "canton-network-acs-import-something", // workflowId
-                  recordTime.toInstant, // effectiveAt
-                  java.util.List.of(
-                    new CreatedEvent(
-                      parseArray(line("observers")), // witnessParties
-                      index, // offset
-                      1, // nodeId
-                      new Identifier(
-                        line("template_id_package_id"),
-                        line("template_id_module_name"),
-                        line("template_id_entity_name"),
-                      ), // templateId
-                      "splice-package", // packageName
-                      line("contract_id"), // contractId
-                      ProtobufCodec
-                        .deserializeValue(line("create_arguments"))
-                        .asRecord()
-                        .get(), // arguments
-                      ByteString.copyFromUtf8(line("create_arguments")), // createdEventBlob
-                      java.util.Map.of(), // interfaceViews
-                      java.util.Map.of(), // failedInterfaceViews
-                      java.util.Optional.empty(), // contractKey
-                      parseArray(line("signatories")), // signatories
-                      parseArray(line("observers")), // observers
-                      recordTime.toInstant, // createdAt
-                      false, // acsDelta
-                      "splice-package", // representativePackageId
-                    )
-                  ), // events
-                  index, // offset
-                  synchronizerId.toProtoPrimitive,
-                  TraceContextOuterClass.TraceContext.newBuilder().build(),
-                  recordTime.toInstant,
-                )
-              ),
-              synchronizerId = synchronizerId,
-            )
+        val templateId = new Identifier(
+          line("template_id_package_id"),
+          line("template_id_module_name"),
+          line("template_id_entity_name"),
+        )
+        val packageName = PackageQualifiedName.getFromResources(templateId).packageName
+        val update = TransactionTreeUpdate(
+          new Transaction(
+            UUID.randomUUID().toString, // updateId
+            "canton-network-acs-import-something", // commandId
+            "canton-network-acs-import-something", // workflowId
+            recordTime.toInstant, // effectiveAt
+            java.util.List.of(
+              new CreatedEvent(
+                parseArray(line("observers")), // witnessParties
+                index, // offset
+                1, // nodeId
+                templateId, // templateId
+                packageName, // packageName
+                line("contract_id"), // contractId
+                ProtobufCodec
+                  .deserializeValue(line("create_arguments"))
+                  .asRecord()
+                  .get(), // arguments
+                ByteString.copyFromUtf8(line("create_arguments")), // createdEventBlob
+                java.util.Map.of(), // interfaceViews
+                java.util.Map.of(), // failedInterfaceViews
+                java.util.Optional.empty(), // contractKey
+                parseArray(line("signatories")), // signatories
+                parseArray(line("observers")), // observers
+                recordTime.toInstant, // createdAt
+                false, // acsDelta
+                templateId.getPackageId, // representativePackageId
+              )
+            ), // events
+            index, // offset
+            synchronizerId.toProtoPrimitive,
+            TraceContextOuterClass.TraceContext.newBuilder().build(),
+            recordTime.toInstant,
           )
+        )
+
+        val before = System.nanoTime()
+        store.multiDomainAcsStore.ingestionSink
+          .ingestUpdate(synchronizerId, update)
           .map { _ =>
-            val after = System.currentTimeMillis()
+            val after = System.nanoTime()
             val duration = after - before
             timings += duration
             val avg = timings.sum.toDouble / timings.size
@@ -119,6 +122,17 @@ class IngestionPerformanceIngestionTest
       .futureValue(timeout = PatienceConfiguration.Timeout(FiniteDuration(12, "hours"))) should be(
       Done
     )
+
+    import slick.jdbc.canton.ActionBasedSQLInterpolation.Implicits.actionBasedSQLInterpolationCanton
+    storage
+      .querySingle(
+        sql"select count(*) from dso_acs_store".as[Int].headOption,
+        "count",
+      )
+      .value
+      .failOnShutdown("")
+      .futureValue
+      .valueOrFail("count is there") should be >= 600_000
   }
 
   private def parseArray(str: String) = {
