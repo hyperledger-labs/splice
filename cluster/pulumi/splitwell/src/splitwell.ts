2f6de91ca08e28a80d99768fa9e07c154c860cef
Itai Segall <itai.segall@digitalasset.com>
2025-11-03 11:52:04 -0500

    backport auth0 changes (#3046)
    Signed-off-by: Itai Segall <itai.segall@digitalasset.com>

--- cluster/pulumi/splitwell/src/splitwell.ts
+++ cluster/pulumi/splitwell/src/splitwell.ts
@@ -16,7 +16,7 @@ import {
   ExactNamespace,
   failOnAppVersionMismatch,
   imagePullSecret,
-  installAuth0Secret,
+  installLedgerApiSecret,
   installSpliceHelmChart,
   ValidatorTopupConfig,
 } from '@lfdecentralizedtrust/splice-pulumi-common';
@@ -128,7 +128,7 @@ export async function installSplitwell(
   const validatorDbName = 'val_splitwell';
 
   const extraDependsOn = imagePullDeps.concat(
-    await installAuth0Secret(auth0Client, xns, 'splitwell', 'splitwell')
+    await installLedgerApiSecret(auth0Client, xns, 'splitwell')
   );
 
   return await installValidatorApp({
