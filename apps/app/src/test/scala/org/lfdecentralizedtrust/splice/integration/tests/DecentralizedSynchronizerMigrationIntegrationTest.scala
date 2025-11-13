e039c17684a793f0e7aba67ff3545952dfeff813
Martin Florian <martin.florian@digitalasset.com>
2025-11-13 15:56:41 +0000

    extra test: no rights, but a primary party
    [ci]
    
    Signed-off-by: Martin Florian <martin.florian@digitalasset.com>

--- apps/app/src/test/scala/org/lfdecentralizedtrust/splice/integration/tests/DecentralizedSynchronizerMigrationIntegrationTest.scala
+++ apps/app/src/test/scala/org/lfdecentralizedtrust/splice/integration/tests/DecentralizedSynchronizerMigrationIntegrationTest.scala
@@ -1416,7 +1416,12 @@ class DecentralizedSynchronizerMigrationIntegrationTest
             false,
           )
           participant.ledger_api.users.create(
-            s"fake-user-4-${suffix}"
+            s"fake-user-4-${suffix}",
+            Set(),
+            Option(someParties(0)),
+          )
+          participant.ledger_api.users.create(
+            s"fake-user-5-${suffix}"
           )
         }
       },
