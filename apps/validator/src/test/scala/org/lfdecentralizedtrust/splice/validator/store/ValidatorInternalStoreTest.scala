aa041daf0225f974dd01280472bd1d23700f8674
Nicu Reut <nicu.reut@digitalasset.com>
2025-11-13 19:46:39 +0000

    [ci] .
    Signed-off-by: Nicu Reut <nicu.reut@digitalasset.com>

--- apps/validator/src/test/scala/org/lfdecentralizedtrust/splice/validator/store/ValidatorInternalStoreTest.scala
+++ apps/validator/src/test/scala/org/lfdecentralizedtrust/splice/validator/store/ValidatorInternalStoreTest.scala
@@ -3,27 +3,24 @@
 
 package org.lfdecentralizedtrust.splice.validator.store
 
-import com.digitalasset.canton.config.ProcessingTimeout
 import com.digitalasset.canton.lifecycle.FutureUnlessShutdown
 import com.digitalasset.canton.resource.DbStorage
-import com.digitalasset.canton.{HasActorSystem, HasExecutionContext}
+import com.digitalasset.canton.topology.PartyId
+import com.digitalasset.canton.tracing.TraceContext
+import com.digitalasset.canton.{BaseTest, HasActorSystem}
 import org.lfdecentralizedtrust.splice.store.db.SplicePostgresTest
+import org.lfdecentralizedtrust.splice.validator.store.ValidatorConfigProvider.ScanUrlInternalConfig
 import org.lfdecentralizedtrust.splice.validator.store.db.DbValidatorInternalStore
-import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AsyncWordSpec
 
 import scala.concurrent.Future
 
-abstract class ValidatorInternalStoreTest
-    extends AsyncWordSpec
-    with Matchers
-    with HasExecutionContext {
+abstract class ValidatorInternalStoreTest extends AsyncWordSpec with BaseTest {
 
   protected def mkStore(): Future[ValidatorInternalStore]
   protected def mkProvider(): Future[ValidatorConfigProvider]
 
   "ValidatorInternalStore" should {
-    implicit val tc: TraceContext = TraceContext.empty
 
     val configKey = "key1"
     val configValue = "payload1"
@@ -36,7 +33,7 @@ abstract class ValidatorInternalStoreTest
         _ <- store.setConfig(configKey, configValue)
         retrievedValue <- store.getConfig[String](configKey).value
       } yield {
-        retrievedValue shouldBe Some(configValue)
+        retrievedValue.value.value shouldBe configValue
       }
     }
 
@@ -56,7 +53,7 @@ abstract class ValidatorInternalStoreTest
         _ <- store.setConfig(configKey, otherValue)
         retrievedValue <- store.getConfig[String](configKey).value
       } yield {
-        retrievedValue shouldBe Some(otherValue)
+        retrievedValue.value.value shouldBe otherValue
       }
     }
 
@@ -69,50 +66,93 @@ abstract class ValidatorInternalStoreTest
         configKeyValue <- store.getConfig[String](configKey).value
         otherKeyValue <- store.getConfig[String](otherKey).value
       } yield {
-        configKeyValue shouldBe Some(configValue)
-        otherKeyValue shouldBe Some(otherValue)
+        configKeyValue.value.value shouldBe configValue
+        otherKeyValue.value.value shouldBe otherValue
       }
     }
 
-    val scanConfig1: Seq[ScanUrlInternalConfig] = Seq(
-      ScanUrlInternalConfig("sv1", "url1"),
-      ScanUrlInternalConfig("sv2", "url2"),
-    )
-
-    val scanConfig2: Seq[ScanUrlInternalConfig] = Seq(
-      ScanUrlInternalConfig("svA", "urlA"),
-      ScanUrlInternalConfig("svB", "urlB"),
-    )
-
-    "CONFIG_PROVIDER set and retrieve a ScanUrlInternalConfig list successfully" in {
+    "delete single key" in {
       for {
-        provider <- mkProvider()
-        _ <- provider.setScanUrlInternalConfig(scanConfig1)
-        retrievedConfigOption <- provider.getScanUrlInternalConfig().value
+        store <- mkStore()
+        _ <- store.setConfig(configKey, configValue)
+        _ <- store.setConfig(otherKey, otherValue)
+
+        _ <- store.deleteConfig(configKey)
+        configKeyValue <- store.getConfig[String](configKey).value
+        otherKeyValue <- store.getConfig[String](otherKey).value
       } yield {
-        retrievedConfigOption shouldBe Some(scanConfig1)
+        configKeyValue shouldBe None
+        otherKeyValue.value.value shouldBe otherValue
+      }
+    }
+  }
+
+  "Validator config provider" should {
+
+    "handle scan urls" should {
+
+      val scanConfig1: Seq[ScanUrlInternalConfig] = Seq(
+        ScanUrlInternalConfig("sv1", "url1"),
+        ScanUrlInternalConfig("sv2", "url2"),
+      )
+
+      val scanConfig2: Seq[ScanUrlInternalConfig] = Seq(
+        ScanUrlInternalConfig("svA", "urlA"),
+        ScanUrlInternalConfig("svB", "urlB"),
+      )
+
+      "CONFIG_PROVIDER set and retrieve a ScanUrlInternalConfig list successfully" in {
+        for {
+          provider <- mkProvider()
+          _ <- provider.setScanUrlInternalConfig(scanConfig1)
+          retrievedConfigOption <- provider.getScanUrlInternalConfig().value
+        } yield {
+          retrievedConfigOption shouldBe Some(scanConfig1)
+        }
+      }
+
+      "CONFIG_PROVIDER update an existing ScanUrlInternalConfig list" in {
+        for {
+          provider <- mkProvider()
+          _ <- provider.setScanUrlInternalConfig(scanConfig1)
+          _ <- provider.setScanUrlInternalConfig(scanConfig2) // Overwrite
+          retrievedConfigOption <- provider.getScanUrlInternalConfig().value
+        } yield {
+          retrievedConfigOption shouldBe Some(scanConfig2)
+        }
+      }
+
+      "CONFIG_PROVIDER return None when no ScanUrlInternalConfig is found" in {
+        for {
+          provider <- mkProvider()
+          retrievedConfigOption <- provider.getScanUrlInternalConfig().value
+        } yield {
+          retrievedConfigOption shouldBe None
+        }
       }
     }
 
-    "CONFIG_PROVIDER update an existing ScanUrlInternalConfig list" in {
-      for {
-        provider <- mkProvider()
-        _ <- provider.setScanUrlInternalConfig(scanConfig1)
-        _ <- provider.setScanUrlInternalConfig(scanConfig2) // Overwrite
-        retrievedConfigOption <- provider.getScanUrlInternalConfig().value
-      } yield {
-        retrievedConfigOption shouldBe Some(scanConfig2)
+    "handle migrating parties" should {
+
+      "handle set delete update" in {
+        for {
+          provider <- mkProvider()
+          parties = Set(
+            PartyId.tryFromProtoPrimitive("party::test")
+          )
+          _ <- provider.setPartiesToMigrate(parties)
+          partiesRead <- provider.getPartiesToMigrate().value
+          _ <- provider.clearPartiesToMigrate()
+          partiesReadAfterClear <- provider.getPartiesToMigrate().value
+        } yield {
+          partiesRead shouldBe Some(parties)
+          partiesReadAfterClear shouldBe None
+        }
+
       }
+
     }
 
-    "CONFIG_PROVIDER return None when no ScanUrlInternalConfig is found" in {
-      for {
-        provider <- mkProvider()
-        retrievedConfigOption <- provider.getScanUrlInternalConfig().value
-      } yield {
-        retrievedConfigOption shouldBe None
-      }
-    }
   }
 
 }
@@ -122,21 +162,9 @@ class DbValidatorInternalStoreTest
     with HasActorSystem
     with SplicePostgresTest {
 
-  override protected def timeouts = new ProcessingTimeout
-  override protected def loggerFactory: NamedLoggerFactory = NamedLoggerFactory.root
-
   private def buildDbStore(): ValidatorInternalStore = {
-    val elc = ErrorLoggingContext(
-      loggerFactory.getTracedLogger(getClass),
-      loggerFactory.properties,
-      TraceContext.empty,
-    )
-    implicit val cc = closeContext
-
     new DbValidatorInternalStore(
       storage = storage,
-      loggingContext = elc,
-      closeContext = cc,
       loggerFactory = loggerFactory,
     )
   }
@@ -147,7 +175,7 @@ class DbValidatorInternalStoreTest
 
   override protected def mkProvider(): Future[ValidatorConfigProvider] = Future.successful {
     val internalStore = buildDbStore()
-    new ValidatorConfigProvider(internalStore)
+    new ValidatorConfigProvider(internalStore, loggerFactory)
   }
 
   override protected def cleanDb(
