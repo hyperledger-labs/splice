a99ca68f1f1e15102750e60bf219fa1633d82726
Pasindu Tennage <pasindu.tennage@digitalasset.com>
2025-11-18 09:50:14 +0000

    Reset table
    [ci]
    
    Signed-off-by: Pasindu Tennage <pasindu.tennage@digitalasset.com>

--- apps/common/src/main/resources/db/migration/canton-network/postgres/stable/V050__create_scan_config.sql
+++ apps/common/src/main/resources/db/migration/canton-network/postgres/stable/V050__create_scan_config.sql
@@ -1,8 +1,6 @@
 CREATE TABLE validator_internal_config
 (
-    store_id int not null,
     config_key   text  not null,
     config_value jsonb not null,
-    foreign key (store_id) references store_descriptors(id)
     CONSTRAINT uc_validator_internal_config PRIMARY KEY (config_key)
 );
