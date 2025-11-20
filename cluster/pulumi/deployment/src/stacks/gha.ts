a858bf5a94c758dce05cfe15d0ce71e43f3b47f8
Mateusz Błażejewski <mateusz.blazejewski@digitalasset.com>
2025-11-20 10:12:36 +0100

    make operator's pulumi projects scope configurable and add the GHA project (#2989)
    * make operator's pulumi projects scope configurable and add the GHA project
    
    [static]
    
    Signed-off-by: Mateusz Błażejewski <mateusz.blazejewski@digitalasset.com>
    
    * move deployment project config definition
    
    [static]
    
    Signed-off-by: Mateusz Błażejewski <mateusz.blazejewski@digitalasset.com>
    
    * coerce Array to Set in projectsToDeploy config. prop. for operator
    
    [static]
    
    Signed-off-by: Mateusz Błażejewski <mateusz.blazejewski@digitalasset.com>
    
    * modify deployment project dependency on common-sv to avoid problems with CometBFT secrets
    
    [static]
    
    Signed-off-by: Mateusz Błażejewski <mateusz.blazejewski@digitalasset.com>
    
    * make explicit deployment.projectsToDeploy configuration suppress env. var. flags
    
    [static]
    
    Signed-off-by: Mateusz Błażejewski <mateusz.blazejewski@digitalasset.com>
    
    ---------
    
    Signed-off-by: Mateusz Błażejewski <mateusz.blazejewski@digitalasset.com>

---
+++ cluster/pulumi/deployment/src/stacks/gha.ts
@@ -0,0 +1,32 @@
+// Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+import * as k8s from '@pulumi/kubernetes';
+import { CLUSTER_BASENAME } from '@lfdecentralizedtrust/splice-pulumi-common';
+import { deploymentConf } from '@lfdecentralizedtrust/splice-pulumi-common/src/operator/config';
+import {
+  GitFluxRef,
+  StackFromRef,
+} from '@lfdecentralizedtrust/splice-pulumi-common/src/operator/flux-source';
+import {
+  createStackCR,
+  EnvRefs,
+} from '@lfdecentralizedtrust/splice-pulumi-common/src/operator/stack';
+
+export function getGithubActionsStackFromMainReference(): Array<StackFromRef> {
+  if (deploymentConf.projectsToDeploy.has('gha')) {
+    return [{ project: 'gha', stack: `gha.${CLUSTER_BASENAME}` }];
+  } else {
+    return [];
+  }
+}
+
+export function installGithubActionsStack(
+  reference: GitFluxRef,
+  envRefs: EnvRefs,
+  namespace: string,
+  gcpSecret: k8s.core.v1.Secret
+): void {
+  if (deploymentConf.projectsToDeploy.has('gha')) {
+    createStackCR('gha', 'gha', namespace, false, reference, envRefs, gcpSecret);
+  }
+}
