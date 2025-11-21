71305a0e21a4de723d6cb74e45e4305ba1d144b5
Julien Nicoulaud <julien.nicoulaud@ledger.fr>
2025-11-21 08:53:50 +0100

    chore: retry on 503

--- scripts/scan-txlog/scan_txlog.py
+++ scripts/scan-txlog/scan_txlog.py
@@ -312,7 +312,7 @@ class ScanClient:
             params=params,
             max_retries=30,
             delay_seconds=0.5,
-            statuses={404, 429},
+            statuses={404, 429, 503},
         )
         return (party, DamlDecimal(json["wallet_balance"]))
 
