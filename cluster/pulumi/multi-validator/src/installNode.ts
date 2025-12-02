b97a9bc36a85c7e147455e715a4a9d48d4c5fcb6
Pasindu Tennage <pasindu.tennage@digitalasset.com>
2025-12-02 13:43:02 +0000

    Added role and role binding in multi-validator namespace
    [ci]
    
    Signed-off-by: Pasindu Tennage <pasindu.tennage@digitalasset.com>

--- cluster/pulumi/multi-validator/src/installNode.ts
+++ cluster/pulumi/multi-validator/src/installNode.ts
@@ -1,5 +1,6 @@
 // Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
+import * as k8s from '@pulumi/kubernetes';
 import {
   exactNamespace,
   numInstances,
@@ -14,6 +15,46 @@ import { installPostgres } from './postgres';
 
 export async function installNode(): Promise<void> {
   const namespace = exactNamespace('multi-validator', true);
+  const podManagementRole = new k8s.rbac.v1.Role(
+    'gc-pod-reaper-pod-manager-role',
+    {
+      metadata: {
+        name: 'gc-pod-reaper-pod-manager-role',
+        namespace: namespace.ns.metadata.name,
+      },
+      rules: [
+        {
+          apiGroups: [''], // Core API group for Pods
+          resources: ['pods'],
+          verbs: ['list', 'create', 'delete', 'update'],
+        },
+      ],
+    },
+    { parent: namespace.ns }
+  );
+
+  new k8s.rbac.v1.RoleBinding(
+    'gc-pod-reaper-pod-manager-binding',
+    {
+      metadata: {
+        name: 'gc-pod-reaper-pod-manager-binding',
+        namespace: namespace.ns.metadata.name,
+      },
+      subjects: [
+        {
+          kind: 'ServiceAccount',
+          name: 'gc-pod-reaper-service-account',
+          namespace: 'gc-pod-reaper',
+        },
+      ],
+      roleRef: {
+        kind: 'Role',
+        name: podManagementRole.metadata.name,
+        apiGroup: 'rbac.authorization.k8s.io',
+      },
+    },
+    { parent: namespace.ns, dependsOn: [podManagementRole] }
+  );
   installLoopback(namespace);
 
   const imagePullDeps = imagePullSecret(namespace);
