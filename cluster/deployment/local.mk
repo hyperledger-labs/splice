713dbdd5b8526f6f045b3f0c1fa83e41db47771f
Mateusz Błażejewski <mateusz.blazejewski@digitalasset.com>
2025-12-04 09:47:05 +0000

    implement resolved config versioning
    [static]
    
    Signed-off-by: Mateusz Błażejewski <mateusz.blazejewski@digitalasset.com>

---
+++ cluster/deployment/local.mk
@@ -0,0 +1,26 @@
+# Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+dir := $(call current_dir)
+
+cluster_dirs := $(foreach config,$(wildcard $(dir)/*/config.yaml),$(shell dirname $(config)))
+resolved_config_targets := $(foreach cluster_dir,$(cluster_dirs),$(cluster_dir)/config.resolved.yaml)
+
+# We use .PHONY because it is hard to pinpoint exact deps for config resolution as it might depend
+# on many config files and the config loader implementation. At some point, when we get rid of
+# more specific config loading rules, it might make sense to try list the dependencies here.
+define update_resolved_config
+.PHONY: $(1)/config.resolved.yaml
+$(1)/config.resolved.yaml:
+	source $(1)/.envrc.vars && \
+	cd "${SPLICE_ROOT}/cluster/pulumi" && \
+	npm run resolve-config
+endef
+$(foreach cluster_dir,$(cluster_dirs),$(eval $(call update_resolved_config,$(cluster_dir))))
+
+.PHONY: $(dir)/update-resolved-config
+$(dir)/update-resolved-config: $(resolved_config_targets)
+
+.PHONY: $(dir)/check-resolved-config
+$(dir)/check-resolved-config: $(resolved_config_targets)
+	git diff --exit-code --quiet $(resolved_config_targets)
---
+++ cluster/pulumi/resolvedConfigEmitter.ts
@@ -0,0 +1,17 @@
+// Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+import { loadClusterYamlConfig, getMainConfigPath } from '@lfdecentralizedtrust/splice-pulumi-common/src/config/configLoader';
+import { writeFileSync } from 'fs';
+import { dump } from 'js-yaml';
+import { dirname, resolve } from 'path';
+
+const config = loadClusterYamlConfig();
+const header = '# This file is generated for preview. Do not edit directly.\n';
+const resolvedConfigYaml = dump(config, {
+  sortKeys: true,
+  lineWidth: -1,
+  noRefs: true,
+  forceQuotes: true,
+});
+const resolvedConfigPath = resolve(dirname(getMainConfigPath()), 'config.resolved.yaml');
+writeFileSync(resolvedConfigPath, `${header}${resolvedConfigYaml}`);
