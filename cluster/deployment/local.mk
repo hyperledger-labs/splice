87f7723fb3d005709e5f0ee428d2e04f19ceafac
Mateusz Błażejewski <mateusz.blazejewski@digitalasset.com>
2025-12-08 16:23:36 +0100

    add build to the resolved config update target to fix splice-bump and remove resolved config versioning for the mock cluster as it fails in the internal repo (#3364)
    [static]
    
    Signed-off-by: Mateusz Błażejewski <mateusz.blazejewski@digitalasset.com>

--- cluster/deployment/local.mk
+++ cluster/deployment/local.mk
@@ -1,17 +1,18 @@
 # Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-deployment_dir := $(call current_dir)
+deployment_dir ::= $(call current_dir)
 
-cluster_dirs := $(foreach config,$(wildcard $(deployment_dir)/*/config.yaml),$(shell dirname $(config)))
-resolved_config_targets := $(foreach cluster_dir,$(cluster_dirs),$(cluster_dir)/config.resolved.yaml)
+clusters ::= scratchneta scratchnetb scratchnetc scratchnetd scratchnete
+cluster_dirs ::= $(foreach cluster,$(clusters),$(deployment_dir)/$(cluster))
+resolved_config_targets ::= $(foreach cluster_dir,$(cluster_dirs),$(cluster_dir)/config.resolved.yaml)
 
 # We use .PHONY because it is hard to pinpoint exact deps for config resolution as it might depend
 # on many config files and the config loader implementation. At some point, when we get rid of
 # more specific config loading rules, it might make sense to try list the dependencies here.
 define update_resolved_config
 .PHONY: $(1)/config.resolved.yaml
-$(1)/config.resolved.yaml:
+$(1)/config.resolved.yaml: $(shell dirname $(deployment_dir))/pulumi/build
 	source $(1)/.envrc.vars && \
 	cd "${SPLICE_ROOT}/cluster/pulumi" && \
 	npm run resolve-config
