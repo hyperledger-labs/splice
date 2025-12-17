df8da1b2dc08151737cc30cf14e9c1555734a58a
Pasindu Tennage <pasindu.tennage@digitalasset.com>
2025-12-17 14:48:07 +0100

    Cluster Reaper job for deleting bad pods (#3414)
    [ci]
    
    Signed-off-by: Pasindu Tennage <pasindu.tennage@digitalasset.com>

--- cluster/pulumi/infra/src/index.ts
+++ cluster/pulumi/infra/src/index.ts
@@ -6,7 +6,13 @@ import { config } from '@lfdecentralizedtrust/splice-pulumi-common';
 import { clusterIsResetPeriodically, enableAlerts } from './alertings';
 import { configureAuth0 } from './auth0';
 import { configureCloudArmorPolicy } from './cloudArmor';
-import { cloudArmorConfig, clusterBaseDomain, clusterBasename, monitoringConfig } from './config';
+import {
+  cloudArmorConfig,
+  clusterBaseDomain,
+  clusterBasename,
+  enableGCReaperJob,
+  monitoringConfig,
+} from './config';
 import { installExtraCustomResources } from './extraCustomResources';
 import {
   getNotificationChannel,
@@ -15,6 +21,7 @@ import {
   installClusterMaintenanceUpdateAlerts,
 } from './gcpAlerts';
 import { configureIstio, istioMonitoring } from './istio';
+import { deployGCPodReaper } from './maintenance';
 import { configureNetwork } from './network';
 import { configureObservability } from './observability';
 import { configureStorage } from './storage';
@@ -47,6 +54,10 @@ configureStorage();
 configureCloudArmorPolicy(cloudArmorConfig);
 
 installExtraCustomResources();
+
+if (enableGCReaperJob) {
+  deployGCPodReaper('cluster-pod-gc-reaper', ['multi-validator'], { parent: network.ingressNs.ns });
+}
 
 let configuredAuth0;
 if (config.envFlag('CLUSTER_CONFIGURE_AUTH0', true)) {
