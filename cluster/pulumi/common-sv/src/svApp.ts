b8c3064ad04c7a7560fb49cf41c271b9e60691cf
Nicu Reut <nicu.reut@digitalasset.com>
2026-01-07 11:19:08 +0100

    Expose pruning config through pulumi
    Add ability to configure pruning for mediator/sequencer/cometbft/participant for an SV through pulumi config.yaml and remove the env variables that were used.
    
    Also refactor the sv app/validator app installation to remove some duplication between canton-network and the sv-runbook project.
    
    [static]
    
    Signed-off-by: Nicu Reut <nicu.reut@digitalasset.com>

--- cluster/pulumi/common-sv/src/svApp.ts
+++ cluster/pulumi/common-sv/src/svApp.ts
@@ -1,3 +1,5 @@
+// Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
 import * as pulumi from '@pulumi/pulumi';
 import { DecentralizedSynchronizerMigrationConfig } from '@lfdecentralizedtrust/splice-pulumi-common';
 
