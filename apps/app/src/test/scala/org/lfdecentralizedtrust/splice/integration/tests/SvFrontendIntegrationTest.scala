1a31bf5f7c09b0975693d7757403ed892cb431c2
Paweł Perek <pawel.perek@digitalasset.com>
2026-01-10 16:44:21 +0000

    restore grant featured app right test [ci]
    Signed-off-by: Paweł Perek <pawel.perek@digitalasset.com>

--- apps/app/src/test/scala/org/lfdecentralizedtrust/splice/integration/tests/SvFrontendIntegrationTest.scala
+++ apps/app/src/test/scala/org/lfdecentralizedtrust/splice/integration/tests/SvFrontendIntegrationTest.scala
@@ -1335,28 +1335,28 @@ class SvFrontendIntegrationTest
       }
     }
 
-    // "NEW UI: Grant and Revoke Featured App Right" in { implicit env =>
-    //   // First, create a Grant proposal and capture the contract ID
-    //   val grantProposalContractId = createProposal(
-    //     "SRARC_GrantFeaturedAppRight",
-    //     "grant-featured-app",
-    //   ) { implicit webDriver =>
-    //     eventually() {
-    //       inside(find(id("grant-featured-app-idValue"))) { case Some(element) =>
-    //         element.underlying.sendKeys("test-provider-party-id")
-    //       }
-    //     }
-    //   }
+    "NEW UI: Grant and Revoke Featured App Right" in { implicit env =>
+      // First, create a Grant proposal and capture the contract ID
+      val grantProposalContractId = createProposal(
+        "SRARC_GrantFeaturedAppRight",
+        "grant-featured-app",
+      ) { implicit webDriver =>
+        eventually() {
+          inside(find(id("grant-featured-app-idValue"))) { case Some(element) =>
+            element.underlying.sendKeys("test-provider-party-id")
+          }
+        }
+      }
 
-    //   // Now create a Revoke proposal using the Grant proposal's contract ID
-    //   createProposal("SRARC_RevokeFeaturedAppRight", "revoke-featured-app") { implicit webDriver =>
-    //     eventually() {
-    //       inside(find(id("revoke-featured-app-idValue"))) { case Some(element) =>
-    //         element.underlying.sendKeys(grantProposalContractId)
-    //       }
-    //     }
-    //   }
-    // }
+      // Now create a Revoke proposal using the Grant proposal's contract ID
+      // createProposal("SRARC_RevokeFeaturedAppRight", "revoke-featured-app") { implicit webDriver =>
+      //   eventually() {
+      //     inside(find(id("revoke-featured-app-idValue"))) { case Some(element) =>
+      //       element.underlying.sendKeys(grantProposalContractId)
+      //     }
+      //   }
+      // }
+    }
 
     "NEW UI: Set Dso Rules Configuration" in { implicit env =>
       createProposal("SRARC_SetConfig", "set-dso-config-rules") { _ =>
