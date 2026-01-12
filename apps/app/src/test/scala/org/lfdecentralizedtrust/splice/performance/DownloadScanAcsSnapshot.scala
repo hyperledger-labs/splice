845f8322a5d65443bbd4542a126324c189756fc6
Stephen Compall <stephen.compall@digitalasset.com>
2026-01-12 20:47:58 +0000

    use HttpScanAppClient commands for DownloadScan* [skip ci]
    Signed-off-by: Stephen Compall <stephen.compall@digitalasset.com>

--- apps/app/src/test/scala/org/lfdecentralizedtrust/splice/performance/DownloadScanAcsSnapshot.scala
+++ apps/app/src/test/scala/org/lfdecentralizedtrust/splice/performance/DownloadScanAcsSnapshot.scala
@@ -1,15 +1,15 @@
 package org.lfdecentralizedtrust.splice.performance
 
 import cats.data.Chain
+import com.daml.metrics.api.MetricHandle.LabeledMetricsFactory
 import com.digitalasset.canton.config.NonNegativeDuration
 import com.digitalasset.canton.logging.{NamedLoggerFactory, NamedLogging}
 import com.digitalasset.canton.tracing.TraceContext
 import org.apache.pekko.actor.{ActorSystem, Scheduler}
-import org.lfdecentralizedtrust.splice.admin.api.client.commands.HttpClientBuilder
-import org.lfdecentralizedtrust.splice.http.HttpClient
+import org.lfdecentralizedtrust.splice.http.{HttpClient, HttpClientMetrics}
 import org.lfdecentralizedtrust.splice.http.v0.definitions.AcsRequest.RecordTimeMatch
-import org.lfdecentralizedtrust.splice.http.v0.definitions.{AcsRequest, AcsResponse, CreatedEvent}
-import org.lfdecentralizedtrust.splice.http.v0.scan as http
+import org.lfdecentralizedtrust.splice.http.v0.definitions.{AcsResponse, CreatedEvent}
+import org.lfdecentralizedtrust.splice.scan.admin.api.client.commands.HttpScanAppClient.GetAcsSnapshotAt
 
 import java.nio.charset.StandardCharsets
 import java.nio.file.{Files, Path}
@@ -28,33 +28,35 @@ class DownloadScanAcsSnapshot(
     as: ActorSystem,
     override val loggerFactory: NamedLoggerFactory,
     tc: TraceContext,
+    metricsFactory: LabeledMetricsFactory,
 ) extends Runnable
     with NamedLogging {
 
   def run(): Unit = {
     implicit val httpClient: HttpClient = {
       // request timeout slightly higher than the default 38s in Splice servers
-      HttpClient(HttpClient.HttpRequestParameters(NonNegativeDuration.ofSeconds(40L)), logger)
+      HttpClient(
+        HttpClient.HttpRequestParameters(NonNegativeDuration.ofSeconds(40L)),
+        HttpClientMetrics(metricsFactory),
+        logger,
+      )
     }
     implicit val scheduler: Scheduler = as.scheduler
-
-    val client = http.ScanClient.httpClient(HttpClientBuilder().buildClient(), host)
 
     def query(after: Option[Long]): Future[AcsResponse] = {
       org.apache.pekko.pattern
         .retry(
           () => {
             logger.info(s"Querying at $after")
-            client
-              .getAcsSnapshotAt(
-                AcsRequest(
-                  migrationId.toLong,
-                  snapshotTime.atOffset(ZoneOffset.UTC),
-                  Some(RecordTimeMatch.AtOrBefore),
-                  after,
-                  pageSize = 1000,
-                )
-              )
+            val command = GetAcsSnapshotAt(
+              snapshotTime.atOffset(ZoneOffset.UTC),
+              migrationId = migrationId.toLong,
+              Some(RecordTimeMatch.AtOrBefore),
+              after = after,
+              pageSize = 1000,
+            )
+            command
+              .submitRequest(command.createClient(host), Nil)
               .value
               .map(
                 _.fold(
