6e01fd51e5d98b20b224b4e80225e72aad275315
Itai Segall <itai.segall@digitalasset.com>
2026-01-12 22:51:45 +0000

    [ci] fix scan_txlog python script too
    Signed-off-by: Itai Segall <itai.segall@digitalasset.com>

--- scripts/scan-txlog/scan_txlog.py
+++ scripts/scan-txlog/scan_txlog.py
@@ -4360,7 +4360,7 @@ async def main():
                     TransactionTree.parse(tx)
                     for tx in json_batch
                     if (stop_at_record_time is None)
-                    or (datetime.fromisoformat(tx["record_time"]) < stop_at_record_time)
+                    or (datetime.fromisoformat(tx["record_time"]) <= stop_at_record_time)
                 ]
                 LOG.debug(
                     f"Processing batch of size {len(batch)} starting at {app_state.pagination_key}"
