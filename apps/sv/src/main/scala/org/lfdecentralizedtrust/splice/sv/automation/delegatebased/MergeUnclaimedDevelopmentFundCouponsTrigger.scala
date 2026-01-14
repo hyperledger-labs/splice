06d59ac1169eee652c6a9ca86f2e87fa64204cb2
Jose Velasco - IEU <jose.velasco@intellecteu.com>
2026-01-14 15:07:05 +0000

    CIP-82 (Scala): Add MergeUnclaimedDevelopmentFundCouponsTrigger (#3522)
    PR reviewed in #3522
    CI run #3522
    
    Signed-off-by: Jose Velasco <jose.velasco@intellecteu.com>

---
+++ apps/sv/src/main/scala/org/lfdecentralizedtrust/splice/sv/automation/delegatebased/MergeUnclaimedDevelopmentFundCouponsTrigger.scala
@@ -0,0 +1,115 @@
+// Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package org.lfdecentralizedtrust.splice.sv.automation.delegatebased
+
+import org.lfdecentralizedtrust.splice.automation.{
+  PollingParallelTaskExecutionTrigger,
+  TaskOutcome,
+  TaskSuccess,
+  TriggerContext,
+}
+import org.lfdecentralizedtrust.splice.codegen.java.splice.amulet.UnclaimedDevelopmentFundCoupon
+import org.lfdecentralizedtrust.splice.codegen.java.splice.dsorules.DsoRules_MergeUnclaimedDevelopmentFundCoupons
+import org.lfdecentralizedtrust.splice.store.PageLimit
+import org.lfdecentralizedtrust.splice.util.Contract
+import org.lfdecentralizedtrust.splice.util.PrettyInstances.*
+import com.digitalasset.canton.logging.pretty.{Pretty, PrettyPrinting}
+import com.digitalasset.canton.tracing.TraceContext
+import io.opentelemetry.api.trace.Tracer
+import org.apache.pekko.stream.Materializer
+import org.lfdecentralizedtrust.splice.codegen.java.splice.amuletrules.AmuletRules_MergeUnclaimedDevelopmentFundCoupons
+import org.lfdecentralizedtrust.splice.store.AppStoreWithIngestion.SpliceLedgerConnectionPriority
+import org.lfdecentralizedtrust.splice.sv.config.SvAppBackendConfig
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.jdk.CollectionConverters.*
+
+class MergeUnclaimedDevelopmentFundCouponsTrigger(
+    svConfig: SvAppBackendConfig,
+    override protected val context: TriggerContext,
+    override protected val svTaskContext: SvTaskBasedTrigger.Context,
+)(implicit
+    override val ec: ExecutionContext,
+    mat: Materializer,
+    tracer: Tracer,
+) extends PollingParallelTaskExecutionTrigger[MergeUnclaimedDevelopmentFundCouponsTask]
+    with SvTaskBasedTrigger[MergeUnclaimedDevelopmentFundCouponsTask] {
+
+  private val store = svTaskContext.dsoStore
+
+  protected def retrieveTasks()(implicit
+      tc: TraceContext
+  ): Future[Seq[MergeUnclaimedDevelopmentFundCouponsTask]] = {
+    val threshold = svConfig.unclaimedDevelopmentFundCouponsThreshold
+    val limit = PageLimit.tryCreate(2 * threshold)
+    store.listUnclaimedDevelopmentFundCoupons(limit).map { unclaimedDevelopmentFundCoupons =>
+      if (unclaimedDevelopmentFundCoupons.length >= 2 * threshold) {
+        Seq(
+          MergeUnclaimedDevelopmentFundCouponsTask(
+            // Merge the `threshold` smallest coupons (by amount) to keep larger coupons stable and
+            // reduce contention with externally prepared transactions referencing contract-ids.
+            unclaimedDevelopmentFundCoupons.sortBy(_.payload.amount).take(threshold)
+          )
+        )
+      } else {
+        Seq()
+      }
+    }
+  }
+
+  protected def isStaleTask(
+      unclaimedDevelopmentFundCouponsTask: MergeUnclaimedDevelopmentFundCouponsTask
+  )(implicit tc: TraceContext): Future[Boolean] = store.multiDomainAcsStore.containsArchived(
+    unclaimedDevelopmentFundCouponsTask.contractsToMerge.map(_.contractId)
+  )
+
+  override def completeTaskAsDsoDelegate(
+      unclaimedDevelopmentFundCouponsTask: MergeUnclaimedDevelopmentFundCouponsTask,
+      controller: String,
+  )(implicit tc: TraceContext): Future[TaskOutcome] = {
+    for {
+      dsoRules <- store.getDsoRules()
+      amuletRules <- store.getAmuletRules()
+      choiceArg = new AmuletRules_MergeUnclaimedDevelopmentFundCoupons(
+        unclaimedDevelopmentFundCouponsTask.contractsToMerge
+          .map(_.contractId)
+          .asJava
+      )
+      arg = new DsoRules_MergeUnclaimedDevelopmentFundCoupons(
+        amuletRules.contractId,
+        choiceArg,
+        controller,
+      )
+      cmd = dsoRules.exercise(_.exerciseDsoRules_MergeUnclaimedDevelopmentFundCoupons(arg))
+      res <- for {
+        outcome <- svTaskContext
+          .connection(SpliceLedgerConnectionPriority.Low)
+          .submit(
+            Seq(store.key.svParty),
+            Seq(store.key.dsoParty),
+            cmd,
+          )
+          .noDedup
+          .yieldResult()
+      } yield Some(outcome)
+    } yield {
+      res
+        .map(cid => {
+          TaskSuccess(
+            s"Merged unclaimed development fund coupons into contract ${cid.exerciseResult.result.unclaimedDevelopmentFundCouponCid.contractId}"
+          )
+        })
+        .getOrElse(TaskSuccess(s"Not enough unclaimed development fund coupons to merge"))
+    }
+  }
+}
+
+case class MergeUnclaimedDevelopmentFundCouponsTask(
+    contractsToMerge: Seq[
+      Contract[UnclaimedDevelopmentFundCoupon.ContractId, UnclaimedDevelopmentFundCoupon]
+    ]
+) extends PrettyPrinting {
+  override def pretty: Pretty[this.type] =
+    prettyOfClass(param("contractsToMerge", _.contractsToMerge))
+}
