3108735b776acfffbad311959c6d1e43e0c89e29
Itai Segall <itai.segall@digitalasset.com>
2026-01-15 19:50:18 +0000

    [static] nix256
    Signed-off-by: Itai Segall <itai.segall@digitalasset.com>

--- build-tools/bump-canton.sh
+++ build-tools/bump-canton.sh
@@ -27,11 +27,16 @@ set_value version "$NEW_VERSION"
 
 _info "Fetching enterprise tar.gz hash..."
 enterprise_sha256=$(nix store prefetch-file --json --hash-type sha256 "https://digitalasset.jfrog.io/artifactory/canton-enterprise/canton-enterprise-${NEW_VERSION}.tar.gz" | jq -r '.hash')
-set_value enterprise_sha256 "$enterprise_sha256"
+# Apparently, nix256 is a thing... While nix seems to accept both, when you have the wrong one,
+# nix shows the nix256-formatted one, so we use that one too
+# (also for backward compatibility with the format we used before this script)
+enterprise_nix256="sha256:"$(nix hash convert --to nix32 "$enterprise_sha256")
+set_value enterprise_sha256 "$enterprise_nix256"
 
 _info "Fetching open source tar.gz hash..."
 oss_sha256=$(nix store prefetch-file --json --hash-type sha256 "https://www.canton.io/releases/canton-open-source-${NEW_VERSION}.tar.gz" | jq -r '.hash')
-set_value oss_sha256 "$oss_sha256"
+oss_nix256="sha256:"$(nix hash convert --to nix32 "$oss_sha256")
+set_value oss_sha256 "$oss_nix256"
 
 for img in base participant mediator sequencer; do
   _info "Fetching image sha256 for canton-$img..."
