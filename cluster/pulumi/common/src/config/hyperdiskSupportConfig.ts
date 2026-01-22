80486ddc2dd67ef5b3130c821a2ccae540e8c761
Nicu Reut <nicu.reut@digitalasset.com>
2026-01-22 09:28:14 +0000

    headers
    [static]
    
    Signed-off-by: Nicu Reut <nicu.reut@digitalasset.com>

--- cluster/pulumi/common/src/config/hyperdiskSupportConfig.ts
+++ cluster/pulumi/common/src/config/hyperdiskSupportConfig.ts
@@ -1,3 +1,5 @@
+// Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
 import { z } from 'zod';
 
 import { clusterSubConfig } from './config';
@@ -6,6 +8,7 @@ const HyperdiskSupportConfigSchema = z.object({
   hyperdiskSupport: z
     .object({
       enabled: z.boolean().default(false),
+      migrating: z.boolean().default(false),
     })
     .default({}),
 });
--- cluster/pulumi/common/src/storage/storageClass.ts
+++ cluster/pulumi/common/src/storage/storageClass.ts
@@ -1,3 +1,5 @@
+// Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
 import { hyperdiskSupportConfig } from '../config/hyperdiskSupportConfig';
 
 export const standardStorageClassName = hyperdiskSupportConfig.hyperdiskSupport.enabled
