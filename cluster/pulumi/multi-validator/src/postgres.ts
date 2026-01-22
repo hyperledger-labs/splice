0711ca2672a887b0629d09f37874cf4a540df6e1
Nicu Reut <nicu.reut@digitalasset.com>
2026-01-22 18:21:05 +0100

    add support for migrating postgres pvcs to hyperdisk (#3653)
    Signed-off-by: Nicu Reut <nicu.reut@digitalasset.com>

--- cluster/pulumi/multi-validator/src/postgres.ts
+++ cluster/pulumi/multi-validator/src/postgres.ts
@@ -4,14 +4,18 @@ import * as pulumi from '@pulumi/pulumi';
 import * as random from '@pulumi/random';
 import {
   activeVersion,
+  appsAffinityAndTolerations,
   CnInput,
   ExactNamespace,
-  installSpliceRunbookHelmChart,
-  installPostgresPasswordSecret,
   InstalledHelmChart,
-  nonHyperdiskAppsAffinityAndTolerations,
+  installPostgresPasswordSecret,
+  installSpliceRunbookHelmChart,
+  spliceConfig,
+  standardStorageClassName,
 } from '@lfdecentralizedtrust/splice-pulumi-common';
+import { CustomResource } from '@pulumi/kubernetes/apiextensions';
 
+import { hyperdiskSupportConfig } from '../../common/src/config/hyperdiskSupportConfig';
 import { multiValidatorConfig } from './config';
 
 export function installPostgres(
@@ -32,17 +36,66 @@ export function installPostgres(
   }
   const config = multiValidatorConfig!;
 
+  let hyperdiskMigrationValues = {};
+  if (
+    hyperdiskSupportConfig.hyperdiskSupport.enabled &&
+    hyperdiskSupportConfig.hyperdiskSupport.migrating
+  ) {
+    const pvcSnapshot = new CustomResource(`pg-data-${xns.logicalName}-${name}-snapshot`, {
+      apiVersion: 'snapshot.storage.k8s.io/v1',
+      kind: 'VolumeSnapshot',
+      metadata: {
+        name: `pg-data-${name}-snapshot`,
+        namespace: xns.logicalName,
+      },
+      spec: {
+        volumeSnapshotClassName: 'dev-vsc',
+        source: {
+          persistentVolumeClaimName: `pg-data-${name}-0`,
+        },
+      },
+    });
+    hyperdiskMigrationValues = {
+      dataSource: {
+        kind: 'VolumeSnapshot',
+        name: pvcSnapshot.metadata.name,
+        apiGroup: 'snapshot.storage.k8s.io',
+      },
+    };
+  }
   return installSpliceRunbookHelmChart(
     xns,
     name,
     'splice-postgres',
     {
       persistence: { secretName },
-      db: { volumeSize: config.postgresPvcSize, maxConnections: 1000 },
+      db: {
+        volumeSize: config.postgresPvcSize,
+        maxConnections: 1000,
+        ...(hyperdiskSupportConfig.hyperdiskSupport.enabled
+          ? {
+              volumeStorageClass: standardStorageClassName,
+              pvcTemplateName: 'pg-data-hd',
+              ...hyperdiskMigrationValues,
+            }
+          : {}),
+      },
       resources: config.resources?.postgres,
-      nonHyperdiskAppsAffinityAndTolerations,
+      appsAffinityAndTolerations,
     },
     activeVersion,
-    { dependsOn: [passwordSecret, ...dependsOn] }
+    {
+      dependsOn: [passwordSecret, ...dependsOn],
+      ...((hyperdiskSupportConfig.hyperdiskSupport.enabled &&
+        // during the migration we first delete the stateful set, which keeps the old pvcs, and the recreate with the new pvcs
+        // the stateful sets are immutable so they need to be recreated to force the change of the pvcs
+        hyperdiskSupportConfig.hyperdiskSupport.migrating) ||
+      spliceConfig.pulumiProjectConfig.replacePostgresStatefulSetOnChanges
+        ? {
+            replaceOnChanges: ['*'],
+            deleteBeforeReplace: true,
+          }
+        : {}),
+    }
   );
 }
