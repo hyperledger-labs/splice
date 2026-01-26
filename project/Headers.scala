e4725dd637f48edf5bc8486c26fb67c903aab666
Brian Healey <brian.healey@digitalasset.com>
2026-01-26 22:37:03 +0000

    update Headers.scala
    Signed-off-by: Brian Healey <brian.healey@digitalasset.com>

--- project/Headers.scala
+++ project/Headers.scala
@@ -1,4 +1,4 @@
-// Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// Copyright (c) 2026 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
 import de.heikoseeberger.sbtheader.FileType.firstLinePattern
@@ -30,7 +30,7 @@ object Headers {
   val ApacheDAHeaderLicense = Some(
     HeaderLicense
       .Custom(
-        """|Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+        """|Copyright (c) 2026 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
          |SPDX-License-Identifier: Apache-2.0
          |""".stripMargin
       )
