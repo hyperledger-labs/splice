20660c7dab87787226edec6b58efb95debfd2083
Jose Velasco <jose.velasco@intellecteu.com>
2026-01-27 08:49:11 +0000

    cip-82: revert bumping scan store version
    Signed-off-by: Jose Velasco <jose.velasco@intellecteu.com>

--- apps/scan/src/main/scala/org/lfdecentralizedtrust/splice/scan/store/db/DbScanStore.scala
+++ apps/scan/src/main/scala/org/lfdecentralizedtrust/splice/scan/store/db/DbScanStore.scala
@@ -112,7 +112,7 @@ class DbScanStore(
       // Any change in the store descriptor will lead to previously deployed applications
       // forgetting all persisted data once they upgrade to the new version.
       acsStoreDescriptor = StoreDescriptor(
-        version = 4,
+        version = 3,
         name = "DbScanStore",
         party = key.dsoParty,
         participant = participantId,
