6884bdc74fd6ffaeeca1bffab7414af7b0286c4a
Mateusz Błażejewski <mateusz.blazejewski@digitalasset.com>
2026-01-30 10:10:56 +0100

    [backport] add config resolution to the _update_cluster_config script (#3747)
    [static]
    
    Signed-off-by: Mateusz Błażejewski <mateusz.blazejewski@digitalasset.com>

--- cluster/scripts/get-resolved-config.sh
+++ cluster/scripts/get-resolved-config.sh
@@ -17,11 +17,7 @@ fi
 resolved_config_file="$cluster_dir/config.resolved.yaml"
 # if we are not in CI we might have some local config changes and the resolved config was not yet updated
 if [ -z "${CI:-}" ]; then
-  # resolve make parameters to allow running both for splice and internal clusters
-  make_directory="${cluster_dir}/../../.."
-  resolved_config_target="$(realpath --no-symlinks --relative-to "$make_directory" "$resolved_config_file")"
-  # make resolved config to apply local config changes
-  make --directory "$make_directory" "$resolved_config_target" > /dev/null
+  "${SPLICE_ROOT}/cluster/scripts/resolve-config.sh"
 fi
 
 # using resolved config to avoid duplicating config loader implementation
