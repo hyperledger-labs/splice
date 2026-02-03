9dcfad25a47d038d1cdeb3a88567bab87270a6b2
Itai Segall <itai.segall@digitalasset.com>
2026-02-03 05:05:18 -0500

    retry setting up external party (#3792)
    Fixes https://github.com/DACH-NY/cn-test-failures/issues/7126
    
    Signed-off-by: Itai Segall <itai.segall@digitalasset.com>

--- apps/app/src/test/scala/org/lfdecentralizedtrust/splice/integration/tests/WalletFrontendIntegrationTest.scala
+++ apps/app/src/test/scala/org/lfdecentralizedtrust/splice/integration/tests/WalletFrontendIntegrationTest.scala
@@ -231,16 +231,11 @@ class WalletFrontendIntegrationTest
 
         // Onboard three external parties as beneficiaries
         val beneficiary1Onboarding =
-          onboardExternalParty(aliceValidatorBackend, Some("beneficiary1"))
-        createAndAcceptExternalPartySetupProposal(aliceValidatorBackend, beneficiary1Onboarding)
-
+          onboardAndSetupExternalParty(aliceValidatorBackend, Some("beneficiary1"))
         val beneficiary2Onboarding =
-          onboardExternalParty(aliceValidatorBackend, Some("beneficiary2"))
-        createAndAcceptExternalPartySetupProposal(aliceValidatorBackend, beneficiary2Onboarding)
-
+          onboardAndSetupExternalParty(aliceValidatorBackend, Some("beneficiary2"))
         val beneficiary3Onboarding =
-          onboardExternalParty(aliceValidatorBackend, Some("beneficiary3"))
-        createAndAcceptExternalPartySetupProposal(aliceValidatorBackend, beneficiary3Onboarding)
+          onboardAndSetupExternalParty(aliceValidatorBackend, Some("beneficiary3"))
 
         // 2. Verify empty initial state via API
         clue("Check that no minting delegation proposals exist initially") {
