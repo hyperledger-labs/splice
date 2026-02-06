837b1bdaf20681ecf586233eb7a09f739e35b59d
moritzkiefer-da <45630097+moritzkiefer-da@users.noreply.github.com>
2026-02-06 15:03:58 +0100

    Upgrade pulumi operator to 2.4.1 (#3876)
    [static]
    
    Signed-off-by: Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
    Co-authored-by: Moritz Kiefer <moritz.kiefer@purelyfunctional.org>

--- cluster/pulumi/operator/src/operator.ts
+++ cluster/pulumi/operator/src/operator.ts
@@ -25,7 +25,7 @@ export const operator = new k8s.helm.v3.Release(
   {
     name: 'pulumi-kubernetes-operator',
     chart: 'oci://ghcr.io/pulumi/helm-charts/pulumi-kubernetes-operator',
-    version: '2.2.0',
+    version: '2.4.1',
     namespace: namespace.ns.metadata.name,
     values: {
       resources: {
