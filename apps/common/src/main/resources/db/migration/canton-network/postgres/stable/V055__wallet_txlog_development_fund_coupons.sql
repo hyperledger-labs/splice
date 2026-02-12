e913371b1c7c20dd47928624baf7ef26522fb44a
Jose Velasco <jose.velasco@intellecteu.com>
2026-02-12 10:48:04 +0000

    [ci] cip-82: remove entry_type and tx_log_id columns in the index given they're already present in the partial index's where
    Signed-off-by: Jose Velasco <jose.velasco@intellecteu.com>

--- apps/common/src/main/resources/db/migration/canton-network/postgres/stable/V055__wallet_txlog_development_fund_coupons.sql
+++ apps/common/src/main/resources/db/migration/canton-network/postgres/stable/V055__wallet_txlog_development_fund_coupons.sql
@@ -1,6 +1,6 @@
 alter table user_wallet_txlog_store add column development_fund_coupon_contract_id text;
 
 -- fast archival listing
-create index user_wallet_txlog_store_archcoupon_sid_tli_et_en on user_wallet_txlog_store(store_id, tx_log_id, entry_type, entry_number desc) where tx_log_id='dev' and entry_type = 'fca';
+create index user_wallet_txlog_store_archcoupon_sid_en on user_wallet_txlog_store(store_id, entry_number desc) where tx_log_id='dev' and entry_type = 'fca';
 -- fast create retrieval
-create index user_wallet_txlog_store_createdcoupon_sid_tli_et_dccid on user_wallet_txlog_store(store_id, tx_log_id, entry_type, development_fund_coupon_contract_id) where tx_log_id='dev' and entry_type = 'fcc';
+create index user_wallet_txlog_store_createdcoupon_sid_dccid on user_wallet_txlog_store(store_id, development_fund_coupon_contract_id) where tx_log_id='dev' and entry_type = 'fcc';
