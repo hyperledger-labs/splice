db102ca244f9a22b53e5805afb86031855180a32
Stephen Compall <stephen.compall@digitalasset.com>
2026-02-17 21:25:49 +0000

    avoid cast [skip ci]
    Signed-off-by: Stephen Compall <stephen.compall@digitalasset.com>

--- cluster/pulumi/common/src/config/scanEndpoints.ts
+++ cluster/pulumi/common/src/config/scanEndpoints.ts
@@ -1,15 +1,17 @@
 // Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 import * as path from 'path';
+import { z } from 'zod';
 
 import { readAndParseYaml } from './configLoader';
 
 const scanYamlPath = path.join(__dirname, '../../../../../apps/scan/src/main/openapi/scan.yaml');
 
+const MinimalOpenApiSchema = z.object({ paths: z.object({}).catchall(z.unknown()).default({}) });
+
 export function parseScanYamlEndpoints(): string[] {
-  // TODO (DACH-NY/canton-network-internal#3697) minimal type to retrieve paths
-  const yaml = readAndParseYaml(scanYamlPath) as { paths: { [p: string]: unknown } };
-  const paths = yaml.paths || {};
+  const yaml = MinimalOpenApiSchema.parse(readAndParseYaml(scanYamlPath));
+  const paths = yaml.paths;
 
   const endpoints = new Set<string>();
 
