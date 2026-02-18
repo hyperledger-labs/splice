4a28143e9e7ef17726298c3fc530380c15a3451e
Jagath Weerasinghe <jagath.weerasinghe@digitalasset.com>
2026-02-18 08:16:57 +0000

    update flyway doc
    [ci]
    
    Signed-off-by: Jagath Weerasinghe <jagath.weerasinghe@digitalasset.com>

--- apps/common/src/main/resources/db/migration/canton-network/postgres/stable/V056__update_history_for_hash.sql
+++ apps/common/src/main/resources/db/migration/canton-network/postgres/stable/V056__update_history_for_hash.sql
@@ -1,6 +1,5 @@
 ALTER TABLE update_history_transactions ADD COLUMN external_transaction_hash bytea;
 
--- Creating the 'updt_hist_tran_hi_eth' index on a production-sized database (~150M rows) can take ~300s.
--- To avoid long migration waits, the index is created asynchronously by the application. The SQL is left commented for reference.
-
+-- To avoid long migration waits, the index is created asynchronously by the application (SqlIndexInitializationTrigger)
+-- The canonical DDL is kept commented here for reference and future use if we move away from app-managed index creation or need manual backfill:
 -- create index updt_hist_tran_hi_eth on update_history_transactions (history_id, external_transaction_hash) where external_transaction_hash is not null;
