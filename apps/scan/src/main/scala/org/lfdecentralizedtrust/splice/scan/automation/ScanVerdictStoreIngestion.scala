301b73a91b7513353090587e1a10c4599f5dff46
Tim Emiola <adetokunbo@emio.la>
2026-02-20 15:19:27 +0900

    Add the TODO(#2856) link that was inadvertently removed
    Signed-off-by: Tim Emiola <adetokunbo@emio.la>

--- apps/scan/src/main/scala/org/lfdecentralizedtrust/splice/scan/automation/ScanVerdictStoreIngestion.scala
+++ apps/scan/src/main/scala/org/lfdecentralizedtrust/splice/scan/automation/ScanVerdictStoreIngestion.scala
@@ -200,6 +200,9 @@ class ScanVerdictStoreIngestion(
         case Failure(ex) =>
           ingestionMetrics.errors.mark()
           Failure(ex)
+        // TODO(#2856): just failing here may result in skipping ingestion.
+        // This must be fixed as otherwise we loose determinism!
+        // Fix this to ensure ingestion always works reliably.
       }
     }
   }
