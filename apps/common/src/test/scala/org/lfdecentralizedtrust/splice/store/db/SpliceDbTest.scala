8417325072173deb019c49343de47192b2b129bc
Divam <dfordivam@gmail.com>
2026-02-20 15:43:50 +0900

    Add DbScanTemporalAcsStore
    Signed-off-by: Divam <dfordivam@gmail.com>

--- apps/common/src/test/scala/org/lfdecentralizedtrust/splice/store/db/SpliceDbTest.scala
+++ apps/common/src/test/scala/org/lfdecentralizedtrust/splice/store/db/SpliceDbTest.scala
@@ -91,6 +91,8 @@ trait SpliceDbTest extends DbTest with BeforeAndAfterAll { this: Suite =>
                 update_history_backfilling,
                 acs_snapshot_data,
                 acs_snapshot,
+                scan_tcs_store_active,
+                scan_tcs_store_archived,
                 scan_verdict_store,
                 scan_verdict_transaction_view_store,
                 key_value_store
