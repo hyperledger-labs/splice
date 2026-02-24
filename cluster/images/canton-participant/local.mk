3e046857313477551bf24ee19e9e5b288a2e9c64
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2026-02-24 08:11:37 +0000

    Set autovacuum and autoanalyze settings for common_sequenced_events
    [ci]
    
    Didn't do a super comprehensive check for what other tables may matter
    but this was the only one we actually saw issues with so seems good
    enough for enabling pruning for SVs. Worst case we can easily
    overwrite it now without a new release.
    
    Signed-off-by: Moritz Kiefer <moritz.kiefer@purelyfunctional.org>

--- cluster/images/canton-participant/local.mk
+++ cluster/images/canton-participant/local.mk
@@ -4,4 +4,4 @@
 dir := $(call current_dir)
 
 $(dir)/$(docker-build): build_arg := --build-arg canton_version=${CANTON_VERSION} --build-arg image_sha256=${CANTON_PARTICIPANT_IMAGE_SHA256}
-
+$(dir)/$(docker-build): $(dir)/additional-config.conf $(dir)/R__repeatable-migration.sql
