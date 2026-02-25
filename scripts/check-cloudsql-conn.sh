f07aa939e21dcda92b845622aaf93d77cfa15215
Oriol Muñoz <oriol.munoz@digitalasset.com>
2026-02-25 16:16:18 +0000

    [ci] nix nix nix
    Signed-off-by: Oriol Muñoz <oriol.munoz@digitalasset.com>

---
+++ scripts/check-cloudsql-conn.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+# check-cloudsql-conn.sh
+
+echo "Checking connection to Splice services..."
+PGPASSWORD="$PERFORMANCE_TESTS_DB_PASSWORD" psql -h"$PERFORMANCE_TESTS_DB_HOST" -U"$PERFORMANCE_TESTS_DB_USER" -dcantonnet -c '\l' || (echo "Connection to DB failed" && exit 1)
