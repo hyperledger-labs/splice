ba5bf665e3135a6f5104e27e73d2a1099fb1d941
Julien Tinguely <julien.tinguely@digitalasset.com>
2026-02-25 12:36:58 +0000

    overwrite logback files and fix log in scratch [static]
    Signed-off-by: Julien Tinguely <julien.tinguely@digitalasset.com>

--- cluster/images/canton-participant/local.mk
+++ cluster/images/canton-participant/local.mk
@@ -3,5 +3,11 @@
 
 dir := $(call current_dir)
 
+target-logback := $(dir)/target/logback.xml
+
 $(dir)/$(docker-build): build_arg := --build-arg canton_version=${CANTON_VERSION} --build-arg image_sha256=${CANTON_PARTICIPANT_IMAGE_SHA256}
 $(dir)/$(docker-build): $(dir)/additional-config.conf $(dir)/R__repeatable-migration.sql
+$(dir)/$(docker-build): $(target-logback)
+
+$(target-logback): ${SPLICE_ROOT}/scripts/canton-logback.xml | $(dir)/target
+	cp $< $@
