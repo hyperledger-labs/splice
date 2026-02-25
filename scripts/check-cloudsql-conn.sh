cfa5a618a8ff24196bafa5b5e9b854a56d0335d3
Oriol Muñoz <oriol.munoz@digitalasset.com>
2026-02-25 16:48:06 +0000

    [ci] sanity check
    Signed-off-by: Oriol Muñoz <oriol.munoz@digitalasset.com>

--- scripts/check-cloudsql-conn.sh
+++ scripts/check-cloudsql-conn.sh
@@ -5,5 +5,5 @@
 
 # check-cloudsql-conn.sh
 
-echo "Checking connection to Splice services..."
+echo "Checking connection to Splice services... Connecting to '$PERFORMANCE_TESTS_DB_HOST' with user '$PERFORMANCE_TESTS_DB_USER'..."
 PGPASSWORD="$PERFORMANCE_TESTS_DB_PASSWORD" psql -h"$PERFORMANCE_TESTS_DB_HOST" -U"$PERFORMANCE_TESTS_DB_USER" -dcantonnet -c '\l' || (echo "Connection to DB failed" && exit 1)
