b97eee632bb3fe972e8e0303dda49c74d4448541
Pasindu Tennage <pasindu.tennage@digitalasset.com>
2026-03-10 13:11:45 +0000

    Upgrade versions
    [ci]
    
    Signed-off-by: Pasindu Tennage <pasindu.tennage@digitalasset.com>

--- apps/dar-resources-generator/src/main/scala/org/lfdecentralizedtrust/splice/darutils/DarResources.scala
+++ apps/dar-resources-generator/src/main/scala/org/lfdecentralizedtrust/splice/darutils/DarResources.scala
@@ -81,6 +81,7 @@ object DarResources {
   lazy val amulet_0_1_14 = DarResource("splice-amulet-0.1.14.dar")
   lazy val amulet_0_1_15 = DarResource("splice-amulet-0.1.15.dar")
   lazy val amulet_0_1_16 = DarResource("splice-amulet-0.1.16.dar")
+  lazy val amulet_0_1_17 = DarResource("splice-amulet-0.1.17.dar")
   lazy val amulet_current = DarResource("splice-amulet-current.dar")
   lazy val amulet = PackageResource(
     amulet_current,
@@ -103,6 +104,7 @@ object DarResources {
       amulet_0_1_14,
       amulet_0_1_15,
       amulet_0_1_16,
+      amulet_0_1_17,
     ),
   )
 
@@ -129,6 +131,7 @@ object DarResources {
   lazy val dsoGovernance_0_1_20 = DarResource("splice-dso-governance-0.1.20.dar")
   lazy val dsoGovernance_0_1_21 = DarResource("splice-dso-governance-0.1.21.dar")
   lazy val dsoGovernance_0_1_22 = DarResource("splice-dso-governance-0.1.22.dar")
+  lazy val dsoGovernance_0_1_23 = DarResource("splice-dso-governance-0.1.23.dar")
   lazy val dsoGovernance_current = DarResource("splice-dso-governance-current.dar")
   lazy val dsoGovernance = PackageResource(
     dsoGovernance_current,
@@ -157,6 +160,7 @@ object DarResources {
       dsoGovernance_0_1_20,
       dsoGovernance_0_1_21,
       dsoGovernance_0_1_22,
+      dsoGovernance_0_1_23,
     ),
   )
 
@@ -178,6 +182,7 @@ object DarResources {
   lazy val amuletNameService_0_1_15 = DarResource("splice-amulet-name-service-0.1.15.dar")
   lazy val amuletNameService_0_1_16 = DarResource("splice-amulet-name-service-0.1.16.dar")
   lazy val amuletNameService_0_1_17 = DarResource("splice-amulet-name-service-0.1.17.dar")
+  lazy val amuletNameService_0_1_18 = DarResource("splice-amulet-name-service-0.1.18.dar")
   lazy val amuletNameService_current = DarResource("splice-amulet-name-service-current.dar")
   lazy val amuletNameService = PackageResource(
     amuletNameService_current,
@@ -201,6 +206,7 @@ object DarResources {
       amuletNameService_0_1_15,
       amuletNameService_0_1_16,
       amuletNameService_0_1_17,
+      amuletNameService_0_1_18,
     ),
   )
 
@@ -222,6 +228,7 @@ object DarResources {
   lazy val splitwell_0_1_15 = DarResource("splitwell-0.1.15.dar")
   lazy val splitwell_0_1_16 = DarResource("splitwell-0.1.16.dar")
   lazy val splitwell_0_1_17 = DarResource("splitwell-0.1.17.dar")
+  lazy val splitwell_0_1_18 = DarResource("splitwell-0.1.18.dar")
   lazy val splitwell_current = DarResource("splitwell-current.dar")
   lazy val splitwell = PackageResource(
     splitwell_current,
@@ -245,6 +252,7 @@ object DarResources {
       splitwell_0_1_15,
       splitwell_0_1_16,
       splitwell_0_1_17,
+      splitwell_0_1_18,
     ),
   )
 
@@ -266,6 +274,7 @@ object DarResources {
   lazy val wallet_0_1_15 = DarResource("splice-wallet-0.1.15.dar")
   lazy val wallet_0_1_16 = DarResource("splice-wallet-0.1.16.dar")
   lazy val wallet_0_1_17 = DarResource("splice-wallet-0.1.17.dar")
+  lazy val wallet_0_1_18 = DarResource("splice-wallet-0.1.18.dar")
   lazy val wallet_current = DarResource("splice-wallet-current.dar")
   lazy val wallet = PackageResource(
     wallet_current,
@@ -289,6 +298,7 @@ object DarResources {
       wallet_0_1_15,
       wallet_0_1_16,
       wallet_0_1_17,
+      wallet_0_1_18,
     ),
   )
 
@@ -309,6 +319,7 @@ object DarResources {
   lazy val walletPayments_0_1_14 = DarResource("splice-wallet-payments-0.1.14.dar")
   lazy val walletPayments_0_1_15 = DarResource("splice-wallet-payments-0.1.15.dar")
   lazy val walletPayments_0_1_16 = DarResource("splice-wallet-payments-0.1.16.dar")
+  lazy val walletPayments_0_1_17 = DarResource("splice-wallet-payments-0.1.17.dar")
   lazy val walletPayments_current = DarResource("splice-wallet-payments-current.dar")
   lazy val walletPayments = PackageResource(
     walletPayments_current,
@@ -331,6 +342,7 @@ object DarResources {
       walletPayments_0_1_14,
       walletPayments_0_1_15,
       walletPayments_0_1_16,
+      walletPayments_0_1_17,
     ),
   )
 
