3e5bedf93a71aaf6512d8b3bab9ad7c14099b08a
Itai Segall <itai.segall@digitalasset.com>
2026-03-13 22:13:41 +0000

    [ci] hdr
    Signed-off-by: Itai Segall <itai.segall@digitalasset.com>

--- apps/scan/src/main/scala/org/lfdecentralizedtrust/splice/scan/admin/api/client/ScanStreamClient.scala
+++ apps/scan/src/main/scala/org/lfdecentralizedtrust/splice/scan/admin/api/client/ScanStreamClient.scala
@@ -1,3 +1,6 @@
+// Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package org.lfdecentralizedtrust.splice.scan.admin.api.client
 
 import org.lfdecentralizedtrust.splice.http.v0.Implicits.*
