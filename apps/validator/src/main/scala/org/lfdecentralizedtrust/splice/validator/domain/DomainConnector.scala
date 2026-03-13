e270c6758cb694ba549f17856b76ad1c3303abfe
Martin Florian <martin.florian@digitalasset.com>
2026-03-13 09:49:38 +0000

    Revert "Fix validator restart error resulting from failed scan service (#4255)"
    This reverts commit aae4f8b7f15b51e2643d37417526c544f1670d7d.
    
    [static]
    
    Signed-off-by: Martin Florian <martin.florian@digitalasset.com>

--- apps/validator/src/main/scala/org/lfdecentralizedtrust/splice/validator/domain/DomainConnector.scala
+++ apps/validator/src/main/scala/org/lfdecentralizedtrust/splice/validator/domain/DomainConnector.scala
@@ -134,7 +134,7 @@ class DomainConnector(
       // The only case where this can happen is during a domain migration and even then
       // it is fairly unlikely outside of tests for validators to come up fast enough that
       // scan has not yet updated.
-      RetryFor.WaitingOnInitDependency, // because the scan connections might still be in bootstrap phase
+      RetryFor.ClientCalls,
       "scan_sequencer_connections",
       "non-empty sequencer connections from scan",
       getSequencerConnectionsFromScan(Right(clock))
