c05a62220ff7e5f47d019c26524fdb30eaca99e7
Pasindu Tennage <pasindu.tennage@digitalasset.com>
2026-03-18 14:16:13 +0000

    Work arround for validator-participant race condition, when recovering using DB
    [ci]
    
    Signed-off-by: Pasindu Tennage <pasindu.tennage@digitalasset.com>

--- apps/common/src/main/scala/org/lfdecentralizedtrust/splice/setup/NodeInitializer.scala
+++ apps/common/src/main/scala/org/lfdecentralizedtrust/splice/setup/NodeInitializer.scala
@@ -29,6 +29,7 @@ import org.lfdecentralizedtrust.splice.identities.NodeIdentitiesDump
 import org.lfdecentralizedtrust.splice.util.PrettyInstances.prettyString
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration.*
 
 class NodeInitializer(
     connection: TopologyAdminConnection & StatusAdminConnection,
@@ -109,17 +110,13 @@ class NodeInitializer(
       nodeIdentity: UniqueIdentifier => Member & NodeIdentity,
   )(implicit tc: TraceContext, ec: ExecutionContext): Future[Unit] = {
     logger.info(s"Making sure canton node has an identity")
+    var idDeadline: Option[Deadline] = None
     for {
       // If the node was started concurrently with the app, it might not immediately be responding, so we're
       // retrying the getId() call.
       // Note that Canton nodes enable their endpoints one at a time, and return NOT_IMPLEMENTED while an endpoint
       // is not yet enabled. E.g., even if a node returned something to a getStatus() request, it might still fail
       // a subsequent getId() request with NOT_IMPLEMENTED.
-      //
-      // Because initialized == uniqueIdentifier.isDefined, we cannot distinguish between:
-      //   (a) no ID has been set yet (node has fully booted and is waiting for an ID), and
-      //   (b) the node is still booting and hasn't yet loaded its previously-set ID.
-      // To disambiguate, we cross-check with the status endpoint:
 
       nodeId <- retryProvider.retry(
         RetryFor.WaitingOnInitDependency,
@@ -129,19 +126,27 @@ class NodeInitializer(
           if (result.uniqueIdentifier.isDefined || result.initialized) {
             Future.successful(result)
           } else {
-            // No ID set yet according to GetId. Check the status endpoint to determine whether
-            // the node is genuinely waiting for an ID. If not we assume that it is still bootstrapping.
-            connection.getStatus.flatMap {
-              case NodeStatus.NotInitialized(_, Some(WaitingForId)) =>
-                Future.successful(result)
-              case status =>
-                Future.failed(
-                  Status.UNAVAILABLE
-                    .withDescription(
-                      s"Node is still bootstrapping (status: $status). Retrying getId..."
-                    )
-                    .asRuntimeException()
-                )
+            // currently we don't have a mechanism to distinguish between the following 2 states.
+            //    a. a participant that has completed bootstrapping and waiting for the validator to set a new Id
+            //    b. a participant that is still bootstrapping, and not yet set its Id from the db
+            // as a (temporary) work around, if participant has no id set, we retry getId for a maximum of 5s duration
+            // we chose 5s, because, according to logs, 5s is enough for the participant to set it's id, if it already has one
+
+            val deadline = idDeadline.getOrElse {
+              val d = 5.seconds.fromNow
+              idDeadline = Some(d)
+              d
+            }
+            if (deadline.hasTimeLeft()) {
+              Future.failed(
+                Status.UNAVAILABLE
+                  .withDescription(
+                    s"Node returned empty ID. Retrying to ensure it is not still bootstrapping... (Time left: ${deadline.timeLeft.toMillis}ms)"
+                  )
+                  .asRuntimeException()
+              )
+            } else {
+              Future.successful(result)
             }
           }
         },
