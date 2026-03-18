2b62a535b1abaa7c90ff404bbfdad529731bd0ee
Nicu Reut <nicu.reut@digitalasset.com>
2026-03-18 13:10:09 +0100

    enable hyperdisk by default and switch to n4d by default (#4475)
    [static]
    
    Signed-off-by: Nicu Reut <nicu.reut@digitalasset.com>

--- cluster/pulumi/common/src/config/hyperdiskSupportConfig.ts
+++ cluster/pulumi/common/src/config/hyperdiskSupportConfig.ts
@@ -7,12 +7,12 @@ import { clusterSubConfig } from './config';
 const HyperdiskSupportConfigSchema = z.object({
   hyperdiskSupport: z
     .object({
-      enabled: z.boolean().default(false),
-      enabledForInfra: z.boolean().default(false),
+      enabled: z.boolean(),
+      enabledForInfra: z.boolean(),
       migrating: z.boolean().default(false),
       migratingInfra: z.boolean().default(false),
     })
-    .default({}),
+    .strict(),
 });
 
 export type HyperdiskSupportConfig = z.infer<typeof HyperdiskSupportConfigSchema>;
