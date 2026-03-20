db0604a341bb6b2cf7c0994b135450d8d7f8a2cc
Robert Autenrieth <robert.autenrieth@digitalasset.com>
2026-03-20 09:59:50 +0000

    Also drop constraint
    [ci]
    
    Signed-off-by: Robert Autenrieth <robert.autenrieth@digitalasset.com>

--- apps/common/src/main/resources/db/migration/canton-network/postgres/stable/V062__drop_redundant_acs_snapshot_indexes.sql
+++ apps/common/src/main/resources/db/migration/canton-network/postgres/stable/V062__drop_redundant_acs_snapshot_indexes.sql
@@ -1,5 +1,8 @@
--- Not needed anymore after switching for incremental snapshot generation
+-- Not needed anymore after switching for incremental snapshot generation.
 drop index acs_snapshot_data_cid;
 
--- Redundant with acs_snapshot_data_all_filters by including stakeholder = DSO
+-- Redundant with acs_snapshot_data_all_filters by including stakeholder = DSO.
 drop index acs_snapshot_data_template_only_filter;
+
+-- Enforcing this constraint is too expensive. We'll trust our application code not to break referential integrity.
+alter table acs_snapshot_data drop constraint acs_snapshot_data_create_id_fkey;
