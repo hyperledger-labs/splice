36c870bed5bf8593f9f8f52c385b195cc89ab574
Eric Mann <eric@avrofi.com>
2026-03-31 18:52:45 +0000

    Merge CopyVotes integration coverage into a single test
    Reduce integration test startup overhead by exercising the accept,
    reject, update, and no-op scenarios within one end-to-end test.
    
    Signed-off-by: Eric Mann <eric@avrofi.com>

--- apps/app/src/test/scala/org/lfdecentralizedtrust/splice/integration/tests/CopyVotesIntegrationTest.scala
+++ apps/app/src/test/scala/org/lfdecentralizedtrust/splice/integration/tests/CopyVotesIntegrationTest.scala
@@ -35,195 +35,196 @@ class CopyVotesIntegrationTest extends IntegrationTestWithIsolatedEnvironment wi
           )(config),
       )
 
-  "CopyVotesTrigger copies votes from source SV" in { implicit env =>
-    def runTriggerOnSv2 =
-      sv2Backend.dsoAutomation
-        .trigger[CopyVotesTrigger]
-        .runOnce()
-        .futureValue
+  "CopyVotesTrigger mirrors source votes across accept, reject, update, and no-op cases" in {
+    implicit env =>
+      def runTriggerOnSv2 =
+        sv2Backend.dsoAutomation
+          .trigger[CopyVotesTrigger]
+          .runOnce()
+          .futureValue
 
-    val sv4Party = sv4Backend.getDsoInfo().svParty.toProtoPrimitive
-    val action = new ARC_DsoRules(
-      new SRARC_OffboardSv(new DsoRules_OffboardSv(sv4Party))
-    )
+      def voteRequestOnSv2(
+          trackingId: org.lfdecentralizedtrust.splice.codegen.java.splice.dsorules.VoteRequest.ContractId
+      ) =
+        sv2Backend
+          .listVoteRequests()
+          .find(vr => getTrackingId(vr) == trackingId)
+          .value
 
-    val (_, voteRequest) = actAndCheck(
-      "sv1 creates a vote request to offboard sv4",
-      sv1Backend.createVoteRequest(
-        sv1Backend.getDsoInfo().svParty.toProtoPrimitive,
-        action,
-        "url",
-        "offboard sv4",
-        sv1Backend.getDsoInfo().dsoRules.payload.config.voteRequestTimeout,
-        None,
-      ),
-    )(
-      "vote request is created with sv1's accept vote",
-      _ => {
-        val vr = sv2Backend.listVoteRequests().loneElement
-        vr.payload.votes.asScala should have size 1
-        vr
-      },
-    )
+      def voteRequestOnSv2ByBody(body: String) =
+        sv2Backend
+          .listVoteRequests()
+          .find(_.payload.reason.body == body)
+          .value
 
-    actAndCheck(
-      "run CopyVotesTrigger on sv2", {
-        runTriggerOnSv2
-      },
-    )(
-      "sv2 copies sv1's accept vote",
-      _ => {
-        val vr = sv2Backend.listVoteRequests().loneElement
-        val votes = vr.payload.votes.asScala
-        votes should have size 2
-        val sv2Vote = votes("Digital-Asset-Eng-2")
-        sv2Vote.accept shouldBe true
-        sv2Vote.reason.body should include("Automatically Copied from Digital-Asset-2")
-      },
-    )
+      val sv4Party = sv4Backend.getDsoInfo().svParty.toProtoPrimitive
+      val action = new ARC_DsoRules(
+        new SRARC_OffboardSv(new DsoRules_OffboardSv(sv4Party))
+      )
 
-    actAndCheck(
-      "run CopyVotesTrigger again on sv2 (should be no-op)", {
-        runTriggerOnSv2
-      },
-    )(
-      "sv2 still has exactly one vote (no duplicate)",
-      _ => {
-        val vr = sv2Backend.listVoteRequests().loneElement
-        val votes = vr.payload.votes.asScala
-        votes should have size 2
-      },
-    )
-  }
+      val (_, acceptVoteRequest) = actAndCheck(
+        "sv1 creates a vote request to offboard sv4",
+        sv1Backend.createVoteRequest(
+          sv1Backend.getDsoInfo().svParty.toProtoPrimitive,
+          action,
+          "url",
+          "offboard sv4",
+          sv1Backend.getDsoInfo().dsoRules.payload.config.voteRequestTimeout,
+          None,
+        ),
+      )(
+        "the initial vote request is visible to sv2",
+        _ => {
+          val vr = sv2Backend.listVoteRequests().loneElement
+          vr.payload.votes.asScala should have size 1
+          vr
+        },
+      )
+      val acceptTrackingId = getTrackingId(acceptVoteRequest)
 
-  "CopyVotesTrigger copies reject votes" in { implicit env =>
-    def runTriggerOnSv2 =
-      sv2Backend.dsoAutomation
-        .trigger[CopyVotesTrigger]
-        .runOnce()
-        .futureValue
+      actAndCheck(
+        "run CopyVotesTrigger on sv2 for the initial accept vote",
+        runTriggerOnSv2,
+      )(
+        "sv2 copies sv1's accept vote",
+        _ => {
+          val vr = voteRequestOnSv2(acceptTrackingId)
+          val votes = vr.payload.votes.asScala
+          votes should have size 2
+          val sv2Vote = votes("Digital-Asset-Eng-2")
+          sv2Vote.accept shouldBe true
+          sv2Vote.reason.body should include("Automatically Copied from Digital-Asset-2")
+        },
+      )
 
-    val sv4Party = sv4Backend.getDsoInfo().svParty.toProtoPrimitive
-    val action = new ARC_DsoRules(
-      new SRARC_OffboardSv(new DsoRules_OffboardSv(sv4Party))
-    )
+      actAndCheck(
+        "run CopyVotesTrigger again on sv2 for the same vote",
+        runTriggerOnSv2,
+      )(
+        "sv2 does not create a duplicate vote",
+        _ => {
+          val vr = voteRequestOnSv2(acceptTrackingId)
+          vr.payload.votes.asScala should have size 2
+        },
+      )
 
-    val (_, voteRequest) = actAndCheck(
-      "sv3 creates a vote request to offboard sv4",
-      sv3Backend.createVoteRequest(
-        sv3Backend.getDsoInfo().svParty.toProtoPrimitive,
-        action,
-        "url",
-        "offboard sv4",
-        sv3Backend.getDsoInfo().dsoRules.payload.config.voteRequestTimeout,
-        None,
-      ),
-    )(
-      "vote request is created",
-      _ => sv2Backend.listVoteRequests().loneElement,
-    )
+      val (_, rejectVoteRequest) = actAndCheck(
+        "sv3 creates another vote request to offboard sv4",
+        sv3Backend.createVoteRequest(
+          sv3Backend.getDsoInfo().svParty.toProtoPrimitive,
+          action,
+          "url",
+          "offboard sv4 again",
+          sv3Backend.getDsoInfo().dsoRules.payload.config.voteRequestTimeout,
+          None,
+        ),
+      )(
+        "the second vote request is visible to sv2",
+        _ => {
+          val vr = voteRequestOnSv2ByBody("offboard sv4 again")
+          vr.payload.votes.asScala should have size 1
+          vr
+        },
+      )
+      val rejectTrackingId = getTrackingId(rejectVoteRequest)
 
-    actAndCheck(
-      "sv1 casts a reject vote",
-      sv1Backend.castVote(
-        getTrackingId(voteRequest),
-        false,
-        "url",
-        "I disagree",
-      ),
-    )(
-      "sv1's reject vote is recorded",
-      _ => {
-        val vr = sv2Backend.listVoteRequests().loneElement
-        vr.payload.votes.asScala should have size 2
-      },
-    )
+      actAndCheck(
+        "sv1 casts a reject vote on the second request",
+        sv1Backend.castVote(
+          getTrackingId(rejectVoteRequest),
+          false,
+          "url",
+          "I disagree",
+        ),
+      )(
+        "sv1's reject vote is recorded",
+        _ => {
+          val vr = voteRequestOnSv2(rejectTrackingId)
+          vr.payload.votes.asScala should have size 2
+        },
+      )
 
-    actAndCheck(
-      "run CopyVotesTrigger on sv2", {
-        runTriggerOnSv2
-      },
-    )(
-      "sv2 copies sv1's reject vote",
-      _ => {
-        val vr = sv2Backend.listVoteRequests().loneElement
-        val votes = vr.payload.votes.asScala
-        votes should have size 3
-        val sv2Vote = votes("Digital-Asset-Eng-2")
-        sv2Vote.accept shouldBe false
-        sv2Vote.reason.body should include("Automatically Copied from Digital-Asset-2")
-        sv2Vote.reason.body should include("I disagree")
-      },
-    )
+      actAndCheck(
+        "run CopyVotesTrigger on sv2 for the reject vote",
+        runTriggerOnSv2,
+      )(
+        "sv2 copies sv1's reject vote",
+        _ => {
+          val vr = voteRequestOnSv2(rejectTrackingId)
+          val votes = vr.payload.votes.asScala
+          votes should have size 3
+          val sv2Vote = votes("Digital-Asset-Eng-2")
+          sv2Vote.accept shouldBe false
+          sv2Vote.reason.body should include("Automatically Copied from Digital-Asset-2")
+          sv2Vote.reason.body should include("I disagree")
+        },
+      )
 
-    actAndCheck(
-      "sv1 changes its vote to accept",
-      sv1Backend.castVote(
-        getTrackingId(voteRequest),
-        true,
-        "new-url",
-        "I changed my mind",
-      ),
-    )(
-      "sv1's updated vote is recorded",
-      _ => {
-        val vr = sv2Backend.listVoteRequests().loneElement
-        val votes = vr.payload.votes.asScala
-        votes should have size 3
-        votes("Digital-Asset-2").accept shouldBe true
-      },
-    )
+      actAndCheck(
+        "sv1 changes its vote on the second request to accept",
+        sv1Backend.castVote(
+          getTrackingId(rejectVoteRequest),
+          true,
+          "new-url",
+          "I changed my mind",
+        ),
+      )(
+        "sv1's updated vote is recorded",
+        _ => {
+          val vr = voteRequestOnSv2(rejectTrackingId)
+          val votes = vr.payload.votes.asScala
+          votes should have size 3
+          votes("Digital-Asset-2").accept shouldBe true
+        },
+      )
 
-    actAndCheck(
-      "run CopyVotesTrigger on sv2 again to copy the updated vote", {
-        runTriggerOnSv2
-      },
-    )(
-      "sv2 updates its copied vote to match sv1",
-      _ => {
-        val vr = sv2Backend.listVoteRequests().loneElement
-        val votes = vr.payload.votes.asScala
-        votes should have size 3
-        val sv2Vote = votes("Digital-Asset-Eng-2")
-        sv2Vote.accept shouldBe true
-        sv2Vote.reason.url shouldBe "new-url"
-        sv2Vote.reason.body should include("Automatically Copied from Digital-Asset-2")
-        sv2Vote.reason.body should include("I changed my mind")
-      },
-    )
-  }
+      actAndCheck(
+        "run CopyVotesTrigger on sv2 again for the updated vote",
+        runTriggerOnSv2,
+      )(
+        "sv2 updates its copied vote to match sv1",
+        _ => {
+          val vr = voteRequestOnSv2(rejectTrackingId)
+          val votes = vr.payload.votes.asScala
+          votes should have size 3
+          val sv2Vote = votes("Digital-Asset-Eng-2")
+          sv2Vote.accept shouldBe true
+          sv2Vote.reason.url shouldBe "new-url"
+          sv2Vote.reason.body should include("Automatically Copied from Digital-Asset-2")
+          sv2Vote.reason.body should include("I changed my mind")
+        },
+      )
 
-  "CopyVotesTrigger does nothing when source SV has not voted" in { implicit env =>
-    val sv4Party = sv4Backend.getDsoInfo().svParty.toProtoPrimitive
-    val action = new ARC_DsoRules(
-      new SRARC_OffboardSv(new DsoRules_OffboardSv(sv4Party))
-    )
+      val (_, noVoteRequest) = actAndCheck(
+        "sv3 creates a third vote request that sv1 has not voted on",
+        sv3Backend.createVoteRequest(
+          sv3Backend.getDsoInfo().svParty.toProtoPrimitive,
+          action,
+          "url",
+          "offboard sv4 once more",
+          sv3Backend.getDsoInfo().dsoRules.payload.config.voteRequestTimeout,
+          None,
+        ),
+      )(
+        "the third vote request is visible to sv2",
+        _ => {
+          val vr = voteRequestOnSv2ByBody("offboard sv4 once more")
+          vr.payload.votes.asScala should have size 1
+          vr
+        },
+      )
+      val noVoteTrackingId = getTrackingId(noVoteRequest)
 
-    actAndCheck(
-      "sv3 creates a vote request (sv1 has not voted on it)",
-      sv3Backend.createVoteRequest(
-        sv3Backend.getDsoInfo().svParty.toProtoPrimitive,
-        action,
-        "url",
-        "offboard sv4",
-        sv3Backend.getDsoInfo().dsoRules.payload.config.voteRequestTimeout,
-        None,
-      ),
-    )(
-      "vote request is visible to sv2",
-      _ => sv2Backend.listVoteRequests().loneElement,
-    )
-
-    actAndCheck(
-      "run CopyVotesTrigger on sv2 (should be no-op since sv1 hasn't voted)",
-      sv2Backend.dsoAutomation.trigger[CopyVotesTrigger].runOnce().futureValue,
-    )(
-      "sv2 has not voted",
-      _ => {
-        val vr = sv2Backend.listVoteRequests().loneElement
-        vr.payload.votes.asScala should have size 1
-        vr.payload.votes.asScala.contains("Digital-Asset-Eng-2") shouldBe false
-      },
-    )
+      actAndCheck(
+        "run CopyVotesTrigger on sv2 when sv1 has not voted",
+        runTriggerOnSv2,
+      )(
+        "sv2 does not create a vote for the third request",
+        _ => {
+          val vr = voteRequestOnSv2(noVoteTrackingId)
+          vr.payload.votes.asScala should have size 1
+          vr.payload.votes.asScala.contains("Digital-Asset-Eng-2") shouldBe false
+        },
+      )
   }
 }
