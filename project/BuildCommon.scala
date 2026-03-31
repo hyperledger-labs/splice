db323ad87d9a24b6dc6ba6da819f79954d11a359
Tim Emiola <adetokunbo@emio.la>
2026-03-31 06:37:51 +0000

    [ci] Reverts 4b90a0ae5, those BUILD dependency changes are no longer needed
    Signed-off-by: Tim Emiola <adetokunbo@emio.la>

--- project/BuildCommon.scala
+++ project/BuildCommon.scala
@@ -785,12 +785,11 @@ object BuildCommon {
       )
       .settings(
         removeTestSources,
-        // We only need a few files out of a lot of test files so add them explicitly
+        // We only need 3 files out of a lot of test files so add them explicitly
         Test / managedSources := Seq(
           (Test / sourceDirectory).value / "scala/com/digitalasset/canton/HasActorSystem.scala",
           (Test / sourceDirectory).value / "scala/com/digitalasset/canton/store/db/DbTest.scala",
           (Test / sourceDirectory).value / "scala/com/digitalasset/canton/store/db/DbStorageIdempotency.scala",
-          (Test / sourceDirectory).value / "scala/com/digitalasset/canton/crypto/TestSalt.scala",
         ),
         disableTests,
         sharedCantonSettings,
@@ -1266,10 +1265,6 @@ object BuildCommon {
       ) // to accommodate different daml repo coding style
       .settings(
         removeTestSources,
-        // Re-add TestEngine so Splice tests can run Daml code in-memory
-        Test / unmanagedSources := Seq(
-          (Test / sourceDirectory).value / "scala/com/digitalasset/canton/util/TestEngine.scala"
-        ),
         sharedCantonSettings,
         sharedSettings,
         scalacOptions += "-Wconf:src=src_managed/.*:silent",
