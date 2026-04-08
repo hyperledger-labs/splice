804e7c68cc68135821a96307198d3d77b4c0043b
Robert Autenrieth <31539813+rautenrieth-da@users.noreply.github.com>
2026-04-08 21:36:42 +0200

    Refactor ACS snapshot alerts (#4925)
    [static]
    
    Signed-off-by: Robert Autenrieth <robert.autenrieth@digitalasset.com>

--- cluster/pulumi/infra/src/observability.ts
+++ cluster/pulumi/infra/src/observability.ts
@@ -850,6 +850,15 @@ function createGrafanaAlerting(namespace: Input<string>) {
                 '$INGESTION_ENTRIES_PER_BATCH_THRESHOLD',
                 monitoringConfig.alerting.alerts.ingestion.thresholdEntriesPerBatch.toString()
               ),
+            'acs_snapshots_alerts.yaml': readGrafanaAlertingFile('acs_snapshots_alerts.yaml')
+              .replaceAll(
+                '$ACS_SNAPSHOT_SAVE_LATENCY_THRESHOLD_SECONDS',
+                monitoringConfig.alerting.alerts.acsSnapshots.saveLatencyThresholdSeconds.toString()
+              )
+              .replaceAll(
+                '$ACS_SNAPSHOT_UPDATE_LATENCY_THRESHOLD_SECONDS',
+                monitoringConfig.alerting.alerts.acsSnapshots.updateLatencyThresholdSeconds.toString()
+              ),
             'sv-status-report_alerts.yaml': readAndSetAlertRulesGrafanaAlertingFile(
               'sv-status-report_alerts.yaml',
               [
