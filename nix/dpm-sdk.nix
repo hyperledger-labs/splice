71625d8aa2f7c073c7216a5eeb72f329f7616249
Brian Healey <brian.healey@digitalasset.com>
2026-04-10 20:11:14 +0000

    extract shas to json
    Signed-off-by: Brian Healey <brian.healey@digitalasset.com>

--- nix/dpm-sdk.nix
+++ nix/dpm-sdk.nix
@@ -1,25 +1,19 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  dpmVersion = "3.5.0-weekly-snapshot.20260409.59.0.v43aaa4c9";
+  sources = builtins.fromJSON (builtins.readFile ./dpm-sources.json);
   system = pkgs.stdenv.hostPlatform.system;
   os = if pkgs.stdenv.isDarwin then "darwin" else "linux";
   arch = if pkgs.stdenv.isAarch64 then "arm64" else "amd64";
-
-  dpmHashes = {
-    "x86_64-linux" = "sha256:1088zinqiaiz4krmcz7rpw66md4q7wdapshxvb183iggwi10mqia";
-    "aarch64-linux" = "sha256:1dw1qxwpw4vcrh3crknpr3bvxvlzvl8lq4iy8nj440ksnfaskldq";
-    "aarch64-darwin" = "sha256:0cg1jdic4nxcqlz5lg4jam2g4xnw12d7y8dwbgiwiz116j5x1kr2";
-  };
-  dpmHash = dpmHashes.${system} or (throw "Unsupported system: ${system}");
+  dpmHash = sources.${system} or (throw "Unsupported system: ${system}");
 in
 pkgs.stdenv.mkDerivation {
   pname = "dpm";
-  version = dpmVersion;
+  version = sources.version;
 
   src = builtins.fetchurl {
-    name = "dpm-sdk-${dpmVersion}.tar.gz";
-    url = "https://get.digitalasset.com/unstable/install/dpm-sdk/dpm-${dpmVersion}-${os}-${arch}.tar.gz";
+    name = "dpm-sdk-${sources.version}.tar.gz";
+    url = "https://get.digitalasset.com/unstable/install/dpm-sdk/dpm-${sources.version}-${os}-${arch}.tar.gz";
     sha256 = dpmHash;
   };
   nativeBuildInputs = [ pkgs.yq-go ];
@@ -44,6 +38,6 @@ pkgs.stdenv.mkDerivation {
 
     mkdir -p $out/nix-support
     echo export DPM_HOME=$out > $out/nix-support/setup-hook
-    echo export DPM_SDK_VERSION=${dpmVersion} >> $out/nix-support/setup-hook
+    echo export DPM_SDK_VERSION=${sources.version} >> $out/nix-support/setup-hook
   '';
 }
