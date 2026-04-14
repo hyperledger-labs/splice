hyperledger-labs/splice

commit fcd478b8a86d63a13502760ccb3e85ddf31c4848

Upgrade pulumi packages (#3967)
Upgrade pulumi packages to fix dependabot findings

Signed-off-by: krzysztofczyz-da <krzysztof.czyz@digitalasset.com>

cluster/pulumi/common/src/auth0/auth0.ts
@@ -120,12 +120,12 @@ export class Auth0Fetch implements Auth0Client {
     const cacheMap = {} as Auth0CacheMap;
 
     try {
-      const cacheSecret = await this.k8sApi.readNamespacedSecret(
-        this.cfg.fixedTokenCacheName,
-        'default'
-      );
+      const cacheSecret = await this.k8sApi.readNamespacedSecret({
+        name: this.cfg.fixedTokenCacheName,
+        namespace: 'default',
+      });
 
-      const { data } = cacheSecret.body;
+      const data = cacheSecret.stringData;
 
       for (const clientId in data) {
         cacheMap[clientId] = JSON.parse(Buffer.from(data[clientId], 'base64').toString('ascii'));
@@ -161,27 +161,36 @@ export class Auth0Fetch implements Auth0Client {
 
     try {
       await pulumi.log.info('Attempting to create secret');
-      await this.k8sApi.createNamespacedSecret('default', {
-        apiVersion: 'v1',
-        kind: 'Secret',
-        metadata: {
-          name: this.cfg.fixedTokenCacheName,
-        },
-        data,
-      });
-    } catch (_) {
-      try {
-        await pulumi.log.info('Deleting existing secret');
-        await this.k8sApi.deleteNamespacedSecret(this.cfg.fixedTokenCacheName, 'default');
-
-        await pulumi.log.info('Creating new secret');
-        await this.k8sApi.createNamespacedSecret('default', {
+      await this.k8sApi.createNamespacedSecret({
+        namespace: 'default',
+        body: {
           apiVersion: 'v1',
           kind: 'Secret',
           metadata: {
             name: this.cfg.fixedTokenCacheName,
           },
           data,
+        },
+      });
+    } catch (_) {
+      try {
+        await pulumi.log.info('Deleting existing secret');
+        await this.k8sApi.deleteNamespacedSecret({
+          name: this.cfg.fixedTokenCacheName,
+          namespace: 'default',
+        });
+
+        await pulumi.log.info('Creating new secret');
+        await this.k8sApi.createNamespacedSecret({
+          namespace: 'default',
+          body: {
+            apiVersion: 'v1',
+            kind: 'Secret',
+            metadata: {
+              name: this.cfg.fixedTokenCacheName,
+            },
+            data,
+          },
         });
       } catch (e) {
         await pulumi.log.error(`Auth0 cache update failed: ${JSON.stringify(e)}`);

commit cb4898fe727b8a034209510fefe73987c328369d

Fix pulumi auth0 cache
From the docs:

> The stringData field is never output when reading from the API.

So currently we have 100% cache misses which is clearly not what we
want.

Tested on a scratch that with this I actually get some cache hits with
this change.

Seems like it broke with https://github.com/hyperledger-labs/splice/pull/3967

[static]

Signed-off-by: moritz.kiefer@digitalasset.com <moritz.kiefer@purelyfunctional.org>

cluster/pulumi/common/src/auth0/auth0.ts
@@ -130,7 +130,7 @@ export class Auth0Fetch implements Auth0Client {
         namespace: 'default',
       });
 
-      const data = cacheSecret.stringData;
+      const data = cacheSecret.data;
 
       for (const clientId in data) {
         cacheMap[clientId] = JSON.parse(Buffer.from(data[clientId], 'base64').toString('ascii'));