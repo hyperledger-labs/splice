3d89962ce248992d816222f94626dd361bd570fa
Oriol Muñoz <oriol.munoz@digitalasset.com>
2026-04-15 15:42:31 +0000

    [ci] apparently this thing really hates newlines
    Signed-off-by: Oriol Muñoz <oriol.munoz@digitalasset.com>

--- apps/common/src/main/scala/org/lfdecentralizedtrust/splice/store/StoreMetrics.scala
+++ apps/common/src/main/scala/org/lfdecentralizedtrust/splice/store/StoreMetrics.scala
@@ -30,8 +30,8 @@ class StoreMetrics(metricsFactory: LabeledMetricsFactory)(metricsContext: Metric
         prefix :+ "signal-when-ingested-latency",
         "How long it takes to signal offset ingestion.",
         Latency,
-        "This metric measures the time taken for the future returned by `signalWhenIngestedOrShutdown` to complete as an indication for how far our transaction ingestion lags behind ledger end.\n" +
-          "Note that this only works for apps that do submissions, i.e., Scan does not have any data for this metric.\n" +
+        "This metric measures the time taken for the future returned by `signalWhenIngestedOrShutdown` to complete as an indication for how far our transaction ingestion lags behind ledger end. " +
+          "Note that this only works for apps that do submissions, i.e., Scan does not have any data for this metric. " +
           "Use ingestionTimePerEntry to know how fast ingestion is.",
       )
     )
@@ -42,7 +42,7 @@ class StoreMetrics(metricsFactory: LabeledMetricsFactory)(metricsContext: Metric
         prefix :+ "entry-ingestion-latency",
         "How long it takes to ingest a single GetTreeUpdatesResponse.",
         Latency,
-        "Measures the amount of time it takes to ingest a batch of GetTreeUpdatesResponse." +
+        "Measures the amount of time it takes to ingest a batch of GetTreeUpdatesResponse. " +
           "Note that it is somewhat affected by the size of the batch, so you should correlate with batchSize.",
       )
     )
