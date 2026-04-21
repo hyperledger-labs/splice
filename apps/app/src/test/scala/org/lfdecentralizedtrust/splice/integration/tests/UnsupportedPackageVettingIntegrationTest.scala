032bed61cc1bf1d921ce9eaab77dce9a0a577d44
Julien Tinguely <julien.tinguely@digitalasset.com>
2026-04-21 07:38:36 +0200

    UnsupportedPackageVettingIntegrationTest: Wrap tap package id check in eventually  (#5104)
    Signed-off-by: Julien Tinguely <julien.tinguely@digitalasset.com>

--- apps/app/src/test/scala/org/lfdecentralizedtrust/splice/integration/tests/UnsupportedPackageVettingIntegrationTest.scala
+++ apps/app/src/test/scala/org/lfdecentralizedtrust/splice/integration/tests/UnsupportedPackageVettingIntegrationTest.scala
@@ -201,7 +201,9 @@ class UnsupportedPackageVettingIntegrationTest
             synchronizerId,
           ) should contain allElementsOf validatorDarsAbovePackageConfigVersion.map(_.packageId)
         }
-        alicesTapsWithPackageId(DarResources.amulet_0_1_16.packageId)
+        eventually() {
+          alicesTapsWithPackageId(DarResources.amulet_0_1_16.packageId)
+        }
       }
   }
 }
