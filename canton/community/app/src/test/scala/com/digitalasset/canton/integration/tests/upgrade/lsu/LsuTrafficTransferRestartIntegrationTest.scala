7eadf859af36376b124d159af70844a6c256c48e
Oriol Muñoz <oriol.munoz@digitalasset.com>
2026-04-27 17:58:02 +0200

    Bump canton to 3.5.1-snapshot.20260427.18774.0.vc93b1522 (#5256)
    * Bump canton to 3.5.1-snapshot.20260427.18774.0.vc93b1522
    
    [ci]
    
    Includes Meriam's logs to debug https://github.com/DACH-NY/cn-test-failures/issues/8132
    also fix for CILR
    
    Signed-off-by: Oriol Muñoz <oriol.munoz@digitalasset.com>

--- canton/community/app/src/test/scala/com/digitalasset/canton/integration/tests/upgrade/lsu/LsuTrafficTransferRestartIntegrationTest.scala
+++ canton/community/app/src/test/scala/com/digitalasset/canton/integration/tests/upgrade/lsu/LsuTrafficTransferRestartIntegrationTest.scala
@@ -17,6 +17,7 @@ import com.digitalasset.canton.integration.tests.TrafficBalanceSupport
 import com.digitalasset.canton.integration.tests.upgrade.lsu.LogicalUpgradeUtils.SynchronizerNodes
 import com.digitalasset.canton.integration.util.TestUtils.waitForTargetTimeOnSequencer
 import com.digitalasset.canton.logging.SuppressingLogger.LogEntryOptionality
+import com.digitalasset.canton.synchronizer.sequencer.block.bftordering.bindings.canton.sequencing.BftBlockOrderer
 import com.digitalasset.canton.synchronizer.sequencer.errors.SequencerError
 
 import java.time.Duration
@@ -37,9 +38,16 @@ import java.time.Duration
   *   - Old synchronizer: sequencer1, sequencer2
   *   - New synchronizer: sequencer3, sequencer4
   */
-abstract class LsuTrafficTransferRestartIntegrationTest extends LsuBase with TrafficBalanceSupport {
+final class LsuTrafficTransferRestartIntegrationTest extends LsuBase with TrafficBalanceSupport {
 
   override protected def testName: String = "lsu-traffic-transfer-crash-recovery"
+
+  registerPlugin(
+    new UseBftSequencer(
+      loggerFactory,
+      MultiSynchronizer.tryCreate(Set("sequencer1", "sequencer2"), Set("sequencer3", "sequencer4")),
+    )
+  )
 
   registerPlugin(new UsePostgres(loggerFactory))
 
@@ -189,11 +197,21 @@ abstract class LsuTrafficTransferRestartIntegrationTest extends LsuBase with Tra
          */
         (
           LogEntryOptionality.Optional,
-          _.warningMessage should (include("Response message for request") and include("timed out")),
+          _.warningMessage should (include("Response message for request") and include(
+            "timed out"
+          )),
         ),
         (
           LogEntryOptionality.OptionalMany,
           _.shouldBeCantonErrorCode(SequencerError.NotAtUpgradeTimeOrBeyond),
+        ),
+        // TODO(#29833) Remove this rule when shutdown of the BFT orderer is improved
+        (
+          LogEntryOptionality.Optional,
+          entry => {
+            entry.loggerName shouldBe include(BftBlockOrderer.getClass.getSimpleName)
+            entry.warningMessage should include("shutdown did not complete gracefully in allotted")
+          },
         ),
       )
     }
