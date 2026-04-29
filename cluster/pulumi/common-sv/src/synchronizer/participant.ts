c8e30480a185ff3093d6cf5270281a956ee77d10
Martin Florian <martin.florian@digitalasset.com>
2026-04-29 15:59:18 +0000

    Bump the size of the persistent heap dump pvc for participants
    I think that this was the actual intention in https://github.com/hyperledger-labs/splice/pull/5307
    
    [static]
    
    Signed-off-by: Martin Florian <martin.florian@digitalasset.com>

--- cluster/pulumi/common-sv/src/synchronizer/participant.ts
+++ cluster/pulumi/common-sv/src/synchronizer/participant.ts
@@ -149,7 +149,7 @@ function installParticipantChart(
     resources: participant?.resources,
     pvc: spliceConfig.configuration.persistentHeapDumps
       ? {
-          size: '10Gi',
+          size: '35Gi',
           volumeStorageClass: standardStorageClassName,
         }
       : undefined,
