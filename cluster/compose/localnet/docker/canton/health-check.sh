c12139c2667a8a6a227e55c6a323d4c9fa5749b8
krzysztofczyz-da <krzysztof.czyz@digitalasset.com>
2026-04-30 10:29:20 +0000

    [ci] just use grpc-client-cli
    Signed-off-by: krzysztofczyz-da <krzysztof.czyz@digitalasset.com>

--- cluster/compose/localnet/docker/canton/health-check.sh
+++ cluster/compose/localnet/docker/canton/health-check.sh
@@ -6,13 +6,13 @@ set -eou pipefail
 
 if [ "$APP_USER_PROFILE" = "on" ]; then
   echo "Checking 2${CANTON_GRPC_HEALTHCHECK_PORT_SUFFIX}"
-  grpcurl -plaintext "localhost:2${CANTON_GRPC_HEALTHCHECK_PORT_SUFFIX}" grpc.health.v1.Health/Check
+  grpc-client-cli health "localhost:2${CANTON_GRPC_HEALTHCHECK_PORT_SUFFIX}"
 fi
 if [ "$APP_PROVIDER_PROFILE" = "on" ]; then
   echo "Checking 3${CANTON_GRPC_HEALTHCHECK_PORT_SUFFIX}"
-  grpcurl -plaintext "localhost:3${CANTON_GRPC_HEALTHCHECK_PORT_SUFFIX}" grpc.health.v1.Health/Check
+  grpc-client-cli health "localhost:3${CANTON_GRPC_HEALTHCHECK_PORT_SUFFIX}"
 fi
 if [ "$SV_PROFILE" = "on" ]; then
   echo "Checking 4${CANTON_GRPC_HEALTHCHECK_PORT_SUFFIX}"
-  grpcurl -plaintext "localhost:4${CANTON_GRPC_HEALTHCHECK_PORT_SUFFIX}" grpc.health.v1.Health/Check
+  grpc-client-cli health "localhost:4${CANTON_GRPC_HEALTHCHECK_PORT_SUFFIX}"
 fi
